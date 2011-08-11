d4750aec9fa7e4d7c1a53d0c67358c31c2591b58
James Douglass <jdouglass@stanford.edu>
2011-08-11 15:59:14 -0700

    Update issue 4 Update issue 7
    Removing arc_interface, based on the latest version of the API (accurate as of 4pm, 8/11/2011)

--- python/carbon.py
+++ python/carbon.py
@@ -4,12 +4,12 @@
 #attempting to extract the basic structure of the Arc tool
 
 
-import sys, string, os, arcgisscripting, math, time, datetime, re, arc_interface
+import sys, string, os, arcgisscripting, math, time, datetime, re, invest_core.lhl
 
 gp = arcgisscripting.create()
 
 testParameter = gp.GetParameterAsText(0)
 gp.addmessage("You entered " + testParameter)
 
-a = arc_interface.arcInterface()
+invest_core.lhl.invokeHelper()
 gp.addmessage("done.")
