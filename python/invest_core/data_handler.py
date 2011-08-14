c241f499bcabc1b73bfd21a4c7c4f05be92f72a3
richpsharp <devnull@localhost>
2011-08-14 00:02:27 -0700

    Basic functionality filled in for loading urls.

--- python/invest_core/data_handler.py
+++ python/invest_core/data_handler.py
@@ -1,8 +1,16 @@
 # Some tracer code to see how urlllib and gdal work together
 
+from urllib import urlopen
 
+def open(url):
+    try:
+        urlopen(url)
+        print 'Opened %s' % url
+    except IOError as (errno, strerror):
+        print "I/O error({0}): {1}: {2}".format(errno, strerror, url)
 
-if __name__ == "main":
+if __name__ == "__main__":
     print 'load url'
+    open("../../lulc_samp_cur")
     print 'load gdal'
     
