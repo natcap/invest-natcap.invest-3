9e5c7b0ef5c0670ae125a57c654d8964c9ea56d1
James Douglass <jdouglass@stanford.edu>
2011-08-15 16:36:43 -0700

    Adding carbon_seq.py
    Initial implementation uses numpy arrays for all three inputs (lulc, pools, output)

---
+++ python/invest_core/carbon_seq.py
@@ -0,0 +1,15 @@
+from numpy import *
+
+#lulc is a numpy array
+#pools is a numpy array, expecting the index to be the pool index and the value to be the sum of the carbon values for that index.
+i#output is a numpy array
+
+def carbon_seq(lulc, pools, output):
+  lulc_dimensions = list(lulc.shape) #convert to a list to access dimensions by index
+ 
+  #iterate through the array and map the pool values to the output array 
+  for x in range(0, lulc_dimensions[1]):
+    for y in range(0, lulc_dimensions[1]):
+      output[x][y] = pools[lulc[x][y]]
+
+
