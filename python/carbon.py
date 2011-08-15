fe5c04a612d023efcfca55a2cad0b6550aa763dc
James Douglass <jdouglass@stanford.edu>
2011-08-15 13:50:43 -0700

    Added necessary parameters for the most simple execution of the Carbon Model.

--- python/carbon.py
+++ python/carbon.py
@@ -1,15 +1,31 @@
-#carbon_refactored.py
+#carbon.py
 #
+#Extract the arguments of the Geoprocessing Object to a Python Dictionary
 
-#attempting to extract the basic structure of the Arc tool
-
-
-import sys, string, os, arcgisscripting, math, time, datetime, re, invest_core.lhl
+import sys, string, os, arcgisscripting, math, time, datetime, re, invest_core.uri_carbon
 
 gp = arcgisscripting.create()
 
 testParameter = gp.GetParameterAsText(0)
-gp.addmessage("You entered " + testParameter)
+lulc_uri      = gp.GetParameterAsText(1)
+pool_uri      = gp.GetParameterAsText(2)
+output_uri    = gp.GetParameterAsText(3)
 
-invest_core.lhl.invokeHelper()
+lulc_dictionary   = {'uri'  : lulc_uri,
+                     'type' :'gdal',
+                     'input': True}
+
+pool_dictionary   = {'uri'  : pool_uri}
+
+output_dictionary = {'uri'  : output_uri,
+                     'type' : 'gdal',
+                     'input': False}
+
+
+arguments = {'lulc': lulc_dictionary,
+             'carbon_pools' : pool_dictionary,
+             'output' : output_dictionary}
+
+uri_carbon(arguments)
+
 gp.addmessage("done.")
