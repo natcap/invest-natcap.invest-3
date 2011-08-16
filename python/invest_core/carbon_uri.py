522ec9150d4e731b412ae663362c05e56f8e301a
James Douglass <jdouglass@stanford.edu>
2011-08-16 15:17:29 -0700

    Fixed broken pointers to imput/output variables in carbon_uri

--- python/invest_core/carbon_uri.py
+++ python/invest_core/carbon_uri.py
@@ -1,12 +1,12 @@
 import numpy as np
-import gdal
+from osgeo import *
 import carbon_core
 
 
 def carbon_uri(in_args):
     #preprocess in_args
     out_args = {}
-    for key, value in args.iteritems():
+    for key, value in in_args.iteritems():
         if type(value) == 'dict':
             #probably need to process a gdal object hee
             out_args[key] = dst_ds = driver.Create(value[uri], 10, 10, 1, gdal.GDT_Byte)
@@ -14,7 +14,7 @@ def carbon_uri(in_args):
             out_args[key] = value
 
     #invoke core function
-    carbon_core.carbon_core(args)
+    carbon_core.carbon_core(out_args)
 
     #process any return result (save to disk)
     print 'TODO: save stuff'
