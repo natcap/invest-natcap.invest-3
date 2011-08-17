e8e37fe81061cef577e20a164bb45e8f1f757354
jamesdouglassusa <devnull@localhost>
2011-08-17 16:20:53 -0700

    Update issue 22
    Added basic processing line-by-line.

--- python/invest_core/carbon_core.py
+++ python/invest_core/carbon_core.py
@@ -2,23 +2,25 @@ from numpy import *
 import osgeo.gdal
 
 def execute(args):
-  #args is a dictionary
-  #GDAL URI is handled before this function is called, so GDAL object should be passed with args
-  #carbon pool should have been processed from its file into a dictionary, passed with args
-  #output file URI should also have been passed with args
-  #The purpose of this function is to assemble calls to the various carbon components into a conhesive result
+    #args is a dictionary
+    #GDAL URI is handled before this function is called, so GDAL object should be passed with args
+    #carbon pool should have been processed from its file into a dictionary, passed with args
+    #output file URI should also have been passed with args
+    #The purpose of this function is to assemble calls to the various carbon components into a conhesive result
 
-  #convert the passed GDAL object to a 2D array
-  cols = args['lulc']['uri'].RasterXSize
-  rows = args['lulc'].RasterYSize
-  lulc = args['lulc'].GetRasterBand(1).ReadAsArray(0, 0, cols, rows)
+    #convert the passed GDAL object to a 2D array
+    cols = args['lulc']['uri'].RasterXSize
+    rows = args['lulc'].RasterYSize
+    lulc = args['lulc'].GetRasterBand(1).ReadAsArray(0, 0, cols, rows)
 
-  #get the carbon pools
-  pools = args['carbon_pool']
+    #get the carbon pools
+    pools = args['carbon_pool']
 
-  #initialize an output array
-  #it's the same dimensions as the lulc
-  output = zeros(lulc.shape)
+    #initialize an output array
+    #it's the same dimensions as the lulc
+    output = zeros(lulc.shape)
 
-  carbon_seq(lulc, pools, output)
+    for i in range(rows):
+        data = lulc.ReadAsArray(0, i, cols, 1)
+        carbon_seq(data, pools, output)
 
