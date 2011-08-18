3cec38f96fb1dc23fd73dcdfa700dec33c498862
jamesdouglassusa <devnull@localhost>
2011-08-17 17:33:28 -0700

    Reads dbf file into an array for passing to carbon_seq.

--- python/invest_core/carbon_core.py
+++ python/invest_core/carbon_core.py
@@ -1,5 +1,6 @@
 from numpy import *
 import osgeo.gdal
+from dbfpy import dbf
 
 def execute(args):
     #args is a dictionary
@@ -8,13 +9,21 @@ def execute(args):
     #output file URI should also have been passed with args
     #The purpose of this function is to assemble calls to the various carbon components into a conhesive result
 
-    #convert the passed GDAL object to a 2D array
-    cols = args['lulc']['uri'].RasterXSize
-    rows = args['lulc'].RasterYSize
-    lulc = args['lulc'].GetRasterBand(1).ReadAsArray(0, 0, cols, rows)
+    cols = args['lulc'].RasterXSize
+    lulc = args['lulc'].GetRasterBand(1)
 
-    #get the carbon pools
-    pools = args['carbon_pool']
+    #get the carbon pools as an array that we can use
+    #The new array uses key -> value pairings as you would expect,
+    #where key = LULC index and value = sum of all carbon pools for that key
+    pools = args['carbon_pool'] #pools is a dbf object
+    poolsArray = None
+    for i in range(pools.recordCount):
+        sum = 0
+        for field in ('C_ABOVE', 'C_BELOW', 'C_SOIL', 'C_DEAD'):
+            sum = sum + pools[i][field]
+        poolsArray[pools[i]['LULC']] = sum
+    
+    
 
     #initialize an output array
     #it's the same dimensions as the lulc
@@ -22,5 +31,5 @@ def execute(args):
 
     for i in range(rows):
         data = lulc.ReadAsArray(0, i, cols, 1)
-        carbon_seq(data, pools, output)
+        carbon_seq(data, poolsArray, output)
 
