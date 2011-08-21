9f93cf31810c0749137cfa987391ba39614d67e7
James Douglass <jdouglass@stanford.edu>
2011-08-21 19:50:28 -0400

    Update issue 20
    Initial commit for a simple execution of the carbon model from the invest module.

---
+++ python/invest_core/invest_test.py
@@ -0,0 +1,29 @@
+import unittest
+import invest
+
+class TestInvest(unittest.TestCase):
+    def test_carbon_model(self):
+        """Test to run Carbon model on sample data"""
+        lulc_dictionary = {'uri'  : '../../lulc_samp_cur',
+                           'type' :'gdal',
+                           'input': True}
+
+        pool_dictionary = {'uri'  : '../../carbon_pools_samp.dbf',
+                           'type' : 'dbf',
+                           'input': True}
+
+        output_dictionary = {'uri'  : '../../carbon_output',
+                             'type' : 'gdal',
+                             'input': False}
+
+        arguments = {'lulc': lulc_dictionary,
+                     'carbon_pools' : pool_dictionary,
+                     'output' : output_dictionary}
+        
+        invest.execute('carbon', arguments)
+        pass
+    
+    
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestInvest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
