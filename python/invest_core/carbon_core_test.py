3e8bfec2c18607255c84d4eb37f9e5c03ba0f17a
James Douglass <jdouglass@stanford.edu>
2011-08-22 15:52:37 -0700

    Update issue 16
    Updated smoke test to operate with proper files, albeit with blank inputs.

--- python/invest_core/carbon_core_test.py
+++ python/invest_core/carbon_core_test.py
@@ -1,6 +1,8 @@
 import unittest
 import carbon_core
 import numpy as np
+import os
+from dbfpy import dbf
 
 try:
     from osgeo import ogr, gdal
@@ -16,11 +18,14 @@ class TestCarbonCore(unittest.TestCase):
     def test_carbon_core_smoke(self):
         """Smoke test for carbon_core function.  Shouldn't crash with
         zero length inputs"""
-        driver = gdal.GetDriverByName("AIG")
-        args = { 'lulc': driver.Create(None, 0, 0, 1, gdal.GDT_Byte),
-                'carbon_pool': {},
-                'carbon_map_output': driver.Create(None, 0, 0, 1, gdal.GDT_Byte)}
+        driver = gdal.GetDriverByName("GTIFF")
+        args = { 'lulc': driver.Create('../../test_blank_input', 1, 1, 1, gdal.GDT_Byte),
+                'carbon_pool': dbf.Dbf('../../test_blank_dbf', new=True),
+                'output': driver.Create('../../test_blank_output', 1, 1, 1, gdal.GDT_Byte)}
         carbon_core.execute(args)
+        os.remove('../../test_blank_input')
+        os.remove('../../test_bank_dbf')
+        os.remove('../../test_blank_output')
         pass
 
 if __name__ == '__main__':
