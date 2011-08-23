c55023a88a6c47251a03aefab67e08aa0853ed84
James Douglass <jdouglass@stanford.edu>
2011-08-22 18:05:22 -0700

    Update issue 17
    Updated code to work properly with the values and arrays being passed to carbon_seq.  Still having some issues, however.  See issue 31 for the current issue.

--- python/invest_core/carbon_seq.py
+++ python/invest_core/carbon_seq.py
@@ -1,5 +1,5 @@
 from numpy import *
-
+import numpy as np
 #lulc is a numpy array
 #pools is a numpy array, expecting the index to be the pool index and the value to be the sum of the carbon values for that index.
 #output is a numpy array
@@ -7,11 +7,10 @@ from numpy import *
 def execute(lulc, pools, output):
     lulc_dimensions = list(lulc.shape) #convert to a list to access dimensions by index
     #iterate through the array and map the pool values to the output array 
-    for x in range(0, lulc_dimensions[0]):
-        for y in range(0, lulc_dimensions[1]):
-            index = lulc[x][y]
-            if index != 255:
-                output[x][y] = pools[index]
+    for x in range(0, lulc_dimensions[1]):
+        index = lulc[0][x]
+        if (index != 255):
+            output[x] = pools[index]
 
     return output
 
