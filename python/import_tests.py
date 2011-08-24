bd2bc4208f910cf6026cc0c78ab6b7e14299b664
richpsharp <devnull@localhost>
2011-08-24 14:23:30 -0700

    Cleaned up import test; no need to append paths.

--- python/import_tests.py
+++ python/import_tests.py
@@ -1,20 +1,5 @@
 try:
     print('Trying to import.')
-    import os
-    os.environ['PATH'] = 'C:\\OSGeo4W\\bin;' + os.environ['PATH']
-    newpath = ""
-    for p in os.environ['PATH'].split(';'):
-        if 'ArcGIS' not in p:
-            newpath += p + ';'
-    os.environ['PATH'] = newpath
-    #print("\n".join(sorted(os.environ['PATH'].split(';'))))
-    import sys
-    sys.path.insert(0, "C:\\OSGeo4W\\bin")
-    print("\n".join(sys.path))
-
-    print("\n".join(sorted(os.environ.keys())))
-    print(os.getcwd())
-    os.chdir('C:\\Windows')
     from osgeo import gdal
     raw_input('Imported! (Press enter)')
 except Exception, e:
