cb84584370452d0f03c46c2be09749a4f5820890
James Douglass <jdouglass@stanford.edu>
2011-08-24 15:22:39 -0700

    Update issue 16
    Added test for 1D array verifying that carbon sequestration was performed properly.  Verifies with a 1D numpy array (shape=(1, n)) where the value is a random int between 0 and 2 inclusive.

--- python/invest_core/carbon_seq_test.py
+++ python/invest_core/carbon_seq_test.py
@@ -1,6 +1,7 @@
 import unittest
 import carbon_seq
 import numpy as np
+import random
 
 class TestCarbonSeq(unittest.TestCase):
     def test_carbon_seq_smoke(self):
@@ -9,8 +10,34 @@ class TestCarbonSeq(unittest.TestCase):
         lulc = np.array([])
         pools = {}
         output = np.empty(lulc.size)
-        carbon_seq.carbon_seq(lulc, pools, output)
+        carbon_seq.execute(lulc, pools, output)
         pass
+    
+    def test_1D_array(self):
+        """Test of carbon_seq against a 1D input/output array"""
+        #setup the three args to carbon_seq
+        length = 100
+        lulc = np.zeros((1, length))
+        for x in range(length):
+            lulc[0][x] = random.randint(0,2)
+            
+        pools = {0: 3.3,
+                 1: 4.4,
+                 2: 5.5}
+        
+        output = np.zeros((1, length))
+        
+        #run carbon_seq
+        output = carbon_seq.execute(lulc, pools, output)
+        
+        #verify the contents of output against pool and lulc data
+        for x in range(lulc.shape[1]):
+            self.assertEqual(output[0][x],
+                            pools[lulc[0][x]],
+                            'Pool data was not correctly transcribed')
+        pass
+    
+    
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestCarbonSeq)
