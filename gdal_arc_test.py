ff3bdb3520f908d94171681ddb3a2f933a668025
richpsharp <devnull@localhost>
2011-08-24 14:23:13 -0700

    Cleaned up subprocess call.

--- gdal_arc_test.py
+++ gdal_arc_test.py
@@ -1,6 +1,4 @@
 import os
-os.environ['PATH'] = 'C:\\OSGeo4W\\bin;' + os.environ['PATH']
-
 import arcgisscripting
 gp = arcgisscripting.create()
 import subprocess
@@ -8,7 +6,7 @@ import subprocess
 gp.AddMessage(os.getcwd())
 
 gp.AddMessage('Starting subprocess')
-#p = subprocess.Popen(['C:\OSGeo4W\gdal_python_exec.bat', 'X:\\local\\workspace\\invest-natcap.invest-3\python\import_tests.py'])
-os.system('C:\OSGeo4W\gdal_python_exec.bat X:\\local\\workspace\\invest-natcap.invest-3\python\import_tests.py')
-#os.system('C:\OSGeo4W\gdal_python.bat X:\\local\\workspace\\invest-natcap.invest-3\python\import_tests.py')
-#os.system('C:\OSGeo4W\OSGeo4W.bat')
+process = subprocess.Popen(['OSGeo4W\\gdal_python_exec.bat', 'python\import_tests.py'])
+gp.AddMessage('Waiting')
+process.wait()
+gp.AddMessage('Done')
