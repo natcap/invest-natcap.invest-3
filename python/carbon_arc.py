58d2c91b273a24a9c879415f03c79ba244736ea9
richpsharp <devnull@localhost>
2011-08-24 15:07:49 -0700

    Carbon arc refactored to use the OSGeo4W python exec batch script.

--- python/carbon_arc.py
+++ python/carbon_arc.py
@@ -1,43 +1,35 @@
 #carbon_arc.py
-#
-#Extract the arguments of the Geoprocessing Object to a Python Dictionary
-import os, sys
+import os, sys, subprocess, json
 import arcgisscripting
-#os.chdir('Z:\projects\invest3x\python')
-sys.path.append('Z:\\projects\\invest3x\\python')
-#os.chdir('Z:\\')
-os.chdir('Z:\\projects\\invest3x\\python')
-import invest_core
-
-os.chdir('C:\\')
-
-
-
 gp = arcgisscripting.create()
-gp.addmessage(sys.path)
 
-def carbon_arc(gp):
-    lulc_uri = gp.GetParameterAsText(0)
-    pool_uri = gp.GetParameterAsText(1)
-    output_uri = gp.GetParameterAsText(2)
+os.chdir(os.path.dirname(os.path.realpath(__file__)) + "\\..\\")
+gp.AddMessage(os.getcwd())
 
-    lulc_dictionary = {'uri'  : lulc_uri,
-                       'type' :'gdal',
-                       'input': True}
+lulc_uri = gp.GetParameterAsText(0)
+pool_uri = gp.GetParameterAsText(1)
+output_uri = gp.GetParameterAsText(2)
 
-    pool_dictionary = {'uri'  : pool_uri,
-                       'type': 'dbf',
-                       'input': True}
+lulc_dictionary = {'uri'  : lulc_uri,
+                   'type' :'gdal',
+                   'input': True}
 
-    output_dictionary = {'uri'  : output_uri,
-                         'type' : 'gdal',
-                         'input': False}
+pool_dictionary = {'uri'  : pool_uri,
+                   'type': 'dbf',
+                   'input': True}
 
-    arguments = {'lulc': lulc_dictionary,
-                 'carbon_pools' : pool_dictionary,
-                 'output' : output_dictionary}
-    gp.addmessage('Processing')
+output_dictionary = {'uri'  : output_uri,
+                     'type' : 'gdal',
+                     'input': False}
 
-    invest_core.invest.execute('carbon', arguments)
+arguments = {'lulc': lulc_dictionary,
+             'carbon_pools' : pool_dictionary,
+             'output' : output_dictionary}
 
-carbon_arc(gp)
+gp.AddMessage('Starting carbon model')
+process = subprocess.Popen(['OSGeo4W\\gdal_python_exec.bat',
+                            'python\invest.py', 'carbon',
+                            json.dumps(arguments)])
+gp.AddMessage('Waiting')
+process.wait()
+gp.AddMessage('Done')
