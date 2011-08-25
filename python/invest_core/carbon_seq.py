a1e3b940740a24efa36c9db705b904ba7f7fe6a7
richpsharp <devnull@localhost>
2011-08-25 15:03:49 -0700

    Increased efficiency on carbon_seq by using numpy's vectorize routine.

--- python/invest_core/carbon_seq.py
+++ python/invest_core/carbon_seq.py
@@ -1,18 +1,20 @@
 from numpy import *
 import numpy as np
 
-def execute(lulc, pools, output):
+def execute(lulc, pools):
     """iterate through the array and map the pool values to the output array
         lulc is a numpy array
         pools is a numpy array, expecting the index to be the pool index and the value to be the sum of the carbon values for that index.
         output is a numpy array"""
 
-    for x in range(0, lulc.shape[1]):
-        index = lulc[0][x]
-        if (index == 255):
-            output[0][x] = None
+    def mapPool(x, dict):
+        if x in dict:
+            return dict[x]
         else:
-            output[0][x] = pools[index]
+            return None
 
-    return output
-
+    if lulc.size > 0:
+        mapFun = np.vectorize(mapPool)
+        return mapFun(lulc, pools)
+    else:
+        return []
