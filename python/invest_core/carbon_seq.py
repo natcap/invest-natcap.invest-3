4f733f22145ea54815733c4ffb4077be71d0a5ab
richpsharp <devnull@localhost>
2011-08-25 14:46:28 -0700

    Converted comments into docstring for execute function.

--- python/invest_core/carbon_seq.py
+++ python/invest_core/carbon_seq.py
@@ -1,18 +1,18 @@
 from numpy import *
 import numpy as np
-#lulc is a numpy array
-#pools is a numpy array, expecting the index to be the pool index and the value to be the sum of the carbon values for that index.
-#output is a numpy array
 
 def execute(lulc, pools, output):
-    #iterate through the array and map the pool values to the output array 
+    """iterate through the array and map the pool values to the output array
+        lulc is a numpy array
+        pools is a numpy array, expecting the index to be the pool index and the value to be the sum of the carbon values for that index.
+        output is a numpy array"""
+
     for x in range(0, lulc.shape[1]):
         index = lulc[0][x]
         if (index == 255):
             output[0][x] = None
         else:
             output[0][x] = pools[index]
-        
 
     return output
 
