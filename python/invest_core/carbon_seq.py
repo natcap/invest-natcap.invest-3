a1bf544f605facd4813be513fd669ec636e815a4
James Douglass <jdouglass@stanford.edu>
2011-08-29 16:53:49 -0700

    Update issue 53
    In conversation with Rich, updating the value returned if a lulc pixel value is not in the dict to have a carbon value of 0.

--- python/invest_core/carbon_seq.py
+++ python/invest_core/carbon_seq.py
@@ -3,15 +3,13 @@ import numpy as np
 
 def execute(lulc, pools):
     """iterate through the array and map the pool values to the output array
-        lulc is a numpy array
-        pools is a numpy array, expecting the index to be the pool index and the value to be the sum of the carbon values for that index.
-        output is a numpy array"""
+        """
 
     def mapPool(x, dict):
         if x in dict:
             return dict[x]
         else:
-            return None
+            return 0
 
     if lulc.size > 0:
         mapFun = np.vectorize(mapPool)
