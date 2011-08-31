7df9b387114057d2bfe08fbefa1fb8a7277006a2
James Douglass <jdouglass@stanford.edu>
2011-08-30 17:16:43 -0700

    Update issue 55
    Added test.  Still need to test.

--- python/invest_core/invest_test.py
+++ python/invest_core/invest_test.py
@@ -3,17 +3,17 @@ import invest_core
 from osgeo import gdal
 
 class TestInvest(unittest.TestCase):
-    def test_carbon_model(self):
+    def carbon_model_regression_test(self):
         """Test to run Carbon model on sample data"""
         lulc_dictionary = {'uri'  : '../../lulc_samp_cur',
                            'type' :'gdal',
                            'input': True}
 
-        pool_dictionary = {'uri'  : '../../carbon_pools_samp.dbf',
+        pool_dictionary = {'uri'  : '../../test_data/carbon_pools_float.dbf',
                            'type' : 'dbf',
                            'input': True}
 
-        output_dictionary = {'uri'  : '../../carbon_output/out.png',
+        output_dictionary = {'uri'  : '../../carbon_output/out.tif',
                              'type' : 'gdal',
                              'dataType': gdal.GDT_Float32,
                              'input': False}
@@ -24,29 +24,64 @@ class TestInvest(unittest.TestCase):
 
         invest_core.execute('carbon_core', arguments)
 
-        output = gdal.Open('../../carbon_output/out.png', 0)
-        outputBand = output.GetRasterBand(1)
-        obnodata = outputBand.GetNoDataValue()
-        print obnodata
-        
-        invest2 = gdal.Open('../../tot_c_cur', 0)
-        invest2Band = invest2.GetRasterBand(1)
-        i2bnodata = invest2Band.GetNoDataValue()
-        print i2bnodata
-
-        self.assertIsNot(obnodata, None, "Output nodata value read as None")
-        self.assertEqual(outputBand.XSize, invest2Band.XSize, "Dimensions differ: output=" + str(outputBand.XSize) + ", i2output = " + str(invest2Band.XSize))
-        self.assertEqual(outputBand.YSize, invest2Band.YSize, "Dimensions differ: output=" + str(outputBand.YSize) + ", i2output = " + str(invest2Band.YSize))
-
-        for i in range(0, outputBand.YSize):
-            outArray = outputBand.ReadAsArray(1, i, outputBand.XSize-1, 1)
-            i2Array = invest2Band.ReadAsArray(1, i, outputBand.XSize-1, 1)
-            for j in range(0, outputBand.XSize-1):
-                if (i2Array[0][j] == i2bnodata):
-                    self.assertEqual(outArray[0][j], obnodata, "Should have found nodata pixel (value == " + str(obnodata) + ") in output raster at row " + str(i) + " index " + str(j) + ", but found " + str(outArray[0][j]) + " instead")
-                else:
-                    self.assertEqual(outArray[0][j], i2Array[0][j], "Unequal pixel values detected at row " + str(i) + " index " + str(j) + ":" + str(outArray[0][j]) + " " + str(i2Array[0][j]))
+        assert_raster_equality(output_dictionary['output'], '../../test_data/carbon_regression.tif' )
         pass
+
+
+
+
+        def verify_carbon3_against_carbon21_int_pools(self):
+            """Test the InVEST3 carbon model against the known output raster of InVEST2.1 carbon
+                
+                Uses the modified pools dbf, where all values are ints."""
+                
+            lulc_dictionary = {'uri'  : '../../lulc_samp_cur',
+                               'type' :'gdal',
+                               'input': True}
+
+            pool_dictionary = {'uri'  : '../../test_data/carbon_pools_int.dbf',
+                               'type' : 'dbf',
+                               'input': True}
+            
+            output_dictionary = {'uri'  : '../../carbon_output/out.tif',
+                                 'type' : 'gdal',
+                                 'dataType': gdal.GDT_Float32,
+                                 'input': False}
+
+            arguments = {'lulc': lulc_dictionary,
+                     'carbon_pools' : pool_dictionary,
+                     'output' : output_dictionary}
+
+            invest_core.execute('carbon_core', arguments)
+            
+            assert_raster_equality(output_dictionary['output'], '../../test_data/tot_c_cur_int')
+            pass
+        
+        
+            
+        def assert_raster_equality(firstUri, secondUri):
+            output = gdal.Open(firstUri, 0)
+            outputBand = output.GetRasterBand(1)
+            obnodata = outputBand.GetNoDataValue()
+            print obnodata
+            
+            invest2 = gdal.Open(secondUri, 0)
+            invest2Band = invest2.GetRasterBand(1)
+            i2bnodata = invest2Band.GetNoDataValue()
+            print i2bnodata
+    
+            self.assertIsNot(obnodata, None, "Output nodata value read as None")
+            self.assertEqual(outputBand.XSize, invest2Band.XSize, "Dimensions differ: output=" + str(outputBand.XSize) + ", i2output = " + str(invest2Band.XSize))
+            self.assertEqual(outputBand.YSize, invest2Band.YSize, "Dimensions differ: output=" + str(outputBand.YSize) + ", i2output = " + str(invest2Band.YSize))
+    
+            for i in range(0, outputBand.YSize):
+                outArray = outputBand.ReadAsArray(1, i, outputBand.XSize-1, 1)
+                i2Array = invest2Band.ReadAsArray(1, i, outputBand.XSize-1, 1)
+                for j in range(0, outputBand.XSize-1):
+                    if (i2Array[0][j] == i2bnodata):
+                        self.assertEqual(outArray[0][j], obnodata, "Should have found nodata pixel (value == " + str(obnodata) + ") in output raster at row " + str(i) + " index " + str(j) + ", but found " + str(outArray[0][j]) + " instead")
+                    else:
+                        self.assertEqual(outArray[0][j], i2Array[0][j], "Unequal pixel values detected at row " + str(i) + " index " + str(j) + ":" + str(outArray[0][j]) + " " + str(i2Array[0][j]))
 
 
 if __name__ == '__main__':
