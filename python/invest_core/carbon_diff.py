e3369ce071f66d1d50f68f098e9633862d8f5a25
James Douglass <jdouglass@stanford.edu>
2011-09-05 00:29:16 -0400

    Update issue 66
    Reimplemented carbon_diff using numpy.vectorize to take advantage of the ability to rewrite nodata values as nodata values.

--- python/invest_core/carbon_diff.py
+++ python/invest_core/carbon_diff.py
@@ -1,11 +1,16 @@
 from numpy import *
 import numpy as np
 
-def execute(lulc_cur, lulc_fut):
+def execute(nodata, lulc_cur, lulc_fut):
     """iterate through the array and map the difference of the two input arrays to the output array
         """
-          
+    
+    def mapDiff(a, b):
+        if a == nodata['cur']:
+            return nodata['fut']
+        else:
+            return a-b
+    
     if lulc_cur.size > 0:
-        return np.subtract(lulc_cur, lulc_fut)
-    else:
-        return []
+        mapFun = np.vectorize(mapDiff)
+        return mapFun(lulc_cur, lulc_fut)
