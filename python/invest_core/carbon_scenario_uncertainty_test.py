a9270b0196ee6d94bc842cb851aab95034c7c208
richpsharp <devnull@localhost>
2011-09-06 15:01:23 -0700

    Test output now goes in a helpful test_output directory instead of the root.

--- python/invest_core/carbon_scenario_uncertainty_test.py
+++ python/invest_core/carbon_scenario_uncertainty_test.py
@@ -14,12 +14,12 @@ class TestCarbonScenarioUncertainty(unittest.TestCase):
         lulc_cur = driver.Create('../../test_data/test_blank_input', 1, 1, 1, gdal.GDT_Byte)
         lulc_cur.GetRasterBand(1).SetNoDataValue(-1.0)
 
-        output = driver.Create('../../test_data/test_blank_output', 1, 1, 4, gdal.GDT_Float32)
+        output = driver.Create('../../test_data/test_output/test_blank_output', 1, 1, 4, gdal.GDT_Float32)
         for x in [1, 2, 3, 4]: output.GetRasterBand(x).SetNoDataValue(0.0)
 
         args = { 'lulc_cur':lulc_cur,
                 'lulc_fut':lulc_cur,
-                'carbon_pools': dbf.Dbf('../../test_data/test_blank_dbf', new=True),
+                'carbon_pools': dbf.Dbf('../../test_data/test_output/test_blank_dbf', new=True),
                 'output': output}
         carbon_scenario_uncertainty.execute(args)
 
@@ -31,7 +31,7 @@ class TestCarbonScenarioUncertainty(unittest.TestCase):
         driver = gdal.GetDriverByName("GTIFF")
         lulc_cur = gdal.Open('../../test_data/lulc_samp_cur', GA_ReadOnly)
         lulc_fut = gdal.Open('../../test_data/lulc_samp_fut', GA_ReadOnly)
-        output = driver.Create('../../uncertainty_sequestration_scenario.tif',
+        output = driver.Create('../../test_data/test_output/uncertainty_sequestration_scenario.tif',
                                lulc_cur.GetRasterBand(1).XSize,
                                lulc_cur.GetRasterBand(1).YSize, 4, gdal.GDT_Float32)
         for x in [1, 2, 3, 4 ]: output.GetRasterBand(x).SetNoDataValue(0.0)
