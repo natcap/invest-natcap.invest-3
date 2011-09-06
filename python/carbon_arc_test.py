cf3079f62c0184b4eaccdc49b90091754bd27756
James Douglass <jdouglass@stanford.edu>
2011-09-06 13:19:42 -0700

    Update issue 25
    Output from test cases is now printed with gp.AddMessage().

--- python/carbon_arc_test.py
+++ python/carbon_arc_test.py
@@ -1,8 +1,13 @@
 import os, sys, subprocess
 import arcgisscripting
 
+gp = arcgisscripting.create()
 os.chdir(os.path.dirname(os.path.realpath(__file__)) + "\\invest_core\\")
 
+gp.AddMessage('Running tests ...')
 process = subprocess.Popen(['..\\..\\OSGeo4W\\gdal_python_exec_test.bat',
-                            'carbon_test_suite.py'])
+                            'carbon_test_suite.py'],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT,).communicate()[0]
 
+gp.AddMessage(process)
