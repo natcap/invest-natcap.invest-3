f73c2228cb0171452f5bb1183b3c8ddc5f89a107
James Douglass <jdouglass@stanford.edu>
2011-09-06 12:24:35 -0700

    Update issue 25
    carbon_arc test now runs from within ArcGIS.  At the moment, the python shell window closes when the process finishes even when there are errors.

--- python/carbon_arc_test.py
+++ python/carbon_arc_test.py
@@ -1,4 +1,8 @@
 import os, sys, subprocess
+import arcgisscripting
 
-process = subprocess.Popen(['OSGeo4W\\gdal_python_exec.bat',
-                            'python\\invest_core\\carbon_test_suite.py',])
+os.chdir(os.path.dirname(os.path.realpath(__file__)) + "\\invest_core\\")
+
+process = subprocess.Popen(['..\\..\\OSGeo4W\\gdal_python_exec_test.bat',
+                            'carbon_test_suite.py'])
+
