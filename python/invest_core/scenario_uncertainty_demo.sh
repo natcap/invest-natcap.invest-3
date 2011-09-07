88b397c3199fac4158c71f34150c66fc2ccf675b
Rich Sharp <richpsharp@gmail.com>
2011-09-07 13:32:47 -0700

    Putting files directly in as command line arguments so you can see the layers in the correct order.

--- python/invest_core/scenario_uncertainty_demo.sh
+++ python/invest_core/scenario_uncertainty_demo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python carbon_scenario_uncertainty_test.py && qgis --nologo ../../test_data/test_output/*.tif
+python carbon_scenario_uncertainty_test.py && qgis --nologo ../../test_data/test_output/uncertainty_sequestration.tif ../../test_data/test_output/uncertainty_colormap.tif 
