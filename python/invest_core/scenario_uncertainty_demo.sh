62cd24a9f0a420ec4b79abd3af23b78a00d5b363
James Douglass <jdouglass@stanford.edu>
2011-09-21 15:42:08 -0700

    Deleting script used for olympics.

--- python/invest_core/scenario_uncertainty_demo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python carbon_scenario_uncertainty_test.py && qgis --nologo ../../test_data/test_output/uncertainty_sequestration.tif ../../test_data/test_output/uncertainty_colormap.tif 
