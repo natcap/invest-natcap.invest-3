2474a62a8563672e8bee51dceb00b9062dc212f8
James Douglass <jdouglass@stanford.edu>
2011-09-21 15:50:21 -0700

    Update issue 78
    Simplified invest_core for 3.0 design refactor.

--- python/invest_core/invest_core.py
+++ python/invest_core/invest_core.py
@@ -1,58 +1,31 @@
 """InVEST main plugin interface module"""
 
 import imp, sys, os
-try:
-    import json
-except ImportError:
-    import simplejson as json
-import data_handler
+import simplejson as json
 
 def execute(model, args):
-    """This function invokes the model specified, processes the arguments
-    to load data structures, and invokes the underlying model.
+    """This function invokes the model specified and invokes the underlying model.
     
     model - the name of the invest module
-    args - either a JSON string or dictionary object of arguments"""
+    args - dictionary object of arguments
+    
+    returns nothing"""
 
     #load the module
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     module = imp.load_source(model, model + '.py')
 
-    #build up lists of the necessary files for this model
-    outKeys = ['storage_cur']
-    inKeys = ['lulc_cur', 'carbon_pools']
-    if args['calc_value'] == True:
-        outKeys += ['storage_fut', 'seq_delta', 'seq_value']
-        inKeys += ['lulc_fut']
-        
-    if 'hwp_cur_shape' in args:
-        inKeys += ['hwp_cur_shape']
-
-    #process the args for input
-    for key in (inKeys):
-        args[key] = data_handler.open(args[key])
-
-    #process the args for output
-    for key in (outKeys):
-        args[key] = data_handler.mimic(args['lulc_cur'], args[key])
-
     #execute the well known name 'execute' that exists in all invest plugins
     module.execute(args)
 
-    #process the args for output
-    map(data_handler.close, args)
-
 def supportedModels():
     """returns an array of InVEST model names"""
-
     return ['carbon']
-
 
 #This part is for command line invocation and allows json objects to be passed
 #as the argument dictionary
 if __name__ == '__main__':
 
-    print sys.argv
     modulename, model, json_args = sys.argv
     args = json.loads(json_args)
     execute(model, args)
