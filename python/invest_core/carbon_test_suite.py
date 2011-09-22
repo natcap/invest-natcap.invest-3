c7f87f947d9057fae8bf8d338b023ea91e5206a1
James Douglass <jdouglass@stanford.edu>
2011-09-21 17:11:48 -0700

    Update issue 78
    Added carbon_core_test to carbon_test_suite.

--- python/invest_core/carbon_test_suite.py
+++ python/invest_core/carbon_test_suite.py
@@ -2,6 +2,19 @@ import unittest
 import carbon_core
 import numpy as np
 import random
+import os
+import data_handler
+from dbfpy import dbf
+
+try:
+    from osgeo import ogr, gdal
+    from osgeo.gdalconst import *
+    import numpy
+    use_numeric = False
+except ImportError:
+    import ogr, gdal
+    from gdalconst import *
+    import Numeric
 
 def suite():
     tests = ['carbon_add_test',
@@ -109,6 +122,116 @@ class CarbonTestSuite(unittest.TestCase):
         #Each value should equal 2.66666666666
         for x in range(lulc.shape[1]):
             self.assertAlmostEqual(output[0][x],2.6666666666, 8)
+
+
+    def test_carbon_core_smoke(self):
+        """Smoke test for carbon_core function.  Shouldn't crash with
+        zero length inputs"""
+        driver = gdal.GetDriverByName("GTIFF")
+        lulc_path = '../../test_data/test_blank_input'
+        lulc = driver.Create(lulc_path, 1, 1, 1, gdal.GDT_Byte)
+        lulc.GetRasterBand(1).SetNoDataValue(-1.0)
+
+        output_path = '../../test_data/test_blank_output'
+        output = driver.Create(output_path, 1, 1, 1, gdal.GDT_Byte)
+        output.GetRasterBand(1).SetNoDataValue(-1.0)
+
+        args = { 'lulc_cur':lulc,
+                'carbon_pools': dbf.Dbf('../../test_data/test_blank_dbf', new=True),
+                'storage_cur': output,
+                'calc_value' : False}
+
+        carbon_core.execute(args)
+
+        #close the two created datasets and dbf file.
+        lulc = None
+        output = None
+        data_handler.close(args['carbon_pools'])
+
+        os.remove(output_path)
+        os.remove('../../test_data/test_blank_dbf')
+        os.remove(lulc_path)
+        pass
+
+    def test_carbon_core_with_inputs(self):
+        """Test carbon_core using realistic inputs."""
+        driver = gdal.GetDriverByName("GTIFF")
+        lulc = gdal.Open('../../test_data/lulc_samp_cur', GA_ReadOnly)
+        out_dict = {'uri':'../../test_real_output.tif',
+                    'input':False,
+                    'type': 'gdal',
+                    'dataType': 6}
+        output = data_handler.mimic(lulc, out_dict)
+        args = { 'lulc_cur': lulc,
+                'carbon_pools': dbf.Dbf('../../test_data/carbon_pools_int.dbf'),
+                'storage_cur': output,
+                'calc_value' : False}
+
+        carbon_core.execute(args)
+        output = data_handler.close(output)
+        os.remove(out_dict['uri'])
+        pass
+
+    def test_carbon_core_with_HWP_inputs(self):
+        """Test carbon using realistic inputs.  Includes HWP"""
+        driver = gdal.GetDriverByName("GTIFF")
+        lulc = gdal.Open('../../test_data/lulc_samp_cur', GA_ReadOnly)
+        out_dict = {'uri':'../../carbon_output/test_real_output_hwp.tif',
+                    'input':False,
+                    'type': 'gdal',
+                    'dataType': 6}
+        output = data_handler.mimic(lulc, out_dict)
+        args = { 'lulc_cur': lulc,
+                'carbon_pools': dbf.Dbf('../../test_data/carbon_pools_int.dbf'),
+                'storage_cur': output,
+                'calc_value' : False,
+                'hwp_cur_shape': ogr.Open('../../test_data/harv_samp_cur/harv_samp_cur.shp'),
+                'lulc_cur_year' : 2000}
+
+
+
+        carbon_core.execute(args)
+        output = data_handler.close(output)
+        #os.remove(out_dict['uri'])
+        pass
+
+    def test_carbon_core_HWP_cur_fut(self):
+        """Test carbon_core with cur and fut HWP"""
+        driver = gdal.GetDriverByName("GTIFF")
+        lulc = gdal.Open('../../test_data/lulc_samp_cur', GA_ReadOnly)
+        out_dict = {'uri':'../../carbon_output/test_real_output_hwp.tif',
+                    'input':False,
+                    'type': 'gdal',
+                    'dataType': 6}
+        output = data_handler.mimic(lulc, out_dict)
+        args = { 'lulc_cur': lulc,
+                'carbon_pools': dbf.Dbf('../../test_data/carbon_pools_int.dbf'),
+                'storage_cur': output,
+                'calc_value' : False,
+                'hwp_cur_shape': ogr.Open('../../test_data/harv_samp_cur/harv_samp_cur.shp'),
+                'hwp_fut_shape': ogr.Open('../../test_data/harv_samp_fut/harv_samp_fut.shp'),
+                'lulc_cur_year' : 2000,
+                'lulc_fut_year' : 2030}
+
+        carbon_core.execute(args)
+        output = data_handler.close(output)
+        #os.remove(out_dict['uri'])
+        pass
+
+
+
+    def test_carbon_core_build_pools(self):
+        """Verify the correct construction of the pools dict"""
+        db = dbf.Dbf('../../test_data/carbon_pools_float.dbf', readOnly=1)
+        pools = carbon_core.build_pools_dict(db, 1, -1, 255)
+        numRecords = db.recordCount
+        poolsLen = len(pools)
+
+        #adding one extra value to the expected length of the pools dict
+        #Extra entry represents the nodata value.
+        self.assertEqual(numRecords + 1, poolsLen, 'Expected ' + str(numRecords) +
+                         ' records in the pools dict, but found ' + str(poolsLen) + ' instead')
+        pass
 
 
 if __name__ == '__main__':
