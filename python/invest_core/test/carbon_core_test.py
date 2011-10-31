2fb3b22555748a75a222105ea25e969fc4c602ed
richpsharp <devnull@localhost>
2011-10-31 11:23:00 -0700

    A test to burn through several raster sizes containing only nodata values.

--- python/invest_core/test/carbon_core_test.py
+++ python/invest_core/test/carbon_core_test.py
@@ -6,33 +6,41 @@ import unittest
 from osgeo import ogr, gdal
 from osgeo.gdalconst import *
 from dbfpy import dbf
+import numpy as np
 
 class TestInvestCarbonCore(unittest.TestCase):
-    def test_carbon_core_smoke(self):
-        """Smoke test for carbon_uncertainty function.  Shouldn't crash with
-        zero length inputs"""
+    def test_carbon_core_nodata(self):
+        """Test several raster sizes that have only nodata values"""
         driver = gdal.GetDriverByName("GTIFF")
 
-        #Create a blank 1x1 raster
-        lulc = driver.Create('../../../test_data/test_blank_input', 1, 1, 1,
-                             gdal.GDT_Byte)
-        lulc.GetRasterBand(1).SetNoDataValue(-1.0)
-        #Fill raster with nodata 
-        lulc.GetRasterBand(1).Fill(-1.0)
+        for xDim, yDim, inNoData, outNodata in [(1, 1, 255, 1.0),
+                                                (10, 10, 255, 21.0),
+                                                (443, 953, 2, 1.0),
+                                                (719, 211, 12, 111.5)]:
+            #Create a blank xDim x yDim raster
+            lulc = driver.Create('../../../test_data/test_blank_input', xDim, yDim, 1,
+                                 gdal.GDT_Byte)
+            lulc.GetRasterBand(1).SetNoDataValue(inNoData)
+            #Fill raster with nodata 
+            lulc.GetRasterBand(1).Fill(lulc.GetRasterBand(1).GetNoDataValue())
 
-        #Create the output raster
-        tot_C_cur = driver.Create('../../../test_data/tot_C_cur_blank_output',
-            lulc.RasterXSize, lulc.RasterYSize, 1, gdal.GDT_Float32)
-        tot_C_cur.GetRasterBand(1).SetNoDataValue(-1.0)
+            #Create the output raster
+            tot_C_cur = driver.Create('../../../test_data/tot_C_cur_blank_output',
+                xDim, yDim, 1, gdal.GDT_Float32)
+            tot_C_cur.GetRasterBand(1).SetNoDataValue(outNodata)
 
-        args = { 'lulc_cur': lulc,
-                'carbon_pools': dbf.Dbf('../../../test_data/test_blank_dbf',
-                                        new=True),
-                'tot_C_cur': tot_C_cur}
-        carbon_core.biophysical(args)
+            args = { 'lulc_cur': lulc,
+                    'carbon_pools': dbf.Dbf('../../../test_data/test_blank_dbf',
+                                            new=True),
+                    'tot_C_cur': tot_C_cur}
+            carbon_core.biophysical(args)
 
-        #This is how GDAL closes its datasets in python
-        output = None
+            #Check that the resulting 'tot_C_cur' raster contains only
+            #no data values
+            data = tot_C_cur.GetRasterBand(1).ReadAsArray(0, 0,
+                tot_C_cur.RasterXSize, tot_C_cur.RasterYSize)
+            sum = np.sum(data, dtype=np.float64)
+            self.assertAlmostEqual(sum, xDim * yDim * outNodata)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestInvestCarbonCore)
