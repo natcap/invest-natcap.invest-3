568cc20765f88b94fdff05d2ee318104a65ba72c
richpsharp <devnull@localhost>
2011-10-31 13:06:20 -0700

    Added test and fixed error relating to calculation of sequestration using a cur and fut lulc.

--- python/invest_core/test/carbon_biophysical_test.py
+++ python/invest_core/test/carbon_biophysical_test.py
@@ -9,8 +9,8 @@ import carbon_biophysical
 
 class TestCarbonBiophysical(unittest.TestCase):
     def test_carbon_biophysical_smoke(self):
-        """Smoke test for carbon_biophysical function.  Shouldn't crash with
-        zero length inputs"""
+        """Smoke test for carbon_biophysical function.  Shouldn't crash with \
+zero length inputs"""
 
         args = {}
         args['workspace_dir'] = '../../carbon_output'
@@ -21,6 +21,22 @@ class TestCarbonBiophysical(unittest.TestCase):
         args['carbon_pools_uri'] = '../../test_data/carbon_pools_float.dbf'
 
         carbon_biophysical.execute(args)
+
+    def test_carbon_biophysical_sequestration(self):
+        """Test for carbon_biophysical function running with sample input to do \
+sequestration on lulc maps."""
+
+        args = {}
+        args['workspace_dir'] = '../../carbon_output'
+        args['calculate_sequestration'] = True
+        args['calculate_hwp'] = False
+        args['calc_uncertainty'] = False
+        args['lulc_cur_uri'] = "../../test_data/lulc_samp_cur"
+        args['lulc_fut_uri'] = "../../test_data/lulc_samp_fut"
+        args['carbon_pools_uri'] = '../../test_data/carbon_pools_float.dbf'
+
+        carbon_biophysical.execute(args)
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestCarbonBiophysical)
