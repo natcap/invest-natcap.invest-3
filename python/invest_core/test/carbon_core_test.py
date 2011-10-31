e8b51c2723fcfb4bd3fda69751d6c3f8fd4ea4e0
richpsharp <devnull@localhost>
2011-10-31 11:52:41 -0700

    Added a test that runs through a random lulc array that includes random nodata values and maps to random carbon pools that include integer and float values as well as nodata values.

--- python/invest_core/test/carbon_core_test.py
+++ python/invest_core/test/carbon_core_test.py
@@ -7,6 +7,7 @@ from osgeo import ogr, gdal
 from osgeo.gdalconst import *
 from dbfpy import dbf
 import numpy as np
+import random
 
 class TestInvestCarbonCore(unittest.TestCase):
     def test_carbon_core_nodata(self):
@@ -41,6 +42,41 @@ class TestInvestCarbonCore(unittest.TestCase):
                 tot_C_cur.RasterXSize, tot_C_cur.RasterYSize)
             sum = np.sum(data, dtype=np.float64)
             self.assertAlmostEqual(sum, xDim * yDim * outNodata)
+
+    def test_carbon_seq_1D_array(self):
+        """Test of calculateCarbonStorage against a random LULC array, includes tests on nodata values"""
+
+        xDim, yDim = (277, 109)
+        driver = gdal.GetDriverByName("GTIFF")
+        lulc = driver.Create('../../../test_data/test_input', xDim, yDim, 1,
+                                 gdal.GDT_Byte)
+        #Made up carbon pools
+        pools = {0: 4.3, 1: 4.3, 2: 5.7, 3: 10.0, 4:-255.0}
+
+        lulc.GetRasterBand(1).SetNoDataValue(4)
+
+        #Set the seed so it always has the same "random" output
+        np.random.seed(123456)
+
+
+        #Make a random lulc that consists of lulc types 0..2
+        array = np.random.random_integers(0, len(pools) - 1, size=(yDim, xDim))
+
+        lulc.GetRasterBand(1).WriteArray(array, 0, 0)
+
+        #Create the output raster
+        tot_C_cur = driver.Create('../../../test_data/tot_C_cur',
+            xDim, yDim, 1, gdal.GDT_Float64)
+        tot_C_cur.GetRasterBand(1).SetNoDataValue(-255.0)
+
+        carbon_core.calculateCarbonStorage(pools, lulc, tot_C_cur)
+
+        #verify the contents of output against pool and lulc data
+        data = tot_C_cur.GetRasterBand(1).ReadAsArray(0, 0,
+                tot_C_cur.RasterXSize, tot_C_cur.RasterYSize)
+        for x in xrange(xDim):
+            for y in xrange(yDim):
+                self.assertAlmostEqual(pools[array[y][x]], data[y][x])
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestInvestCarbonCore)
