ffc0998f845e6b12ce22b2186e602e04fc7990b7
dcdenu4 <devnull@localhost>
2011-11-01 14:10:54 -0700

    Fixed typos in timber_core_test.py from 241-250. Also changed assertEqual to assertAlmostEqual since old model rounds to 3 decimal places.

--- python/invest_core/timber_core_test.py
+++ python/invest_core/timber_core_test.py
@@ -238,16 +238,16 @@ class TestTimber(unittest.TestCase):
         
         valid_output_shape = ogr.Open('../../test_data/timber/output/timber.shp')
         valid_output_layer = valid_output_shape.GetLayerByName('timber')
-        
-        for i in range(valid_output_layer.recordCount):
-            feat = valid_output.layer.getFeature(i)
-            feat2 = timber_layer_copy.getFeature(i)
+        num_features = valid_output_layer.GetFeatureCount()
+        for i in range(num_features):
+            feat = valid_output_layer.GetFeature(i)
+            feat2 = timber_layer_copy.GetFeature(i)
             for field in ('TNPV', 'TBiomass', 'TVolume'):
                 field_index = feat.GetFieldIndex(field)
                 field_value = feat.GetField(field_index)
                 field_index2 = feat2.GetFieldIndex(field)
                 field_value2 = feat2.GetField(field_index2)
-                self.assertEqual(field_value, field_value2)
+                self.assertAlmostEqual(field_value, field_value2, 2)
         
 #        #Compare two shapefiles to make sure values correspond correctly
 #        for feat, feat2 in zip(valid_output_layer, timber_layer_copy):
