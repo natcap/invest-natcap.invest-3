38c660b14468e87560de375d5471bd8be928c545
richpsharp <devnull@localhost>
2011-11-01 10:14:16 -0700

    Removed 'getBiomass' and pasted one line of code directly into calculation.

--- python/invest_core/timber.py
+++ python/invest_core/timber.py
@@ -72,7 +72,7 @@ def execute(args):
             summation_two = npvSummationTwo(sumTwo_lowerLimit, sumTwo_upperLimit, maint_Cost, mdr_perc)
 
         #Calculate Biomass
-        biomass = getBiomass(parcl_Area, perc_Harv, harv_Mass, num_Years, freq_Harv)
+        biomass = parcl_Area * (perc_Harv / 100.00) * harv_Mass * math.ceil(T / freq_Harv)
         #Calculate Volume
         volume = getVolume(biomass, BCEF)
 
@@ -128,11 +128,6 @@ def npvSummationTwo(lower, upper, maint_Cost, mdr_perc):
             summation = summation + (maint_Cost / (mdr_perc ** num))
 
     return summation
-
-#Calculates the Biomass for a parcel
-def getBiomass(parcl_Area, perc_Harv, harv_Mass, T, freq_Harv):
-    TBiomass = parcl_Area * (perc_Harv / 100.00) * harv_Mass * math.ceil(T / freq_Harv)
-    return TBiomass
 
 #Calculates the Volume for a parcel
 def getVolume(biomass, BCEF):
