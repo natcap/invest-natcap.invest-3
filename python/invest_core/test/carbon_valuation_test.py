c7ae617eb9beb223e58806edd9905768c1201292
richpsharp <devnull@localhost>
2011-11-08 15:11:51 -0800

    Update issue 111
    Adding unit tests to the valuation module.

---
+++ python/invest_core/test/carbon_valuation_test.py
@@ -0,0 +1,29 @@
+import os, sys
+import unittest
+import invest_test_core
+
+#Add current directory and parent path for import tests
+cmd_folder = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, cmd_folder + '/../')
+
+import carbon_valuation
+
+class TestCarbonBiophysical(unittest.TestCase):
+    def test_carbon_biophysical_smoke(self):
+        """Smoke test for carbon_biophysical function.  Shouldn't crash with \
+#zero length inputs"""
+
+        args = {}
+        args['workspace_dir'] = '../../carbon_output'
+        args['sequest_uri'] = '../../test_data/sequest_regression.tif'
+        args['V'] = 43.0
+        args['r'] = 7.0
+        args['c'] = 0.0
+        args['yr_cur'] = 2000
+        args['yr_fut'] = 2030
+
+        carbon_valuation.execute(args)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCarbonBiophysical)
+    unittest.TextTestRunner(verbosity=2).run(suite)
