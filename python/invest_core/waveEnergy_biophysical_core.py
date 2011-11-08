bbcf4001e066130ee26f5ca9e16245667da6912e
dcdenu4 <devnull@localhost>
2011-11-08 07:22:43 -0800

    Added rough calculations for wave power, wave energy, and npv.

--- python/invest_core/waveEnergy_biophysical_core.py
+++ python/invest_core/waveEnergy_biophysical_core.py
@@ -9,16 +9,35 @@ import invest_core
 
 def biophysical(args):
     """
-    args['wave_base_data'] -
-    arcs['analysis_area'] -
-    arcs['AOI'] -
-    arcs['machine_perf'] -
-    arcs['machine_param'] -
-    arcs['dem'] -
+    args['wave_base_data'] - a dictionary
+    arcs['analysis_area'] - 
+    arcs['AOI'] - a shapefile
+    arcs['machine_perf'] - a dictionary
+    arcs['machine_param'] - a dictionary
+    arcs['dem'] - a GIS raster file
         
     """
     performance_dict = getMachinePerf(args['machine_perf'])
     
+    
+    
+    
 def getMachinePerf(machine_perf):
     performance_dict = {}
     return performance_dict
+
+def wavePower():
+    P = ((p*g)/16)*(H**2)*waveGroupVelocity()
+    return P
+
+def waveGroupVelocity():
+    return (1+((2*k*h)/math.sinh(2*k*h)) * (math.sqrt((g/k)*math.tanh(k*h))))/2
+
+def waveNumber():
+    return (w**2)/(g*math.tanh((w**2)*(h/g)))
+
+def npv():
+    for num in range(1, T+1):
+        sum = sum + (B[num]-C[num])*((1+i)**(-1 * t))
+        
+    return npv
