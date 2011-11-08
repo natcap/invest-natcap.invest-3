14408b37785bdc9d7e13b70cc60b3a92c8375501
richpsharp <devnull@localhost>
2011-11-08 15:43:20 -0800

    Added a full regression test with a python code snippet for testing values by hand if necessary.

--- python/invest_core/test/carbon_valuation_test.py
+++ python/invest_core/test/carbon_valuation_test.py
@@ -9,9 +9,24 @@ sys.path.insert(0, cmd_folder + '/../')
 import carbon_valuation
 
 class TestCarbonBiophysical(unittest.TestCase):
-    def test_carbon_biophysical_smoke(self):
-        """Smoke test for carbon_biophysical function.  Shouldn't crash with \
-#zero length inputs"""
+    def test_carbon_valuation_regression(self):
+        """Regression test for carbon_valuation function.  A few pixels have 
+            been tested by hand against the following python snippet:
+            
+        >>> def f(V,sequest,yr_fut,yr_cur,r,c):
+        ...     sum = 0
+        ...     for t in range(yr_fut-yr_cur):
+        ...             sum += 1/((1+(r/100.0))**t*(1+c/100.0)**t)
+        ...     return sum*V*sequest/(yr_fut-yr_cur)
+        ... 
+        >>> V=43.0
+        >>> yr_cur=2000
+        >>> yr_fut=2030
+        >>> sequest=1.0
+        >>> sequest=-57.8494
+        >>> f(V,sequest,yr_fut,yr_cur,r,c)
+        -1100.9511853253725
+            """
 
         args = {}
         args['workspace_dir'] = '../../carbon_valuation_output'
@@ -23,6 +38,13 @@ class TestCarbonBiophysical(unittest.TestCase):
         args['yr_fut'] = 2030
 
         carbon_valuation.execute(args)
+
+        #assert that the output raster is equivalent to the regression
+        #test
+        invest_test_core.assertTwoDatasetEqualURI(self,
+            args['workspace_dir'] + "/Output/value_seq.tif",
+            '../../test_data/value_seq_regression.tif')
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestCarbonBiophysical)
