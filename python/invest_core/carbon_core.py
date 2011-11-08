878753429a88745dcd26b550103d7fe82047e4f9
richpsharp <devnull@localhost>
2011-11-08 15:11:03 -0800

    Fixed typo, wrote 'r' when should have used 'ratio'.

--- python/invest_core/carbon_core.py
+++ python/invest_core/carbon_core.py
@@ -850,7 +850,7 @@ def valuation(args):
     n = args['yr_fut'] - args['yr_cur'] - 1
     ratio = 1.0 / ((1 + args['r'] / 100.0) * (1 + args['c'] / 100.0))
     valuationConstant = args['V'] / (args['yr_fut'] - args['yr_cur']) * \
-        (1.0 - r ** (n + 1)) / (1.0 - r)
+        (1.0 - ratio ** (n + 1)) / (1.0 - ratio)
 
     def valueOp(sequest):
         return sequest * valuationConstant
