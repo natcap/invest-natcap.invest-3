8742989144378b0520d71bb3e343758459eb01d3
dcdenu4 <devnull@localhost>
2011-11-10 16:40:21 -0800

    First look at using CSV files instead of excel. Commented out excel file handling statements and put in raw basic functionality of opening up a CSV file which holds machine performance and parameters.  Saved machine performance into a 2D array which can be passed to numpy to multiply it against WW 3 data in text file.  Saving parameters in dictionary.

--- python/invest_core/waveEnergy_biophysical.py
+++ python/invest_core/waveEnergy_biophysical.py
@@ -6,11 +6,11 @@ import waveEnergy_biophysical_core
 import invest_core
 from osgeo import gdal, ogr
 from osgeo.gdalconst import *
-import numpy
+import numpy as np
 from dbfpy import dbf
 
 from xlrd import open_workbook
-
+import csv
 def execute(args):
     """This function invokes the wave energy model given URI inputs of files.
         It will do filehandling and open/create appropriate objects to 
@@ -37,23 +37,51 @@ def execute(args):
     #we import gdal stuff we don't get the wrong GDAL version.
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     filesystemencoding = sys.getfilesystemencoding()
+
+    dict = {}
+    arrayHeader = []
+    arrayColumns = []
+    machine_perf_twoDArray = []
     
-#    timber_shape = ogr.Open(args['timber_shape_uri'].encode(filesystemencoding), 1)
+    with open(args['machine_perf_uri'], 'rb') as f:
+        reader = csv.reader(f)
+        i = -1
+        for row in reader:
+            if i==-1:
+                arrayHeader = row
+                arrayHeader.pop(0)
+                i = 0
+            else:
+                arrayColumns.append(row.pop(0))                
+                dict[i] = row
+                i = i + 1
+                
+    for array in dict.itervalues():
+        machine_perf_twoDArray.append(array)
+        
+    machine_params = {}
+    count = 0
+    with open(args['machine_param_uri'], 'rb') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            machine_params[count] = row
+            count = count + 1
+
     
     gdal.AllRegister()
     
-    perfPathList = args['machine_perf_uri'].rsplit(os.sep, 1)
-    perfPathWkbook = perfPathList[0]
-    perfWkshtList = perfPathList[1].split('$')
-    perfWksht = perfWkshtList[0]
-    
-    paramPathList = args['machine_param_uri'].rsplit(os.sep, 1)
-    paramPathWkbook = paramPathList[0]
-    paramWkshtList = paramPathList[1].split('$')
-    paramWksht = paramWkshtList[0]
-    
-    machine_perfSheet = open_workbook(perfPathWkbook).sheet_by_name(perfWksht)
-    machine_paramSheet= open_workbook(paramPathWkbook).sheet_by_name(paramWksht)
+#    perfPathList = args['machine_perf_uri'].rsplit(os.sep, 1)
+#    perfPathWkbook = perfPathList[0]
+#    perfWkshtList = perfPathList[1].split('$')
+#    perfWksht = perfWkshtList[0]
+#    
+#    paramPathList = args['machine_param_uri'].rsplit(os.sep, 1)
+#    paramPathWkbook = paramPathList[0]
+#    paramWkshtList = paramPathList[1].split('$')
+#    paramWksht = paramWkshtList[0]
+#    
+#    machine_perfSheet = open_workbook(perfPathWkbook).sheet_by_name(perfWksht)
+#    machine_paramSheet= open_workbook(paramPathWkbook).sheet_by_name(paramWksht)
         
     wave_base_data = 1
     analysis_area = ogr.Open(args['analysis_area_uri'].encode(filesystemencoding), 1)
@@ -63,8 +91,8 @@ def execute(args):
     arguments = {'wave_base_data': wave_base_data,
                  'analysis_area': analysis_area,
                  'AOI': AOI,
-                 'machine_perf': machine_perfSheet,
-                 'machine_param': machine_paramSheet,
+                 'machine_perf': machine_perf_twoDArray,
+                 'machine_param': machine_params,
                  'dem': dem,
     #             'valuation': gp.GetParameterAsText(7),
     #             'landgridpts_uri': gp.GetParameterAsText(8),
