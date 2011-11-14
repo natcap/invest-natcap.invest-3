2d30bd87718b0befbb32133601e30fb65ef0d0f5
dcdenu4 <devnull@localhost>
2011-11-14 09:29:53 -0800

    Added a skeleton function that will handle the interpolation and trimming of the WW 3 data. Also commented out a chunk of code to test Rich's createRasterFromVectorExtents function in invest_core.

--- python/invest_core/waveEnergy_core.py
+++ python/invest_core/waveEnergy_core.py
@@ -32,40 +32,51 @@ def biophysical(args):
     layer = args['analysis_area'].GetLayer(0)
     
     #Create a new raster that has the values of the WWW shapefile
-    source = cutterLayer.GetSpatialRef()
-    projection = source.ExportToWkt()
+#    source = cutterLayer.GetSpatialRef()
+#    projection = source.ExportToWkt()
     
-    x_min, x_max, y_min, y_max = cutterLayer.GetExtent()
+#    x_min, x_max, y_min, y_max = cutterLayer.GetExtent()
     geoform = global_dem.GetGeoTransform()
     pixelSizeX = geoform[1]
     pixelSizeY = geoform[5]
+#    
+#    x_res = int((x_max-x_min) / pixelSizeX)
+#    y_res = int((y_max-y_min) / pixelSizeY)
+#    y_res = int(math.fabs(y_res))
+#    cols = x_res
+#    rows = y_res
     
-    x_res = int((x_max-x_min) / pixelSizeX)
-    y_res = int((y_max-y_min) / pixelSizeY)
-    y_res = int(math.fabs(y_res))
-    cols = x_res
-    rows = y_res
-    
-    outputpath = '../../test_data/wave_Energy/newRaster5.tif'
-    driver = gdal.GetDriverByName(format)
-    
-    newRaster = driver.Create(outputpath, int(cols), int(rows), 1, gdal.GDT_Float32)
-    
-    newRaster.SetProjection(projection)
-    newRaster.SetGeoTransform((x_min, pixelSizeX, 0, y_max, 0, pixelSizeY))
-    newRaster.GetRasterBand(1).SetNoDataValue(nodata)
-    newRaster.GetRasterBand(1).Fill(nodata)
+    outputpath = '../../test_data/wave_Energy/newRaster.tif'
+#    driver = gdal.GetDriverByName(format)
+#    
+#    newRaster = driver.Create(outputpath, int(cols), int(rows), 1, gdal.GDT_Float32)
+#    
+#    newRaster.SetProjection(projection)
+#    newRaster.SetGeoTransform((x_min, pixelSizeX, 0, y_max, 0, pixelSizeY))
+#    newRaster.GetRasterBand(1).SetNoDataValue(nodata)
+#    newRaster.GetRasterBand(1).Fill(nodata)
+#    
+    invest_core.createRasterFromVectorExtents(23, 23, 
+                                              datatype, nodata, outputpath, cutter)
     
     #Make a duplicate copy of the global_dem to try and crop
-    drv = gdal.GetDriverByName(format)
-    newGlobal = drv.CreateCopy('../../test_data/wave_Energy/newGlobal.tif', newRaster, 1)
-    newGlobal.GetRasterBand(1).SetNoDataValue(0)
-    newGlobal.GetRasterBand(1).Fill(0)
-    #Burn Height values from shapefile onto new raster
-    raster = gdal.RasterizeLayer(newRaster, [1], layer, options=['ATTRIBUTE=' + 'HSAVG_M'])
+#    drv = gdal.GetDriverByName(format)
+#    newGlobal = drv.CreateCopy('../../test_data/wave_Energy/newGlobal.tif', newRaster, 1)
+#    newGlobal.GetRasterBand(1).SetNoDataValue(0)
+#    newGlobal.GetRasterBand(1).Fill(0)
+#    #Burn Height values from shapefile onto new raster
+#    raster = gdal.RasterizeLayer(newRaster, [1], layer, options=['ATTRIBUTE=' + 'HSAVG_M'])
+#
+#    newRaster = None
 
-    newRaster = None
-
+def interpolateWaveDate(machinePerf, waveBaseData):
+    #Trim down the waveBaseData based on the machinePerf rows/columns
+    #and then interpolate if need be.
+    #A 2D array that will be vectorized with machinePerf
+    interpWaveDate = []
+    
+    return interpWaveData
+    
 def getMachinePerf(machine_perf):
     performance_dict = {}
     return performance_dict
