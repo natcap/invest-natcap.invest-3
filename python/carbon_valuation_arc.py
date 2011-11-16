13943c093a107fe50518e9a1c2700bbbe837fdef
James Douglass <jdouglass@stanford.edu>
2011-11-16 15:52:35 -0800

    Update issue 155
    Replaced existing code in _arc files for carbon_biophysical, carbon_valuation, timber models.
    
    path to carbon_valuation file was not stored as a relative path.  Fixed with this commit.
    
    Added descriptions to the three models.  Unfortunately, there don't appear to be any labels or other 'parameter' features of the parameters area that would allow us to annotate the model.  Instead, the only readily editable text appears to be the help portion of the script window.  Help area seems to strip non-space whitespace, so we have very little options for formatting.

--- python/carbon_valuation_arc.py
+++ python/carbon_valuation_arc.py
@@ -1,30 +1,14 @@
 import os, subprocess
-import getpass
-import invest_core.simplejson as json
 import arcgisscripting
 gp = arcgisscripting.create()
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)) + "\\..\\")
-gp.AddMessage(os.getcwd())
-
-args_dict = {
-    'output_dir': gp.GetParameterAsText(0),
-    'seq_uri': gp.GetParameterAsText(1),
-    'n_years': gp.GetParameterAsText(2),
-    'rate_of_change': gp.GetParameterAsText(3),
-    'discount_rate': gp.GetParameterAsText(4)
-    }
-
-#Save user inputs from the previous run
-args_file = open('C:\Users\\' + getpass.getuser() + '\My Documents\ArcGIS\carbon_biophysical_args.json', 'w')
-args_file.writelines(json.dumps(json_dict))
-args_file.close()
 
 gp.AddMessage('Starting carbon valuation model')
 
 process = subprocess.Popen(['OSGeo4W\\gdal_python_exec.bat',
-                            'python\\invest_core\\carbon_biophysical.py',
-                            json.dumps(arguments)],
+                            'python\\iui\\iui_main.py',
+                            'python\\iui\\carbon_valuation.json'],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT).communicate()[0]
 
