ff82e7923d0ea648433b7b5b52d7ef7b8eb7c02e
James Douglass <jdouglass@stanford.edu>
2011-11-18 15:48:16 -0800

    Update issue 149
    Added a function to check the status of the output directory to validator_core.
    
    
    Former-commit-id: 07df6b55986395f5ce137fb1cb1c52e06337f252

--- python/invest_core/validator_core.py
+++ python/invest_core/validator_core.py
@@ -1,3 +1,7 @@
+"""This module contains general purpose validation functions useful for
+    InVEST model validators."""
+
+
 import os
 
 def checkArgsKeys(argsDict, argsList, outList):
@@ -13,3 +17,20 @@ def checkArgsKeys(argsDict, argsList, outList):
     for argument in argsList:
         if argument not in argsDict:
             outList.append('Missing parameter: ' + argument)
+            
+def checkOutputDir(uri, out):
+    """Verify that the given directory exists and is writeable.  Append error 
+        messages to output list if not.
+        
+        uri - a python string representing the uri to the target folder
+        out - a python list (for appending errors)
+        
+        returns nothing."""
+        
+    prefix = 'Output folder: ' + uri
+    if not os.path.isdir(uri):
+        out.append(prefix + ' not found or is not a folder.')
+    else:
+        #Determine if output dir is writable
+        if not os.access(uri, os.W_OK):
+            out.append(prefix + ' must be writeable.')
