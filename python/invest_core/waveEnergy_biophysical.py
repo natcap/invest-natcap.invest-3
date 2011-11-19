6f23135bac5e6e80e34f13d49f9bae41da2c1813
dcdenu4 <devnull@localhost>
2011-11-19 13:00:53 -0800

    Cleaning up code in file handler. Removed unneeded code for building 2D array for machine performance. Changed number of parameters for extrapolateWaveData to one parameter, the file path.  Will now open the file in this function.
    Former-commit-id: c52c45b195d09790c022e689a208c1f7e791bf92

--- python/invest_core/waveEnergy_biophysical.py
+++ python/invest_core/waveEnergy_biophysical.py
@@ -47,81 +47,67 @@ def execute(args):
         
     biophysicalargs = {}
     
-    dict = {}
+    dict = [[],[]]
     arrayHeader = []
     arrayColumns = []
     machine_perf_twoDArray = []
     
-    #Create a dictionary of arrays which represent the rows
-    #Keep separate arrays for the row header and column header
+    #Create a 2D array of the machine performance table and place the x fields
+    #and y fields as first two arrays in the list of arrays
     try:
         f = open(args['machine_perf_uri'])
         reader = csv.reader(f)
-        i = -1
+        getRow = True
         for row in reader:
-            if i==-1:
-                arrayHeader = row
-                arrayHeader.pop(0)
-                i = 0
+            if getRow:
+                dict[0] = row[1:]
+                getRow = False
             else:
-                arrayColumns.append(row.pop(0))                
-                dict[i] = row
-                i = i + 1
+                dict[1].append(row.pop(0))
+                dict.append(row)
         f.close()
     except IOError, e:
         print 'File I/O error' + e
-        
-    #Create 2D array by compiling rows of arrays from dict
-    #Add on the row/col fields in same order as WW 3 text file
-    machine_perf_twoDArray.append(arrayHeader)
-    machine_perf_twoDArray.append(arrayColumns)
-    for array in dict.itervalues():
+    #Convert all the string values to floats
+    for array in dict:
         for index, val in enumerate(array):
             array[index] = float(val)
         machine_perf_twoDArray.append(array)
     
-    for index, val in enumerate(arrayHeader):
-        arrayHeader[index] = float(val)
-    for index, val in enumerate(arrayColumns):
-        arrayColumns[index] = float(val)
-    
     biophysicalargs['machine_perf'] = machine_perf_twoDArray
+    
     #Create a dictionary of dictionaries where the inner dictionaries keys are the column fields.
+    #This is for the machine parameter values.
     machine_params = {}
-    count = 0
     try:
         machineParamFile = open(args['machine_param_uri'])
         reader = csv.DictReader(machineParamFile)
         for row in reader:
-            machine_params[count] = row
-            count = count + 1
+            machine_params[row['NAME']] = row
         machineParamFile.close()
         biophysicalargs['machine_param'] = machine_params
     except IOError, e:
         print 'File I/O error' + e
+        
     #WaveDate information depends on analysis area
     if args['analysis_area_uri'] == 'West Coast of North America and Hawaii':
         analysis_area_path = args['wave_base_data_uri'] + os.sep + 'NAmerica_WestCoast_4m.shp'
-        waveFile = open(args['wave_base_data_uri']+os.sep+'NAmerica_WestCoast_4m.txt')
-        biophysicalargs['wave_base_data'] = extrapolateWaveData(analysis_area_path, waveFile)
+        biophysicalargs['wave_base_data'] = extrapolateWaveData(args['wave_base_data_uri']+os.sep+'NAmerica_WestCoast_4m.txt')
         biophysicalargs['analysis_area'] = ogr.Open(analysis_area_path.encode(filesystemencoding), 1)
         
     elif args['analysis_area_uri'] == 'East Coast of North America and Puerto Rico':
         analysis_area_path = args['wave_base_data_uri'] + os.sep + 'NAmerica_EastCoast_4m.shp'
-        waveFile = open(args['wave_base_data_uri']+os.sep+'NAmerica_EastCoast_4m.txt')
-        biophysicalargs['wave_base_data'] = extrapolateWaveData(analysis_area_path, waveFile)
+        biophysicalargs['wave_base_data'] = extrapolateWaveData(args['wave_base_data_uri']+os.sep+'NAmerica_EastCoast_4m.txt')
         biophysicalargs['analysis_area'] = ogr.Open(analysis_area_path.encode(filesystemencoding), 1)
         
     elif args['analysis_area_uri'] == 'Global(Eastern Hemisphere)':
         analysis_area_path = args['wave_base_data_uri'] + os.sep + 'Global_EastHemi_30m.shp'
-        waveFile = open(args['wave_base_data_uri']+os.sep+'Global_EastHemi_30m.txt')
-        biophysicalargs['wave_base_data'] = extrapolateWaveData(analysis_area_path, waveFile)
+        biophysicalargs['wave_base_data'] = extrapolateWaveData(args['wave_base_data_uri']+os.sep+'Global_EastHemi_30m.txt')
         biophysicalargs['analysis_area'] = ogr.Open(analysis_area_path.encode(filesystemencoding), 1)
         
     elif args['analysis_area_uri'] == 'Global(Western Hemisphere)':
         analysis_area_path = args['wave_base_data_uri'] + os.sep + 'Global_WestHemi_30m.shp'
-        waveFile = open(args['wave_base_data_uri']+os.sep+'Global_WestHemi_30m.txt')      
-        biophysicalargs['wave_base_data'] = extrapolateWaveData(analysis_area_path, waveFile)
+        biophysicalargs['wave_base_data'] = extrapolateWaveData(args['wave_base_data_uri']+os.sep+'Global_WestHemi_30m.txt')
         biophysicalargs['analysis_area'] = ogr.Open(analysis_area_path.encode(filesystemencoding), 1)
         
     else:
@@ -129,6 +115,7 @@ def execute(args):
     
     gdal.AllRegister()
     
+    #If the area of interest is present add it to the output arguments
     AOI = None
     if 'AOI_uri' in args:
         try:
@@ -137,7 +124,7 @@ def execute(args):
             
         except IOError, e:
             print 'File I/O error' + e
-
+    #If the valuation is true, then create dictionaries for the following files
     if 'calculate_valuation' in args:
         for file, id in (('machine_econ', 'NAME'), ('landgridpts', 'ID')):
             try:
@@ -155,46 +142,48 @@ def execute(args):
         
     waveEnergy_core.biophysical(biophysicalargs)
 
-def extrapolateWaveData(analysis_path, waveOpen):
-    analysis_area_path = analysis_path
-    waveFile = waveOpen
-    waveDict = {}
-    waveArray = []
-    waveRow = []
-    waveCol = []
-    key = ''
-    lineCount = 0
-    check = 0
-    rowcolGrab = False
-    for line in waveFile:
-        if line[0] == 'I':
-            iVal = int(line.split(',')[1])
-            jVal = int(line.split(',')[3])
-            key = (iVal, jVal)
-            waveArray = []
-            rowcolGrab = True
-        elif rowcolGrab:
-            if lineCount == 0:
-                if check != -1:
-                    waveRow.append(line.split(','))
-                    waveRow = waveRow[0]
-                lineCount = lineCount + 1
-            elif lineCount == 1:
-                if check != -1:
-                    waveCol.append(line.split(','))
-                    waveCol = waveCol[0]
-                    check = -1
-                lineCount = 0
-                rowcolGrab = False
-        else:
-            waveArray.append(line.split(','))
-            waveDict[key] = waveArray
-    for i, val in enumerate(waveRow):
-        waveRow[i] = float(val)
-    for i, val in enumerate(waveCol):
-        waveCol[i] = float(val)
+def extrapolateWaveData(waveFile):
+    try:
+        waveOpen = open(waveFile)
+        waveDict = {}
+        waveArray = []
+        waveRow = []
+        waveCol = []
+        key = ''
+        lineCount = 0
+        check = 0
+        rowcolGrab = False
+        for line in waveOpen:
+            if line[0] == 'I':
+                iVal = int(line.split(',')[1])
+                jVal = int(line.split(',')[3])
+                key = (iVal, jVal)
+                waveArray = []
+                rowcolGrab = True
+            elif rowcolGrab:
+                if lineCount == 0:
+                    if check != -1:
+                        waveRow.append(line.split(','))
+                        waveRow = waveRow[0]
+                    lineCount = lineCount + 1
+                elif lineCount == 1:
+                    if check != -1:
+                        waveCol.append(line.split(','))
+                        waveCol = waveCol[0]
+                        check = -1
+                    lineCount = 0
+                    rowcolGrab = False
+            else:
+                waveArray.append(line.split(','))
+                waveDict[key] = waveArray
+        for i, val in enumerate(waveRow):
+            waveRow[i] = float(val)
+        for i, val in enumerate(waveCol):
+            waveCol[i] = float(val)
         
-    waveDict[0] = waveRow
-    waveDict[1] = waveCol
-    return waveDict
-    
+        waveOpen.close()
+        waveDict[0] = waveRow
+        waveDict[1] = waveCol
+        return waveDict
+    except IOError, e:
+        print 'File I/O error' + e
