3efb356fd8cefa3603b7915bcc815a37ea7b7ba0
richpsharp <devnull@localhost>
2011-11-29 15:21:52 -0800

    Added a dynamic constructor to the queue and a cdef for extend.
    Former-commit-id: c53a5ddd13ae723f2ccab9f9701c0db6630fa0e3

--- python/invest_core/queue.pyx
+++ python/invest_core/queue.pyx
@@ -2,10 +2,15 @@ cimport cqueue
 
 cdef class Queue:
     cdef cqueue.Queue *_c_queue
-    def __cinit__(self):
+    def __cinit__(self,items=None):
         self._c_queue = cqueue.queue_new()
-        
+        if items != None:
+            for i in items:
+                cqueue.queue_push_tail(self._c_queue,i)
+
     def __dealloc__(self):
         cqueue.queue_free(self._c_queue)
 
-            
+    cdef extend(self, int* items, size_t count):
+        for i in range(count):
+            cqueue.queue_push_tail(self._c_queue,items[i])
