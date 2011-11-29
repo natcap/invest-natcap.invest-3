d0760c15f76c95ee110edd40fb5563722241b4b0
dcdenu4 <devnull@localhost>
2011-11-29 13:18:28 -0800

    Edited the file paths to accomodate the changes I made to the wave_Energy test folder for sample inputs.
    Former-commit-id: de662ab0de07984ff6da6b81478c799cde910f82

--- python/invest_core/test/waveEnergy_biophysical_test.py
+++ python/invest_core/test/waveEnergy_biophysical_test.py
@@ -12,12 +12,12 @@ class TestWaveEnergyBiophysical(unittest.TestCase):
     def test_waveEnergy_smoke(self):
         args = {}
         args['workspace_dir'] = '../../test_data/wave_Energy'
-        args['wave_base_data_uri'] = '../../test_data/wave_Energy/samp_data/input/WaveData'
+        args['wave_base_data_uri'] = '../../test_data/wave_Energy/samp_input/WaveData'
         args['analysis_area_uri'] = 'West Coast of North America and Hawaii'
-        args['machine_perf_uri'] = '../../test_data/wave_Energy/samp_data/input/Machine_AquaBuOYCSV.csv'
-        args['machine_param_uri'] = '../../test_data/wave_Energy/samp_data/input/Machine_AquaBuOYParamCSV.csv'
-        args['dem_uri'] = '../../test_data/wave_Energy/samp_data/input/global_dem'
-        args['AOI_uri'] = '../../test_data/wave_Energy/samp_data/input/AOI_WCVI.shp'
+        args['machine_perf_uri'] = '../../test_data/wave_Energy/samp_input/Machine_AquaBuOYCSV.csv'
+        args['machine_param_uri'] = '../../test_data/wave_Energy/samp_input/Machine_AquaBuOYParamCSV.csv'
+        args['dem_uri'] = '../../test_data/wave_Energy/samp_input/global_dem'
+        args['AOI_uri'] = '../../test_data/wave_Energy/samp_input/AOI_WCVI.shp'
         waveEnergy_biophysical.execute(args)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestWaveEnergyBiophysical)
