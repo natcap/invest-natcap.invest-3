0848dc20bbeabd3c558a9334da384c41a7c9ee17
dcdenu4 <devnull@localhost>
2011-12-08 14:10:53 -0800

    Added more comprehensive comments.
    Former-commit-id: feabfc9ae19790b689b682ce87e99f894c2a806c

--- python/invest_core/waveEnergy_biophysical.py
+++ python/invest_core/waveEnergy_biophysical.py
@@ -12,24 +12,25 @@ from dbfpy import dbf
 import csv
 
 def execute(args):
-    """This function invokes the wave energy model given URI inputs of files.
+    """This function invokes the biophysical part of the wave energy model given URI inputs.
         It will do filehandling and open/create appropriate objects to 
         pass to the core wave energy biophysical processing function.  It may write
         log, warning, or error messages to stdout.
         
-        args - a python dictionary with at the following possible entries:
-        args['workspace_dir'] - 
-        args['wave_base_data_uri'] - 
-        args['analysis_area_uri'] - 
-        args['AOI_uri'] - 
-        args['machine_perf_uri'] - 
-        args['machine_param_uri'] - 
-        args['dem_uri'] - 
-        args['calculate_valuation'] - 
-        args['landgridpts_uri'] - 
-        args['machine_econ_uri'] - 
-        args['number_machines'] - 
-        args['projection'] - 
+        args - a python dictionary with at least the following possible entries:
+        args['workspace_dir'] - Where the intermediate and ouput folder/files will be saved.
+        args['wave_base_data_uri'] - Directory location of wave base data including WW3 data and analyis area
+                                     shapefile.
+        args['analysis_area_uri'] - A string identifying the analysis area of interest
+        args['AOI_uri'] - A polygon shapefile outlining a more detailed area within the analyis area.
+        args['machine_perf_uri'] - A string of the location of a CSV file that holds the machine performace table. 
+        args['machine_param_uri'] - A string of the location of a CSV file that holds the machien parameter table.
+        args['dem_uri'] - The path of the Global Digital Elevation Model (DEM)
+        args['calculate_valuation'] - A boolean value indicating whether to compute the economic evaluation.
+        args['landgridpts_uri'] - The path to a CSV file containing the Landing and Power Grid Connection Points table.
+        args['machine_econ_uri'] - The path to a CSV file containing the machine economic table.
+        args['number_machines'] - An integer representing the number of machines.
+        args['projection'] - The path to a projection used for economic evaluation.
         
         returns nothing."""
 
@@ -50,8 +51,8 @@ def execute(args):
     biophysicalargs['workspace_dir'] = args['workspace_dir']
     biophysicalargs['wave_data_dir'] = args['wave_base_data_uri']
     biophysicalargs['dem'] = gdal.Open(args['dem_uri'])
-    #Create a 2D array of the machine performance table and place the x fields
-    #and y fields as first two arrays in the list of arrays
+    #Create a 2D array of the machine performance table and place the row
+    #and column headers as the first two arrays in the list of arrays
     try:
         machine_perf_twoDArray = [[],[]]
         machinePerfFile = open(args['machine_perf_uri'])
@@ -69,8 +70,9 @@ def execute(args):
     except IOError, e:
         print 'File I/O error' + e
         
-    #Create a dictionary of dictionaries where the inner dictionaries keys are the column fields.
-    #This is for the machine parameter values.
+    #Create a dictionary whose keys are the 'NAMES' from the machine parameter table
+    #and whose corresponding values are dictionaries whose keys are the column headers of
+    #the machine parameter table with corresponding values
     try:
         machine_params = {}
         machineParamFile = open(args['machine_param_uri'])
@@ -82,7 +84,10 @@ def execute(args):
     except IOError, e:
         print 'File I/O error' + e
         
-    #WaveDate information depends on analysis area
+    #Depending on which analyis area is selected:
+    #Extrapolate the corresponding WW3 Data and place in the arguments dictionary
+    #Open the point geometry analysis area shapefile contaning the corresponding seastate bins
+    #Open the polygon geometry analysis area extract shapefile contaning the outline of the area of interest
     if args['analysis_area_uri'] == 'West Coast of North America and Hawaii':
         analysis_area_path = args['wave_base_data_uri'] + os.sep + 'NAmerica_WestCoast_4m.shp'
         analysis_area_extract_path = args['wave_base_data_uri'] + os.sep + 'WCNA_extract.shp'
@@ -110,7 +115,7 @@ def execute(args):
     else:
         print 'Analysis Area ERROR'
     
-    #If the area of interest is present add it to the output arguments
+    #If the area of interest is present add it to the dictionary arguments
     AOI = None
     if 'AOI_uri' in args:
         try:
@@ -135,6 +140,15 @@ def execute(args):
     waveEnergy_core.biophysical(biophysicalargs)
 
 def extrapolateWaveData(waveFile):
+    """The extrapolateWaveData function converts WW3 text data into a dictionary who's
+    keys are the corresponding (I,J) values and whose value is a two-dimensional array
+    representing a matrix of the number of hours a seastate occurs over a 5 year period.
+    The row and column headers are extracted once and stored in the dictionary as well.
+    
+    waveFile - The path to a text document that holds the WW3 data.
+    
+    returns - A dictionary of matrices representing hours of specific seastates.  
+    """
     try:
         waveOpen = open(waveFile)
         waveDict = {}
