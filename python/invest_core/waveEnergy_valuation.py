0d2a1bb214252498f0c2bc6231d40ec5f414be79
dcdenu4 <devnull@localhost>
2011-12-12 11:29:53 -0800

    Added more comments for the skeleton of the module and what it needs to do.
    Former-commit-id: c41cadce24d35f72a5ca5a8b9317cd13589e366b

--- python/invest_core/waveEnergy_valuation.py
+++ python/invest_core/waveEnergy_valuation.py
@@ -12,17 +12,28 @@ from dbfpy import dbf
 import csv
 
 def execute(args):
-    """This function invokes the biophysical part of the wave energy model given URI inputs.
+    """This function invokes the valuation part of the wave energy model given URI inputs.
         It will do filehandling and open/create appropriate objects to 
-        pass to the core wave energy biophysical processing function.  It may write
+        pass to the core wave energy valuation processing function.  It may write
         log, warning, or error messages to stdout.
         
         args - a python dictionary with at least the following possible entries:
         args['workspace_dir'] - Where the intermediate and ouput folder/files will be saved.
-        args['landPowergridpts_uri'] - 
-        args['machine_econParams_uri'] - 
-        args['numberOfMachines'] - 
-        args['projection_uri'] - 
-        
+        args['landPowergridpts_uri'] - A CSV file path for the landing and grid point coordinates
+        args['machine_econParams_uri'] - A CSV file path for the machine economic parameters
+        args['numberOfMachines'] - An integer specifying the number of machines
+        args['projection_uri'] - A path for the projection to transform coordinates from decimal degrees to meters
+        args['captureWE'] - We need the captured wave energy output from biophysical run.
+        args['globa_dem'] - We need the depth of the locations for calculating costs
         """
+        #Open/create the output directory
         
+        #Read machine economic parameters into a dictionary
+        
+        #Read landing and power grid connection points into a dictionary
+        
+        #Open the output files for capturedWE from the biophysical run
+        
+        #Open the file that contains the depth values
+        
+        #Call the valuation core module with attached arguments to run the economic valuation
