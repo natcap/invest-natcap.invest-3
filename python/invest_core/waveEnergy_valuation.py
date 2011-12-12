acca4395e0c1cd7fafcc922b514953a7eb8aface
dcdenu4 <devnull@localhost>
2011-12-12 11:41:20 -0800

    Added functionality to the opening of CSV files for machine economic parameters and for the landing and power grid points.  Both are now saved in a dictionary for access.
    Former-commit-id: f0ca2a3b57576d9a7e523bb61f5c9439218e5be5

--- python/invest_core/waveEnergy_valuation.py
+++ python/invest_core/waveEnergy_valuation.py
@@ -19,21 +19,47 @@ def execute(args):
         
         args - a python dictionary with at least the following possible entries:
         args['workspace_dir'] - Where the intermediate and ouput folder/files will be saved.
-        args['landPowergridpts_uri'] - A CSV file path for the landing and grid point coordinates
-        args['machine_econParams_uri'] - A CSV file path for the machine economic parameters
+        args['land_gridPts_uri'] - A CSV file path for the landing and grid point coordinates
+        args['machine_econ_uri'] - A CSV file path for the machine economic parameters
         args['numberOfMachines'] - An integer specifying the number of machines
         args['projection_uri'] - A path for the projection to transform coordinates from decimal degrees to meters
         args['captureWE'] - We need the captured wave energy output from biophysical run.
         args['globa_dem'] - We need the depth of the locations for calculating costs
         """
-        #Open/create the output directory
         
-        #Read machine economic parameters into a dictionary
-        
-        #Read landing and power grid connection points into a dictionary
-        
-        #Open the output files for capturedWE from the biophysical run
-        
-        #Open the file that contains the depth values
-        
-        #Call the valuation core module with attached arguments to run the economic valuation
+    #This ensures we are not in Arc's python directory so that when
+    #we import gdal stuff we don't get the wrong GDAL version.
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    filesystemencoding = sys.getfilesystemencoding()
+    
+    valuationargs = []
+    
+    #Open/create the output directory
+    
+    #Read machine economic parameters into a dictionary
+    try:
+        machine_econ = {}
+        machineEconFile = open(args['machine_econ_uri'])
+        reader = csv.DictReader(machineEconFile)
+        for row in reader:
+            machine_econ[row['NAME'].strip()] = row
+        machineEconFile.close()
+        valuationargs['machine_econ'] = machine_econ
+    except IOError, e:
+        print 'File I/O error' + e
+    #Read landing and power grid connection points into a dictionary
+    try:
+        landGridPts = {}
+        landGridPtsFile = open(args['land_gridPts_uri'])
+        reader = csv.DictReader(landGridPtsFile)
+        for row in reader:
+            landGridPts[row['ID'].strip()] = row
+        landGridPtsFile.close()
+        valuationargs['machine_param'] = landGridPts
+    except IOError, e:
+        print 'File I/O error' + e
+    #Open the output files for capturedWE from the biophysical run
+    
+    #Open the file that contains the depth values
+    
+    #Call the valuation core module with attached arguments to run the economic valuation
