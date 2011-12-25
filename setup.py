fa81844213d45cbdaf88725ea648434ec13377f2
richpsharp <devnull@localhost>
2011-12-24 23:35:00 -0800

    Update issue 287
    Created a skeleton setup.py for distutils distribution.
    
    
    Former-commit-id: e37d99a9702a83e52bebae035ffc312a74f560ca

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='invest',
+      version='tip',
+      packages=['invest', 'invest.debpy', 'invest.invest_core', 'invest.iui',
+                  'invest.carbon',
+                  'invest.sediment', 'invest.timber', 'invest.validator_core',
+                  'invest.wave_energy'],
+      )
