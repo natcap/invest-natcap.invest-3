d311b340de88531dbc861175f99c3531526072d6
richpsharp <devnull@localhost>
2011-12-26 00:18:21 -0800

    Update issue 287
    Trying to build a sub-package.  Read about distuils buildilng subpackage extensions.
    
    
    Former-commit-id: cffe87e08c080fba4e7a948b2ee5fccf975e5760

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ from Cython.Distutils import build_ext
 cython_source_files = ['invest/invest_core/invest_cython_core.pyx',
                        'invest/invest_core/simplequeue.c']
 
+setup(name='invest.invest_core.invest_cython_core',
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[Extension("invest.invest_core.invest_cython_core",
+                             sources=cython_source_files)],
+      )
+
 setup(name='invest',
       version='tip',
-      cmdclass={'build_ext': build_ext},
-      packages=['invest', 'invest.dbfpy', 'invest.invest_core', 'invest.iui',
-                  'invest.carbon',
-                  'invest.sediment', 'invest.timber', 'invest.validator_core',
-                  'invest.wave_energy'],
-      ext_modules=[Extension("invest.invest_core.invest_cython_core",
-                             cython_source_files)],
+      packages=['invest', 'invest.dbfpy', 'invest.invest_core',
+                'invest.invest_core', 'invest.iui',
+                'invest.carbon',
+                'invest.sediment', 'invest.timber', 'invest.validator_core',
+                'invest.wave_energy'],
       )
