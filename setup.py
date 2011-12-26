d7406f0e06eda2b8707f7111a44ccc76c26aa5e3
richpsharp <devnull@localhost>
2011-12-25 23:04:19 -0800

    Update issue 287
    This seems to build the cython core on installation.
    
    
    Former-commit-id: 338e53f8843d97edeaab2c43c37c957dc6437e2e

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+cython_source_files = ['invest/invest_core/invest_cython_core.pyx']
+
+cqueue_source_files = ['invest/invest_core/simplequeue.c',
+                       'invest/invest_core/cqueue.pxd']
+
 setup(name='invest',
       version='tip',
-      packages=['invest', 'invest.debpy', 'invest.invest_core', 'invest.iui',
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[Extension('invest.invest_core.cqueue', cqueue_source_files),
+                   Extension("invest.invest_core.invest_cython_core", cython_source_files)],
+      packages=['invest', 'invest.dbfpy', 'invest.invest_core', 'invest.iui',
                   'invest.carbon',
                   'invest.sediment', 'invest.timber', 'invest.validator_core',
                   'invest.wave_energy'],
