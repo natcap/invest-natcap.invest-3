cc1d7540108b6ff3a9f8a1537cbc087a96edad69
richpsharp <devnull@localhost>
2011-12-26 14:03:08 -0800

    Reorganized for PEP 8 and fixed data directories to be local.
    Former-commit-id: da6a6a816e93dc1a5ebb07dff2176e9038876920

--- test/temporary_sediment_test.py
+++ test/temporary_sediment_test.py
@@ -1,18 +1,18 @@
 import os, sys
-cmd_folder = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, cmd_folder + '/../')
-os.chdir(cmd_folder)
-import invest_core
-import invest_cython_core
-import invest_test_core
-import unittest
-from osgeo import ogr, gdal, osr
-from osgeo.gdalconst import *
-from dbfpy import dbf
-import numpy as np
 import random
 import logging
 import math
+import unittest
+
+import numpy as np
+from osgeo import ogr, gdal, osr
+from osgeo.gdalconst import *
+
+from invest.invest_core import invest_core
+import invest_cython_core
+import invest_test_core
+from invest.dbfpy import dbf
+
 logger = logging.getLogger('invest_core_test')
 logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -20,23 +20,23 @@ logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
 class TestInvestCore(unittest.TestCase):
     def testflowDirection(self):
         """Regression test for flow direction on a DEM"""
-        dem = gdal.Open('../../../sediment_test_data/dem')
+        dem = gdal.Open('./data/sediment_test_data/dem')
         flow = invest_cython_core.newRasterFromBase(dem,
-            '../../../test_out/flow.tif', 'GTiff', 0, gdal.GDT_Float32)
+            './data/test_out/flow.tif', 'GTiff', 0, gdal.GDT_Float32)
         invest_cython_core.flowDirectionD8(dem, flow)
         regressionFlow = \
-            gdal.Open('../../../sediment_test_data/flowregression.tif')
+            gdal.Open('./data/sediment_test_data/flowregression.tif')
         invest_test_core.assertTwoDatasetsEqual(self, flow, regressionFlow)
 
     def testflowAccumulation(self):
         """Regression test for flowDirection accumulation on a DEM"""
-        dem = gdal.Open('../../../sediment_test_data/dem')
+        dem = gdal.Open('./data/sediment_test_data/dem')
         flowDirection = invest_cython_core.newRasterFromBase(dem,
-            '../../../test_out/flowDirection.tif', 'GTiff', 0, gdal.GDT_Byte)
+            './data/test_out/flowDirection.tif', 'GTiff', 0, gdal.GDT_Byte)
         invest_cython_core.flowDirectionD8(dem, flowDirection)
 
         accumulation = invest_cython_core.newRasterFromBase(dem,
-            '../../../test_out/accumulation.tif', 'GTiff', -1, gdal.GDT_Float32)
+            './data/test_out/accumulation.tif', 'GTiff', -1, gdal.GDT_Float32)
         invest_cython_core.flowAccumulationD8(flowDirection, accumulation)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestInvestCore)
