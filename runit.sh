a46c9c41b485501f8e43f603baeba3e413ae7753
richpsharp <devnull@localhost>
2011-12-26 12:13:33 -0800

    Update issue 287
    Apparently, cython can't be compiled as a subpackage.  Here's the link: http://wiki.cython.org/FAQ#HowtocompileCythonwithsubpackages
    
    So it looks like it's top level for cython_core.
    
    
    Former-commit-id: e1bf93c747026dbf4a970aea85bad78e9256d0cc

--- runit.sh
+++ runit.sh
@@ -5,5 +5,6 @@ python bootstrap_invest_environment.py > setup_environment.py
 python setup_environment.py --clear --system-site-packages $ENVDIR
 source $ENVDIR/bin/activate
 python setup.py install
-nosetests test/timber_core_test.py
-nosetests test/invest_core_test.py
+#nosetests test/timber_core_test.py
+#nosetests test/invest_core_test.py
+python -c "print 'import invest_cython_core'; import invest_cython_core"
--- setup.py
+++ setup.py
@@ -2,20 +2,27 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-cython_source_files = ['invest/invest_core/invest_cython_core.pyx',
-                       'invest/invest_core/simplequeue.c']
+cython_source_files = ['invest_cython_core/invest_cython_core.pyx',
+                       'invest_cython_core/simplequeue.c']
 
-setup(name='invest.invest_core.invest_cython_core',
-      cmdclass={'build_ext': build_ext},
-      ext_modules=[Extension("invest.invest_core.invest_cython_core",
-                             sources=cython_source_files)],
+setup(name='invest_cython_core',
+      version='tip',
+      cmdclass = {'build_ext': build_ext},
+      ext_package = 'invest_cython_core',
+      ext_modules = [Extension(name="invest_cython_core",
+                             sources=cython_source_files)]
       )
 
 setup(name='invest',
       version='tip',
-      packages=['invest', 'invest.dbfpy', 'invest.invest_core',
-                'invest.invest_core', 'invest.iui',
+      packages=['invest',
                 'invest.carbon',
-                'invest.sediment', 'invest.timber', 'invest.validator_core',
+                'invest.dbfpy',
+                'invest.invest_core',
+                'invest.iui',
+                'invest.sediment',
+                'invest.simplejson',
+                'invest.timber',
+                'invest.validator_core',
                 'invest.wave_energy'],
       )
