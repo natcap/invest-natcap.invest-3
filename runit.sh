1d1554dd00f9c4093640553344478791d645d3da
richpsharp <devnull@localhost>
2011-12-26 15:37:06 -0800

    Better formatted "runit.sh" that includes all the tests but will let you uncomment as necessary.
    Former-commit-id: 2a0c88cf307054c875d8e8d2b6f57b6572634c0a

--- runit.sh
+++ runit.sh
@@ -7,10 +7,16 @@ python setup_environment.py --clear --system-site-packages $ENVDIR
 source $ENVDIR/bin/activate
 python setup.py install
 pushd test
-nosetests timber_core_test.py
-nosetests timber_test.py
-nosetests carbon_core_test.py
-nosetests carbon_biophysical_test.py
-nosetests carbon_valuation_test.py
-nosetests sediment_biophysical_test.py
+
+#Uncomment any of these lines to do the tests
+
+#nosetests timber_core_test.py
+#nosetests timber_test.py
+#nosetests carbon_core_test.py
+#nosetests carbon_biophysical_test.py
+#nosetests carbon_valuation_test.py
+#nosetests sediment_biophysical_test.py
+#nosetests invest_core_test.py
+#nosetests waveEnergy_biophysical_test.py
+#nosetests waveEnergy_core_test.py
 popd
