3b396a5c3f05e7e9c204bce153ec171f65dc7211
richpsharp <devnull@localhost>
2011-12-26 14:18:06 -0800

    Update issue 287
    Changing paths to test data to local test data directory.
    
    
    Former-commit-id: 97e104aff5d373f05409be993cc3bbb1bac19c1f

--- runit.sh
+++ runit.sh
@@ -5,5 +5,9 @@ python bootstrap_invest_environment.py > setup_environment.py
 python setup_environment.py --clear --system-site-packages $ENVDIR
 source $ENVDIR/bin/activate
 python setup.py install
-#nosetests test/timber_core_test.py
-#nosetests test/invest_core_test.py
+pushd test
+nosetests timber_core_test.py
+nosetests timber_test.py
+nosetests carbon_core_test.py
+nosetests carbon_biophysical_test.py
+popd
--- test/carbon_biophysical_test.py
+++ test/carbon_biophysical_test.py
@@ -2,13 +2,9 @@
 
 import os, sys
 import unittest
+
+from invest.carbon import carbon_biophysical
 import invest_test_core
-
-#Add current directory and parent path for import tests
-cmd_folder = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, cmd_folder + '/../')
-
-import carbon_biophysical
 
 class TestCarbonBiophysical(unittest.TestCase):
     def test_carbon_biophysical_smoke(self):
@@ -16,9 +12,9 @@ class TestCarbonBiophysical(unittest.TestCase):
 #zero length inputs"""
 
         args = {}
-        args['workspace_dir'] = '../../carbon_output'
-        args['lulc_cur_uri'] = "../../base_data/terrestrial/lulc_samp_cur"
-        args['carbon_pools_uri'] = '../../carbon/input/carbon_pools_samp.dbf'
+        args['workspace_dir'] = './data/carbon_output'
+        args['lulc_cur_uri'] = "./data/base_data/terrestrial/lulc_samp_cur"
+        args['carbon_pools_uri'] = './data/carbon/input/carbon_pools_samp.dbf'
 
         carbon_biophysical.execute(args)
 
@@ -27,10 +23,10 @@ class TestCarbonBiophysical(unittest.TestCase):
 do sequestration on lulc maps."""
 
         args = {}
-        args['workspace_dir'] = '../../carbon_output'
-        args['lulc_cur_uri'] = "../../base_data/terrestrial/lulc_samp_cur"
-        args['lulc_fut_uri'] = "../../base_data/terrestrial/lulc_samp_fut"
-        args['carbon_pools_uri'] = '../../carbon/input/carbon_pools_samp.dbf'
+        args['workspace_dir'] = './data/carbon_output'
+        args['lulc_cur_uri'] = "./data/base_data/terrestrial/lulc_samp_cur"
+        args['lulc_fut_uri'] = "./data/base_data/terrestrial/lulc_samp_fut"
+        args['carbon_pools_uri'] = './data/carbon/input/carbon_pools_samp.dbf'
 
         carbon_biophysical.execute(args)
 
@@ -39,54 +35,54 @@ do sequestration on lulc maps."""
 do sequestration and harvested wood products on lulc maps."""
 
         args = {}
-        args['workspace_dir'] = '../../carbon_output'
-        args['lulc_cur_uri'] = "../../base_data/terrestrial/lulc_samp_cur"
-        args['lulc_fut_uri'] = "../../base_data/terrestrial/lulc_samp_fut"
-        args['carbon_pools_uri'] = '../../carbon/input/carbon_pools_samp.dbf'
+        args['workspace_dir'] = './data/carbon_output'
+        args['lulc_cur_uri'] = "./data/base_data/terrestrial/lulc_samp_cur"
+        args['lulc_fut_uri'] = "./data/base_data/terrestrial/lulc_samp_fut"
+        args['carbon_pools_uri'] = './data/carbon/input/carbon_pools_samp.dbf'
         args['lulc_cur_year'] = 2000
         args['lulc_fut_year'] = 2030
-        args['hwp_cur_shape_uri'] = "../../carbon/input/harv_samp_cur.shp"
-        args['hwp_fut_shape_uri'] = "../../carbon/input/harv_samp_fut.shp"
+        args['hwp_cur_shape_uri'] = "./data/carbon/input/harv_samp_cur.shp"
+        args['hwp_fut_shape_uri'] = "./data/carbon/input/harv_samp_fut.shp"
 
         carbon_biophysical.execute(args)
 
-        #assert that '../../test_data/tot_C_cur.tif' equals
-        #../../carbon_output/Output/tot_C_cur.tif
+        #assert that './data/test_data/tot_C_cur.tif' equals
+        #./data/carbon_output/Output/tot_C_cur.tif
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Output/tot_C_cur.tif",
-            '../../test_data/tot_C_cur_regression.tif')
+            './data/test_data/tot_C_cur_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Output/tot_C_fut.tif",
-            '../../test_data/tot_C_fut_regression.tif')
+            './data/test_data/tot_C_fut_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Output/sequest.tif",
-            '../../test_data/sequest_regression.tif')
+            './data/test_data/sequest_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Intermediate/bio_hwp_cur.tif",
-            '../../test_data/bio_hwp_cur_regression.tif')
+            './data/test_data/bio_hwp_cur_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Intermediate/bio_hwp_fut.tif",
-            '../../test_data/bio_hwp_fut_regression.tif')
+            './data/test_data/bio_hwp_fut_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Intermediate/c_hwp_cur.tif",
-            '../../test_data/c_hwp_cur_regression.tif')
+            './data/test_data/c_hwp_cur_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Intermediate/c_hwp_fut.tif",
-            '../../test_data/c_hwp_fut_regression.tif')
+            './data/test_data/c_hwp_fut_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Intermediate/vol_hwp_cur.tif",
-            '../../test_data/vol_hwp_cur_regression.tif')
+            './data/test_data/vol_hwp_cur_regression.tif')
 
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + "/Intermediate/vol_hwp_fut.tif",
-            '../../test_data/vol_hwp_fut_regression.tif')
+            './data/test_data/vol_hwp_fut_regression.tif')
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestCarbonBiophysical)
 unittest.TextTestRunner(verbosity=2).run(suite)
