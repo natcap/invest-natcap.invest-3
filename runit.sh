955a69a06ce47186a10e72aa7127d33e0802789f
richpsharp <devnull@localhost>
2011-12-26 00:02:54 -0800

    Forgot to actually install the invest package.
    Former-commit-id: def23406d4a97f54d76642732f216a0da92f8fe0

--- runit.sh
+++ runit.sh
@@ -4,5 +4,6 @@ deactivate
 python bootstrap_invest_environment.py > setup_environment.py
 python setup_environment.py --clear --system-site-packages $ENVDIR
 source $ENVDIR/bin/activate
+python setup.py install
 nosetests test/timber_core_test.py
 nosetests test/invest_core_test.py
