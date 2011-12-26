295c5791e8e06e2c74431fade531f43dc7729b25
richpsharp <devnull@localhost>
2011-12-26 00:03:24 -0800

    Simplified setup.py since it doens't need cqueue anymore.
    Former-commit-id: 9e82f5e0868b928160214a7cde3eae05c5c08b1a

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-cython_source_files = ['invest/invest_core/invest_cython_core.pyx']
-
-cqueue_source_files = ['invest/invest_core/simplequeue.c',
-                       'invest/invest_core/cqueue.pxd']
+cython_source_files = ['invest/invest_core/invest_cython_core.pyx',
+                       'invest/invest_core/simplequeue.c']
 
 setup(name='invest',
       version='tip',
       cmdclass={'build_ext': build_ext},
-      ext_modules=[Extension('invest.invest_core.cqueue', cqueue_source_files),
-                   Extension("invest.invest_core.invest_cython_core", cython_source_files)],
       packages=['invest', 'invest.dbfpy', 'invest.invest_core', 'invest.iui',
                   'invest.carbon',
                   'invest.sediment', 'invest.timber', 'invest.validator_core',
                   'invest.wave_energy'],
+      ext_modules=[Extension("invest.invest_core.invest_cython_core",
+                             cython_source_files)],
       )
