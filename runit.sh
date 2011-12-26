2c7acc34e15d2f5c1894402f75dfb060112ac8e5
richpsharp <devnull@localhost>
2011-12-25 23:34:39 -0800

    Update issue 287
    Setting up runit.sh to build the virtual environment and then automatically run tests.
    
    
    Former-commit-id: e4e7187791d4e3f28e35db9c5045908a01561b51

--- runit.sh
+++ runit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ENVDIR=invest_python_environment
 deactivate
 python bootstrap_invest_environment.py > setup_environment.py
-python setup_environment.py --system-site-packages invest_python_environment
-
-#Put a trigger to run unit tests here, or start front end, or whatever
+python setup_environment.py --clear --system-site-packages $ENVDIR
+source $ENVDIR/bin/activate
+nosetests test/timber_core_test.py
+nosetests test/invest_core_test.py
