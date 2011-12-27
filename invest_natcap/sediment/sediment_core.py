ce3830424360964a32996ce1b57d8ab161452e08
richpsharp <devnull@localhost>
2011-12-26 17:40:55 -0800

    Reverting sediment_core, since that's what will hold the core functionality of the model.
    Former-commit-id: 103f37c65e7fd74e484b753c7bc7b78108a75dbd

---
+++ invest_natcap/sediment/sediment_core.py
@@ -0,0 +1,62 @@
+"""Module that contains the core computational components for the carbon model
+    including the biophysical and valuation functions"""
+
+import math
+import logging
+
+import numpy as np
+from osgeo import gdal
+from osgeo import ogr
+
+from invest_natcap.dbfpy import dbf
+from invest_natcap.invest_core import invest_core
+
+logger = logging.getLogger('carbon_core')
+
+def biophysical(args):
+    """Executes the basic sediment model
+
+        args - is a dictionary with at least the following entries:
+        args['dem'] - a digital elevation raster file (required)
+        args['erosivity'] - an input raster describing the 
+            rainfall eroisivity index (required)
+        args['erodibility'] - an input raster describing soil 
+            erodibility (required)
+        args['landuse'] - a land use/land cover raster whose
+            LULC indexes correspond to indexs in the biophysical table input.
+            Used for determining soil retention and other biophysical 
+            properties of the landscape.  (required)
+        args['watersheds'] - an input shapefile of the watersheds
+            of interest as polygons. (required)
+        args['subwatersheds'] - an input shapefile of the 
+            subwatersheds of interest that are contained in the
+            'watersheds' shape provided as input. (required)
+        args['reservoir_locations'] - an input shape file with 
+            points indicating reservoir locations with IDs. (optional)
+        args['reservoir_properties'] - an input CSV table 
+            describing properties of input reservoirs provided in the 
+            reservoirs shapefile (optional)
+        args['biophysical_table'] - an input CSV file with 
+            biophysical information about each of the land use classes.
+        args['threshold_flow_accumulation'] - an integer describing the number
+            of upstream cells that must flow int a cell before it's considered
+            part of a stream.  required if 'v_stream' is not provided.
+        args['slope_threshold'] - A percentage slope threshold as described in
+            the user's guide.
+        args['v_stream_out'] - An output raster file that classifies the
+            watersheds into stream and non-stream regions based on the
+            value of 'threshold_flow_accumulation'
+            
+        returns nothing"""
+
+    logger.info("do it up")
+
+def valuation(args):
+    """Executes the basic carbon model that maps a carbon pool dataset to a
+        LULC raster.
+    
+        args - is a dictionary with the following entries:
+        
+        returns nothing"""
+
+    logger.info('do it up')
