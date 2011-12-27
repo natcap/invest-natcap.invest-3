99e457516aa049cacc06bb5ce833cbd2b9f3ae84
richpsharp <devnull@localhost>
2011-12-26 17:37:41 -0800

    PEP8'd sediment_biophysical
    Former-commit-id: 7a436a6048bc1c25a1be051a61dba71b03dcfb7a

--- invest_natcap/sediment/sediment_biophysical.py
+++ invest_natcap/sediment/sediment_biophysical.py
@@ -14,7 +14,7 @@ from invest_natcap.sediment import sediment_core
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
 
-logger = logging.getLogger('sediment_biophysical')
+LOGGER = logging.getLogger('sediment_biophysical')
 
 def execute(args):
     """This function invokes the biophysical part of the sediment model given
@@ -56,72 +56,72 @@ def execute(args):
         returns nothing."""
 
     #Sets up the intermediate and output directory structure for the workspace
-    outputDirectoryPrefix = args['workspace_dir'] + os.sep + 'Output' + os.sep
-    intermediateDirectoryPrefix = args['workspace_dir'] + os.sep + \
-        'Intermediate' + os.sep
-    for d in [outputDirectoryPrefix, intermediateDirectoryPrefix]:
-        if not os.path.exists(d):
-            logger.debug('creating directory %s', d)
-            os.makedirs(d)
+    for directory in [args['workspace_dir'] + os.sep + 'Output' + os.sep,
+              args['workspace_dir'] + os.sep + 'Intermediate' + os.sep]:
+        if not os.path.exists(directory):
+            LOGGER.debug('creating directory %s', directory)
+            os.makedirs(directory)
 
-    logger.info('Loading data sources')
+    LOGGER.info('Loading data sources')
 
     #Load and copy relevant inputs from args into a dictionary that
     #can be passed to the biophysical core model
-    biophysicalArgs = {}
+    biophysical_args = {}
 
     #load rasters
-    for rasterName in ['dem', 'erosivity', 'erodibility', 'landuse']:
-        biophysicalArgs[rasterName] = gdal.Open(args[rasterName + '_uri'],
+    for raster_name in ['dem', 'erosivity', 'erodibility', 'landuse']:
+        biophysical_args[raster_name] = gdal.Open(args[raster_name + '_uri'],
                                                 gdal.GA_ReadOnly)
-        logger.debug('load %s as: %s' % (args[rasterName + '_uri'],
-                                         biophysicalArgs[rasterName]))
+        LOGGER.debug('load %s as: %s' % (args[raster_name + '_uri'],
+                                         biophysical_args[raster_name]))
     #check that they have the same projection
-    for rasterName in ['dem', 'erosivity', 'erodibility', 'landuse']:
-        logger.debug(biophysicalArgs[rasterName].GetProjection())
+    for raster_name in ['dem', 'erosivity', 'erodibility', 'landuse']:
+        LOGGER.debug(biophysical_args[raster_name].GetProjection())
 
     #load shapefiles
-    for shapeFileName in ['watersheds', 'subwatersheds', 'reservoir_locations']:
+    for shapefile_name in ['watersheds',
+                           'subwatersheds',
+                           'reservoir_locations']:
         fsencoding = sys.getfilesystemencoding()
-        uriName = shapeFileName + '_uri'
-        if uriName in args:
-            biophysicalArgs[shapeFileName] = \
-                ogr.Open(args[uriName].encode(fsencoding))
-            logger.debug('load %s as: %s' % (args[uriName],
-                                         biophysicalArgs[shapeFileName]))
+        uri_name = shapefile_name + '_uri'
+        if uri_name in args:
+            biophysical_args[shapefile_name] = \
+                ogr.Open(args[uri_name].encode(fsencoding))
+            LOGGER.debug('load %s as: %s' % (args[uri_name],
+                                         biophysical_args[shapefile_name]))
 
     #table
-    for x, idColumnName in [('reservoir_properties', 'id'),
+    for value, column_name in [('reservoir_properties', 'id'),
                             ('biophysical_table', 'lucode')]:
         try:
-            uriName = x + '_uri'
-            logger.debug('load %s' % args[uriName])
-            file = open(args[uriName])
-            csvDictReader = csv.DictReader(open(args[x + '_uri']))
-            idTable = {}
-            for row in csvDictReader:
-                idTable[row[idColumnName]] = row
-            biophysicalArgs[x] = idTable
-        except IOError, e:
-            #Get here if file x is not found
-            logger.warning(e)
+            uri_name = value + '_uri'
+            LOGGER.debug('load %s' % args[uri_name])
+            local_file = open(args[uri_name])
+            csv_dict_reader = csv.DictReader(open(args[value + '_uri']))
+            id_table = {}
+            for row in csv_dict_reader:
+                id_table[row[column_name]] = row
+            biophysical_args[value] = id_table
+        except IOError, error:
+            #Get here if file value is not found
+            LOGGER.warning(error)
 
     #primatives
-    for x in ['threshold_flow_accumulation', 'slope_threshold']:
-        biophysicalArgs[x] = args[x]
-        logger.debug('%s=%s' % (x, biophysicalArgs[x]))
+    for value in ['threshold_flow_accumulation', 'slope_threshold']:
+        biophysical_args[value] = args[value]
+        LOGGER.debug('%s=%s' % (value, biophysical_args[value]))
 
     #build output rasters
     #first determine the minimum resolution of each of the input rasters
 
 
-    logger.info('starting biophysical model')
-    sediment_core.biophysical(biophysicalArgs)
-    logger.info('finished biophysical model')
+    LOGGER.info('starting biophysical model')
+    sediment_core.biophysical(biophysical_args)
+    LOGGER.info('finished biophysical model')
 
 #This part is for command line invocation and allows json objects to be passed
 #as the argument dictionary
 if __name__ == '__main__':
-    modulename, json_args = sys.argv
-    args = json.loads(json_args)
-    execute(args)
+    MODULE_NAME, JSON_ARGS = sys.argv
+    MAIN_ARGS = json.loads(JSON_ARGS)
+    execute(MAIN_ARGS)
