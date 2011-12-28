0b21bbd472093f0a0d26c27c7ab253c4a72caa65
richpsharp <devnull@localhost>
2011-12-28 14:09:27 -0800

    Making a square output graph.
    Former-commit-id: f3e490dd4aa090e38de189bbc4ea4a2b210a545b

--- invest_natcap/postprocessing.py
+++ invest_natcap/postprocessing.py
@@ -5,22 +5,35 @@ import logging
 
 import numpy as np
 import pylab
+from osgeo import gdal
 
 LOGGER = logging.getLogger('postprocessing')
 
-def plot_flow_direction(flow_matrix):
+def plot_flow_direction(flow_dataset_uri, output_uri):
     """Generates a quiver plot (arrows on a grid) of a flow matrix
     
-    flow_matrix - a raster of floats indicating the direciton of flow out of
-        each pixel.  Values are given in radians
+    flow_dataset_uri - a uri to a GDAL compatable raster whose values are
+        radians indicating the direction of outward flow.
+    output_uri - the location to disk to save the resulting plot png file
         
     returns nothing"""
 
+    LOGGER.info('Loading %s' % flow_dataset_uri)
+    flow_dataset = gdal.Open(flow_dataset_uri, gdal.GA_ReadOnly)
+    flow_matrix = (flow_dataset.GetRasterBand(1).ReadAsArray(0, 0,
+        flow_dataset.RasterXSize, flow_dataset.RasterYSize))
+    LOGGER.info('Done loading %s' % flow_dataset_uri)
+
     LOGGER.info('Starting plot of flow direction')
     pylab.figure()
-    pylab.quiver(np.cos(flow_matrix), np.sin(flow_matrix),
-                 units='xy',
-                 pivot='middle',
-                 scale=1)
-    pylab.savefig('flow.png', dpi=3200)
+    steps = flow_matrix.size
+    xmax = flow_matrix.shape[0]
+    ymax = flow_matrix.shape[1]
+    steps = 10
+    pylab.quiver(np.sin(flow_matrix[0:xmax:steps, 0:ymax:steps]),
+                 np.cos(flow_matrix[0:xmax:steps, 0:ymax:steps]))
+    pylab.axes().set_aspect('equal')
+    LOGGER.info('Saving plot as %s' % output_uri)
+    pylab.savefig(output_uri, dpi=1600,
+                  units='x')
     LOGGER.info('Done with plot of flow direction')
