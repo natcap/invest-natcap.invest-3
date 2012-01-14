39414a3685ddb6e6efb139b9668381b7f8661c50
richpsharp <devnull@localhost>
2012-01-13 16:31:53 -0800

    Added a guard to the percentage printer that made sure we wouldn't % by zero.
    Former-commit-id: de501c17b44718f9bf6a95ceb7fbe45485bd1437

--- invest_cython_core/invest_cython_core.pyx
+++ invest_cython_core/invest_cython_core.pyx
@@ -686,7 +686,7 @@ def flow_accumulation_dinf(flow_direction, flow_accumulation, dem):
     
     q = CQueue()
     for i in range(valid_pixel_count):
-            if i % (valid_pixel_count/100) == 0:
+            if valid_pixel_count/100 != 0 and i % (valid_pixel_count/100) == 0:
                 LOGGER.debug('percent complete %2.2f %%' % 
                              (100*(i+1.0)/valid_pixel_count))
             q.append(dem_pixel_pairs[i].i)
