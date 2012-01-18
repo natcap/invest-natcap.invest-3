a6ee572a2898e1165beb6826b1787595a5a8e998
dcdenu4 <devnull@localhost>
2012-01-18 11:40:05 -0800

    I did a total makeover to this file, copying over what carbon_biophysical_arc lookis like and changing the specified lines.
    Former-commit-id: c403ee0a3a0eb46eede132dc162194e8865e3a03

--- invest_natcap/arcgis_support/waveEnergy_biophysical_arc.py
+++ invest_natcap/arcgis_support/waveEnergy_biophysical_arc.py
@@ -1,38 +1,17 @@
-#waveEnergy_arc.py
-import os, sys, subprocess
-import getpass
-try:
-    import json
-except ImportError:
-    import invest_core.simplejson as json
-    
+import os, subprocess
 import arcgisscripting
 gp = arcgisscripting.create()
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)) + "\\..\\")
-gp.AddMessage(os.getcwd())
 
-#build up the JSON dictionary for saving previously used parameters to disk.
-arguments = {'workspace_dir': gp.GetParameterAsText(0),
-             'wave_base_data_uri': gp.GetParameterAsText(1),
-             'analysis_area_uri': gp.GetParameterAsText(2),
-             'machine_perf_uri': gp.GetParameterAsText(3),
-             'machine_param_uri': gp.GetParameterAsText(4),
-             'dem_uri': gp.GetParameterAsText(5),
-             'aoi_uri': gp.GetParameterAsText(6)
-             }
-
-args_file = open('C:\Users\\' + getpass.getuser() + '\My Documents\ArcGIS\\waveEnergy_biophysical_args.json', 'w')
-args_file.writelines(json.dumps(arguments))
-args_file.close()
-
-gp.AddMessage('Starting wave energy model')
+gp.AddMessage('Starting wave energy biophysical model')
 
 process = subprocess.Popen(['OSGeo4W\\gdal_python_exec.bat',
-                            'python\\invest_core\\waveEnergy_biophysical.py',
-                            json.dumps(arguments)],
+                            'python\\iui\\iui_main.py',
+                            'python\\iui\wave_energy_biophysical.json'],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT).communicate()[0]
 
 gp.AddMessage(process)
 gp.AddMessage('Done')
+
