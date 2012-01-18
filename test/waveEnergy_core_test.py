1f3b4d163fb0054109548228dd01647a45f74467
dcdenu4 <devnull@localhost>
2012-01-18 15:09:08 -0800

    Removed docstring in wave energy core test that I was using to reference what I needed to build up the arguments with
    Former-commit-id: 077967d7dd5f9a0b7a1695b5439285393d7da2c2

--- test/waveEnergy_core_test.py
+++ test/waveEnergy_core_test.py
@@ -18,19 +18,9 @@ import invest_test_core
 class TestWaveEnergy(unittest.TestCase):
 
     def test_wave_energy_biophysical_regression(self):
-        """Runs the biophysical part of the Wave Energy Model (WEM).
-    
-        args['wave_base_data'] - a dictionary of seastate bin data.
-        args['analysis_area'] - a point geometry shapefile representing the relevant WW3 points
-        args['analysis_area_extract'] - a polygon geometry shapefile encompassing the broader range
-                                        of interest.
-        args['AOI'] - a polygon geometry shapefile outlining a more specific area of interest.
-        args['machine_perf'] - a 2D array representing the machine performance table.
-        args['machine_param'] - a dictionary which holds the machine parameter values.
-        args['dem'] - a GIS raster file of the global elevation model
-        args['workspace_dir'] - the workspace path
-        args['wave_data_dir'] - the wave data path, used for retreiving other relevant files.
-            
+        """Runs the biophysical part of the Wave Energy Model (WEM),
+        and does regression tests against the raster outputs and shapefile
+        output.
         """
         test_dir = './data/test_data/wave_Energy'
         analysis_path = test_dir + os.sep + 'samp_input/WaveData/NAmerica_WestCoast_4m.shp'
