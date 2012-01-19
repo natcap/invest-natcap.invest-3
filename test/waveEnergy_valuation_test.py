799191d27c93cd398a611e04bf9dda82b8ea1b95
dcdenu4 <devnull@localhost>
2012-01-19 11:50:00 -0800

    Update issue 314
    Added regression check for npv_usd raster, landpt shapefile, and gridpt shapefile.
    
    
    Former-commit-id: f96dcb758083fed7c90e1dc86b46b8c2bf9f28fb

--- test/waveEnergy_valuation_test.py
+++ test/waveEnergy_valuation_test.py
@@ -7,7 +7,7 @@ from invest_natcap.wave_energy import waveEnergy_valuation
 import invest_test_core
 
 class TestWaveEnergyValuation(unittest.TestCase):
-    def test_waveEnergy_smoke(self):
+    def test_wave_energy_valuation_regression(self):
         """This function invokes the valuation part of the wave energy model given URI inputs.
         It will do filehandling and open/create appropriate objects to 
         pass to the core wave energy valuation processing function.  It may write
@@ -35,11 +35,74 @@ class TestWaveEnergyValuation(unittest.TestCase):
         
         waveEnergy_valuation.execute(args)
 
-#        #assert that the output raster is equivalent to the regression
-#        #test
-#        invest_test_core.assertTwoDatasetEqualURI(self,
-#            args['workspace_dir'] + '/Output/npv_usd.tif',
-#            args['workspace_dir'] + '/regression_tests/npv_usd_regression.tif')
+        #assert that the output raster is equivalent to the regression
+        #test
+        invest_test_core.assertTwoDatasetEqualURI(self,
+            args['workspace_dir'] + '/Output/npv_usd.tif',
+            args['workspace_dir'] + '/regression_tests/npv_usd_regression.tif')
 
         #Need to check the shapefiles landingpoints and gridpoint to make sure
         #those are both correct
+        #Check that output/intermediate files have been made
+        regression_landing_shape = ogr.Open(args['workspace_dir'] + '/regression_tests/LandPts_prj_regression.shp')
+        landing_shape = ogr.Open(args['workspace_dir'] + '/Output/LandPts_prj.shp')
+        
+        regression_layer = regression_landing_shape.GetLayer(0)
+        layer = landing_shape.GetLayer(0)
+        
+        regression_feat_count = regression_layer.GetFeatureCount()
+        feat_count = layer.GetFeatureCount()
+        self.assertEqual(regression_feat_count, feat_count)
+        
+        layer_def = layer.GetLayerDefn()
+        reg_layer_def = regression_layer.GetLayerDefn()
+        field_count = layer_def.GetFieldCount()
+        reg_field_count = reg_layer_def.GetFieldCount()
+        self.assertEqual(field_count, reg_field_count, 'The shapes DO NOT have the same number of fields')
+        
+        reg_feat = regression_layer.GetNextFeature()
+        feat = layer.GetNextFeature()
+        while reg_feat is not None:            
+            for fld_index in range(field_count):
+                field = feat.GetField(fld_index)
+                reg_field = reg_feat.GetField(fld_index)
+                self.assertEqual(field, reg_field, 'The field values DO NOT match')
+            feat.Destroy()
+            reg_feat.Destroy()
+            feat = layer.GetNextFeature()
+            reg_feat = regression_layer.GetNextFeature()
+            
+        regression_landing_shape.Destroy()
+        landing_shape.Destroy()
+        
+        
+        regression_grid_shape = ogr.Open(args['workspace_dir'] + '/regression_tests/GridPt_prj_regression.shp')
+        grid_shape = ogr.Open(args['workspace_dir'] + '/Output/GridPt_prj.shp')
+        
+        regression_layer = regression_grid_shape.GetLayer(0)
+        layer = grid_shape.GetLayer(0)
+        
+        regression_feat_count = regression_layer.GetFeatureCount()
+        feat_count = layer.GetFeatureCount()
+        self.assertEqual(regression_feat_count, feat_count)
+        
+        layer_def = layer.GetLayerDefn()
+        reg_layer_def = regression_layer.GetLayerDefn()
+        field_count = layer_def.GetFieldCount()
+        reg_field_count = reg_layer_def.GetFieldCount()
+        self.assertEqual(field_count, reg_field_count, 'The shapes DO NOT have the same number of fields')
+        
+        reg_feat = regression_layer.GetNextFeature()
+        feat = layer.GetNextFeature()
+        while reg_feat is not None:            
+            for fld_index in range(field_count):
+                field = feat.GetField(fld_index)
+                reg_field = reg_feat.GetField(fld_index)
+                self.assertEqual(field, reg_field, 'The field values DO NOT match')
+            feat.Destroy()
+            reg_feat.Destroy()
+            feat = layer.GetNextFeature()
+            reg_feat = regression_layer.GetNextFeature()
+            
+        regression_grid_shape.Destroy()
+        grid_shape.Destroy()
