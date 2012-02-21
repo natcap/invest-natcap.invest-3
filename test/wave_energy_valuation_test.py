27e8fd2246cdd14a8febf0af033883a8dddb4982
dcdenu4 <devnull@localhost>
2012-02-21 15:30:56 -0800

    Update issue 314
    Adding regression check for percentile rasters and dbf files.
    
    
    Former-commit-id: e0aea514d51afee253c1376a2d0d85cb8c482256

--- test/wave_energy_valuation_test.py
+++ test/wave_energy_valuation_test.py
@@ -7,6 +7,7 @@ import numpy as np
 from osgeo import ogr
 from invest_natcap.wave_energy import wave_energy_valuation
 import invest_test_core
+from invest_natcap.dbfpy import dbf
 
 LOGGER = logging.getLogger('wave_energy_valuation_test')
 logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
@@ -28,12 +29,14 @@ class TestWaveEnergyValuation(unittest.TestCase):
         
         wave_energy_valuation.execute(args)
         regression_dir = './data/wave_energy_regression_data'
-        #assert that the output raster is equivalent to the regression
-        #test
+        #Regression Check for NPV raster
         invest_test_core.assertTwoDatasetEqualURI(self,
             args['workspace_dir'] + '/Output/npv_usd.tif',
             regression_dir + '/npv_usd_regression.tif')
-
+        #Regression Check for NPV percentile raster
+        invest_test_core.assertTwoDatasetEqualURI(self,
+            args['workspace_dir'] + '/Output/npv_rc.tif',
+            regression_dir + '/npv_rc_regression.tif')
         #Regression Check for LandPts_prj shapefile
         landing_shape_path = args['workspace_dir'] + '/Output/LandPts_prj.shp'
         regression_landing_shape_path = regression_dir + '/LandPts_prj_regression.shp'
@@ -46,3 +49,18 @@ class TestWaveEnergyValuation(unittest.TestCase):
         wave_data_shape_path = args['workspace_dir'] + '/Intermediate/WEM_InputOutput_Pts.shp'
         regression_wave_data_shape_path = regression_dir + '/WEM_InputOutput_Pts_val_regression.shp'
         invest_test_core.assertTwoShapesEqualURI(self, wave_data_shape_path, regression_wave_data_shape_path)
+
+        try:
+            regression_table = dbf.Dbf(regression_dir + os.sep + 'npv_rc_regression.tif.vat.dbf')
+            db_file = dbf.Dbf(args['workspace_dir'] + os.sep + 'Output/npv_rc.tif.vat.dbf')
+            value_array = []
+            count_array = []
+            val_range_array = []
+            for rec, reg_rec in zip(db_file, regression_table):
+                self.assertEqual(rec['VALUE'], reg_rec['VALUE'])
+                self.assertEqual(rec['COUNT'], reg_rec['COUNT'])
+                self.assertEqual(rec['VAL_RANGE'], reg_rec['VAL_RANGE'])
+            db_file.close()
+            regression_table.close()
+        except IOError, error:
+            self.assertTrue(False, 'The dbf file could not be opened')
