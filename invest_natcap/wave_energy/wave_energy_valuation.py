71e5c3ce8a8f4b48a38c736933fa8e0a0a9fd3ae
dcdenu4 <devnull@localhost>
2012-02-23 15:06:01 -0800

    Update issue 551
    Removed try/except cases in w_e_valuation and added some nice spacing
    
    
    Former-commit-id: 7506cd394ada98bd66349057064b0afbb23654ed

--- invest_natcap/wave_energy/wave_energy_valuation.py
+++ invest_natcap/wave_energy/wave_energy_valuation.py
@@ -38,48 +38,46 @@ def execute(args):
     valuation_args['global_dem'] = gdal.Open(args['global_dem'])
     valuation_args['wave_data_shape'] = ogr.Open(args['wave_data_shape_path'], 1)
     valuation_args['number_machines'] = int(args['number_of_machines'])
+    
     #Open/create the output directory
     output_dir = args['workspace_dir'] + os.sep + 'Output' + os.sep
     intermediate_dir = args['workspace_dir'] + os.sep + 'Intermediate' + os.sep
     for directory in [output_dir, intermediate_dir]:
         if not os.path.exists(directory):
             os.makedirs(directory)
+            
     #Read machine economic parameters into a dictionary
-    try:
-        machine_econ = {}
-        machine_econ_file = open(args['machine_econ_uri'])
-        reader = csv.DictReader(machine_econ_file)
-        LOGGER.debug('reader fieldnames : %s ', reader.fieldnames)
-        #Read in the field names from the column headers
-        name_key = reader.fieldnames[0]
-        value_key = reader.fieldnames[1]
-        for row in reader:
-            #Convert name to lowercase
-            name = row[name_key].strip().lower()
-            LOGGER.debug('Name : %s and Value : % s', name, row[value_key])
-            machine_econ[name] = row[value_key]
-        machine_econ_file.close()
-        valuation_args['machine_econ'] = machine_econ
-    except IOError, error:
-        print 'File I/O error' + error
+    machine_econ = {}
+    machine_econ_file = open(args['machine_econ_uri'])
+    reader = csv.DictReader(machine_econ_file)
+    LOGGER.debug('reader fieldnames : %s ', reader.fieldnames)
+    #Read in the field names from the column headers
+    name_key = reader.fieldnames[0]
+    value_key = reader.fieldnames[1]
+    for row in reader:
+        #Convert name to lowercase
+        name = row[name_key].strip().lower()
+        LOGGER.debug('Name : %s and Value : % s', name, row[value_key])
+        machine_econ[name] = row[value_key]
+    machine_econ_file.close()
+    valuation_args['machine_econ'] = machine_econ
+    
     #Read landing and power grid connection points into a dictionary
-    try:
-        land_grid_pts = {}
-        land_grid_pts_file = open(args['land_gridPts_uri'])
-        reader = csv.DictReader(land_grid_pts_file)
-        for row in reader:
-            LOGGER.debug('Land Grid Row: %s', row)
-            if row['ID'] in land_grid_pts:
-                land_grid_pts[row['ID'].strip()][row['TYPE']] = [row['LAT'],
-                                                                 row['LONG']]
-            else:
-                land_grid_pts[row['ID'].strip()] = {row['TYPE']:[row['LAT'],
-                                                                 row['LONG']]}
-        LOGGER.debug('New Land_Grid Dict : %s', land_grid_pts)
-        land_grid_pts_file.close()
-        valuation_args['land_gridPts'] = land_grid_pts
-    except IOError, error:
-        print 'File I/O error' + error
+    land_grid_pts = {}
+    land_grid_pts_file = open(args['land_gridPts_uri'])
+    reader = csv.DictReader(land_grid_pts_file)
+    for row in reader:
+        LOGGER.debug('Land Grid Row: %s', row)
+        if row['ID'] in land_grid_pts:
+            land_grid_pts[row['ID'].strip()][row['TYPE']] = [row['LAT'],
+                                                             row['LONG']]
+        else:
+            land_grid_pts[row['ID'].strip()] = {row['TYPE']:[row['LAT'],
+                                                             row['LONG']]}
+    LOGGER.debug('New Land_Grid Dict : %s', land_grid_pts)
+    land_grid_pts_file.close()
+    valuation_args['land_gridPts'] = land_grid_pts
+    
     #Call the valuation core module with attached arguments to run the economic valuation
     LOGGER.info('Beginning Wave Energy Valuation.')
     wave_energy_core.valuation(valuation_args)
