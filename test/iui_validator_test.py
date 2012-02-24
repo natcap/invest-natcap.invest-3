01d598d806a937efd7dc34ff886d1c03005fb3fa
James Douglass <jdouglass@stanford.edu>
2012-02-24 13:17:15 -0800

    Update issue 549
    Adding a FolderCheckerTester class.  Also, abstracting certain check-function
    specific logic regarding assertions away into a new CheckerTester class.
    
    
    Former-commit-id: 8c8ae7aed3038081da6e1eab6f681b4b78378817

--- test/iui_validator_test.py
+++ test/iui_validator_test.py
@@ -4,16 +4,43 @@ from invest_natcap.iui import iui_validator
 
 TEST_DATA = 'data/'
 
-class URICheckerTester(unittest.TestCase):
-    def test_uri_exists(self):
-        validate_as = {'type': 'file',
+class CheckerTester(unittest.TestCase):
+    def assertError(self, error):
+        if error != None:
+            self.assertEqual(error, '')
+
+    def assertNotError(self, error):
+        if error != None:
+            self.assertNotEqual(error, '')
+
+class URICheckerTester(CheckerTester):
+    def setUp(self):
+        self.validate_as = {'type': 'file',
                        'value': TEST_DATA +
                        '/base_data/terrestrial/lulc_samp_cur/hdr.adf'}
-        checker = iui_validator.URIChecker()
-        error = checker.run_checks(validate_as)
-        self.assertEqual(error, '')
+        self.checker = iui_validator.URIChecker()
 
+    def test_uri_exists(self):
+        error = self.checker.run_checks(self.validate_as)
+        self.assertError(error)
+
+    def test_nonexistent_uri(self):
         #this should fail, so we check that an error message is there.
-        validate_as['value'] += 'a'
-        error = checker.run_checks(validate_as)
-        self.assertNotEqual(error, '')
+        self.validate_as['value'] += 'a'
+        error = self.checker.run_checks(self.validate_as)
+        self.assertNotError(error)
+
+
+class FolderCheckerTester(CheckerTester):
+    def setUp(self):
+        self.validate_as = {'type': 'folder',
+                       'value': TEST_DATA}
+        self.checker = iui_validator.FolderChecker()
+
+    def test_folder_exists(self):
+        error = self.checker.run_checks(self.validate_as)
+        self.assertError(error)
+
+    def test_not_folder(self):
+        error = self.checker.run_checks(self.validate_as)
+        self.assertNotError(error)
