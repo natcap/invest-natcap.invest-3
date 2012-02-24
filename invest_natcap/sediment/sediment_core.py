c4d6b45936f2adc7387129c56b7b4f9526dc6263
richpsharp <devnull@localhost>
2012-02-24 15:47:03 -0800

    Update issue 537
    Calling bounding_box function for each watershed.
    
    
    Former-commit-id: 6eb810da7caec1821da092580ed27eb6325d254b

--- invest_natcap/sediment/sediment_core.py
+++ invest_natcap/sediment/sediment_core.py
@@ -61,10 +61,11 @@ def biophysical(args):
     LOGGER = logging.getLogger('sediment_core: biophysical')
     for watershed_feature in args['watersheds'].GetLayer():
         LOGGER.info('Working on watershed_feature %s' % watershed_feature.GetFID())
-        watershed_geom = watershed_feature.GetGeometryRef()
-        watershed_bounding_box = watershed_geom.GetEnvelope()
+        watershed_bounding_box = \
+            invest_core.bounding_box_index(watershed_feature, args['dem'])
         LOGGER.info('Bounding box %s' % (watershed_bounding_box))
-        #Convert bounding box coordinates to DEM indexes
+
+
 
     LOGGER.info("calculating flow direction")
     invest_cython_core.flow_direction_inf(args['dem'], args['flow_direction'])
