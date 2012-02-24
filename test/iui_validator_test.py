0163bb324da6b88d1efc4bd5d93d3f2c50eec353
James Douglass <jdouglass@stanford.edu>
2012-02-24 13:42:33 -0800

    Update issue 549
    Adding an OGRChecker class.
    
    
    Former-commit-id: a9b76c03743e1271e8248d0a2b6ce7651e056467

--- test/iui_validator_test.py
+++ test/iui_validator_test.py
@@ -13,12 +13,12 @@ class CheckerTester(unittest.TestCase):
         if error != None:
             self.assertNotEqual(error, '')
 
-class URICheckerTester(CheckerTester):
+class FileCheckerTester(CheckerTester):
     def setUp(self):
         self.validate_as = {'type': 'file',
                        'value': TEST_DATA +
                        '/base_data/terrestrial/lulc_samp_cur/hdr.adf'}
-        self.checker = iui_validator.URIChecker()
+        self.checker = iui_validator.FileChecker()
 
     def test_uri_exists(self):
         error = self.checker.run_checks(self.validate_as)
@@ -44,3 +44,26 @@ class FolderCheckerTester(CheckerTester):
     def test_not_folder(self):
         error = self.checker.run_checks(self.validate_as)
         self.assertNotError(error)
+
+
+class OGRCheckerTester(CheckerTester):
+    def setUp(self):
+        self.validate_as = {'type':'OGR',
+                            'value':TEST_DATA +
+                            '/wave_energy_data/samp_input/AOI_WCVI.shp'}
+        self.checker = iui_validator.OGRChecker()
+
+    def test_file_layers(self):
+        layer = {'name': {'inheritFrom': 'file'}}
+        self.validate_as['layers'] = [layer]
+
+        incremental_additions = [('name', {'inheritFrom': 'file'}),
+                                 ('type', 'polygons'),
+                                 ('projection', 'Transverse_Mercator')]
+
+        for key, value in incremental_additions:
+            self.validate_as['layers'][0][key] = value
+            error = self.checker.run_checks(self.validate_as)
+            self.assertError(error)
+
+
