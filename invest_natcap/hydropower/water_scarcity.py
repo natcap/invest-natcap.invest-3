f3da7a98bd1395d84546f5f808c218aa924655cf
dcdenu4 <devnull@localhost>
2012-03-09 13:22:54 -0800

    Update issue 571
    Added docstring with inputs and description.  Also added some basica imports and some basic file handling.
    
    Next would be to handle dbf files and store appropriate fields/values in dictionary
    
    
    Former-commit-id: deec1a1070248a29c1df76e9844690b941aefbdb

---
+++ invest_natcap/hydropower/water_scarcity.py
@@ -0,0 +1,70 @@
+"""InVEST Water Scarcity module at the "uri" level"""
+
+import sys
+import os
+import logging
+
+from osgeo import gdal
+from osgeo import ogr
+
+from invest_natcap.hydropower import hydropower_core
+
+logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
+%(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('water_scarcity')
+
+def execute(args):
+    """This function invokes the water scarcity model given
+        URI inputs of files. It will do filehandling and open/create
+        appropriate objects to pass to the core sediment biophysical 
+        processing function.  It may write log, warning, or error messages to 
+        stdout.
+        
+        args - a python dictionary with at the following possible entries:
+    
+        args['workspace_dir'] - a uri to the directory that will write output
+            and other temporary files during calculation. (required)
+        args['water_yield_vol_uri'] - a uri to an input raster, generated from
+            the water_yield model, describing the total water yield per
+            sub-watershed. The approximate absolute annual water yield across
+            the landscape (cubic meters) (required) 
+        args['water_yield_mean_uri'] - a uri to an input raster, generated from
+            the water_yield model, describing the mean water yield per
+            sub-watershed (mm) (required)
+        args['lulc_uri'] - a uri to a land use/land cover raster whose
+            LULC indexes correspond to indexs in the biophysical table input.
+            Used for determining soil retention and other biophysical 
+            properties of the landscape.  (required)
+        args['watersheds_uri'] - a uri to an input shapefile of the watersheds
+            of interest as polygons. (required)
+        args['sub_watersheds_uri'] - a uri to an input shapefile of the 
+            subwatersheds of interest that are contained in the
+            'watersheds_uri' shape provided as input. (required)
+        args['demand_table_uri'] - a uri to an input dbf table of LULC classes,
+            showing consumptive water use for each landuse / land-cover type
+            (required)
+        args['hydro_calibration_table_uri'] - a  uri to an input dbf table of 
+            hydropower stations with associated calibration values (required)
+        
+        returns nothing"""
+    
+    
+    workspace_dir = args['workspace']
+    #Create the output directories
+    for folder_name in ['Output', 'Service', 'Intermediate']:
+        folder_path = workspace_dir + os.sep + folder_name
+        if not os.path.isdir(folder_path):
+            os.path.mkdir(folder_path)
+            
+    #Open all of the gdal files and place in dictionary
+    args['lulc'] = gdal.Open(args['lulc_uri'])
+    
+    #Open all the shapefiles and place in dictionary
+    args['watersheds'] = ogr.Open(args['watersheds_uri'])
+    args['sub_watersheds'] = ogr.Open(args['sub_watersheds_uri'])
+    
+    #Open/read in the dbf files into a dictionary and add to
+    #dictionary
+    
+    #Call water_scarcity_core.py
