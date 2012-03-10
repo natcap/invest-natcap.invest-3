6857fa26fcdfa4b5a81574020e6a8dc97cd1108a
dcdenu4 <devnull@localhost>
2012-03-09 16:15:22 -0800

    Update issue 577
    Created a file to hold the test suite and am adding it to repo.  Created a very basic test that gathers required inputs and sends to water_yield.execute
    
    
    Former-commit-id: 032723131af273d07565360b3b87ec55e96f68bb

---
+++ test/water_yield_test.py
@@ -0,0 +1,37 @@
+"""URI level tests for the water yield module"""
+
+import unittest
+import logging
+import os
+
+from osgeo import gdal
+from nose.plugins.skip import SkipTest
+import numpy as np
+
+from invest_natcap import postprocessing
+from invest_natcap.hydropower import water_yield
+import invest_cython_core
+import invest_test_core
+
+LOGGER = logging.getLogger('water_yield_test')
+logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
+    %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+class TestWaterYield(unittest.TestCase):
+    """Main testing class for the water yield tests"""
+    def test_water_yield_re(self):
+        base = './data/hydropower_data/'
+        args = {}
+        args['workspace_dir'] = base
+        args['lulc_uri'] = base + 'test_input/landuse_90'
+        args['soil_depth_uri'] = base + 'test_input/soil_depth'
+        args['precipitaion_uri'] = base + 'test_input/precip'
+        args['pawc_uri'] = base + 'test_input/pawc'
+        args['ape_uri'] = base + 'test_input/eto'
+        args['watersheds_uri'] = base + 'test_input/watersheds.shp'
+        args['sub_watersheds_uri'] = base + 'test_input/subwatersheds.shp'
+        args['biophysical_table_uri'] = \
+            base + 'test_input/Biophysical_Models.csv'
+        args['zhang'] = 7.0
+        
+        water_yield.execute(args)
