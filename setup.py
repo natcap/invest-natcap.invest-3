f0240f5ccffc85babcc38a95d3aed86efecf9de3
richpsharp <devnull@localhost>
2012-03-15 12:18:35 -0700

    Update issue 594
    Modified setup.py and added a bunch of .dll's to allow cython core and py2exe to be built on windows.
    
    
    Former-commit-id: bad1310d60b0140d227ca200ea623929ea8d4b4c

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import platform
+
+import numpy as np
 
 cython_source_files = ['invest_cython_core/invest_cython_core.pyx',
                        'invest_cython_core/simplequeue.c']
 
-#setup(name='invest_cython_core', 
-#      )
+options = {}
+console = []
+if platform.system() == 'Windows':
+    import py2exe
+    options = {"py2exe":{"includes":["sip"]}}
+    console = ['invest_carbon.py']
+
 
 setup(name='invest_natcap',
       version='tip',
@@ -16,11 +24,15 @@ setup(name='invest_natcap',
                 'invest_natcap.hydropower',
                 'invest_natcap.invest_core',
                 'invest_natcap.iui',
+                'invest_natcap.iui.dbfpy',
                 'invest_natcap.sediment',
                 'invest_natcap.timber',
                 'invest_natcap.validator_core',
                 'invest_natcap.wave_energy'],
       cmdclass={'build_ext': build_ext},
+      include_dirs = [np.get_include()],
       ext_modules=[Extension(name="invest_cython_core",
-                             sources=cython_source_files)]
+                             sources = cython_source_files)],
+      console = console,
+      options = options
       )
