05a2885104ceb7fd5b008923593fde9dacad7198
James Douglass <jdouglass@stanford.edu>
2012-04-04 11:20:15 -0700

    Fixing an issue with dropdown widgets in general ... I was incorrectly checking the number of elements in Dropdown.value().  Now value() correctly checks to see if there are no elements in the dropdown.
    Former-commit-id: a2f0af6a0e9663464060b41d5c7ae7e6fbabdaef

--- invest_natcap/iui/base_widgets.py
+++ invest_natcap/iui/base_widgets.py
@@ -910,7 +910,7 @@ class Dropdown(LabeledElement):
         self.dropdown.setCurrentIndex(index)
 
     def value(self):
-        if self.dropdown.count > 0:
+        if self.dropdown.count == 0:
             #if there are no elements in the dropdown, don't return a value
             return None
         elif 'returns' in self.attributes:
