200e3009d9a99c8824ca7d46e95316fa959b0b79
James Douglass <jdouglass@stanford.edu>
2012-05-01 13:45:51 -0700

    Update issue 719
    Adding docstrings and basic outline for smoke test of polliation_biophysical.
    
    
    Former-commit-id: cea6f631423c998a3b570a970b82ebfa89e7d743

---
+++ test/pollination_biophysical_test.py
@@ -0,0 +1,9 @@
+"""This file will contain unittests for the biophysical component of the
+pollination model."""
+
+import unittest
+
+class PollinationBiophysicalTest(unittest.TestCase):
+    def test_smoke(self):
+        """Smoke test for pollination_biophysical."""
+        pass
