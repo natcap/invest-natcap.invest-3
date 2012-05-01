71741d7e7eb0408d6cb1e25d02e5d57f70b72a86
James Douglass <jdouglass@stanford.edu>
2012-05-01 13:47:17 -0700

    Update issue 682
    Adding a couple lines to open the LULC and use the fileio.TableHandler class to
    open a user's provided attribute table.
    
    
    Former-commit-id: 5fd30c5c08fd6397ffa4729868a322d2c0c90887

--- invest_natcap/pollination/pollination_biophysical.py
+++ invest_natcap/pollination/pollination_biophysical.py
@@ -1,5 +1,14 @@
 """InVEST Pollination model file handler module"""
 
+from osgeo import gdal
+
+import pollination_core
+import invest_natcap.iui.fileio
+
+import logging
+logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
+     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
 def execute(args):
     """Open files necessary for the biophysical portion of the pollination
         model.
@@ -18,4 +27,21 @@ def execute(args):
 
         returns nothing."""
 
-    pass
+    gdal.AllRegister()
+
+    biophysical_args = {}
+    workspace = args['workspace_dir']
+
+    # Open the landcover raster
+    biophysical_args['landuse'] = gdal.Open(str(args['landuse_uri']),
+                                           gdal.GA_ReadOnly)
+
+    # Open a Table Handler for the land use attributes table
+    att_table_handler = fileio.TableHandler(args['landuse_attributes_uri'])
+
+    # Retrieve a list of row dictionaries as a representation of the input
+    # attributes table.
+    biophysical_args['landuse_attributes'] = att_table_handler.get_table_list()
+
+
+
