23ad8f27247f89efac0181799c831925edeec381
James Douglass <jdouglass@stanford.edu>
2012-05-02 16:17:09 -0700

    Update issue 682
    Adding docstring entries for the new floral and nesting subdictionaries of args.
    
    
    Former-commit-id: d3101ef200153b665241f9a0944a7c7409e0b0c9

--- invest_natcap/pollination/pollination_core.py
+++ invest_natcap/pollination/pollination_core.py
@@ -11,6 +11,14 @@ def biophysical(args):
             classes in the landuse map.  This list may be empty to represent the
             fact that no landuse classes are to be designated as strictly
             agricultural.
+        args['floral'] - a python dictionary with the following structure:
+            {'f_<season>': gdal dataset for the floral season}, where
+            'f_<season>' is taken from the appropriate column label in the
+            landuse_attributes table.
+        args['nesting'] - a python dictionary with the following structure:
+            {'n_<type>': gdal dataset for the nesting type}, where
+            'n_<type>' is taken from the appropriate column label in the
+            landuse_attributes table.
 
         returns nothing."""
     pass
