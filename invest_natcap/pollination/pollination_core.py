580d923663d2cb5a3eb514f2f64ab795f0546b7a
James Douglass <jdouglass@stanford.edu>
2012-05-03 15:04:40 -0700

    Update issue 684
    Making the following changes to pollination_core.make_ag_raster():
        * make_ag_raster is now sensitive to nodata values.  If a nodata value
          is detected in the input raster, a nodata value is saved to the output
          raster.
        * the call to invest_core.vectorize1ArgOp() now correctly passes raster
          bands instead of the whole dataset.
    
    
    Former-commit-id: cfeffd892e3de1d4c5db28596ba56c441fe4916c

--- invest_natcap/pollination/pollination_core.py
+++ invest_natcap/pollination/pollination_core.py
@@ -1,5 +1,8 @@
 """InVEST Pollination model core function  module"""
 
+import invest_cython_core
+from invest_natcap.invest_core import invest_core
+
 def biophysical(args):
     """Execute the biophysical component of the pollination model.
 
@@ -43,10 +46,12 @@ def make_ag_raster(landuse_raster, ag_classes, ag_raster):
 
         returns nothing."""
 
-    # If the user has provided agricultural classes, we should only consider
-    # those classes as agricultural.  If the user has not provided a list of
-    # agricultural classes, we should consider the entire landuse raster as
-    # agricultural.
+    # Fetch the landcover raster's nodata value
+    lu_nodata = landuse_raster.GetRasterBand(1).GetNoDataValue()
+
+    # Fetch the output raster's nodata valye
+    ag_nodata = ag_raster.GetRasterBand(1).GetNoDataValue()
+
     # This case is triggered when the user provides agricultural classes.
     if len(ag_classes) > 0:
         # Preprocess ag_classes into a dictionary to improve access times in the
@@ -56,20 +61,25 @@ def make_ag_raster(landuse_raster, ag_classes, ag_raster):
 
         def ag_func(lu_class):
             """Check to see if the input pixel value is an agricultural pixel.  If
-                so, return 1.  Otherwise, return 0."""
+                so, return 1.  Otherwise, return 0.  If the pixel is a nodata
+                pixel, return nodata."""
+            if lu_class == lu_nodata:
+                return ag_nodata
             if lu_class in ag_dict:
                 return 1
-            else:
-                return 0
+            return 0
     else:
         # This case is triggered if the user does not provide any land cover
         # classes.  In this case, we fill the raster with 1's to indicate that
         # all pixels are to be considered agricultural.
         def ag_func(lu_class):
             """Indicate that we want all land cover classes considered as
-                agricultural.  Always return 1."""
+                agricultural.  Always return 1 unless it's a nodata pixel."""
+            if lu_class == lu_nodata:
+                return ag_nodata
             return 1
 
     # Vectorize all of this to the output (ag) raster.
-    invest_core.vectorize1ArgOp(landuse_raster, ag_func, ag_dict, ag_raster)
+    invest_core.vectorize1ArgOp(landuse_raster.GetRasterBand(1), ag_func,
+        ag_raster.GetRasterBand(1))
 
