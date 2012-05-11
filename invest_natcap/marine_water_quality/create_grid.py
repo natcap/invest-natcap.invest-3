e3fc9da2a3e99c288b4065d7bc1cce579ac29a82
Richard Sharp <richpsharp@gmail.com>
2012-05-10 22:33:33 -0700

    Update issue 735
    Iterating through all the points that will be in the grid.
    
    
    Former-commit-id: f45bffbe4582f9522a4f2d5efc27e5812544eb34

--- invest_natcap/marine_water_quality/create_grid.py
+++ invest_natcap/marine_water_quality/create_grid.py
@@ -10,11 +10,20 @@ try:
     land_ds = ogr.Open(land_poly_file)
     aoi_ds = ogr.Open(aoi_poly_file)
 
-    aoi_extent = aoi_ds.GetLayer(0).GetExtent()
-
     #format of aoi_extent [xleft, xright, ybot, ytop]
+    aoi_extent = aoi_ds.GetLayer(0).GetExtent()
+    xleft,xright,ybot,ytop = aoi_extent
+
+    x_ticks = int((xright-xleft)/cell_size)
+    y_ticks = int((ytop-ybot)/cell_size)
 
     print aoi_extent
+    print x_ticks, y_ticks
+
+    for x_index in range(x_ticks):
+        for y_index in range(y_ticks):
+            x_coord = xleft+x_index*cell_size
+            y_coord = ytop-y_index*cell_size
 
 except:
     print "Usage create_grid.py land_poly_file aoi_poly_file cell_size"
