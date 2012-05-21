adb041e5e246831505e36c08b51eb37b17ac77a4
James Douglass <jdouglass@stanford.edu>
2012-05-21 14:05:41 -0700

    Update issue 759
    Adding some comments to the pollination test suite.
    
    
    Former-commit-id: 89a41327fb124f42a0b10c6fdc00da64f4e03e02

--- test/pollination_biophysical_test.py
+++ test/pollination_biophysical_test.py
@@ -16,9 +16,19 @@ TEST_DATA_DIR = 'data/pollination/samp_input'
 REGRESSION_FOLDER_BASE = 'data/pollination/'
 
 class PollinationTest(unittest.TestCase):
+    """This class contains information specific to the Pollination models, but
+        shared between both the biophysical and Valuation components."""
+
     def setUp(self):
+        """Reimplemented from unittest.TestCase, this has information that is
+            used by both the biophysical and valuation components. """
         self.workspace_dir = 'data/pollination/test_workspace'
         self.guilds_uri = TEST_DATA_DIR + '/Guild.dbf'
+
+        # The structure of self.intermediate_rasters and self.output_rasters
+        # should be maintained across subclasses, although their contents may be
+        # altered according to the needs of the subclass.  These two
+        # dictionaries are used by self.assert_pollination_rasters().
         self.intermediate_rasters = {'workspace_subfolder': 'intermediate',
                                      'raster_uri_base': [],
                                      'uri_mid' : []}
@@ -28,6 +38,17 @@ class PollinationTest(unittest.TestCase):
                                'uri_mid': []}
 
     def assert_pollination_rasters(self, test_path):
+        """Assert that all rasters produced by this component of the pollination
+            model match their corresponding regression rasters.  This function
+            uses self.intermediate_rasters and self.output_rasters dictionaries
+            to assemble the correct URIs.
+
+            test_path - a python string representing the path to the folder
+                containing regression rasters.  Note that this folder's
+                structure should match the structure of the pollination model.
+
+            returns nothing"""
+
         for test_folder in [self.intermediate_rasters, self.output_rasters]:
             out_folder_name = os.path.join(self.args['workspace_dir'],
                 test_folder['workspace_subfolder'])
@@ -94,15 +115,29 @@ class PollinationValuationTest(PollinationTest):
                                'raster_uri_base': ['sup_val'],
                                'uri_mid': ['']}
 
+        # The valuation component needs to be started with the appropriate
+        # datasets in the workspace, so we need to delete the existing workspace
+        # if it exists in the filesystem, we should remove it and copy over a
+        # complete set of regression rasters for use by the valuation
+        # component.
         self.biophysical_sample_dir = os.path.join(REGRESSION_FOLDER_BASE,
             'biophysical_output', 'with_ag_classes')
-        shutil.rmtree(self.workspace_dir)
+        try:
+            shutil.rmtree(self.workspace_dir)
+        except OSError:
+            # OSError is thrown when self.workspace_dir doesn't exist in the
+            # filesystem.  In this case, we don't need to do anything.
+            pass
         shutil.copytree(self.biophysical_sample_dir, self.workspace_dir)
 
     def tearDown(self):
+        """This function is called at the end of each test.  For
+            pollination_valuation, this function removes the workspace directory
+            from the filesystem."""
         shutil.rmtree(self.workspace_dir)
 
     def test_regression(self):
+        """Regression test for pollination_valuation."""
         pollination_valuation.execute(self.args)
         self.assert_pollination_rasters(self.biophysical_sample_dir)
 
