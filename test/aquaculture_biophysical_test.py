cc652348170ef9932417eb3e2e947fbf37ef1815
kathryn.glowinski <devnull@localhost>
2012-05-23 10:48:32 -0700

    Change name of aquaculture test to finfish_aquaculture.
    Former-commit-id: 03308848be32236912fd8c3c7443282832846b57

--- test/aquaculture_biophysical_test.py
+++ test/aquaculture_biophysical_test.py
@@ -3,15 +3,21 @@
 import os, sys
 import unittest
 
-from invest_natcap.aquaculture import aquaculture_biophysical
+from invest_natcap.finfish_aquaculture import finfish_aquaculture_biophysical
 import invest_test_core
 
-class TestAquacultureBiophysical(unittest.TestCase):
-    def test_aquaculture_biophysical_smoke(self):
-        """Smoke test for aquaculture_biophysical function.  Shouldn't crash with \
+class TestFinfishAquacultureBiophysical(unittest.TestCase):
+    def test_finfish_aquaculture_biophysical_smoke(self):
+        """Smoke test for finfish_aquaculture_biophysical function.  Shouldn't crash with \
 #zero length inputs"""
 
         args = {}
-        
+        args['workspace_dir'] = './data/aquaculture_output'
+        args['ff_farm_loc'] = './data/aquaculture_data/Finfish_Netpens.shp'
+        args['farm_ID'] = 'FarmID'
+        args['g_param_a'] = 0.038
+        args['g_param_b'] = 0.6667
+        args['water_temp_tbl'] = './data/aquaculture_data/Temp_Daily.csv'
+        args['farm_op_tbl'] = './data/aquaculture_data/Farm_Operations.csv'
 
-        aquaculture_biophysical.execute(args)
+        finfish_aquaculture_biophysical.execute(args)
