05409b52a232fe1e2450d966f6312f6a791e1c7f
Richard Sharp <richpsharp@gmail.com>
2012-05-22 22:58:14 -0700

    Update issue 777
    Loading up the shapefiles necessary for the interpolation.
    
    
    Former-commit-id: 095237f372196b2c834dc87718aa6288a728ebed

--- invest_natcap/marine_water_quality/interpolate_points_to_raster.py
+++ invest_natcap/marine_water_quality/interpolate_points_to_raster.py
@@ -3,6 +3,12 @@ import sys
 from osgeo import gdal
 from osgeo import ogr
 
+#This craziness is just for development to import a relative link so I can
+#run in the current directory that has a soft link.
+try:
+    from invest_natcap import raster_utils
+except:
+    import raster_utils
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -13,5 +19,3 @@ if __name__ == '__main__':
     aoi_ds = ogr.Open(sys.argv[1])
     point_ds = ogr.Open(sys.argv[2])
     attribute = sys.argv[3]
-
-    print aoi_ds, point_ds, attribute
