c4d9bff6294a2d464d07f220fdfee8fd1ce32aaf
Richard Sharp <richpsharp@gmail.com>
2012-05-28 15:04:22 -0700

    Fixes issue 790
    Same deal for rsupply_mean_op.
    
    Poor vectorize_rasters, we always blame it.  :(
    
    
    Former-commit-id: aa54a1b3b369c01f723fd43e8aeb922cf2262dfc

--- invest_natcap/hydropower/hydropower_core.py
+++ invest_natcap/hydropower/hydropower_core.py
@@ -937,6 +937,7 @@ def water_scarcity(args):
     wyield_mn_nodata = wyield_mean.GetRasterBand(1).GetNoDataValue()
     mn_raster_nodata = mean_raster.GetRasterBand(1).GetNoDataValue()
     LOGGER.info('Creating rsupply_mn raster')
+    rsupply_mean_out_nodata = 0.0
     def rsupply_mean_op(wyield_mean, consump_mean):
         """Function that computes the mean realized water supply
         
@@ -951,12 +952,13 @@ def water_scarcity(args):
         if wyield_mean != wyield_mn_nodata and consump_mean != mn_raster_nodata:
             return wyield_mean - consump_mean
         else:
-            return out_nodata
+            return rsupply_mean_out_nodata
         
     rsupply_mn_vec = np.vectorize(rsupply_mean_op)
     
     raster_utils.vectorize_rasters([wyield_mean, mean_raster], rsupply_mn_vec, 
-                                 raster_out_uri=rsupply_mean_path)
+                                   raster_out_uri=rsupply_mean_path,
+                                   nodata=rsupply_mean_out_nodata)
     
     #Make sub watershed and watershed tables by adding values onto the tables
     #provided from sub watershed yield and watershed yield
