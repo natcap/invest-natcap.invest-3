36e5b9e1fa61dd68e1dee33c44f5103f3723ed8d
James Douglass <jdouglass@stanford.edu>
2012-06-19 10:14:42 -0700

    Update issue 880
    Adding a test for the get_map function.
    
    Also, I've changed the docstrings to inline comments.  The docstrings were not
    proving helpful because 1) the function names themselves are pretty descriptive,
    2) the unittests are the same across all table filetypes, so it's more helpful
    to see which class and test failed rather than the docstring.
    
    
    Former-commit-id: 670f070a3e10ddc13f52792744371107e3d0076e

--- test/fileio_test.py
+++ test/fileio_test.py
@@ -7,8 +7,8 @@ from invest_natcap.iui import fileio
 TEST_DATA = 'data/'
 
 class CSVHandlerTest(unittest.TestCase):
-    """This is a unittest to test the table handling functionality of the
-    CSVHandler class."""
+    # This is a unittest to test the table handling functionality of the
+    # CSVHandler class.
     def setUp(self):
         # If we haven't already declared what self.input_file should be (as
         # happens in subclasses of CSVHandlerTest), define it here.
@@ -19,7 +19,7 @@ class CSVHandlerTest(unittest.TestCase):
         self.handler = fileio.find_handler(self.input_file)
 
     def test_get_fieldnames(self):
-        """Assert fieldnames found in self.file match expected fieldnames"""
+        #Assert fieldnames found in self.file match expected fieldnames
         expected_fieldnames = ['species', 'ns_cavity', 'ns_ground', 'fs_spring',
             'fs_summer', 'alpha']
 
@@ -28,11 +28,24 @@ class CSVHandlerTest(unittest.TestCase):
             self.assertEqual(name, exp_name)
 
     def test_get_file_object(self):
-        """Assert that the file object is not None.  The better way to test this
-        would be to check the python object type ... don't yet know a
-        generalized way to do this."""
+        # Assert that the file object is not None.  The better way to test this
+        # would be to check the python object type ... don't yet know a
+        # generalized way to do this.
         file_obj = self.handler.get_file_object()
         self.assertNotEqual(file_obj, None, 'File object found to be None')
+
+    def test_get_map(self):
+        table_map = self.handler.get_map('species', 'ns_ground')
+
+        self.assertEqual(issubclass(table_map.__class__, dict), True,
+            'get_map() does not return a dict.')
+
+        expected_map = {'Apis': 1.0,
+                        'Bombus': 0.0}
+        for species, ns_ground in table_map.iteritems():
+            self.assertEqual(species in expected_map, True,
+                'Species %s not expected' % species)
+            self.assertEqual(expected_map[species], ns_ground)
 
 class DBFHandlerTest(CSVHandlerTest):
     """This is a unittest to test the table handling functionality of the
