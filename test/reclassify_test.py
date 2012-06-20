45447f1ae7c412d5fe6560ac036f04bf2835aaf5
Richard Sharp <richpsharp@gmail.com>
2012-06-20 13:36:39 -0700

    Update issue 855
    Tracer code for relcassify_by_dictionary.
    
    
    Former-commit-id: 2ba0bc5dce1c0e53da95b5d5ec6ae8ff4accc83f

---
+++ test/reclassify_test.py
@@ -0,0 +1,20 @@
+from osgeo import gdal
+
+import invest_natcap.raster_utils
+
+dataset = gdal.Open('./data/hydropower_regression_data/sub_shed_mask.tif')
+rules = {1: 50.2, 2: -1838, 3: 0.1}
+output_uri = './data/test_out/reclassified_raster.tif'
+
+nodata = -1.0e-30
+datatype = gdal.GDT_Float32
+
+invest_natcap.raster_utils.reclassify_by_dictionary(dataset,rules,output_uri,nodata,datatype)
+
+rules = {1: 10, 2: 20, 3: 30}
+output_uri = './data/test_out/reclassified_raster_byte.tif'
+
+nodata = 0
+datatype = gdal.GDT_Byte
+
+invest_natcap.raster_utils.reclassify_by_dictionary(dataset,rules,output_uri,nodata,datatype)
