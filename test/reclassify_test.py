eb5a83d9c717a3ed37038ac1a1cf62297b407aaf
Richard Sharp <richpsharp@gmail.com>
2012-06-20 13:39:34 -0700

    Update issue 855
    Updated the tracer code to use format.
    
    
    Former-commit-id: cf5aaec4172ddea6e641143a0ca99f88c984084a

--- test/reclassify_test.py
+++ test/reclassify_test.py
@@ -9,7 +9,7 @@ output_uri = './data/test_out/reclassified_raster.tif'
 nodata = -1.0e-30
 datatype = gdal.GDT_Float32
 
-invest_natcap.raster_utils.reclassify_by_dictionary(dataset,rules,output_uri,nodata,datatype)
+invest_natcap.raster_utils.reclassify_by_dictionary(dataset,rules,output_uri,'MEM', nodata,datatype)
 
 rules = {1: 10, 2: 20, 3: 30}
 output_uri = './data/test_out/reclassified_raster_byte.tif'
@@ -17,4 +17,4 @@ output_uri = './data/test_out/reclassified_raster_byte.tif'
 nodata = 0
 datatype = gdal.GDT_Byte
 
-invest_natcap.raster_utils.reclassify_by_dictionary(dataset,rules,output_uri,nodata,datatype)
+invest_natcap.raster_utils.reclassify_by_dictionary(dataset,rules,output_uri,'GTiff',nodata,datatype)
