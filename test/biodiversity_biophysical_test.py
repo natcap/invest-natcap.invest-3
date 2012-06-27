3118168d38c6616b9c4f2afb087187eaa8f02a64
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-06-27 14:52:32 -0700

    Update issue 918
    I now use os.path.join for concatenating file paths.
    
    
    Former-commit-id: 878906662f2865bc96855ac73b41b8232850c0bc

--- test/biodiversity_biophysical_test.py
+++ test/biodiversity_biophysical_test.py
@@ -10,18 +10,19 @@ class TestBiodiversityBiophysical(unittest.TestCase):
     def test_biodiversity_biophysical_smoke(self):
         """Smoke test for biodiversity_biophysical function.  Shouldn't crash with \
            zero length inputs"""
-        input_dir = './data/biodiversity_data/samp_input/'
+        input_dir = './data/biodiversity_regression_data/samp_input'
         out_dir = './data/test_out/biodiversity/'
         if not os.path.isdir(out_dir):
             os.makedirs(out_dir)
         args = {}
         args['workspace_dir'] = input_dir
-        args['landuse_cur_uri'] = input_dir + 'lc_samp_cur_b/w001001.adf'
+        args['landuse_cur_uri'] = \
+                os.path.join(input_dir, 'lc_samp_cur_b.tif')
         #args['landuse_bas_uri'] = input_dir + 'lc_samp_bse_b/w001001.adf'
         #args['landuse_fut_uri'] = input_dir + 'lc_samp_fut_b/w001001.adf'
-        args['threat_uri'] = input_dir + 'threats_samp.csv'
-        args['sensitivity_uri'] = input_dir + 'sensitivity_samp.csv'
-        args['access_uri'] = input_dir + 'access_samp.shp'
+        args['threat_uri'] = os.path.join(input_dir, 'threats_samp.csv')
+        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         args['half_saturation_constant'] = 30
         args['results_suffix'] = ''
 
