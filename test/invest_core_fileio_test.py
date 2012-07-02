3e93744a852c65e8854c58e3a5fbd08e5db47e53
James Douglass <jdouglass@stanford.edu>
2012-07-02 09:59:36 -0700

    Update issue 927
    Adding some simple smoke tests for invest_core.fileio
    
    
    Former-commit-id: a25e697641686287ef24671440822c60a825b232

--- test/invest_core_fileio_test.py
+++ test/invest_core_fileio_test.py
@@ -1,6 +1,24 @@
+import unittest
+import invest_natcap.invest_core.fileio
+
+GUILDS_URI = './data/iui/Guild.csv'
+
 class CSVDriverTest(unittest.TestCase):
     def setUp(self):
-        self.driver = invest_natcap.fileio.CSVDriver('./data/pollination/samp_input/Guild.csv')
+        self.driver = invest_natcap.invest_core.fileio.CSVDriver(GUILDS_URI)
 
-    def test_get_file_object():
+    def test_get_file_object(self):
         print self.driver.get_file_object()
+
+    def test_get_fieldnames(self):
+        print self.driver.get_fieldnames()
+
+    def test_read_table(self):
+        print self.driver.read_table()
+
+class TableHandlerTest(unittest.TestCase):
+    def setUp(self):
+        self.handler = invest_natcap.invest_core.fileio.TableHandler(GUILDS_URI)
+
+    def test_get_table(self):
+        print self.handler.get_table()
