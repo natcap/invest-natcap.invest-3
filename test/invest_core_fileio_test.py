900b6935a160bd48ce6018e37e69ddd3172818fc
James Douglass <jdouglass@stanford.edu>
2012-07-02 16:15:11 -0700

    Update issue 927
    Adding some extra printing to invest_core_fileio_test to aid in debugging smoke
    tests.
    
    
    Former-commit-id: 07a8b494c03ac923413b8c64a1a7ad3bd00d199a

--- test/invest_core_fileio_test.py
+++ test/invest_core_fileio_test.py
@@ -5,6 +5,7 @@ GUILDS_URI = './data/iui/Guild.csv'
 
 class CSVDriverTest(unittest.TestCase):
     def setUp(self):
+        print ''
         self.driver = invest_natcap.invest_core.fileio.CSVDriver(GUILDS_URI)
 
     def test_get_file_object(self):
@@ -18,7 +19,16 @@ class CSVDriverTest(unittest.TestCase):
 
 class TableHandlerTest(unittest.TestCase):
     def setUp(self):
+        print ''  # print a newline before each test.  makes printing prettier
         self.handler = invest_natcap.invest_core.fileio.TableHandler(GUILDS_URI)
 
     def test_get_table(self):
-        print self.handler.get_table()
+        table = self.handler.get_table()
+        for row in table:
+            print row
+
+    def test_set_field_mask(self):
+        self.handler.set_field_mask('^fs_', 3)
+        for row in self.handler.table:
+            print row
+
