803583731c800bb3ea3b7b6c0b2b7ffd195d4f77
richpsharp <devnull@localhost>
2012-07-05 16:03:55 -0700

    Update issue 948
    Verified that a dem that slopes to the right has flow direction of 0 on every pixel.
    
    
    Former-commit-id: f8b6a01d79300e5e853d47cc3ec56b178e5a7273

--- test/create_test_dems.py
+++ test/create_test_dems.py
@@ -1,4 +1,8 @@
+from osgeo import gdal
+
 import invest_test_core
+import invest_cython_core
+from invest_natcap import raster_utils
 
 points = {
     (0.0,0.0): 50,
@@ -7,4 +11,18 @@ points = {
     (1.0,1.0): 0,
     (0.5,0.5): 45}
 
-invest_test_core.make_sample_dem(100,100,points, 1.0, -1, 'random_dem.tif')
+points_down = {
+    (0.0,0.0): 100,
+    (0.0,1.0): 100,
+    (1.0,0.0): 0,
+    (1.0,1.0): 0}
+
+
+dem = invest_test_core.make_sample_dem(100,100,points_down, 0.0, -1, 'random_dem.tif')
+
+flow_dataset = raster_utils.new_raster_from_base(dem, 'random_dem_flow.tif', 'GTiff',
+                                                 -1, gdal.GDT_Float32)
+
+invest_cython_core.flow_direction_inf(dem, [0, 0, 100, 100], flow_dataset)
+raster_utils.calculate_raster_stats(dem)
+raster_utils.calculate_raster_stats(flow_dataset)
