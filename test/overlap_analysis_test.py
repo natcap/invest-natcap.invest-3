0b046e997d9b60bfe0574653d9ca1dde6233f671
kathryn.glowinski <devnull@localhost>
2012-07-17 12:23:48 -0700

    Update issue 962
    Finalizing the test_format_over_table function.
    
    > We need to determine whether we are just going to test the whole function by checking the tangible outputs/intermediate files, or if I need to figure out how to extract the data that is part of the run of the function and check that before it's written to file form.
    
    
    Former-commit-id: 0a0ac96b2562cdd651aba6e281c05fe7dfa27df8

--- test/overlap_analysis_test.py
+++ test/overlap_analysis_test.py
@@ -33,10 +33,17 @@ class TestOverlapAnalysis(unittest.TestCase):
         args['overlap_data_dir_loc'] = './data/overlap_analysis/FisheriesLayer_RI'
         args['overlap_layer_tbl'] = './data/overlap_analysis/Fisheries_Inputs.csv'
         
+        self.args = args
+        
     def test_execute(self):
+        #DO WE WANT TO TEST FINAL OUTPUTS OR INTERMEDIATE CREATED FILES
         pass
+        
     def test_format_over_table(self):
-        pass
+        
+        output_table = overlap_analysis.format_over_table(self.args['overlap_layer_tbl'])
+        
+        reg_overlap_table = {'CommGf_Fish': (2.00, 0), 'CommSalmonTroll_Fish': (1.50, 0),
+                             'CommShrimp_Fish': (1.50, 0)}
     
-    
-    
+        invest_test_core.assertAlmostEqual(output_table, reg_overlap_table)
