6882c5ba9a1dad24f1dc1550c0af4a8c3d0607fd
kathryn.glowinski <devnull@localhost>
2012-07-17 14:00:35 -0700

    Update issue 962
    Fixed test_format_over_table in overlap_analysis_test so that it correctly compares dictionary elements where each element is a string pointing to a tuple.
    
    
    Former-commit-id: 29588b8861d73d4c4c45524aae412fba0bcf520e

--- test/overlap_analysis_test.py
+++ test/overlap_analysis_test.py
@@ -43,7 +43,23 @@ class TestOverlapAnalysis(unittest.TestCase):
         
         output_table = overlap_analysis.format_over_table(self.args['overlap_layer_tbl'])
         
-        reg_overlap_table = {'CommGf_Fish': (2.00, 0), 'CommSalmonTroll_Fish': (1.50, 0),
+        reg_overlap_table = {'CommGF_Fish': (2.00, 0), 'CommSalmonTroll_Fish': (1.50, 0),
                              'CommShrimp_Fish': (1.50, 0)}
     
-        invest_test_core.assertAlmostEqual(output_table, reg_overlap_table)
+        #Dictionaries are not included in the self.assertAlmostEqual function, so writing
+        #a quick function here to check them. This will use assertAlmostEqual inside of it.
+        
+        for element in reg_overlap_table:
+            
+            if output_table[element]:
+                    
+                #Need to pull out both elements and compare, then break down so
+                #we can do an almost equal on the individual elements
+                w1, b1 = output_table[element]
+                w2, b2 = reg_overlap_table[element]
+                
+                self.assertAlmostEqual(w1, w2)
+                self.assertAlmostEqual(b1, b2)
+            
+            else:
+                self.fail("Element %s is not a key in the test-created table.", element)
