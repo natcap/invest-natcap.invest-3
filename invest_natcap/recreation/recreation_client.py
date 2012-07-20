731464d23adf7dbe97ef515a7a007e77b77ad141
mlacayoemery <devnull@localhost>
2012-07-20 14:15:07 -0700

    Changed default test case.
    Former-commit-id: b4b62627296f3f96c138c91dcde6df262bf55043

--- invest_natcap/recreation/recreation_client.py
+++ invest_natcap/recreation/recreation_client.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     else:
         LOGGER.info("Runnning model with test parameters")
         dirname=os.sep.join(os.path.abspath(os.path.dirname(sys.argv[0])).split(os.sep)[:-2])+"/test/data/"
-        aoiFileName = dirname+"recreation_data/"+"aoi.shp"
+        aoiFileName = dirname+"recreation_data/"+"FIP-11001.shp"
         cellSize = 5000
         workspace_dir = dirname+"test_out/"
         comments = "Runnning model with test parameters"
