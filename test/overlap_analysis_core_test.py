193c518ffcc7e73b4003dea7fd4a02f3be7b23f4
kathryn.glowinski <devnull@localhost>
2012-08-07 00:05:34 -0700

    Update issue 962
    Creating skeleton for overarching test. Creating the args dictionary to run based on methods from overlap_analysis.py. Will still need to drill down into folders to find outputs, but shouldn't be too much of an issue.
    
    
    Former-commit-id: 4a7defe5b7574fd3706489788fcda659d85b8894

--- test/overlap_analysis_core_test.py
+++ test/overlap_analysis_core_test.py
@@ -7,7 +7,9 @@ import os
 import unittest
 
 import invest_test_core
+
 from invest_natcap.overlap_analysis import overlap_analysis_core
+from invest_natcap.overlap_analysis import overlap_analysis
 from osgeo import ogr
 
 class TestOverlapAnalysisCore(unittest.TestCase):
@@ -28,11 +30,16 @@ class TestOverlapAnalysisCore(unittest.TestCase):
         
 	def test_reg_overall:
 		
-		self.args['zone_layer_file'] = ogr.Open('./data/overlap_analysis/AOI_WCVI.shp')
+		self.args['zone_layer_file'] = ogr.Open('./data/test_out/Overlap/Input/test_aoi.shp')
 		self.args['do_inter'] = True
 		self.args['do_intra'] = True
 
 		files_loc = './data/test_out/Overlap/Input/Test_Activity'
 
-		files_dict = {}
+		files_dict = overlap_analysis.get_files_dict(files_loc)
 
+		args['overlap_files'] = files_dict
+	 
+	 	inter_table_loc = './data/test_out/Overlap/Input/inter_activ_table.csv'
+		inter_table = overlap_analysis.format_over_table(inter_table_loc)
+		args['over_layer_dict'] = inter_table
