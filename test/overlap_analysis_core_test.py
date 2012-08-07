3d7cfdaf32eeb84cfac7362ca21c453185c18a11
kathryn.glowinski <devnull@localhost>
2012-08-07 09:56:07 -0700

    Update issue 962
    Pulls out the files completed by the test fun. Need to compare them against something. Is there a way to rasterize a vector by hand in qgis? That way I could create my 1000x1000 pixel blocks, and just set them to whatever I calculate the output of the frequency or the weighting should be.
    
    
    Former-commit-id: aa76d676ad72829dcbfd5cc1855c969b8d063509

--- test/overlap_analysis_core_test.py
+++ test/overlap_analysis_core_test.py
@@ -28,7 +28,7 @@ class TestOverlapAnalysisCore(unittest.TestCase):
 
 		self.args = args
         
-	def test_reg_overall:
+	def test_reg_overall(self):
 		
 		self.args['zone_layer_file'] = ogr.Open('./data/test_out/Overlap/Input/test_aoi.shp')
 		self.args['do_inter'] = True
@@ -38,8 +38,20 @@ class TestOverlapAnalysisCore(unittest.TestCase):
 
 		files_dict = overlap_analysis.get_files_dict(files_loc)
 
-		args['overlap_files'] = files_dict
+		self.args['overlap_files'] = files_dict
 	 
 	 	inter_table_loc = './data/test_out/Overlap/Input/inter_activ_table.csv'
 		inter_table = overlap_analysis.format_over_table(inter_table_loc)
-		args['over_layer_dict'] = inter_table
+		self.args['over_layer_dict'] = inter_table
+
+		overlap_analysis_core.execute(self.args)
+
+		#Now, using the workspace folder that we have selected, need to retrieve it and compare
+		#the two output files against hand-calculated rasters? Or somehow make raster myself.
+		
+		#In the meantime, get the created files.
+		output_dir = os.path.join(self.args['workspace'], 'Output')
+		unweighted_output = os.path.join(output_dir, 'hu_freq.tif')
+		weighted_output = os.path.join(output_dir, 'hu_impscore.tif')
+
+
