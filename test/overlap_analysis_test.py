5e22cc0ee5aa1c7478e102926a1d091cce3da26f
kathryn.glowinski <devnull@localhost>
2012-08-08 11:37:00 -0700

    Fixing spacing within the code from leftover tab vs space issues.
    Former-commit-id: e92dc7cbb88c1f28a873359df21e721294f084b7

--- test/overlap_analysis_test.py
+++ test/overlap_analysis_test.py
@@ -10,64 +10,62 @@ from invest_natcap.overlap_analysis import overlap_analysis
 
 class TestOverlapAnalysis(unittest.TestCase):
     
-    def setUp(self):
+	def setUp(self):
         
-        args = {}
-        '''
-            Still need to add:
-                
-        --Optional--
-        args['import_field']- string which corresponds to a field within the
-            layers being passed in within overlap analysis directory. This is
-            the intra-activity importance for each activity.
-        args['hum_use_hubs_loc']- URI that points to a shapefile of major hubs
-            of human activity. This would allow you to degrade the weight of
-            activity zones as they get farther away from these locations.
-        args['decay']- float between 0 and 1, representing the decay of interest
-            in areas as you get farther away from human hubs.
-        '''
-        args['workspace_dir'] = './data/overlap_analysis'
-        args['zone_layer_loc'] = './data/overlap_analysis/AOI_WCVI.shp'
-        args['do_grid'] = True
-        args['grid_size'] = 1000
-        args['overlap_data_dir_loc'] = './data/overlap_analysis/FisheriesLayers_RI'
-        args['overlap_layer_tbl'] = './data/overlap_analysis/Fisheries_Inputs.csv'
+		args = {}
+		'''Still need to add:
+				
+		--Optional--
+		args['import_field']- string which corresponds to a field within the
+			layers being passed in within overlap analysis directory. This is
+			the intra-activity importance for each activity.
+		args['hum_use_hubs_loc']- URI that points to a shapefile of major hubs
+			of human activity. This would allow you to degrade the weight of
+			activity zones as they get farther away from these locations.
+		args['decay']- float between 0 and 1, representing the decay of interest
+			in areas as you get farther away from human hubs.
+		'''
+		args['workspace_dir'] = './data/overlap_analysis'
+		args['zone_layer_loc'] = './data/overlap_analysis/AOI_WCVI.shp'
+		args['do_grid'] = True
+		args['grid_size'] = 1000
+		args['overlap_data_dir_loc'] = './data/overlap_analysis/FisheriesLayers_RI'
+		args['overlap_layer_tbl'] = './data/overlap_analysis/Fisheries_Inputs.csv'
 		args['do_inter'] = True
 		args['do_intra'] = True
 
-
         self.args = args
         
-    def test_execute(self):
+	def test_execute(self):
         
-        overlap_analysis.execute(self.args)
+		overlap_analysis.execute(self.args)
         
-        output_dir = os.path.join(self.args['workspace'], 'Output')
+		output_dir = os.path.join(self.args['workspace'], 'Output')
         
         #Want to get the file with the .tif extension, and compare it to a pre-made one.
         #The question is which to compare it to.
         
-    def test_format_over_table(self):
+	def test_format_over_table(self):
         
-        output_table = overlap_analysis.format_over_table(self.args['overlap_layer_tbl'])
+		output_table = overlap_analysis.format_over_table(self.args['overlap_layer_tbl'])
         
-        reg_overlap_table = {'CommGF_Fish': (2.00, 0), 'CommSalmonTroll_Fish': (1.50, 0),
+		reg_overlap_table = {'CommGF_Fish': (2.00, 0), 'CommSalmonTroll_Fish': (1.50, 0),
                              'CommShrimp_Fish': (1.50, 0)}
     
         #Dictionaries are not included in the self.assertAlmostEqual function, so writing
         #a quick function here to check them. This will use assertAlmostEqual inside of it.
         
-        for element in reg_overlap_table:
+		for element in reg_overlap_table:
             
-            if output_table[element]:
+			if output_table[element]:
                     
                 #Need to pull out both elements and compare, then break down so
                 #we can do an almost equal on the individual elements
-                w1, b1 = output_table[element]
-                w2, b2 = reg_overlap_table[element]
-                
-                self.assertAlmostEqual(w1, w2)
-                self.assertAlmostEqual(b1, b2)
-            
-            else:
-                self.fail("Element %s is not a key in the test-created table.", element)
+				w1, b1 = output_table[element]
+				w2, b2 = reg_overlap_table[element]
+
+				self.assertAlmostEqual(w1, w2)
+				self.assertAlmostEqual(b1, b2)
+						
+			else:
+				self.fail("Element %s is not a key in the test-created table.", element)
