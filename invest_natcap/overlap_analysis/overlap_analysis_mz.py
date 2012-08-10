ac13e38061df4afa33bf40df03822280167f20ba
kathryn.glowinski <devnull@localhost>
2012-08-10 00:28:18 -0700

    New issue
    Summary: Splitting Managament Zones and Gridded Rasters in OA
    
    Initializing new files for splitting MZ and the original OA. MZ will simply do an activity count in each of the polygons, and output a modified shapefile as a function. The other overlap analysis, which has yet to be named, will perform the original functions of overlap analysis.
    
    
    Former-commit-id: cfa7271ac28d77dd7a9ddf79b8b821f19750918c

---
+++ invest_natcap/overlap_analysis/overlap_analysis_mz.py
@@ -0,0 +1,4 @@
+import os
+import datetime
+
+from osgeo import ogr
---
+++ invest_natcap/overlap_analysis/overlap_analysis_mz_core.py
@@ -0,0 +1,9 @@
+import os
+import math
+import logging
+import operator
+import datetime
+
+from osgeo import ogr
+from osgeo import gdal
+from invest_natcap import raster_utils
