2390238f257c48fff6256d910eeffad81cfaff7e
kathryn.glowinski <devnull@localhost>
2012-08-13 14:14:59 -0700

    Update issue 962
    Making overlap_analysis unittests work, as well as creating the initial MZ core test.
    
    
    Former-commit-id: dd4ab0ef3ea66d07af56a76147a923f6a938b053

--- test/overlap_analysis_mz_core_test.py
+++ test/overlap_analysis_mz_core_test.py
@@ -2,12 +2,32 @@
 overarching test of the core module, then compare it against a pre-edited
 version.'''
 
-
 import unittest
 import os
 
+import invest_test_core
+
+from osgeo import ogr
 from invest_natcap.overlap_analysis import overlap_analysis_mz_core
+from invest_natcap.overlap_analysis import overlap_analysis_mz
 
-class TestMZCore(unittest.Testcase):
+class TestMZCore(unittest.TestCase):
 
-    def setUp
+    def setUp(self):
+        
+        args = {}
+
+        args['workspace_dir'] = './data/test_out/Overlap'
+        args['zone_layer_file'] = ogr.Open('./data/test_out/Overlap/Input/test_mz.shp')
+        
+        files_loc = './data/test_out/Overlap/Input/Test_Activity'
+
+        files_dict = overlap_analysis_mz.get_files_dict(files_loc)
+
+        args['over_layer_dict'] = files_dict
+
+        self.args = args
+    
+    def test_reg_overall(self):
+
+        overlap_analysis_mz_core.execute(self.args)
