561c53a64676fb0e22ba8e8811cdce8075b23912
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-08-21 14:34:26 -0700

    In one of my regression tests I realized I needed to set the suffix argument to pass in since we are bypassing the URI level and directly accessing the core
    Former-commit-id: 2946fca62796ce00a8065a013b2bc6feebd559dd

--- test/biodiversity_core_test.py
+++ test/biodiversity_core_test.py
@@ -284,7 +284,7 @@ class TestInvestBiodiversityCore(unittest.TestCase):
 
     def test_biodiversity_biophysical_regression(self):
         """A regression test for the biodiversity model with all possible inputs"""
-        raise SkipTest
+        #raise SkipTest
         input_dir = './data/biodiversity_regression_data/samp_input'
         out_dir = './data/biodiversity_regression_data/samp_input/output/'
         
@@ -304,7 +304,7 @@ class TestInvestBiodiversityCore(unittest.TestCase):
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
         args['half_saturation'] = 1 
-        #args['suffix'] = ''
+        args['suffix'] = ''
 
         threats_dict = {}
         threats_file = open(threats_uri)
