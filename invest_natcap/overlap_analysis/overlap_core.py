69039d3257fe66efbe0d847b58c778ea70ad2968
kathryn.glowinski <devnull@localhost>
2012-08-22 15:40:40 -0700

    Woah! Moarrrrr PEP-8. I can haz kill pylint now?
    Former-commit-id: 7a64c97a0e077e4f26be942d3b9f455f0b18c315

--- invest_natcap/overlap_analysis/overlap_core.py
+++ invest_natcap/overlap_analysis/overlap_core.py
@@ -1,34 +1,38 @@
+'''Core module for both overlap analysis and management zones. This function
+can be used by either of the secondary modules within the OA model.'''
 import glob
 import os
+
+from osgeo import ogr
 
 def get_files_dict(folder):
     '''Returns a dictionary of all .shp files in the folder.
 
         Input:
-            folder- The location of all layer files. Among these, there should be
-                files with the extension .shp. These will be used for all
+            folder- The location of all layer files. Among these, there should 
+                be files with the extension .shp. These will be used for all
                 activity calculations.
 
         Returns:
-            file_dict- A dictionary which maps the name (minus file extension) of
-                a shapefile to the open datasource itself. The key in this dictionary
-                is the name of the file (not including file path or extension), and 
-                the value is the open shapefile.
+            file_dict- A dictionary which maps the name (minus file extension) 
+                of a shapefile to the open datasource itself. The key in this
+                dictionary is the name of the file (not including file path or
+                extension), and the value is the open shapefile.
     '''
 
-    #Glob.glob gets all of the files that fall into the form .shp, and makes them
-    #into a list. Then, each item in the list is added to a dictionary as an open
-    #file with the key of it's filename without the extension, and that whole
-    #dictionary is made an argument of the mz_args dictionary
+    #Glob.glob gets all of the files that fall into the form .shp, and makes 
+    #them into a list. Then, each item in the list is added to a dictionary as
+    #an open file with the key of it's filename without the extension, and that
+    #whole dictionary is made an argument of the mz_args dictionary
     file_names = glob.glob(os.path.join(folder, '*.shp'))
     file_dict = {}
     
     for file in file_names:
         
-        #The return of os.path.split is a tuple where everything after the final slash
-        #is returned as the 'tail' in the second element of the tuple
-        #path.splitext returns a tuple such that the first element is what comes before
-        #the file extension, and the second is the extension itself 
+        #The return of os.path.split is a tuple where everything after the final
+        #slash is returned as the 'tail' in the second element of the tuple
+        #path.splitext returns a tuple such that the first element is what comes
+        #before the file extension, and the second is the extension itself 
         name = os.path.splitext(os.path.split(file)[1])[0]
         file_dict[name] = ogr.Open(file)
    
