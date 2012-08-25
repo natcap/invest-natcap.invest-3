3fdaed1b2eeb038062b7947c4110ab7f90a81bc9
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-08-25 14:31:29 -0700

    Update issue 1158
    Created files for biophysical URI and core modules. Besides a few all purpose
    imports they are empty
    
    
    Former-commit-id: cf38d64e338801b54cd11a3279f91a73e0a17c52

---
+++ invest_natcap/wind_energy/wind_energy_biophysical.py
@@ -0,0 +1,28 @@
+"""InVEST Wind Energy model file handler module"""
+import os.path
+import logging
+import csv
+
+from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
+
+from invest_natcap.wind_energy import wind_energy_core
+
+logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
+     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('wind_energy_biophysical')
+
+def execute(args):
+    """This is where the doc string lives"""
+
+    # create output folders
+
+    # handle suffix
+
+    # handle opening of relevant files
+
+    # handle any pre-processing that must be done
+
+    # call on the core module
---
+++ invest_natcap/wind_energy/wind_energy_core.py
@@ -0,0 +1,21 @@
+"""InVEST Wind_Energy model core function  module"""
+import math
+import os.path
+import logging
+
+from osgeo import gdal
+import numpy as np
+import scipy.ndimage as ndimage
+
+from invest_natcap import raster_utils
+LOGGER = logging.getLogger('wind_energy_core')
+
+def biophysical(args):
+    """This is where the doc string lives"""
+
+    # fill in skeleton below
+
+def valuation(args):
+    """This is where the doc string lives"""
+
+    # fill in skeleton below
