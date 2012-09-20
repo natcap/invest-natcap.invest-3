0ec90882ccef84de51a03e8cbd10a75ff5e30a9e
kathryn.glowinski <devnull@localhost>
2012-09-20 14:56:46 -0700

    Update issue 1230
    Docstring for calculate_exposure_value. Have not yet decided what will be passed as params into the function.
    
    At the very least, we will need an iterable for the criteria. For now, just a "for each" loop and assume that whatever we get as a param will be iterable within a for each loop. Can decide later the exact details.
    
    
    Former-commit-id: 3064bb04f8c0cfdb843abe1821d9de9366a4cd23

--- invest_natcap/habitat_risk_assessment/hra_core.py
+++ invest_natcap/habitat_risk_assessment/hra_core.py
@@ -1,3 +1,18 @@
 '''This is the core module for HRA functionality. This will perform all HRA
 calcs, and return the appropriate outputs.
 '''
+
+def calculate_exposure_value(SOME VARS):
+    '''This is the exposure value for a single criteria (i) as determined on a
+    run by run basis. The equation is as follows:
+
+    E = SUM{i=1, N} ((e{i}/(d{i}*w{i})/(1/(d{i}*w{i}))
+        
+        i = The current criteria that we are evaluating.
+        e = Exposure value for criteria i.
+        N = Total number of criteria being evaluated for the combination of
+            habitat and stressor.
+        d = Data quality rating for criteria i.
+        w = The importance weighting for that criteria relative to other
+            criteria being evaluated.
+    '''
