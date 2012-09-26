4f0c0fa29810f9b992d31e03c64330913d05643f
kathryn.glowinski <devnull@localhost>
2012-09-26 15:45:35 -0700

    Update issue 1230
    Created hra.py module. Added imports and docstrings for the execute function.
    
    
    Former-commit-id: eb33cef1adf240233d48caff643fd50c3f4f6bf3

---
+++ invest_natcap/habitat_risk_assessment/hra.py
@@ -0,0 +1,40 @@
+'''This will be the preperatory module for HRA. It will take all unprocessed
+and pre-processed data from the UI and pass it to the hra_core module.'''
+
+import os
+import shutil
+import logging
+
+from osgeo import gdal, ogr
+from invest_natcap.habitat_risk_assessment import hra_core
+
+LOGGER = logging.getLogger('HRA')
+logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
+    %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+def execute(args):
+    '''This function will prepare files passed from the UI to be sent on to the
+    hra_core module.
+
+    Input:
+        args- A python dictionary created by the UI for use in the HRA model. It
+            will contain the following data.
+        args['workspace_dir']- String which points to the directory into which
+            intermediate and output files should be placed.
+        args['habitat_dir']- The string describing a directory location of all
+            habitat shapefiles. These will be parsed though and rasterized to 
+            be passed to hra_core module.
+        args['stressors_dir']- The string describing a directory location of
+            all stressor shapefiles. Will be parsed through and rasterized
+            to be passed on to hra_core.'
+        args['ratings']- A structure which holds all exposure and consequence
+            rating for each combination of habitat and stressor. The inner
+            structure is a dictionary whose key is a tuple which points to a
+            tuple of lists which contain tuples.
+
+            {(Habitat A, Stressor 1): ([(E1Rating, E1DataQuality, E1Weight), ...],
+                                       [(C1Rating, C1DataQuality, C1Weight), ...])
+                                       .
+                                       .
+                                       . }
+    '''
