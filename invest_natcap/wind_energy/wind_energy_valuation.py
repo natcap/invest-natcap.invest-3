289c257a2146a8268661d8f160c6707c546cdff6
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-09-25 22:02:54 -0700

    Update issue 1242
    Wrote the first main doc string
    
    
    Former-commit-id: 2c64c38680abe971d9e167cc648ea588b4ee86eb

--- invest_natcap/wind_energy/wind_energy_valuation.py
+++ invest_natcap/wind_energy/wind_energy_valuation.py
@@ -15,7 +15,24 @@ logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
 LOGGER = logging.getLogger('wind_energy_valuation')
 
 def execute(args):
-    """This is where the doc string lives"""
+    """Takes care of all file handling for the valuation part of the wind
+        energy model
+    
+        args[workspace_dir] - a python string which is the uri path to where the
+            outputs will be saved (required)
+        args[biophysical_data_uri] - a uri to an OGR datasource of type point
+            from the output of the biophysical model run (required) 
+        args[turbine_info_uri] - a uri to a CSV file that has the parameters
+            for the type of turbine (required)
+        args[grid_points_uri] - a uri to a CSV file that specifies the landing
+            and grid point locations (optional)
+        args[number_of_machines] - an integer value for the number of machines
+            for the wind farm (required)
+        args[dollar_per_kWh] - a float value for the amount of dollars per
+            kilowatt hour (kWh) (required)
+   
+        returns - nothing
+    """
 
     # create output folders
 
