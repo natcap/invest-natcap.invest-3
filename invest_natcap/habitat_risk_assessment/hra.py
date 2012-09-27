40dd4d922d2f056e647138dc07881c1f76829a1c
kathryn.glowinski <devnull@localhost>
2012-09-27 12:14:38 -0700

    Update issue 1245
    Instead of having to have users search for each raster file's corresponding dictionary entry, I am placing an open copy of the datasource within the dictuonary itself as the third tuple value after consequence and exposure values.
    
    HOWEVER: Unsure about space limitations. Is this going to make a copy, or just open what's already on disk? Worry about running out of memory.
    
    Former-commit-id: 04f6dcfae2b4b4b3d678312d394ede3ffb83c414

--- invest_natcap/habitat_risk_assessment/hra.py
+++ invest_natcap/habitat_risk_assessment/hra.py
@@ -78,10 +78,18 @@ def execute(args):
 
     #INSERT WAY OF BUFFERING STRESSORS HERE
     
-    #Now, want to make all potential combinations of the rasters.
-    combine_hs_rasters(inter_dir, h_rast, s_rast)
+    #Now, want to make all potential combinations of the rasters, and add it to
+    #the structure containg data about the H-S combination.
+    ratings_with_rast = combine_hs_rasters(inter_dir, h_rast, s_rast, args['ratings'])
 
-def combine_hs_rasters(dir, h_rast, s_rast):
+    hra_args['ratings'] = ratings_with_rast
+
+def combine_hs_rasters(dir, h_rast, s_rast, ratings):
+    '''
+    
+    Returns an edited version of 'ratings' that contains an open raster
+    datasource correspondoing to the appropriate H-S key for the dictionary.
+    '''
 
     #They will be output with the form 'H[habitat_name]_S[stressor_name].tif'
     h_rast_files = glob.glob(os.path.join(h_rast, '*.tif'))
@@ -113,6 +121,14 @@ def combine_hs_rasters(dir, h_rast, s_rast):
             raster_utils.vectorize_rasters([h_dataset, s_dataset], 
                             combine_hs_pixels, raster_out_uri = out_uri,
                             datatype = gdal.GDT_Int32, nodata=[0])
+            
+            #Now place the datasource into the corresponding dictionary entry
+            #in 'ratings'. We will make the open datasource the third item in
+            #the tuple. The first two are the exposure and consequence ratings
+            #that were gleaned from the IUI.
+            ratings[(h_name, s_name)][2] = gdal.Open(out_uri)
+
+    return ratings
 
 def make_rasters(dir, file_names, grid_size):
     '''Takes a shapefile and make s rasterized version which will be used to
