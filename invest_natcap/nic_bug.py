185893f4b19ed22b100c2eed9e50c44fbb2c861e
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-10-09 14:35:30 -0700

    Update issue 1279
    Updated the script to reflect the proper inputs using the population raster.
    
    It does seem now that there is an error and discrepancy.
    
    
    Former-commit-id: 8fcdbf711fbe13217f7a80ef3dac7df4352890e7

--- invest_natcap/nic_bug.py
+++ invest_natcap/nic_bug.py
@@ -2,17 +2,16 @@ from osgeo import gdal
 from osgeo import ogr
 from osgeo import osr
 
-from invest_natcap import raster_utils
-from invest_natcap.wind_energy import wind_energy_biophysical
+import raster_utils
 
 aoi_uri = '../../invest-data/CoastalProtection/Input/AOI_BarkClay.shp'
-pop_uri = '../../invest-data/Base_Data/Marine/Land/global_polygon.shp'
+pop_uri = '../../invest-data/Base_Data/Marine/Population/global_pop/w001001.adf'
 
 aoi = ogr.Open(aoi_uri)
-pop = ogr.Open(pop_uri)
+pop = gdal.Open(pop_uri)
 
-xRes = 200
-yRes = 200
+xRes = 250
+yRes = 250
 form = gdal.GDT_Float32
 nodata = 0
 
@@ -26,37 +25,32 @@ raster_1 = raster_utils.create_raster_from_vector_extents(
 gdal.RasterizeLayer(raster_1, [1], aoi.GetLayer(), burn_values=[1])
 
 # project aoi to pop
-pop_layer = pop.GetLayer()
-pop_sr = pop_layer.GetSpatialRef()
-pop_wkt = pop_sr.ExportToWkt()
+pop_wkt = pop.GetProjection()
 
 aoi_proj = raster_utils.reproject_datasource(
         aoi, pop_wkt, './nic_aoi_to_pop.shp')
 
+raster_2 = raster_utils.create_raster_from_vector_extents(
+        .008333, .008333 , form, nodata, './nic_aoi_proj.tif', aoi_proj)
+
+gdal.RasterizeLayer(raster_2, [1], aoi_proj.GetLayer(), burn_values=[1])
+
 # clip pop from reprojected aoi
-clipped_pop = wind_energy_biophysical.clip_datasource(
-        aoi_proj, pop, './nic_pop_clipped.shp')
-
-
-print clipped_pop
+clipped_pop = raster_utils.clip_dataset(
+        pop, aoi_proj, './nic_pop_clipped.tif')
 
 # reproject clipped pop from aoi
 aoi_layer = aoi.GetLayer()
 aoi_sr = aoi_layer.GetSpatialRef()
 aoi_wkt = aoi_sr.ExportToWkt()
 
-proj_pop = raster_utils.reproject_datasource(
-        clipped_pop, aoi_wkt, './nic_pop_proj.shp')
-print proj_pop
-# use create raster from vector extents to build up a raster
-raster_2 = raster_utils.create_raster_from_vector_extents(
-        xRes, yRes, form, nodata, './nic_raster_2.tif', aoi)
+proj_pop = raster_utils.reproject_dataset(
+        clipped_pop, 250, aoi_wkt, './nic_raster_2.tif')
 
-# rasterize pop onto raster from above
-gdal.RasterizeLayer(raster_2, [1], proj_pop.GetLayer(), burn_values=[1])
+proj_aoi_raster = raster_utils.reproject_dataset(
+        raster_2, 250, aoi_wkt, './nic_raster_3.tif')
 
 raster_1 = None
-raster_2 = None
 proj_pop = None
 clipped_pop = None
 aoi_proj = None
