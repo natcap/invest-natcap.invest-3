6219f7cc20c5ff8220fb23678f16e42e9c226817
kathryn.glowinski <devnull@localhost>
2012-10-12 17:23:14 -0700

    Commits and stuff.
    Former-commit-id: 64276b769d7872bb131a074d6ec4babc66f496c9

--- test/hra_test.py
+++ test/hra_test.py
@@ -1,2 +1,24 @@
 '''This will be the test module for the non-core portion of Habitat Risk
 Assessment.'''
+
+import os
+import unittest
+import logging
+
+import invest_test_core
+
+from invest_natcap.habitat_risk_assessment import hra_core
+from invest_natcap.habitat_risk_assessment import hra
+
+LOGGER = logging.getLogger('hra_core_test')
+logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
+    %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+class TestHRACore(unittest.TestCase):
+
+    def setUp(self):
+
+        args = {}
+        args['workspace_dir'] = './data/test_out/HRA' 
+        args['habitat_dir'] = '../../HabitatRiskAssess/Input/HabitatLayers'
+        args['stressors_dir'] = '../../HabitatRiskAssess/Input'
