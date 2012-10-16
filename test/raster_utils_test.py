4711fc469cb2b6d7066f2791cc017815954521a0
Richard Sharp <richpsharp@gmail.com>
2012-10-15 23:08:08 -0700

    Update issue 1296
    Moving the reclassification test to raster_utils.
    
    
    Former-commit-id: b5ba9e5fdd5446ed122b929491d27517b780ca13

--- test/raster_utils_test.py
+++ test/raster_utils_test.py
@@ -16,6 +16,23 @@ import invest_test_core
 LOGGER = logging.getLogger('invest_core')
 
 class TestRasterUtils(unittest.TestCase):
+    def test_reclassify_dataset(self):
+        base_dir = 'data/test_out/reclassify_dataset'
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+
+        output_uri = os.path.join(base_dir, 'reclassified.tif')
+        base_uri = 'data/base_data/terrestrial/lulc_samp_cur'
+        dataset = gdal.Open(base_uri)
+        value_map = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5}
+
+        reclassified_ds = raster_utils.reclassify_dataset(
+            dataset, value_map, output_uri, gdal.GDT_Float32, -1.0)
+
+#        subprocess.Popen(["qgis", output_uri, base_uri])
+        regression_uri = 'data/reclassify_regression/reclassified.tif'
+        invest_test_core.assertTwoDatasetEqualURI(self, regression_uri, output_uri)
+
     def test_gaussian_filter(self):
         base_dir = 'data/test_out/gaussian_filter'
 
