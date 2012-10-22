feab5ec579113782221411dfcfbf28d67b4d22b9
James Douglass <jdouglass@stanford.edu>
2012-10-22 12:13:49 -0700

    Update issue 1324
    Adding a new test class (and skeleton for a setup function) for Nutrient Retention.
    
    
    Former-commit-id: 1b5c2b578a8f15313e50aaa6c3cf538e6bfa23c4

---
+++ test/nutrient_biophysical_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class NutrientBiophysicalTest(unittest.TestCase):
+    """Test class for test functions of the Nutrient retention model
+    (biophysical component)."""
+    def setUp(self):
+        pass
+
