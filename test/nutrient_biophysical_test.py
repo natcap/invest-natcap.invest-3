eafd9b9785f2604463902b45db38baf737594652
James Douglass <jdouglass@stanford.edu>
2012-10-22 12:34:20 -0700

    Update issue 1307
    Updating the args key for the flow accumulation threshold.
    
    
    Former-commit-id: e9f63e737d040c60d9f1ed03fbacd6dff3925ff7

--- test/nutrient_biophysical_test.py
+++ test/nutrient_biophysical_test.py
@@ -4,7 +4,18 @@ class NutrientBiophysicalTest(unittest.TestCase):
     """Test class for test functions of the Nutrient retention model
     (biophysical component)."""
     def setUp(self):
-        pass
+        self.args = {
+            'workspace_uri': '',
+            'dem_uri': '',
+            'pixel_yield_uri': '',
+            'landuse_uri': '',
+            'watersheds_uri': '',
+            'subwatersheds_uri': '',
+            'bio_table_uri': '',
+            'threshold_table_uri': '',
+            'nutrient_type': '',
+            'accum_threshold': 0
+        }_
 
     def test_smoke(self):
         """Smoke test for nutrient retention: biophysical"""
