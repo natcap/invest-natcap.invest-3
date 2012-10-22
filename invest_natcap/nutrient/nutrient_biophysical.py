0c7b23f183b3c941374bacbb7e2d392454268f4c
James Douglass <jdouglass@stanford.edu>
2012-10-22 12:05:12 -0700

    Update issue 1307
    Adding a skeleton execute function.
    
    
    Former-commit-id: c12f8a8edb3e7bb016cc64f3bb284ad6cd411330

--- invest_natcap/nutrient/nutrient_biophysical.py
+++ invest_natcap/nutrient/nutrient_biophysical.py
@@ -1,2 +1,12 @@
 """Module for the execution of the biophysical component of the InVEST Nutrient
 Retention model."""
+
+def execute(args):
+    """File opening layer for the InVEST nutrient retention model.
+
+        args - a python dictionary with the following entries:
+
+        returns nothing.
+    """
+
+    pass
