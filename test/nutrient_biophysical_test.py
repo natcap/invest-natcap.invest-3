755f1581a76058b9d6f12d7a79425c7c9322e8d5
James Douglass <jdouglass@stanford.edu>
2012-10-22 15:33:04 -0700

    Update issue 1324
    Adding test files and uris to these files to the nutrient biophysical test class.
    
    
    Former-commit-id: 716fec2a577ff632dc42e775f73af57eb7c37a26

--- test/nutrient_biophysical_test.py
+++ test/nutrient_biophysical_test.py
@@ -1,21 +1,33 @@
 import unittest
 
+BASE_DATA = os.path.join('data', 'base_data', 'terrestrial')
+NUTR_INPUT = os.path.join('data', 'nutrient', 'input')
+
+WORKSPACE = os.path.join('data', 'test_out', 'nutrient')
+
 class NutrientBiophysicalTest(unittest.TestCase):
     """Test class for test functions of the Nutrient retention model
     (biophysical component)."""
     def setUp(self):
         self.args = {
-            'workspace_uri': '',
-            'dem_uri': '',
-            'pixel_yield_uri': '',
-            'landuse_uri': '',
-            'watersheds_uri': '',
-            'subwatersheds_uri': '',
-            'bio_table_uri': '',
-            'threshold_table_uri': '',
-            'nutrient_type': '',
-            'accum_threshold': 0
-        }_
+            'workspace_uri': WORKSPACE,
+            'dem_uri': \
+                os.path.join(NUTR_INPUT, 'dem'),
+            'pixel_yield_uri': \
+                os.path.join(NUTR_INPUT, 'wyield.tif'),
+            'landuse_uri': \
+                os.path.join(NUTR_INPUT, 'landuse_90')
+            'watersheds_uri': \
+                os.path.join(NUTR_INPUT, 'watersheds.shp'),
+            'subwatersheds_uri': \
+                os.path.join(NUTR_INPUT, 'subwatersheds.shp'),
+            'bio_table_uri': \
+                os.path.join(NUTR_INPUT, 'biophysical_models.csv'),
+            'threshold_table_uri': \
+                os.path.join(NUTR_INPUT, 'water_purification_threshold.csv'),
+            'nutrient_type': 'nitrogen',
+            'accum_threshold': 1000
+        }
 
     def test_smoke(self):
         """Smoke test for nutrient retention: biophysical"""
