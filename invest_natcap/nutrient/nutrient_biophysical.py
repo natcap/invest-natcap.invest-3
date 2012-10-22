ea0881d4ae4bcf763f6be4f7b3e58bd87304075d
James Douglass <jdouglass@stanford.edu>
2012-10-22 12:27:34 -0700

    Update issue 1307
    Adding docstring details to the nutrient retention biophysical execute function.
    
    
    Former-commit-id: a2db33d17569b7286b8402495d458be29f0d42af

--- invest_natcap/nutrient/nutrient_biophysical.py
+++ invest_natcap/nutrient/nutrient_biophysical.py
@@ -5,6 +5,22 @@ def execute(args):
     """File opening layer for the InVEST nutrient retention model.
 
         args - a python dictionary with the following entries:
+            'workspace_dir' - a string uri pointing to the current workspace.
+            'dem_uri' - a string uri pointing to the Digital Elevation Map
+                (DEM), a GDAL raster on disk.
+            'pixel_yield_uri' - a string uri pointing to the water yield raster
+                output from the InVEST Water Yield model.
+            'landuse_uri' - a string uri pointing to the landcover GDAL raster.
+            'watersheds_uri' - a string uri pointing to an OGR shapefile on
+                disk representing the user's watersheds.
+            'subwatersheds_uri' - a string uri pointing to an OGR shapefile on
+                disk representing the user's subwatersheds.
+            'bio_table_uri' - a string uri to a supported table on disk
+                containing nutrient retention values.
+            'threshold_uri' - a string uri to a supported table on disk
+                containing water purification details.
+            'nutrient_type' - a string, either 'nitrogen' or 'phosphorus'
+            'threshold_table_uri' - a string uri to a supported table on disk.
 
         returns nothing.
     """
