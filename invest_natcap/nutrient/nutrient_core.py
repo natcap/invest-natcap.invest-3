069151846ec7b47d29ac6a3a297649e0501ac1c1
James Douglass <jdouglass@stanford.edu>
2012-10-26 11:44:06 -0700

    Update issue 1335
    Adding a new function to nutrient core to get the mean pixel value of a matrix when given a nodata value to ignore.
    
    
    Former-commit-id: 826612e638f62a7f049303bc61bf6bb855a04eff

--- invest_natcap/nutrient/nutrient_core.py
+++ invest_natcap/nutrient/nutrient_core.py
@@ -1,7 +1,14 @@
 """File for core operations of the InVEST Nutrient Retention model."""
 
+import numpy as np
+
 def biophysical(args):
     """This function executes the biophysical Nutrient Retention model.
 
         args - a python dictionary with the following entries:"""
+    print args
     pass
+
+def get_mean_pixel_value(array, nodata):
+    masked_array = np.ma.masked_array(array, array == nodata)
+    return np.mean(masked_array)
