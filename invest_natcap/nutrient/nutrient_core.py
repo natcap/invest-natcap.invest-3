d832482656a49f4049075b07b3c8e5bf47750721
James Douglass <jdouglass@stanford.edu>
2012-10-29 15:04:00 -0700

    Update issue 1335
    Adding a function to get the flow accumulation raster. Doesn't work all the way through quite yet.
    
    
    Former-commit-id: 257230dba1fc84e6eb2ce50d8cb19f17c2adfafd

--- invest_natcap/nutrient/nutrient_core.py
+++ invest_natcap/nutrient/nutrient_core.py
@@ -1,8 +1,10 @@
 """File for core operations of the InVEST Nutrient Retention model."""
 
+from osgeo import gdal
 import numpy as np
 
-from invest_natcap import raster_utils
+import invest_cython_core
+from invest_natcap import raster_utils as raster_utils
 
 def biophysical(args):
     """This function executes the biophysical Nutrient Retention model.
@@ -11,3 +13,13 @@ def biophysical(args):
     print args
     pass
 
+def get_flow_accumulation(dem):
+    # According to the OSGEO python bindings, [0, 0, None, None] should extract
+    # the entire matrix.
+    # trac.osgeo.org/gdal/browser/trunk/gdal/swig/python/osgeo/gdal.py#L767
+    bounding_box = [0, 0, None, None]
+
+    # Create a flow_direction raster for use in the flow_direction function.
+    flow_direction = raster_utils.new_raster_from_base(dem,
+        '/tmp/flow_direction', 'GTiff', -1.0, gdal.GDT_Float32)
+    invest_cython_core.flow_direction_inf(dem, bounding_box, flow_direction)
