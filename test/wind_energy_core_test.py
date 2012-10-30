67dfb5703c8252cfbdb73c5531b8c3b7ff63c0e7
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-10-30 15:11:21 -0700

    Update issue 1200
    Wrote unit test for wind energy core's distance_transform_dataset function.
    
    
    Former-commit-id: c9217578ccf5cde73ad84921fedfde6ce47219b9

--- test/wind_energy_core_test.py
+++ test/wind_energy_core_test.py
@@ -51,7 +51,56 @@ class TestInvestWindEnergyCore(unittest.TestCase):
         invest_test_core.assertTwoDatasetEqualURI(
                 self, reg_transformed_dataset_uri, out_uri)
 
+    def test_wind_energy_core_distance_transform_dataset_unit(self):
+        """A unit test for the distance_transform_dataset function"""
+        
+        output_dir = './data/test_out/wind_energy_biophysical/distance_transform_dataset/'
 
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+
+        min_dist = 40 
+        max_dist = 80
+        out_nodata = -1.0
+        out_uri = os.path.join(output_dir, 'transformed_ds_by_hand.tif')
+
+        driver = gdal.GetDriverByName('MEM')
+        dataset_type = gdal.GDT_Float32
+
+        dataset = driver.Create('', 5, 5, 1, dataset_type)
+
+        srs = osr.SpatialReference()
+        srs.SetUTM( 11, 1 )
+        srs.SetWellKnownGeogCS( 'NAD27' )
+        dataset.SetProjection( srs.ExportToWkt() )
+
+        dataset.SetGeoTransform( [444720, 30, 0, 3751320, 0, -30 ] )
+
+        raster = np.array([[-1.0,0.0,0.0,0.0,0.0],
+                           [-1.0,0.0,0.0,1.0,1.0],
+                           [0.0,0.0,1.0,1.0,1.0],
+                           [0.0,1.0,1.0,1.0,1.0],
+                           [0.0,1.0,1.0,-1.0,-1.0]])
+
+        dataset.GetRasterBand(1).WriteArray(raster)
+        dataset.GetRasterBand(1).SetNoDataValue(out_nodata)
+   
+        expected_results = np.array([[-1.0,-1.0,-1.0,-1.0,-1.0],
+                                     [-1.0,-1.0,-1.0,-1.0,-1.0],
+                                     [-1.0,-1.0,-1.0,42.4264068, 60.0],
+                                     [-1.0,-1.0,42.4264068,67.0820394,-1.0],
+                                     [-1.0,-1.0,60.0,-1.0,-1.0]])
+
+        result = wind_energy_core.distance_transform_dataset(
+                dataset, min_dist, max_dist, out_nodata, out_uri)
+
+
+        for expected, recieved in zip(expected_results, result_array):
+            for val_exp, val_rec in zip(expected, recieved):
+                self.assertAlmostEqual(val_exp, val_rec, 3)
+
+        dataset = None
+    
     def test_wind_energy_core(self):
         """Do the main run here if possible"""
         raise SkipTest
