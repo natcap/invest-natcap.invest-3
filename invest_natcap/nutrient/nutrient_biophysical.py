bdaf01effa8e311c90981b92366e4062ffd4d101
James Douglass <jdouglass@stanford.edu>
2012-11-08 12:28:09 -0800

    Update issue 1307
    Cleaning up some spacing and variable names in nutrient_biophysical.
    
    
    Former-commit-id: b8af847ed63b5a1d8ee4267857af5effea9ca6a3

--- invest_natcap/nutrient/nutrient_biophysical.py
+++ invest_natcap/nutrient/nutrient_biophysical.py
@@ -102,25 +102,28 @@ def execute(args):
     # Extract the threshold data in the threshold table and save it to the new
     # watersheds shapefile.
     watersheds_layer = biophysical_args['watersheds'].GetLayer(0)
-    new_fieldnames = [f for f in biophysical_args['threshold_table'].fieldnames
-                      if f not in ['ws_id', 'id']]
+    threshold_fields = biophysical_args['threshold_table'].fieldnames
+    new_fieldnames = [f for f in threshold_fields if f not in ['ws_id', 'id']]
     LOGGER.debug('New fields to be created in watersheds:%s', new_fieldnames)
+
+    # Create the new fieldnames in the watersheds layer
     for field_name in new_fieldnames:
         LOGGER.debug('Creating new fieldname %s', field_name)
         new_field = ogr.FieldDefn(field_name, ogr.OFTReal)
         watersheds_layer.CreateField(new_field)
 
+    # Extract the data to be saved to the new fields from the threshold table
+    # and set the field values.
     for row in biophysical_args['threshold_table']:
         watershed = watersheds_layer.GetFeature(int(row['ws_id']))
-        LOGGER.debug('Creating feature %s for row %s',watershed, row)
-
+        LOGGER.debug('Creating feature %s for row %s', watershed, row)
         for field_name in new_fieldnames:
             LOGGER.debug('Setting field %s to table value %s', field_name,
                          row[field_name])
             field_index = watershed.GetFieldIndex(field_name)
             LOGGER.debug('field %s index %s', field_name, field_index)
             watershed.SetField(field_index, str(row[field_name]))
-        biophysical_args['watersheds'].GetLayer(0).SetFeature(watershed)
+        watersheds_layer.SetFeature(watershed)
         watershed.Destroy()
 
     LOGGER.info('Copying other values for internal use')
