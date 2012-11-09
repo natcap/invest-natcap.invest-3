eb25cbb68452cc2c7ac3db458b91901443de1f90
James Douglass <jdouglass@stanford.edu>
2012-11-09 14:50:27 -0800

    Update issue 1388
    Adding the option to save the output shapefiles to specific URIs, though I haven't tested this functionality yet.
    
    
    Former-commit-id: dc52d286f6d2318d529f24801d6cbc8d5ba5fcd2

--- invest_natcap/nutrient/nutrient_core.py
+++ invest_natcap/nutrient/nutrient_core.py
@@ -403,21 +403,32 @@ def get_raster_stat_under_polygon(raster, shape, sample_layer, raster_path=None,
 
     return get_stats(temp_raster)
 
-def split_datasource(ds):
+def split_datasource(ds, uris=None):
     """Split the input OGR datasource into a list of datasources, each with a
     single layer containing a single feature.
 
         ds - an OGR datasource.
+        uris - a list of uris to where the new datasources should be saved on
+            disk.  Must have a URI for each feature in ds (even if features are
+            split across multiple layers).
 
     Returns a list of OGR datasources."""
 
+    num_features = sum([l.GetFeatureCount() for l in ds])
+    if uris == None:
+        driver_string = 'Memory'
+        uris = ['/tmp/temp_shapefile'] * num_features
+    else:
+        driver_string = 'ESRI Shapefile'
+
     LOGGER.debug('Splitting datasource into separate shapefiles')
-    ogr_driver = ogr.GetDriverByName('Memory')
+    ogr_driver = ogr.GetDriverByName(driver_string)
     output_shapefiles = []
     for layer in ds:
         for feature in layer:
             LOGGER.debug('Creating new shapefile')
-            temp_shapefile = ogr_driver.CreateDataSource('/tmp/temp_shapefile')
+            uri_index = len(output_shapefiles)
+            temp_shapefile = ogr_driver.CreateDataSource(uris[uri_index])
             temp_layer = temp_shapefile.CreateLayer('temp_shapefile',
                 layer.GetSpatialRef(), geom_type=ogr.wkbPolygon)
             temp_layer_defn = temp_layer.GetLayerDefn()
