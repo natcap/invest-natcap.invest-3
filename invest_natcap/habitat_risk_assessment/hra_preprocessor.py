7f9b9c8ab641720bfc7864295a4a7040750b5b89
richpsharp <devnull@localhost>
2012-11-13 11:31:40 -0800

    Update issue 1401
    Added docstring for hra_preprocessor.
    
    
    Former-commit-id: 6ad55bea399790addfca498f13df2fde23e27151

--- invest_natcap/habitat_risk_assessment/hra_preprocessor.py
+++ invest_natcap/habitat_risk_assessment/hra_preprocessor.py
@@ -1,5 +1,12 @@
 """Entry point for the Habitat Risk Assessment module"""
 
 def execute(args):
-    """Do stuff"""
+    """Read two directories for habitat and stressor layers and make
+        appropriate output csv files
+
+        args['workspace_dir'] - The directory to dump the output CSV files to
+        args['habitat_dir'] - A directory of ArcGIS shapefiles that are habitats
+        args['stressor_dir'] - A directory of ArcGIS shapefiles that are stressors
+
+        returns nothing"""
     pass
