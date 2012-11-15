058d5bcd6ee1bc9a595a97e428caa9f53b4a28d0
richpsharp <devnull@localhost>
2012-11-15 15:10:02 -0800

    Update issue 1411
    Draining the overlap tables in the habitat stressor overlap csv.
    
    
    Former-commit-id: ee4288305c6cd1035fdb81aae9de49a2cccf5033

--- invest_natcap/habitat_risk_assessment/hra_preprocessor.py
+++ invest_natcap/habitat_risk_assessment/hra_preprocessor.py
@@ -258,4 +258,25 @@ def parse_habitat_overlap(uri):
         while line[0] != '':
             habitat_dict['C'][line[0]] = dict(zip(headers, map(int,line[1:])))
             line = csv_reader.next()
+
+        #Drain the next two lines
+        for i in range(2): csv_reader.next()
+        #Drain the overlap dictionaries
+        #This is the overlap header
+        while True:
+            try:
+                line = csv_reader.next()
+                LOGGER.debug(line)
+                stressor = (line[0].split(hab_name+'/')[1]).split(' ')[0]
+                headers = csv_reader.next()[1:]
+                #Drain the overlap table
+                line = csv_reader.next()
+                #Drain the habitat dictionary
+                habitat_dict['C'][(hab_name,stressor)] = {}
+                while line[0] != '':
+                    habitat_dict['C'][(hab_name,stressor)][line[0]] = dict(zip(headers, map(int,line[1:])))
+                    line = csv_reader.next()
+            except StopIteration:
+                break
+
     return habitat_dict
