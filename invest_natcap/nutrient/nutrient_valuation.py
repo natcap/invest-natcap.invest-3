b0625ddd1d54a12e5705019ef0f8a61aec8cb753
James Douglass <jdouglass@stanford.edu>
2012-11-16 15:06:14 -0800

    Update issue 1418
    Modifying nutrient_valuation to only create the nutrient_value field in
    the output shapefile if the field doesn't already exist.
    
    
    Former-commit-id: f7986aa235383d5a6b249f88f5550fb64bcdddf1

--- invest_natcap/nutrient/nutrient_valuation.py
+++ invest_natcap/nutrient/nutrient_valuation.py
@@ -1,11 +1,14 @@
 
 import os
+import logging
 
 from osgeo import ogr
 
 from invest_natcap.nutrient import nutrient_biophysical
 from invest_natcap.nutrient import nutrient_core
 from invest_natcap.invest_core import fileio as fileio
+
+LOGGER = logging.getLogger('nutrient_valuation')
 
 def execute(args):
     print args
@@ -22,10 +25,18 @@ def execute(args):
 
     # Check to see if the watersheds shapefile has the nut_value field.  If not,
     # add it.
-    new_field = ogr.FieldDefn('nut_value', ogr.OFTReal)
-    watersheds_copy.GetLayer(0).CreateField(new_field)
+    value_index = watersheds_copy.GetLayer(0).GetFeature(0).GetFieldIndex('nut_value')
+    if value_index == -1:
+        # Value_index will be -1 if the field does not yet exist.  If it already
+        # exists, we should just use that one field.
+        LOGGER.debug('Making a new nut_value field in the new watersheds .shp')
+        new_field = ogr.FieldDefn('nut_value', ogr.OFTReal)
+        watersheds_copy.GetLayer(0).CreateField(new_field)
+        watersheds_copy.GetLayer(0).SyncToDisk()
+
     valuation_args['watersheds'] = watersheds_copy
 
+    LOGGER.debug('Opening the valuation table')
     valuation_table = fileio.TableHandler(args['valuation_table_uri'])
     valuation_args['valuation_table'] = valuation_table
 
