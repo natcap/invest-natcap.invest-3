214e0d23ab51e61c24c3f24789eb7f6993451d66
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-11-16 15:16:20 -0500

    Update issue 1265
    Added a test for clip_and_reproject_datasource function.
    
    Also changed some URI's and am debugging why the previous tests crash
    
    
    Former-commit-id: 0a36504d8bde2f12d11d7f112fbe0b3193719afd

--- test/wind_energy_valuation_test.py
+++ test/wind_energy_valuation_test.py
@@ -15,7 +15,7 @@ class TestWindEnergyValuation(unittest.TestCase):
             distances"""
         #raise SkipTest
 
-        output_dir = './data/test_out/wind_energy/valuation/full_land_poly/'
+        out_dir = './data/test_out/wind_energy/valuation/full_land_poly/'
         input_dir = './data/wind_energy_data/'
         regression_dir = './data/wind_energy_regression_data/'
 
@@ -40,8 +40,8 @@ class TestWindEnergyValuation(unittest.TestCase):
 
         shape_file_names = [
                 'val_aoi_proj_to_bio_points.shp', 'val_bio_points_clipped.shp', 
-                'val_bio_points_reprojected.shp', 'val_aoi_proj_to_land_poly.shp',
-                'val_land_poly_clipped.shp', 'val_land_poly_reprojected.shp']
+                'val_bio_points_projected.shp', 'val_aoi_proj_to_land_poly.shp',
+                'val_land_poly_clipped.shp', 'val_land_poly_projected.shp']
 
         raster_file_output_names = [
                 'carbon_emissions.tif', 'levelized.tif', 'npv.tif']
@@ -50,7 +50,7 @@ class TestWindEnergyValuation(unittest.TestCase):
             reg_file = os.path.join(
                     regression_dir, 'val_land_poly/' + file_name)
             out_file = os.path.join(
-                    output_dir, 'intermediate/' + file_name)
+                    out_dir, 'intermediate/' + file_name)
             invest_test_core.assertTwoShapesEqualURI(
                     self, reg_file, out_file)
 
@@ -58,15 +58,15 @@ class TestWindEnergyValuation(unittest.TestCase):
             reg_file = os.path.join(
                     regression_dir, 'val_land_poly/' + file_name)
             out_file = os.path.join(
-                    output_dir, 'output/' + file_name)
+                    out_dir, 'output/' + file_name)
             invest_test_core.assertTwoDatasetEqualURI(
                     self, reg_file, out_file)
     
     def test_wind_energy_valuation_grid(self):
         """A test for valuation module using land and grid points to compute
             distances"""
-        #raise SkipTest
-        output_dir = './data/test_out/wind_energy/valuation/full_grid/'
+        raise SkipTest
+        out_dir = './data/test_out/wind_energy/valuation/full_grid/'
         input_dir = './data/wind_energy_data/'
         regression_dir = './data/wind_energy_regression_data/'
 
@@ -91,13 +91,13 @@ class TestWindEnergyValuation(unittest.TestCase):
 
         shape_file_names = [
                 'val_aoi_proj_to_bio_points.shp', 
-                'val_aoi_proj_to_grid_points.shp', 
-                'val_aoi_proj_to_land_points.shp',
+                'val_aoi_proj_to_grid_point.shp', 
+                'val_aoi_proj_to_land_point.shp',
                 'val_bio_points_clipped.shp', 'val_grid_points.shp',
                 'val_land_points.shp', 'val_grid_point_clipped.shp',
-                'val_grid_point_reprojected.shp', 'val_land_point_clipped.shp',
-                'val_land_point_reprojected.shp', 
-                'val_bio_points_reprojected.shp']
+                'val_grid_point_projected.shp', 'val_land_point_clipped.shp',
+                'val_land_point_projected.shp', 
+                'val_bio_points_projected.shp']
 
         raster_file_output_names = [
                 'carbon_emissions.tif', 'levelized.tif', 'npv.tif']
@@ -106,7 +106,7 @@ class TestWindEnergyValuation(unittest.TestCase):
             reg_file = os.path.join(
                     regression_dir, 'val_grid/' + file_name)
             out_file = os.path.join(
-                    output_dir, 'intermediate/' + file_name)
+                    out_dir, 'intermediate/' + file_name)
             invest_test_core.assertTwoShapesEqualURI(
                     self, reg_file, out_file)
 
@@ -114,6 +114,34 @@ class TestWindEnergyValuation(unittest.TestCase):
             reg_file = os.path.join(
                     regression_dir, 'val_grid/' + file_name)
             out_file = os.path.join(
-                    output_dir, 'output/' + file_name)
+                    out_dir, 'output/' + file_name)
             invest_test_core.assertTwoDatasetEqualURI(
                     self, reg_file, out_file)
+    
+    def test_wind_energy_valuation_clip_and_reproject_datasource(self):
+        """A regression test for clipping and reprojecting a datasource"""
+
+        raise SkipTest
+        out_dir = './data/test_out/wind_energy/valuation/clip_and_reproject/'
+        input_dir = './data/wind_energy_data/'
+        regression_dir = './data/wind_energy_regression_data/'
+
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        aoi = ogr.Open(os.path.join(input_dir, 'reprojected_distance_aoi.shp'))
+        dsource = ogr.Open(os.path.join(regression_dir, 'global_poly_clip.shp'))
+        regression_file_uri = os.path.join(
+                regression_dir, 'projected_land_poly.shp')
+        out_uri = os.path.join(out_dir, 'dsource')
+
+        result = wind_energy_valuation.clip_and_reproject_datasource(
+                dsource, aoi, out_uri)
+
+        result = None
+
+        test_uri = os.path.join(out_dir, 'val_dsource_projected.shp')
+
+        invest_test_core.assertTwoShapesEqualURI(
+                self, regression_file_uri, test_uri)
+
