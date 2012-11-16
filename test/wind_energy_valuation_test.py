d6509f278cc0276ce9940221dcc51c7f5a57d40c
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2012-11-16 13:58:53 -0500

    Update issue 1265
    Completed 2 main tests, one using land polygon and one using grid. I suspect
    the outputs will change and I"ll have to shuffle files, but everything is set
    up.
    
    
    Former-commit-id: b74fc81bfa6b7e83ed06dc2a99bf522ee9572668

--- test/wind_energy_valuation_test.py
+++ test/wind_energy_valuation_test.py
@@ -10,10 +10,12 @@ from invest_natcap.wind_energy import wind_energy_valuation
 import invest_test_core
 
 class TestWindEnergyValuation(unittest.TestCase):
-    def test_wind_energy_valuation(self):
-        """A smoke test to make sure valuation does not crash with default
-            input"""
-        out_dir = './data/test_out/wind_energy/valuation/'
+    def test_wind_energy_valuation_land(self):
+        """A test for the valuation module using land polygon to get
+            distances"""
+        #raise SkipTest
+
+        output_dir = './data/test_out/wind_energy/valuation/full_land_poly/'
         input_dir = './data/wind_energy_data/'
         regression_dir = './data/wind_energy_regression_data/'
 
@@ -28,8 +30,6 @@ class TestWindEnergyValuation(unittest.TestCase):
                 regression_dir, 'wind_points_reprojected.shp')
         args['turbine_info_uri'] = os.path.join(
                 input_dir, 'turbine_parameters.csv')
-        #args['grid_points_uri'] = os.path.join(
-        #        input_dir, 'land_grid_points.csv')
         args['land_polygon_uri'] = os.path.join(
                 regression_dir, 'global_poly_clip.shp')
         args['number_of_machines'] = 30
@@ -38,4 +38,82 @@ class TestWindEnergyValuation(unittest.TestCase):
         
         wind_energy_valuation.execute(args)
 
-        # start making up some tests
+        shape_file_names = [
+                'val_aoi_proj_to_bio_points.shp', 'val_bio_points_clipped.shp', 
+                'val_bio_points_reprojected.shp', 'val_aoi_proj_to_land_poly.shp',
+                'val_land_poly_clipped.shp', 'val_land_poly_reprojected.shp']
+
+        raster_file_output_names = [
+                'carbon_emissions.tif', 'levelized.tif', 'npv.tif']
+
+        for file_name in shape_file_names:
+            reg_file = os.path.join(
+                    regression_dir, 'val_land_poly/' + file_name)
+            out_file = os.path.join(
+                    output_dir, 'intermediate/' + file_name)
+            invest_test_core.assertTwoShapesEqualURI(
+                    self, reg_file, out_file)
+
+        for file_name in raster_file_output_names:
+            reg_file = os.path.join(
+                    regression_dir, 'val_land_poly/' + file_name)
+            out_file = os.path.join(
+                    output_dir, 'output/' + file_name)
+            invest_test_core.assertTwoDatasetEqualURI(
+                    self, reg_file, out_file)
+    
+    def test_wind_energy_valuation_grid(self):
+        """A test for valuation module using land and grid points to compute
+            distances"""
+        #raise SkipTest
+        output_dir = './data/test_out/wind_energy/valuation/full_grid/'
+        input_dir = './data/wind_energy_data/'
+        regression_dir = './data/wind_energy_regression_data/'
+
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        args = {}
+        args['workspace_dir'] = out_dir
+        args['aoi_uri'] = os.path.join(
+                input_dir, 'reprojected_distance_aoi.shp')
+        args['biophysical_data_uri'] = os.path.join(
+                regression_dir, 'wind_points_reprojected.shp')
+        args['turbine_info_uri'] = os.path.join(
+                input_dir, 'turbine_parameters.csv')
+        args['grid_points_uri'] = os.path.join(
+                input_dir, 'land_grid_points.csv')
+        args['number_of_machines'] = 30
+        args['dollar_per_kWh'] = 1.81
+        args['suffix'] = ''
+        
+        wind_energy_valuation.execute(args)
+
+        shape_file_names = [
+                'val_aoi_proj_to_bio_points.shp', 
+                'val_aoi_proj_to_grid_points.shp', 
+                'val_aoi_proj_to_land_points.shp',
+                'val_bio_points_clipped.shp', 'val_grid_points.shp',
+                'val_land_points.shp', 'val_grid_point_clipped.shp',
+                'val_grid_point_reprojected.shp', 'val_land_point_clipped.shp',
+                'val_land_point_reprojected.shp', 
+                'val_bio_points_reprojected.shp']
+
+        raster_file_output_names = [
+                'carbon_emissions.tif', 'levelized.tif', 'npv.tif']
+
+        for file_name in shape_file_names:
+            reg_file = os.path.join(
+                    regression_dir, 'val_grid/' + file_name)
+            out_file = os.path.join(
+                    output_dir, 'intermediate/' + file_name)
+            invest_test_core.assertTwoShapesEqualURI(
+                    self, reg_file, out_file)
+
+        for file_name in raster_file_output_names:
+            reg_file = os.path.join(
+                    regression_dir, 'val_grid/' + file_name)
+            out_file = os.path.join(
+                    output_dir, 'output/' + file_name)
+            invest_test_core.assertTwoDatasetEqualURI(
+                    self, reg_file, out_file)
