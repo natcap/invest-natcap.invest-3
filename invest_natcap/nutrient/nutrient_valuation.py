a51305c1d25cdba0d8d8501edbb8133ff5b9cabd
James Douglass <jdouglass@stanford.edu>
2012-11-16 14:32:40 -0800

    Update issue 1418
    Nutrient_valuation now makes a new copy of the input watersheds
    shapefile in the workspace.  I know this is a little redundant, but it
    at least ensures that _if_ the user uses a separate workspace for the
    valuation step from the biphysical step, the watersheds shapefile is
    saved to the valuation workspace.
    
    
    Former-commit-id: 907faea5ff5c21e96d417f208b0cfec9d70cbb00

--- invest_natcap/nutrient/nutrient_valuation.py
+++ invest_natcap/nutrient/nutrient_valuation.py
@@ -1,6 +1,9 @@
 
+import os
+
 from osgeo import ogr
 
+from invest_natcap.nutrient import nutrient_biophysical
 from invest_natcap.nutrient import nutrient_core
 from invest_natcap.invest_core import fileio as fileio
 
@@ -8,13 +11,20 @@ def execute(args):
     print args
 
     valuation_args = {}
-    watersheds = ogr.Open(args['watersheds_uri'])
+    watersheds = ogr.Open(args['watersheds_uri'], 1)
+
+    ogr_driver = ogr.GetDriverByName('ESRI Shapefile')
+    shapefile_folder = os.path.join(args['workspace_dir'], 'watersheds_value')
+    nutrient_biophysical.make_folder(shapefile_folder)
+
+    copy_uri = os.path.join(args['workspace_dir'], 'watersheds_value')
+    watersheds_copy = ogr_driver.CopyDataSource(watersheds, copy_uri)
 
     # Check to see if the watersheds shapefile has the nut_value field.  If not,
     # add it.
     new_field = ogr.FieldDefn('nut_value', ogr.OFTReal)
-    watersheds.GetLayer(0).CreateField(new_field)
-    valuation_args['watersheds'] = watersheds
+    watersheds_copy.GetLayer(0).CreateField(new_field)
+    valuation_args['watersheds'] = watersheds_copy
 
     valuation_table = fileio.TableHandler(args['valuation_table_uri'])
     valuation_args['valuation_table'] = valuation_table
