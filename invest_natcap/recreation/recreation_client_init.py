0db4ac5cd13c319ee8c5cd2e7ad60040252368ba
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2012-11-30 13:53:52 -0800

    See issue 1484. Added projection check.
    Former-commit-id: 67afa72e135205f0e987fe03bd441f3bb2c516a6

--- invest_natcap/recreation/recreation_client_init.py
+++ invest_natcap/recreation/recreation_client_init.py
@@ -47,6 +47,18 @@ def execute(args):
         LOGGER.debug("File %s is missing." % aoiFileNamePRJ)
         LOGGER.error("The shapefile must have a PRJ file.")
         raise IOError, "Missing PRJ file."
+    else:
+        WGS84=['GEOGCS["GCS_WGS_1984",DATUM["WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]',
+               'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]',
+               'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]']
+        aoiPRJ=open(aoiFileNamePRJ)
+        if aoiPRJ.read() in WGS84:
+            LOGGER.info("AOI is in WGS84.")
+            aoiPRJ.close()
+        else:
+            aoiPRJ.close()
+            LOGGER.error("AOI is NOT in WGS84.")
+            raise ValueError, ("AOI must be in WGS84!")
 
     #scanning data directory for shapefiles
     LOGGER.info("Processing predictors.")
@@ -60,6 +72,14 @@ def execute(args):
                 os.path.exists(args["data_dir"]+fileName+".shp") and \
                 os.path.exists(args["data_dir"]+fileName+".prj"):
                     LOGGER.info("Found %s predictor." % (fileName))
+                    aoiPRJ=open(args["data_dir"]+fileName+".prj")
+                    if aoiPRJ.read() in WGS84:
+                        LOGGER.info("%s is in WGS84." % fileName)
+                        aoiPRJ.close()
+                    else:
+                        aoiPRJ.close()
+                        LOGGER.error("%s is NOT in WGS84." % fileName)
+                        raise ValueError, ("%s must be in WGS84!" % fileName)
                     predictors.append(fileName)
                 else:
                     LOGGER.error("Predictor %s is missing file(s)." % (fileName))
