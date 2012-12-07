969aa74621014c7a56219671750586d359f69b70
James Douglass <jdouglass@stanford.edu>
2012-12-07 14:26:40 -0800

    Update issue 1503
    Test what happens when an unexpected linear units string is provided
    (correct answer: an error message should be produced).
    
    
    Former-commit-id: 34c008c2aa5d99008c3ac4263fc8d8e85d707eb8

--- test/iui_validator_test.py
+++ test/iui_validator_test.py
@@ -165,6 +165,14 @@ class OGRCheckerTester(CheckerTester):
         self.validate_as.update(updates)
         self.assertNoError()
 
+        # Verify that if the JSON definition requires a projection that we don't
+        # recognize in validation's known_units dictionary.
+        updates = {'layers': [{'name': 'mn',
+                               'projection': {'units': 'SOMETHING!'}}],
+                   'value': TEST_DATA + '/iui/validation/mn.shp'}
+        self.validate_as.update(updates)
+        self.assertError()
+
         # Check that the layer is projected.
         updates = {'layers': [{'name': 'harv_samp_cur',
                                'projection': {'exists': True}}],
