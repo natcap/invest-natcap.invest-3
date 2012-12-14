0ccb1333796c1bdc03b5cd7cb0803f247aeef8eb
kathryn.glowinski <devnull@localhost>
2012-12-14 13:03:22 -0800

    Update issue 1230
    Ridiculous large commit. Rejiggered the E calc so that it pre-rasterizes each criteria, then adds them to a list to be vectorized a second time with all of the other E-specific rasters. Still need to do the same thing for C calc, except over two dictionaries, rather than just stressors.
    
    
    Former-commit-id: 126137545f5e2434d63d975ff1d006977388374d

--- invest_natcap/habitat_risk_assessment/new_hra_core.py
+++ invest_natcap/habitat_risk_assessment/new_hra_core.py
@@ -73,8 +73,13 @@ def make_risk_rasters(dir, h_s, hab, stress, risk_eq):
         
         h, s = pair
        
-        e_out_path = os.path.join(out_dir, h + s + 'E.tif')
-        c_out_path = os.path.join(out_dir, h + s + 'C.tif')
+        #e_out_path = os.path.join(out_dir, h + s + 'E.tif')
+        #c_out_path = os.path.join(out_dir, h + s + 'C.tif')
+        
+        #Let's try throwing everything (criteria that are E/C burned as
+        #well as the overarching E/C into one file within the out_dir folder.
+        e_out_dir = os.path.join(out_dir, h + '_' +  s)
+        c_out_path = os.path.join(out_dir, h + '_' + s)
 
         #E and C are both raster burned spatially explicit versions of E
         E = calc_E_value(h_s[pair]['DS'], stress[s], denoms[pair]['E'], e_out_path)
@@ -118,12 +123,13 @@ def calc_E_value(ds, s_dicts, denom, out_path):
         ds- The base (h, s) overlap raster which we should use for rasterizing
             the numeric ratings data for combination with the raster ratings data.
         s_dicts- A subdictionary containing two dictionaries 'Crit_Ratings' and
-            'Crit_Rasters' that contain all criteria rating data.
+            'Crit_Rasters' that contain all criteria rating data for the specific
+            stressor being examined. 
         denom- The denominator for the E calculation equation. This should be
             applied after all of the E numerator equations are summed.
-        out_path- The name (directory and filename) that should be used as the
-            URI when vectorizing the E raster.
-    '''
+        out_path- The name (directory) that should be used to hold the E burned
+            criteria, as well as the final E raster value for that pairing of H-S'''
+    e_out_uri = os.path.join(out_path, 'E_Raster.tif'        
     new_ds = raster_utils.new_raster_from_base(ds, out_path, 'GTiff', 0,
                                 gdal.GDT_Float32)
     band, nodata = raster_utils.extract_band_and_nodata(new_ds)
@@ -138,6 +144,31 @@ def calc_E_value(ds, s_dicts, denom, out_path):
 
         e_num += r / (dq * w)
 
+    raster_list = []
+    #Want to re-rasterize each criteria with the r/dq * w value, then throw it
+    #into a list so that they all can be combined on top of one another, and
+    #divided by denom
+    for crit in s_dicts['Crit_Rasters']:
+
+        indiv_crit_uri = os.path.join(out_path, crit + '_E.tif')
+        dataset = raster_utils.new_raster_from_base(crit['DS'], indiv_crit_uri, 
+                            'GTiff', 0, gdal.GDT_Float32)
+        band, nodata = raster_utils.extract_band_and_nodata(dataset)
+        band.Fill(nodata)
+
+        crit_array = crit['DS'].GetRasterBand(1).ReadAsArray()
+
+        dq = crit['DQ']
+        w = crit['Weight']
+        bottom = dq * w
+
+        #We should be burning an array with this criteria's r/dq*w here.
+        new_array = crit_array / bottom
+        band.WriteArray(new_array)
+        
+        #Add to stack so that they all can be added together.
+        raster_list.append(dataset)
+    
     def e_combine(*pixels):
         
         p_sum = o
@@ -145,15 +176,73 @@ def calc_E_value(ds, s_dicts, denom, out_path):
         for p in pixels:
             sum += p
         p_sum += e_num
-
+        
+        #Denom is pre-calculated, and represents the bottom half of the E/C
+        #calculation equation. This allowed us to get all numerator rasters together
+        #before completing the equation. 
         return e_num / denom
 
+    #vectorize using the raster band as the base, and all potential rasters on top of that.
+    #The equation itself will add in the non-raster criteria to the numerator sum as long
+    # as we use e-combine 
+
+def calc_C_value(ds, h_s_dicts, h_dicts, denom, out_path):
+    '''This will take in the habitat-stressor and habitat subdictionaries
+    for the given h,s pair and calculate a "C raster" based on the numerical 
+    criteria and the raster criteria. This will be combined with the E raster 
+    according to the risk equation, and allow us to return a final risk raster. 
+
+    Input:
+        ds- The base (h, s) overlap raster which we should use for rasterizing
+            the numeric ratings data for combination with the raster ratings data.
+        h_s_dicts- A subdictionary containing two dictionaries 'Crit_Ratings' and
+            'Crit_Rasters' that contain all criteria rating data related to the
+            specific overlap of this habitat and stressor.
+        h_dicts- A subdictionary containing two dictionaries 'Crit_Ratings' and
+            'Crit_Rasters' that contain all criteria rating data related to the
+            specific habitat being examined.
+        denom- The denominator for the E calculation equation. This should be
+            applied after all of the E numerator equations are summed.
+        out_path- The name (directory and filename) that should be used as the
+            URI when vectorizing the E raster.
+    '''
+    new_ds = raster_utils.new_raster_from_base(ds, out_path, 'GTiff', 0,
+                                gdal.GDT_Float32)
+    band, nodata = raster_utils.extract_band_and_nodata(new_ds)
+    band.Fill(nodata)
+
+    #We first want to get the rating information for all non-spatially explicit
+    #rasters so that it can be summed along with the raster criteria.
+    c_num = 0
+    for dict in (h_s_dicts['Crit_Ratings'], h_dicts['Crit_Ratings']):
+        for crit in dict:
+        
+            dq = crit['DQ']
+            w = crit['Weight']
+            r = crit['Rating']
+
+            e_num += r / (dq * w)
+
+    def e_combine(*pixels):
+        
+        p_sum = o
+
+        for p in pixels:
+            sum += p
+        p_sum += c_num
+        
+        #Denom is pre-calculated, and represents the bottom half of the E/C
+        #calculation equation. This allowed us to get all numerator rasters together
+        #before completing the equation. 
+        return p_sum / denom
+
+    #First need to do a vectorize on every criteria within crit rasters to get it to
+    #r/dq *w
     raster_list = []
     for crit in s_dicts['Crit_Rasters']:
         raster_list.append(crit['DS'])
     
     #vectorize using the raster band as the base, and all potential rasters on top of that.
-
 def make_risk_mult(band, E, C, out_URI):
     ''' This uses the multiplicative value of risk to produce a final risk
     raster based off of the appropriate E and C rasters for that h-s combination.
