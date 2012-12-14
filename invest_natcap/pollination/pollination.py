a9f3085451d0a444b5c8f004df1639cf1be4d267
James Douglass <jdouglass@stanford.edu>
2012-12-14 15:35:06 -0800

    Update issue 1376
    Adding a skeleton pollination URI layer that will eventually call down
    to the pollination core.
    
    
    Former-commit-id: a586e619052d30cc8d0b295911e2c027b457a4ed

---
+++ invest_natcap/pollination/pollination.py
@@ -0,0 +1,10 @@
+
+import logging
+
+logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
+     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('pollination)
+
+def execute(args):
+    pass
