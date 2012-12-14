018ffb012de274e32c1cce8f801031571131de6d
James Douglass <jdouglass@stanford.edu>
2012-12-14 15:37:17 -0800

    Update issue 1376
    Starting to add URI layer stuff to pollination.py.
    
    
    Former-commit-id: adeabd5ea1cc171c1d1f7d471bf3ee2f8a16f43b

--- invest_natcap/pollination/pollination.py
+++ invest_natcap/pollination/pollination.py
@@ -7,4 +7,27 @@ logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
 LOGGER = logging.getLogger('pollination)
 
 def execute(args):
-    pass
+    workspace = args['workspace_dir']
+
+    # If the user has not provided a results suffix, assume it to be an empty
+    # string.
+    try:
+        suffix = args['results_suffix']
+    except KeyError:
+        suffix = ''
+
+    # Check to see if each of the workspace folders exists.  If not, create the
+    # folder in the filesystem.
+    inter_dir = os.path.join(workspace, 'intermediate')
+    out_dir = os.path.join(workspace, 'output')
+
+    for folder in [inter_dir, out_dir]:
+        if not os.path.isdir(folder):
+            os.makedirs(folder)
+
+    # Determine which land cover scenarios we should run, and append the
+    # appropriate suffix to the landuser_scenarios list as necessary for the
+    # scenario.
+    landuse_scenarios = ['cur']
+    if 'landuse_fut_uri' in args:
+        landuse_scenarios.append('fut')
