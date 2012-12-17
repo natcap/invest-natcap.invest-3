38fce95e14a137d9ca1d02ff3ffea6067ffc522e
James Douglass <jdouglass@stanford.edu>
2012-12-17 12:02:43 -0800

    Update issue 1316
    Adding the rest of a functional test that will eventually verify if the
    contents of the container are, in fact, disabled when appropriate.  This
    currently just prints the ExecRoot element, but it at least demonstrates
    that the whole UI has been built given the target config file.
    
    
    Former-commit-id: 9898ea4cd0c899f4a1ded51b9808cc579eed59a2

--- test/iui_test.py
+++ test/iui_test.py
@@ -1,6 +1,10 @@
 import unittest
 import os.path
 
+from PyQt4 import QtGui
+
+from invest_natcap.iui import base_widgets as base_widgets
+
 JSON_DIR = os.path.join('data', 'iui', 'sample_json')
 
 class ContainerTest(unittest.TestCase):
@@ -8,4 +12,8 @@ class ContainerTest(unittest.TestCase):
         container_test = os.path.join(JSON_DIR, 'test_container.json')
 
         # NEED TO FINISH THIS CALL!
-        self.ui = base_widgets.ExecRoot()
+        self.app = QtGui.QApplication([])
+        self.ui = base_widgets.ExecRoot(container_test)
+
+    def test_outside_element_toglling_container(self):
+        print self.ui
