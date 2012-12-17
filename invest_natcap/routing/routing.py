289b8aafedfcbf48ec703b536699db2f3682a46e
richpsharp <devnull@localhost>
2012-12-17 14:28:19 -0800

    Update issue 1526
    Formal definition of the calculate_routing dataset.
    
    
    Former-commit-id: f6029112254e08b6326321cd35d4c64da60bd3a3

--- invest_natcap/routing/routing.py
+++ invest_natcap/routing/routing.py
@@ -24,3 +24,35 @@
         (index) -> (index div n_columns, index mod n_columns) where 'div' is 
             the integer division operator and 'mod' is the integer remainder
             operation."""
+
+
+def calculate_routing(
+    dem_uri, dataset_uri_list, op, workspace_dir, raster_out_uri,
+    out_nodata_value, aoi_uri = None):
+
+    """This function will route flux across a landscape given a dem to
+        guide flow from a d-infinty flow algorithm, and a custom function
+        that will operate on input flux and other user defined arguments
+        to determine nodal output flux.
+
+        dem_uri - a URI to a DEM raster
+        dataset_uri_list - a list of GDAL readable rasters that will be
+            used as input to the 'op' operation.  These datasets
+            must be aligned with themselves and dem_uri.
+        op - a len(dataset_uri_list)+1 function that operates per pixel
+            on the aligned pixel stack from dataset_uri_list where the
+            first input is the incoming flux from other contributing 
+            nodes.
+        workspace_dir - a directory that can be used for intermediate
+            file generation.  The contents of this directory are not safe
+            for preseveration if files already exist in it.
+        raster_out_uri - the name/uri of the output raster?  TODO: WHAT OUTPUTS?
+        out_nodata_value - the nodata value of the ouput raster TODO: RASTERS?
+        aoi_uri - an OGR datasource for an area of interest polygon.
+            the routing flux calculation will only occur on those pixels
+            and neighboring pixels will either be raw outlets or 
+            non-contibuting inputs depending on the orientation of the DEM.
+
+        returns nothing"""
+
+    pass
