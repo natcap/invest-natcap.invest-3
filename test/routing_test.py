eccec1c14b2604f55a2d8b2820f88af17eff426d
richpsharp <devnull@localhost>
2012-12-17 16:14:40 -0800

    Update issue 1526
    Adding a nosetest module for calculate_routing.
    
    
    Former-commit-id: ee9795930789fc3cbf79d45d1c4f9ad5ef81352c

---
+++ test/routing_test.py
@@ -0,0 +1,28 @@
+"""URI level tests for the sediment biophysical module"""
+
+import unittest
+import os
+import subprocess
+import logging
+
+from osgeo import gdal
+from nose.plugins.skip import SkipTest
+import numpy
+
+from invest_natcap.routing import routing
+import invest_test_core
+
+LOGGER = logging.getLogger('routing_test')
+
+class TestRasterUtils(unittest.TestCase):
+    def test_smoke_routing(self):
+        base_dir = 'data/test_out/routing_test'
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+
+        output_uri = os.path.join(base_dir, 'out.tif')
+        dem_uri = 'data/sediment_test_data/dem'
+        aoi_uri = 'data/sediment_test_data/watersheds.shp'
+
+        out_nodata = -1.0
+        routing.calculate_routing(dem_uri, [], lambda x: x, base_dir, output_uri, out_nodata, aoi_uri = aoi_uri)
