b277272d137a7f55ba7bb5e73b429c4809d5db5d
Richard Sharp <richpsharp@gmail.com>
2013-01-16 00:04:59 -0800

    Update issue 1149
    Since the modern invest API is all URI based, it makes sense to move a lot of what used to be in the core into the main biophysical part.  This calculates all the flow accumulation up to the ls factor which needs the flow direction.
    
    
    Former-commit-id: b2d880b85591fc5efeb340ae009e907be799606b

--- invest_natcap/sediment/sediment_biophysical.py
+++ invest_natcap/sediment/sediment_biophysical.py
@@ -10,7 +10,9 @@ from osgeo import gdal
 from osgeo import ogr
 
 from invest_natcap import raster_utils
+from invest_natcap.routing import routing_utils
 from invest_natcap.sediment import sediment_core
+
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -65,6 +67,53 @@ def execute(args):
         if not os.path.exists(directory):
             LOGGER.debug('creating directory %s', directory)
             os.makedirs(directory)
+
+
+    dem_dataset = gdal.Open(args['dem_uri'])
+    n_rows = dem_dataset.RasterYSize
+    n_cols = dem_dataset.RasterXSize
+    
+    #Calculate slope
+    LOGGER.info("Calculating slope")
+    slope_uri = os.path.join(intermediate_dir, 'slope.tif')
+    slope_dataset = raster_utils.calculate_slope(dem_dataset, slope_uri)
+
+    #Calcualte flow accumulation
+    LOGGER.info("calculating flow accumulation")
+    flow_accumulation_uri = os.path.join(intermediate_dir, 'flow_accumulation.tif')
+    routing_utils.flow_accumulation(args['dem_uri'], flow_accumulation_uri)
+
+    #classify streams from the flow accumulation raster
+    LOGGER.info("Classifying streams from flow accumulation raster")
+    v_stream_uri = os.path.join(intermediate_dir, 'v_stream.tif')
+
+    stream_dataset = routing_utils.stream_threshold(flow_accumulation_uri,
+        float(args['threshold_flow_accumulation']), v_stream_uri)
+
+    #Calculate LS term
+    usle_nodata = -1.0
+    ls_dataset = sediment_core.calculate_ls_factor(flow_accumulation_uri, slope_dataset,
+        args['flow_direction'], args['ls_uri'], usle_nodata)
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     LOGGER.info('Loading data sources')
 
