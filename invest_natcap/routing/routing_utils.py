eb2f5178b017cc1851fcdbc43f49cb2257542466
richpsharp <devnull@localhost>
2013-01-22 09:52:23 -0800

    Update issue 199
    Making a prototype of the percent to sink function to calculate how much load from a single pixel reaches the stream.
    
    
    Former-commit-id: c6f95eca466381e3fecc52785bd7c970304e964f

--- invest_natcap/routing/routing_utils.py
+++ invest_natcap/routing/routing_utils.py
@@ -190,3 +190,21 @@ def calculate_flow_length(flow_direction_uri, flow_length_uri):
         [flow_direction_dataset], flow_length, aoi=None,
         raster_out_uri=flow_length_uri, datatype=gdal.GDT_Float32,
         nodata=flow_length_nodata)
+
+
+def percent_to_sink(sink_pixels_uri, absorption_rate_uri, effect_uri):
+    """This function calculates the amount of load from a single pixel
+        to the source pixels given the percent absorption rate per pixel.
+        
+        sink_pixels_uri - the pixels of interest that will receive flux.
+            This may be a set of stream pixels, or a single pixel at a
+            watershed outlet.
+
+        absorption_rate_uri - a GDAL floating point dataset that has a percent
+            of flux absorbed per pixel
+
+        effect_uri - the output GDAL dataset that shows the percent of flux
+            eminating per pixel that will reach any sink pixel
+
+        returns nothing"""
+    pass
