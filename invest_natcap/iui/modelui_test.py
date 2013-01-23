383d66397f1ea75a66c7f1cc27e2e10c41c8ac1b
James Douglass <jdouglass@stanford.edu>
2013-01-23 14:36:39 -0800

    Update issue 1578
    Adding a skeleton framework for setting up a standard modelUI instance and
    setting the workspace value to something we want, like a predefined
    test workspace.  The only assertion that happens right now is a check to
    verify that there is no validation error once we set the workspace folder
    to a new value.
    
    
    Former-commit-id: 9b42aee79b1daa39d7e4552e730edc59fe3409f8

---
+++ invest_natcap/iui/modelui_test.py
@@ -0,0 +1,60 @@
+import unittest
+import sys
+from PyQt4 import QtGui
+from PyQt4.QtTest import QTest
+import modelui
+import shutil
+import os
+
+TEST_WORKSPACE = '/tmp/test_workspace'
+
+# Define a new exception class for when the worspace args_id is not found in the
+# json definition
+class WorkspaceNotFound(Exception): pass
+
+def locate_workspace_element(ui):
+    for element_id, element in ui.allElements.iteritems():
+        try:
+            args_id = element.attributes['args_id']
+        except KeyError:
+            # A KeyError is triggered when the element doesn't have an args_id.
+            pass
+
+        # If the retrieved arg id is the definde workspace, return the element.
+        if args_id == 'workspace_dir':
+            return element
+
+    # If we can't find the workspace dir element, raise a helpful exception
+    # indicating that it must be clearly identifiable.
+    raise WorkspaceNotFound('The workspace must be identified by the '
+        'args_id "workspace_dir"')
+
+class ModelUITest(unittest.TestCase):
+    def setUp(self):
+        self.app = QtGui.QApplication(sys.argv)
+
+    def tearDown(self):
+        try:
+            # Remove the workspace directory for the next test.
+            shutil.rmtree(TEST_WORKSPACE)
+        except OSError:
+            # Thrown when there's no workspace to remove.
+            pass
+
+    def test_pollination(self):
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(file_path, 'pollination.json')
+        model_ui = modelui.ModelUI(file_path, True)
+
+        workspace_element = locate_workspace_element(model_ui)
+
+        workspace_element.setValue(TEST_WORKSPACE)
+        QTest.qWait(100)  # Wait for the test workspace to validate
+
+        # Assert that the test workspace didn't get a validation error.
+        self.assertEqual(workspace_element.has_error(), False)
+
+if __name__ == '__main__':
+    # This call to unittest.main() runs all unittests in this test suite, much
+    # as we would expect Nose to do when we give it a file with tests to run.
+    unittest.main()
