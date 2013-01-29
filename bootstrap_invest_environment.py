5ab68c9d9d8bb032e7cc4fa938dc679023fe8944
James Douglass <jdouglass@stanford.edu>
2013-01-28 16:23:05 -0800

    Update issue 1589
    Now using the variable bin instead of static 'bin'.
    
    
    Former-commit-id: fabd7237c4a0811147161c9af247194738f94dc1

--- bootstrap_invest_environment.py
+++ bootstrap_invest_environment.py
@@ -1,19 +1,19 @@
 import virtualenv, textwrap
 output = virtualenv.create_bootstrap_script(textwrap.dedent("""
-import os, subprocess
+import os, subprocess, sys
 def after_install(options, home_dir):
     print("Home dir: ", home_dir)
     if sys.platform == 'win32':
         bin = 'Scripts'
     else:
         bin = 'bin'
-    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+    subprocess.call([join(home_dir, bin, 'easy_install'),
                      'numpy'])
-    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+    subprocess.call([join(home_dir, bin, 'easy_install'),
                      'scipy'])
-    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+    subprocess.call([join(home_dir, bin, 'easy_install'),
                      'nose'])
-    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+    subprocess.call([join(home_dir, bin, 'easy_install'),
                      'setuptools'])
 """))
 print output
