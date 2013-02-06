d167a5b14c451fd4f28ff4f8dbb150b4b1ff8185
richpsharp <devnull@localhost>
2013-02-06 15:26:34 -0800

    Update issue 1617
    Added a test for resampling but keeping the bounding box the same.
    
    
    Former-commit-id: ca785e97329a1dce88f1b1ea8ca15270cf79128f

--- test/align_datasets_test.py
+++ test/align_datasets_test.py
@@ -42,6 +42,9 @@ class TestAlignDatasets(unittest.TestCase):
         raster_utils.resize_and_resample_dataset(raster_1, bounding_box, 278, resized_278_raster, "nearest")
 
         bounding_box = raster_utils.get_bounding_box(raster_1)
+        rescaled_278_raster = os.path.join(base_dir, 'rescaled_278.tif')
+        raster_utils.resize_and_resample_dataset(raster_1, bounding_box, 278, rescaled_278_raster, "nearest")
+
 
         bounding_box[0] += width/4.0
         bounding_box[2] -= width/4.0
