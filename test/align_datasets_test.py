210b9ae2fc446e95710dc834691ddf94013d5ba1
Richard Sharp <richpsharp@gmail.com>
2013-02-08 12:58:51 -0800

    Update issue 1617
    Added a test for the aoi intersection.
    
    
    Former-commit-id: 635cc32ac5821c4a9e9c1fb9153f644b74a249d1

--- test/align_datasets_test.py
+++ test/align_datasets_test.py
@@ -87,11 +87,11 @@ class TestAlignDatasets(unittest.TestCase):
         lulc_samp_cur_out = os.path.join(out_dir, os.path.basename(lulc_samp_cur)+'.align.tif')
 
         pixel_size=60.0
-        raster_utils.align_dataset_list([precip, lulc_samp_cur], [precip_out, lulc_samp_cur_out], ["nearest", "nearest"], pixel_size, "intersection", 0)
+        raster_utils.align_dataset_list([precip, lulc_samp_cur], [precip_out, lulc_samp_cur_out], ["nearest", "nearest"], pixel_size, "intersection", 1)
 
         aoi_uri = os.path.join('data', 'hydropower_data', 'test_input', 'watersheds.shp')
 
         precip_aoi_out = os.path.join(out_dir, os.path.basename(precip)+'.aoi_align.tif')
         lulc_samp_cur_aoi_out = os.path.join(out_dir, os.path.basename(lulc_samp_cur)+'.aoi_align.tif')
 
-        raster_utils.align_dataset_list([precip, lulc_samp_cur], [precip_aoi_out, lulc_samp_cur_aoi_out], ["nearest", "nearest"], pixel_size, "intersection", 0, aoi_uri=aoi_uri)
+        raster_utils.align_dataset_list([precip, lulc_samp_cur], [precip_aoi_out, lulc_samp_cur_aoi_out], ["nearest", "nearest"], pixel_size, "intersection", 1, aoi_uri=aoi_uri)
