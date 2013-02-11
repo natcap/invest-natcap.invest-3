9e0e195cb3f2cce88d676cc738a3031271d28230
richpsharp <devnull@localhost>
2013-02-11 10:23:46 -0800

    Update issue 1629
    A complete callthrough to vectorize_datasets.
    
    
    Former-commit-id: 18e1dd591a5f0859e9023466caffb596a0042240

--- test/rasterize_datasets_test.py
+++ test/rasterize_datasets_test.py
@@ -20,3 +20,22 @@ class TestRasterUtils(unittest.TestCase):
         base_dir = 'data/test_out/vectorize_datasets'
         if not os.path.exists(base_dir):
             os.makedirs(base_dir)
+
+        dataset_uri_list = ['data/base_data/terrestrial/lulc_samp_cur',
+                            'data/base_data/Freshwater/precip']
+
+        def vector_op(lulc, precip):
+            return lulc + precip
+
+        datatype_out = gdal.GDT_Float32
+        nodata_out = -100.0
+        pixel_size_out = 30.0
+        bounding_box_mode = "union"
+        dataset_to_align_index = 0
+
+        dataset_out_uri = os.path.join(base_dir, 'vectorized_datasets.tif')
+
+        raster_utils.vectorize_datasets(
+            dataset_uri_list, vector_op, dataset_out_uri, datatype_out, nodata_out,
+            pixel_size_out, bounding_box_mode, resample_method_list=None, 
+            dataset_to_align_index=dataset_to_align_index, aoi_uri=None)
