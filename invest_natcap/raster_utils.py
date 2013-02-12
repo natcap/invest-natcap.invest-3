e39aae7c4f85d5696e18781358ffabc211baa2c5
richpsharp <devnull@localhost>
2013-02-12 11:14:02 -0800

    Update issue 1629
    Adding a depricated warning to vectorize_rasters.
    
    
    Former-commit-id: 311fe756a1dae50d45ba11dd4a05853abcaf1b54

--- invest_natcap/raster_utils.py
+++ invest_natcap/raster_utils.py
@@ -188,8 +188,8 @@ def vectorize_rasters(dataset_list, op, aoi=None, raster_out_uri=None,
         
         returns a single band current_dataset"""
 
-    LOGGER.info('starting vectorize_rasters')
-
+    LOGGER.warn('vectorize_rasters is DEPRECATED.  use vectorize_datasets '
+                'and refactor the code that is calling this function.')
     #We need to ensure that the type of nodata is the same as the raster type so
     #we don't encounter bugs where we return an int nodata for a float raster or
     #vice versa
