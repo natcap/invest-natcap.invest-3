d7da5e4bd7f7e8dcadd10917c97a9b3a3b008a25
kathryn.glowinski <devnull@localhost>
2013-02-11 16:29:18 -0800

    Udpate issue 1245
    Added the stressor raster creation and buffering, in addition to adding the different buffer decay functions.
    
    
    Former-commit-id: 3f89d21f3a056158f0f986366711550682524c63

--- invest_natcap/habitat_risk_assessment/hra.py
+++ invest_natcap/habitat_risk_assessment/hra.py
@@ -167,8 +167,168 @@ def execute(args):
 
 def add_stress_rasters(dir, stressors, stressors_dir, buffer_dict, decay_eq, 
                     grid_size):
-    
+    ''' Takes the stressor shapefiles, and burnes them to a raster buffered
+    using the desired decay equation and individual distances.
 
+    Input:
+        dir- The directory into which completed rasterized stressor shapefiles
+            shapefiles should be placed.
+        stressors- A multi-level dictionary conatining stresor-specific
+            criteria ratings and rasters.
+        stressors_dir- A URI to the directory holding all stressor shapefiles.
+        buffer_dict- A dictionary that holds desired buffer sizes for each
+            stressors. The key is the name of the stressor, and the value is an
+            int which correlates to desired buffer size.
+        decay_eq- A string identifying the equation that should be used
+            in calculating the decay of stressor buffer influence.
+
+    Output:
+        A modified version of stressors, into which have been placed a
+            rasterized version of the stressor shaprefile. It will be placed
+            at stressors[stressName]['DS'].
+    '''
+    stress_list = glob.glob(os.path.join(stressors_dir, '*.shp')
+
+    for shape in stress_list:
+
+        #The return of os.path.split is a tuple where everything after the final
+        #slash is returned as the 'tail' in the second element of the tuple
+        #path.splitext returns a tuple such that the first element is what comes
+        #before the file extension, and the second is the extension itself
+        name = os.path.splitext(os.path.split(shape)[1])[0]
+
+        out_uri = os.path.join(dir, name + '.tif')
+        
+        datasource = ogr.Open(shape)
+        layer = datasource.GetLayer()
+        
+        #Making the nodata value 0 so that it's easier to combine the 
+        #layers later.
+
+        ###Eventually, will use raster_utils.temporary_filename() here.###
+        r_dataset = \
+            raster_utils.create_raster_from_vector_extents(grid_size, grid_size,
+                    gdal.GDT_Int32, 0, out_uri, datasource)
+
+        band, nodata = raster_utils.extract_band_and_nodata(r_dataset)
+        band.Fill(nodata)
+
+        gdal.RasterizeLayer(r_dataset, [1], layer, burn_values=[1], 
+                                                options=['ALL_TOUCHED=TRUE'])
+       
+        #Now, want to take that raster, and make it into a buffered version of
+        #itself.
+        base_array = band.ReadAsArray()
+        buff = buffer_dict[name]
+
+        #Swaps 0's and 1's for use with the distance transform function.
+        swp_array = (base_array + 1) % 2
+
+        #The array with each value being the distance from its own cell to land
+        dist_array = ndimage.distance_transform_edt(swp_array, 
+                                                    sampling=grid_size)
+        
+        if decay_eq == 'None':
+            decay_array = make_no_decay_array(dist_array, buff, nodata)
+        elif decay_eq == 'Exponential':
+            decay_array = make_exp_decay_array(dist_array, buff, nodata)
+        elif decay_eq == 'Linear':
+            decay_array = make_lin_decay_array(dist_array, buff, nodata)
+
+        #Create a new file to which we should write our buffered rasters.
+        #Eventually, we will use the filename without buff, because it will
+        #just be assumed to be buffered
+        new_buff_uri = os.path.join(dir, name + '_buff.tif')
+        
+        new_dataset = raster_utils.new_raster_from_base(r_dataset, new_buff_uri,
+                            'GTiff', 0, gdal.GDT_Float32)
+        
+        n_band, n_nodata = raster_utils.extract_band_and_nodata(new_dataset)
+        n_band.Fill(n_nodata)
+        
+        n_band.WriteArray(decay_array)
+
+        #Now, write the buffered version of the stressor to the stressors
+        #dictionary
+        stressors[name]['DS'] = new_dataset
+
+def make_lin_decay_array(dist_array, buff, nodata):
+    '''Should create an array where the area around land is a function of 
+    linear decay from the values representing the land.
+
+    Input:
+        dist_array- A numpy array where each pixel value represents the
+            distance to the closest piece of land.
+        buff- The distance surrounding the land that the user desires to buffer
+            with linearly decaying values.
+        nodata- The value which should be placed into anything not land or
+            buffer area.
+    Returns:
+        A numpy array reprsenting land with 1's, and everything within the buffer
+        zone as linearly decayed values from 1.
+    '''
+
+    #The decay rate should be approximately -1/distance we want 0 to be at.
+    #We add one to have a proper y-intercept.
+    lin_decay_array = -dist_array/buff + 1.0
+    lin_decay_array[lin_decay_array < 0] = nodata
+
+    return lin_decay_array
+
+def make_exp_decay_array(dist_array, buff, nodata):
+    '''Should create an array where the area around the land is a function of
+    exponential decay from the land values.
+
+    Input:
+        dist_array- Numpy array where each pixel value represents the distance
+            to the closest piece of land.
+        buff- The distance surrounding the land that the user desires to buffer
+            with exponentially decaying values.
+        nodata- The value which should be placed into anything not land or
+            buffer area.
+    Returns:
+        A numpy array representing land with 1's and eveything withing the buffer
+        zone as exponentially decayed values from 1.
+    '''
+
+    #Want a cutoff for the decay amount after which we will say things are
+    #equivalent to nodata, since we don't want to have values outside the buffer
+    #zone.
+    cutoff = 0.01
+
+    #Need to have a value representing the decay rate for the exponential decay
+    rate = -math.log(cutoff)/ buff
+
+    exp_decay_array = np.exp(-rate * dist_array)
+    exp_decay_array[exp_decay_array < cutoff] = nodata
+
+    return exp_decay_array
+
+def make_no_decay_array(dist_array, buff, nodata):
+    '''Should create an array where the buffer zone surrounding the land is
+    buffered with the same values as the land, essentially creating an equally
+    weighted larger landmass.
+
+    Input:
+        dist_array- Numpy array where each pixel value represents the distance
+            to the closest piece of land.
+        buff- The distance surrounding the land that the user desires to buffer
+            with land data values.
+        nodata- The value which should be placed into anything not land or
+            buffer area.
+    Returns:
+        A numpy array representing both land and buffer zone with 1's, and \
+        everything outside that with nodata values.
+    '''
+
+    #Setting anything within the buffer zone to 1, and anything outside
+    #that distance to nodata.
+    inner_zone_index = dist_array <= buff
+    dist_array[inner_zone_index] = 1
+    dist_array[~inner_zone_index] = nodata  
+    
+    return dist_array 
+    
 def add_hab_rasters(dir, habitats, hab_list, grid_size):
     '''Want to get all shapefiles within any directories in hab_list, and burn
     them to a raster.
@@ -176,9 +336,8 @@ def add_hab_rasters(dir, habitats, hab_list, grid_size):
     Input:
         dir- Directory into which all completed habitat rasters should be 
             placed.
-        habitats- Similar to the h-s dictionary, a multi-level
-            dictionary containing all habitat-specific criteria ratings and
-            rasters.
+        habitats- A multi-level dictionary containing all habitat and 
+            species-specific criteria ratings and rasters.
         hab_list- File URI's for all shapefile in habitats dir, species dir, or
             both.
         grid_size- Int representing the desired pixel dimensions of
@@ -186,7 +345,8 @@ def add_hab_rasters(dir, habitats, hab_list, grid_size):
 
     Output:
         A modified version of habitats, into which we have placed a rasterized
-            version of the habitat shapefile.
+            version of the habitat shapefile. It will be placed at
+            habitats[habitatName]['DS'].
    ''' 
     for shape in hab_list:
         
