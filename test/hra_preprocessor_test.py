ca1efccfd143ec8e0fa7e572be872d4ca4123847
kathryn.glowinski <devnull@localhost>
2013-02-22 12:13:49 -0800

    Update issue 1288
    Added test to check raising an exception if both habitats and species are false.
    
    
    Former-commit-id: d80d3128de8d043fdb937850f0690418e9a2916d

--- test/hra_preprocessor_test.py
+++ test/hra_preprocessor_test.py
@@ -18,9 +18,42 @@ class TestHRAPreprocessor(unittest.TestCase):
 
         args = {}
         args['workspace_dir'] = './data/test_out/HRA' 
-        args['habitat_dir'] = './data/test_out/HRA/Input/HabitatLayers'
         args['stressors_dir'] = './data/test_out/HRA/Input/StressorLayers'
         args['exposure_crits' = ['intensity rating', 'management effectiveness']
         args['sensitivity_crits'] = 'temporal overlap rating', \
                     'frequency of disturbance']
         args['resiliance_crits'] = ['natural mortality', 'recruitment rate']
+    
+        self.args = args
+
+    def test_HabsOnly_NoShapes_smoke(self):
+    '''This will use only the habitats directory as an input to overlap
+    stressors, and won't attempt to pull in shapefile criteria.'''
+
+        self.args['do_species'] = False
+        self.args['do_habitats'] = True
+        self.args['habitat_dir'] = './data/test_out/HRA/Input/HabitatLayers'
+        self.args['do_shapes'] = False
+
+        hra_preprocessor.execute(self.args)
+
+    def test_HabsSpecies_NoShapes_smoke(self):
+
+        self.args['do_species'] = True
+        self.args['habitat_dir'] = './data/test_out/HRA/Input/SpeciesLayers'
+        self.args['do_habitats'] = True
+        self.args['habitat_dir'] = './data/test_out/HRA/Input/HabitatLayers'
+        self.args['do_shapes'] = False
+
+        hra_preprocessor.execute(self.args)
+
+    def test_Missing_HabsSpecies_exception(self):
+    '''Want to make sure that if neither a habitat or species is selected for
+    use in overlap, that it throws an error. Should raise a 
+    MissingHabitatOrSpecies exception.'''
+
+        self.args['do_species'] = False
+        self.args['do_habitats'] = True
+
+        self.assertRaises(hra_preprocessor.MissingHabitatOrSpecies,
+                        hra_preprocessor.execute, self.args)
