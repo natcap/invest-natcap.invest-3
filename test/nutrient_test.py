e226fb7feffd1dcbb959a4984901f1a417f146f0
Richard Sharp <richpsharp@gmail.com>
2013-02-24 22:44:38 -0800

    Update issue 1561
    Easier to have it's own test.
    
    
    Former-commit-id: b257a7c6a1b5c56825ff4e2b08c02c7cbd8a63fb

---
+++ test/nutrient_test.py
@@ -0,0 +1,51 @@
+import unittest
+import os
+import shutil
+
+import numpy as np
+from osgeo import gdal
+from osgeo import ogr
+
+from invest_natcap import raster_utils
+from invest_natcap.nutrient import nutrient_biophysical
+from invest_natcap.nutrient import nutrient_valuation
+from invest_natcap.nutrient import nutrient_core
+
+BASE_DATA = os.path.join('data', 'base_data', 'terrestrial')
+NUTR_INPUT = os.path.join('data', 'nutrient', 'input')
+VAL_INPUT = os.path.join('data', 'nutrient', 'valuation_input')
+
+WORKSPACE = os.path.join('data', 'test_out', 'nutrient')
+
+class NutrientBiophysicalTest(unittest.TestCase):
+    """Test class for test functions of the Nutrient retention model
+    (biophysical component)."""
+    def setUp(self):
+        self.args = {
+            'workspace_dir': WORKSPACE,
+            'dem_uri': \
+                os.path.join(NUTR_INPUT, 'dem'),
+            'pixel_yield_uri': \
+                os.path.join(NUTR_INPUT, 'wyield.tif'),
+            'landuse_uri': \
+                os.path.join(NUTR_INPUT, 'landuse_90'),
+            'watersheds_uri': \
+                os.path.join(NUTR_INPUT, 'watersheds.shp'),
+            'subwatersheds_uri': \
+                os.path.join(NUTR_INPUT, 'subwatersheds.shp'),
+            'bio_table_uri': \
+                os.path.join(NUTR_INPUT, 'biophysical_models.csv'),
+            'threshold_table_uri': \
+                os.path.join(NUTR_INPUT, 'water_purification_threshold.csv'),
+            'nutrient_type': 'nitrogen',
+            'accum_threshold': 1000,
+            'folders': {
+                'workspace': WORKSPACE,
+                'intermediate': os.path.join(WORKSPACE, 'intermediate'),
+                'output': os.path.join(WORKSPACE, 'output')
+            }
+        }
+
+    def test_smoke(self):
+        """Smoke test for nutrient retention: biophysical"""
+        nutrient_biophysical.execute(self.args)
