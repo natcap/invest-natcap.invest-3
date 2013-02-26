2534717bf1b7a3c25264c5a51732ca894f3bbc01
richpsharp <devnull@localhost>
2013-02-26 11:09:11 -0800

    Update issue 1342
    Removing the code that deletes the workspace if it exists.
    
    
    Former-commit-id: 508c949a2f12228b745a8fcf20acd0198f7ba519

--- invest_natcap/nutrient/nutrient_biophysical.py
+++ invest_natcap/nutrient/nutrient_biophysical.py
@@ -23,14 +23,6 @@ logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
 
 class InvalidNutrient(Exception):pass
 
-def make_folder(folder):
-    if not os.path.exists(folder):
-        LOGGER.debug('Making the folder %s', folder)
-        os.makedirs(folder)
-    else:
-        LOGGER.debug('Folder %s already exists, deleting and recreating', folder)
-        shutil.rmtree(folder)
-        os.makedirs(folder)
 
 def execute(args):
     """File opening layer for the InVEST nutrient retention model.
@@ -62,9 +54,10 @@ def execute(args):
 
     file_suffix = ''
 
-
     for folder in [workspace, output_dir, service_dir, intermediate_dir]:
-        make_folder(folder)
+        if not os.path.exists(folder):
+            LOGGER.debug('Making folder %s', folder)
+            os.makedirs(folder)
 
     biophysical_args = {}
 
@@ -202,7 +195,6 @@ def execute(args):
         [load_n_uri, upstream_water_yield_log_uri, mean_runoff_uri], alv_calculation, alv_n_uri,
         gdal.GDT_Float32, nodata_load, out_pixel_size, "intersection")
 
-
     #Calculate nutrient retention
     p_retention_uri = os.path.join(intermediate_dir, 'p_retention.tif')
     n_retention_uri = os.path.join(intermediate_dir, 'n_retention.tif')
@@ -228,109 +220,4 @@ def execute(args):
     routing_utils.pixel_amount_exported(
         dem_uri, stream_uri, eff_n_uri, load_n_uri, n_export_uri,
         aoi_uri=args['watersheds_uri'])
-
-
-    return 
-
-
-
-    # Open rasters provided in the args dictionary.
-    LOGGER.info('Opening user-defined rasters')
-    raster_list = ['dem_uri', 'pixel_yield_uri', 'landuse_uri']
-    for raster_key in raster_list:
-        new_key = re.sub('_uri$', '', raster_key)
-        LOGGER.debug('Opening "%s" raster at %s', new_key, str(args[raster_key]))
-        biophysical_args[new_key] = gdal.Open(str(args[raster_key]))
-
-    # Open shapefiles provided in the args dictionary
-    LOGGER.info('Opening user-defined shapefiles')
-    encoding = sys.getfilesystemencoding()
-    ogr_driver = ogr.GetDriverByName('ESRI Shapefile')
-    shapefile_list = ['watersheds_uri', 'subwatersheds_uri']
-    for shape_key in shapefile_list:
-        new_key = re.sub('_uri$', '', shape_key)
-        new_uri = os.path.splitext(str(args[shape_key]))[0]
-        LOGGER.debug('Opening "%s" shapefile at %s', new_key, new_uri)
-
-        base_shape_uri = args[shape_key].encode(encoding)
-        LOGGER.debug('Opening with encoded %s' % base_shape_uri)
-        sample_shape = ogr.Open(base_shape_uri, 0)
-        shapefile_folder = os.path.join(output_dir, new_key)
-        make_folder(shapefile_folder)
-
-        copy_uri = os.path.join(output_dir, new_key)
-        
-        copy = ogr_driver.CopyDataSource(sample_shape, copy_uri)
-        LOGGER.debug('Saving shapefile copy to %s', copy_uri)
-        LOGGER.debug('Copied shape: %s', copy)
-
-        # Create the known new fields for this shapefile.
-        # thresh_c is the per-pixel contributed threshold.  Used in calculating
-        # the service raster, it's the result of (thresh/contrib)
-        for column_name in ['nut_export', 'nut_retain', 'mn_runoff', 'thresh_c']:
-            LOGGER.debug('Creating new field %s in %s', column_name, copy_uri)
-            new_field = ogr.FieldDefn(column_name, ogr.OFTReal)
-            copy.GetLayer(0).CreateField(new_field)
-
-        biophysical_args[new_key] = copy
-
-    LOGGER.info('Opening tables')
-    LOGGER.debug('Nutrient type: %s', args['nutrient_type'])
-    if args['nutrient_type'] == 'nitrogen':
-        suffix = '_n'
-    elif args['nutrient_type'] == 'phosphorus':
-        suffix = '_p'
-    else:
-        raise InvalidNutrient('Invalid Nutrient type %s found.' %
-                              args['nutrient_type'])
-
-    bio_table_regex = '|'.join([r + suffix for r in ['load', 'eff']])
-    LOGGER.debug('Bio table regexp: %s', bio_table_regex)
-
-    thresh_table_regex = 'thresh' + suffix
-    LOGGER.debug('Threshold table regexp: %s', thresh_table_regex)
-
-    biophysical_args['bio_table'] = fileio.TableHandler(args['bio_table_uri'])
-    biophysical_args['bio_table'].set_field_mask(bio_table_regex, 2, 'back')
-    biophysical_args['threshold_table'] =\
-        fileio.TableHandler(args['threshold_table_uri'])
-    biophysical_args['threshold_table'].set_field_mask(thresh_table_regex, 2, 'back')
-
-    # Extract the threshold data in the threshold table and save it to the new
-    # watersheds shapefile.
-    watersheds_layer = biophysical_args['watersheds'].GetLayer(0)
-    threshold_fields = biophysical_args['threshold_table'].fieldnames
-    new_fieldnames = [f for f in threshold_fields if f not in ['ws_id', 'id']]
-    LOGGER.debug('New fields to be created in watersheds:%s', new_fieldnames)
-
-    # Create the new fieldnames in the watersheds layer
-    for field_name in new_fieldnames:
-        LOGGER.debug('Creating new fieldname %s', field_name)
-        new_field = ogr.FieldDefn(field_name, ogr.OFTReal)
-        watersheds_layer.CreateField(new_field)
-
-    # Extract the data to be saved to the new fields from the threshold table
-    # and set the field values.
-    for row in biophysical_args['threshold_table']:
-        watershed = watersheds_layer.GetFeature(int(row['ws_id']))
-        LOGGER.debug('Creating feature %s for row %s', watershed, row)
-        for field_name in new_fieldnames:
-            LOGGER.debug('Setting field %s to table value %s', field_name,
-                         row[field_name])
-            field_index = watershed.GetFieldIndex(field_name)
-            LOGGER.debug('field %s index %s', field_name, field_index)
-            watershed.SetField(field_index, str(row[field_name]))
-        watersheds_layer.SetFeature(watershed)
-        watershed.Destroy()
-
-    LOGGER.info('Copying other values for internal use')
-    biophysical_args['accum_threshold'] = args['accum_threshold']
-    biophysical_args['folders'] = {
-        'workspace': workspace,
-        'intermediate': intermediate_dir,
-        'output': output_dir
-    }
-
-    # Run the nutrient model with the biophysical args dictionary.
-    nutrient_core.biophysical(biophysical_args)
 
