a32f4dd32a102f025e94cdc620e8bb183056958d
richpsharp <richpsharp@gmail.com>
2013-03-18 11:04:37 -0600

    Update issue 1779
    Adding an api for simple single raster max marginal gain calculation.
    
    
    Former-commit-id: a59ded707216d9ff2b779cde6f48a917e68445d0

---
+++ invest_natcap/optimization/optimization.py
@@ -0,0 +1,13 @@
+def static_max_marginal_gain(
+	score_dataset_uri, budget, aoi_uri=None):
+	"""This funciton calculates the maximum marginal gain by selecting pixels
+		in a greedy fashion until the entire budget is spent.
+		
+		score_dataset_uri - gdal dataset to a float raster
+		budget - number of pixels to select
+		aoi_uri - an area to consider selection
+	
+	returns nothing"""
+	pass
+	
+	
