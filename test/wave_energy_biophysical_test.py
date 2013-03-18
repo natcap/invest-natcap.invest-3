4befc5993a78e38677b83058701c0fb29e2356c4
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-03-18 16:50:27 -0700

    Update issue 1785
    Working on some initial edits and cleanup.
    
    
    Former-commit-id: 6fd8a56d41ba039629edc9d6a30fe06e668dfb6e

--- test/wave_energy_biophysical_test.py
+++ test/wave_energy_biophysical_test.py
@@ -8,6 +8,7 @@ from osgeo import ogr
 from invest_natcap.wave_energy import wave_energy_biophysical
 import invest_test_core
 from invest_natcap.dbfpy import dbf
+from nose.plugins.skip import SkipTest
 
 LOGGER = logging.getLogger('wave_energy_biophysical_test')
 logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
@@ -22,17 +23,18 @@ class TestWaveEnergyBiophysical(unittest.TestCase):
         test_dir = './data/wave_energy_data/'
         output_dir = './data/test_out/wave_energy_biophysical_output'
         args['workspace_dir'] = output_dir
-        args['wave_base_data_uri'] = test_dir + 'samp_input/WaveData'
+        args['wave_base_data_uri'] = os.path.join(
+                test_dir, 'samp_input/WaveData')
         args['analysis_area_uri'] = 'West Coast of North America and Hawaii'
-        args['machine_perf_uri'] = \
-            test_dir + 'samp_input/Machine_PelamisPerfCSV.csv'
-        args['machine_param_uri'] = \
-            test_dir + 'samp_input/Machine_PelamisParamCSV.csv'
-        args['dem_uri'] = test_dir + 'samp_input/global_dem'
-        args['aoi_uri'] = test_dir + 'samp_input/AOI_WCVI.shp'
+        args['machine_perf_uri'] = os.path.join(
+                test_dir, 'samp_input/Machine_PelamisPerfCSV.csv')
+        args['machine_param_uri'] = os.path.join(
+                test_dir, 'samp_input/Machine_PelamisParamCSV.csv')
+        args['dem_uri'] = os.path.join(test_dir, 'samp_input/global_dem')
+        args['aoi_uri'] = os.path.join(test_dir, 'samp_input/AOI_WCVI.shp')
         
         if not os.path.isdir(output_dir):
-            os.mkdir(output_dir)
+            os.makedirs(output_dir)
 
         wave_energy_biophysical.execute(args)
         
@@ -41,39 +43,40 @@ class TestWaveEnergyBiophysical(unittest.TestCase):
         #assert that the captured wave energy output raster is equivalent 
         #to the regression test
         invest_test_core.assertTwoDatasetEqualURI(self,
-            output_dir + os.sep + 'Output/capwe_mwh.tif',
-            regression_dir + 'capwe_mwh_regression.tif')
+            os.path.join(output_dir, 'Output/capwe_mwh.tif'),
+            os.path.join(regression_dir, 'capwe_mwh_regression.tif'))
         
         #assert that the wave power output raster is equivalent to the 
         #regression test
         invest_test_core.assertTwoDatasetEqualURI(self,
-            output_dir + os.sep + 'Output/wp_kw.tif',
-            regression_dir + 'wp_kw_regression.tif')
+            os.path.join(output_dir, 'Output/wp_kw.tif'),
+            os.path.join(regression_dir, 'wp_kw_regression.tif'))
         
         #Regression Check for wave power percentile raster
         invest_test_core.assertTwoDatasetEqualURI(self,
-            output_dir + os.sep + 'Output/wp_rc.tif',
-            regression_dir + '/wp_rc_regression.tif')
+            os.path.join(os.sep + 'Output/wp_rc.tif'),
+            os.path.join(regression_dir, '/wp_rc_regression.tif'))
         
         #Regression Check for captured wave energy percentile raster
         invest_test_core.assertTwoDatasetEqualURI(self,
-            output_dir + os.sep + 'Output/capwe_rc.tif',
-            regression_dir + 'capwe_rc_regression.tif')        
+            os.path.join(output_dir, 'Output/capwe_rc.tif'),
+            os.path.join(regression_dir, 'capwe_rc_regression.tif'))    
         
         #Regression Check for WEM_InputOutput_Pts shapefile
-        wave_data_shape_path = \
-            output_dir + os.sep + 'Intermediate/WEM_InputOutput_Pts.shp'
-        regression_shape_path = \
-            regression_dir + 'WEM_InputOutput_Pts_bio_regression.shp'
-        invest_test_core.assertTwoShapesEqualURI(self, wave_data_shape_path, 
-                                                 regression_shape_path)
+        wave_data_shape_path = os.path.join(
+                output_dir, 'Intermediate/WEM_InputOutput_Pts.shp')
+        regression_shape_path = os.path.join(
+                regression_dir, 'WEM_InputOutput_Pts_bio_regression.shp')
+        invest_test_core.assertTwoShapesEqualURI(
+                self, wave_data_shape_path, regression_shape_path)
         
         #Regression check to make sure the dbf files with the attributes for the
         #percentile rasters are correct
         try:
-            regression_table = \
-                dbf.Dbf(regression_dir + 'wp_rc_regression.tif.vat.dbf')
-            db_file = dbf.Dbf(output_dir + os.sep + 'Output/wp_rc.tif.vat.dbf')
+            regression_table = dbf.Dbf(os.path.join(
+                    regression_dir, 'wp_rc_regression.tif.vat.dbf'))
+            db_file = dbf.Dbf(os.path.join(
+                output_dir, 'Output/wp_rc.tif.vat.dbf'))
             value_array = []
             count_array = []
             val_range_array = []
@@ -87,9 +90,10 @@ class TestWaveEnergyBiophysical(unittest.TestCase):
             self.assertTrue(False, 'The dbf file could not be opened')
 
         try:
-            regression_table = \
-                dbf.Dbf(regression_dir + 'capwe_rc_regression.tif.vat.dbf')
-            db_file = dbf.Dbf(output_dir + os.sep + 'Output/capwe_rc.tif.vat.dbf')
+            regression_table = dbf.Dbf(os.path.join(
+                regression_dir, 'capwe_rc_regression.tif.vat.dbf'))
+            db_file = dbf.Dbf(os.path.join(
+                output_dir, 'Output/capwe_rc.tif.vat.dbf'))
             value_array = []
             count_array = []
             val_range_array = []
@@ -107,6 +111,7 @@ class TestWaveEnergyBiophysical(unittest.TestCase):
            compares hand calculated results against the returned
            function generated results
         """
+        raise SkipTest
         wave_base_data_uri = \
             './data/wave_energy_data/test_input/sampWaveDataTest.txt.bin'
         if os.path.isfile(wave_base_data_uri):
