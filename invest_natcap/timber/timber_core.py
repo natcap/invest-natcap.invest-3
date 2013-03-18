5a834f1213b45954233b563330cd8e545503be6c
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-03-18 11:50:46 -0700

    Fixes issue 1781
    Refactored some tests, and did some pylinting
    
    
    Former-commit-id: 77604bc81c92b5699df8cf4a00b5a69b5b7f02a6

--- invest_natcap/timber/timber_core.py
+++ invest_natcap/timber/timber_core.py
@@ -11,18 +11,20 @@ import numpy as np
 from osgeo import ogr
 
 def execute(args):
-    """Executes the basic timber management model that calculates the Total Net Present Value and maps
-        it to an outputted shapefile.
+    """Executes the basic timber management model that calculates the 
+        Total Net Present Value and maps it to an outputted shapefile.
     
         args - is a dictionary with at least the following entries:
         
         args['timber_shape']   - an OGR shapefile which holds a polygon layer 
-                                 representing timber parcels.  Has a Parcl_ID field 
-                                 which corresponds to Parcel_ID field in attr_table
+                                 representing timber parcels.  Has a Parcl_ID
+                                 field which corresponds to Parcel_ID field 
+                                 in attr_table
         args['mdr']            - the market discount rate as a float.
-        args['attr_table']     - the DBF file which has the polygon attribute values 
-                                 of timber_shape. Has a Parcel_ID field that matches
-                                 relative polygons to Parcl_ID field in timber_shape
+        args['attr_table']     - the DBF file which has the polygon attribute
+                                 values of timber_shape. Has a Parcel_ID field
+                                 that matches relative polygons to Parcl_ID
+                                 field in timber_shape
         
         returns nothing"""
     logging.debug('executing timber_core')
@@ -48,8 +50,8 @@ def execute(args):
 
     #Loop through each feature (polygon) in the shapefile layer
     for feat in layer:
-        #Get the correct polygon attributes to be calculated by matching the feature's polygon Parcl_ID
-        #with the attribute tables polygon Parcel_ID
+        #Get the correct polygon attributes to be calculated by matching the
+        #feature's polygon Parcl_ID with the attribute tables polygon Parcel_ID
         parcl_index = feat.GetFieldIndex('Parcl_ID')
         parcl_id = feat.GetField(parcl_index)
         attr_row = parcelIdLookup[parcl_id]
@@ -66,7 +68,8 @@ def execute(args):
         immed_Harv = attr_row['Immed_harv']
 
         sumTwo_upperLimit = int(num_Years - 1)
-        #Variable used in npv summation one equation as a distinguisher between two immed_harv possibilities
+        #Variable used in npv summation one equation as a distinguisher
+        #between two immed_harv possibilities
         subtractor = 0.0
         yr_per_freq = num_Years / freq_Harv
 
@@ -82,17 +85,27 @@ def execute(args):
             sumOne_upperLimit = int(math.floor(yr_per_freq))
             sumOne_lowerLimit = 1
             subtractor = 1.0
-            summation_one = npvSummationOne(sumOne_lowerLimit, sumOne_upperLimit, harvest_value, mdr_perc, freq_Harv, subtractor)
-            summation_two = npvSummationTwo(sumTwo_lowerLimit, sumTwo_upperLimit, maint_Cost, mdr_perc)
+            summation_one = npvSummationOne(
+                    sumOne_lowerLimit, sumOne_upperLimit, harvest_value,
+                    mdr_perc, freq_Harv, subtractor)
+            summation_two = npvSummationTwo(
+                    sumTwo_lowerLimit, sumTwo_upperLimit, maint_Cost, mdr_perc)
             #Calculate Biomass
-            biomass = parcl_Area * (perc_Harv / 100.00) * harv_Mass * math.floor(yr_per_freq)
+            biomass = \
+                    parcl_Area * (perc_Harv / 100.00) * harv_Mass \
+                    * math.floor(yr_per_freq)
         elif immed_Harv.upper() == 'Y' or immed_Harv.upper() == 'YES':
             sumOne_upperLimit = int((math.ceil(yr_per_freq) - 1.0))
             sumOne_lowerLimit = 0
-            summation_one = npvSummationOne(sumOne_lowerLimit, sumOne_upperLimit, harvest_value, mdr_perc, freq_Harv, subtractor)
-            summation_two = npvSummationTwo(sumTwo_lowerLimit, sumTwo_upperLimit, maint_Cost, mdr_perc)
+            summation_one = npvSummationOne(
+                    sumOne_lowerLimit, sumOne_upperLimit, harvest_value,
+                    mdr_perc, freq_Harv, subtractor)
+            summation_two = npvSummationTwo(
+                    sumTwo_lowerLimit, sumTwo_upperLimit, maint_Cost, mdr_perc)
             #Calculate Biomass
-            biomass = parcl_Area * (perc_Harv / 100.00) * harv_Mass * math.ceil(yr_per_freq)
+            biomass = \
+                    parcl_Area * (perc_Harv / 100.00) * harv_Mass \
+                    * math.ceil(yr_per_freq)
         
         #Calculate Volume
         volume = biomass * (1.0 / BCEF)
@@ -101,7 +114,9 @@ def execute(args):
         total_npv = net_present_value * parcl_Area
 
         #For each new field set the corresponding value to the specific polygon
-        for field, value in (('TNPV', total_npv), ('TBiomass', biomass), ('TVolume', volume)):
+        for field, value in (
+                ('TNPV', total_npv), ('TBiomass', biomass),
+                ('TVolume', volume)):
             index = feat.GetFieldIndex(field)
             feat.SetField(index, value)
 
@@ -110,11 +125,13 @@ def execute(args):
         feat.Destroy()
 
 #Calculates the first summation for the net present value of a parcel
-def npvSummationOne(lower, upper, harvest_value, mdr_perc, freq_Harv, subtractor):
+def npvSummationOne(
+        lower, upper, harvest_value, mdr_perc, freq_Harv, subtractor):
     summation = 0.0
     upper = upper + 1
     for num in range(lower, upper):
-        summation = summation + (harvest_value / (mdr_perc ** ((freq_Harv * num) - subtractor)))
+        summation = summation + (
+                harvest_value / (mdr_perc ** ((freq_Harv * num) - subtractor)))
 
     return summation
 
