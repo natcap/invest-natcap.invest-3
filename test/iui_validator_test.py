9b82051343ea9ec0a7fed89689c2457e7d3ab72e
James Douglass <jdouglass@stanford.edu>
2013-03-20 12:02:16 -0700

    Update issue 1797
    Adding a test for meter validation (spelled like 'meter').
    
    
    Former-commit-id: 04926e857799899c151dd753e22c05f3ad0e3524

--- test/iui_validator_test.py
+++ test/iui_validator_test.py
@@ -177,6 +177,14 @@ class OGRCheckerTester(CheckerTester):
         self.validate_as.update(updates)
         self.assertNoError()
 
+        # This should validate that the projection's linear units are in
+        # Meters.
+        updates = {'layers': [{'name': 'meter_proj',
+                               'projection': {'units': 'meters'}}],
+                   'value': TEST_DATA + '/iui/validation/meter_proj.shp'}
+        self.validate_as.update(updates)
+        self.assertNoError()
+
         # Verify that if the JSON definition requires a projection that we don't
         # recognize in validation's known_units dictionary.
         updates = {'layers': [{'name': 'mn',
