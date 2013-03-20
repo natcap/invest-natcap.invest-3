331b691e740590f7f6be314995b53d5cf655a4aa
richpsharp <richpsharp@gmail.com>
2013-03-20 11:23:38 -0600

    Update issue 1795
    Malaria entry point with docstring.
    
    
    Former-commit-id: c7e83e7ebaa19d5e2cd218f5aab6a325a084cdf4

---
+++ invest_natcap/malaria/malaria.py
@@ -0,0 +1,14 @@
+def execute_30(**args):
+	"""Malarial ecosystem disservice model.
+	
+	Keyword arguments:
+	lulc_uri -- a uri to a gdal dataset specifying land cover types
+	dem_uri -- a uri to a gdal dataset specifying the DEM
+	wyield_uri - a uri to a gdal dataset specifying per pixel water yield
+	max_vector_flight -- a floating point number specifying the flight distance of
+		a vector from a breeding site
+	population_uri -- a uri to a gdal dataset specifying population density
+
+	"""
+	pass
+	
