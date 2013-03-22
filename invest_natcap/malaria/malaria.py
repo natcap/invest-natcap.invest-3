130412d3d38c3bf3d27232ac7b6eadf5eb78ade4
richpsharp <devnull@localhost>
2013-03-22 11:06:46 -0700

    Updating files to unix and replacing tabs with 4 spaces.
    Former-commit-id: f82330c01c652bec946bf7f2d51b34c253856960

--- invest_natcap/malaria/malaria.py
+++ invest_natcap/malaria/malaria.py
@@ -1,19 +1,19 @@
 def execute(args):
-	execute_30(**args)
+    execute_30(**args)
 
 def execute_30(**args):
-	"""Malarial ecosystem disservice model.
-	
-	Keyword arguments:
-	lulc_uri -- a uri to a gdal dataset specifying land cover types
-	dem_uri -- a uri to a gdal dataset specifying the DEM
-	max_vector_flight -- a floating point number specifying the flight distance of
-		a vector from a breeding site
-	population_uri -- a uri to a gdal dataset specifying population density
+    """Malarial ecosystem disservice model.
+    
+    Keyword arguments:
+    lulc_uri -- a uri to a gdal dataset specifying land cover types
+    dem_uri -- a uri to a gdal dataset specifying the DEM
+    max_vector_flight -- a floating point number specifying the flight distance of
+        a vector from a breeding site
+    population_uri -- a uri to a gdal dataset specifying population density
 
-	"""
-	
+    """
+    
 
 
-	pass
-	
+    pass
+    
