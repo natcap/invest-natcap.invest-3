0b2d47aecafd8c083567c7345b22f76274f96a3c
richpsharp <devnull@localhost>
2013-03-22 11:05:30 -0700

    Update issue 1795
    Converting the script to unix file format, not dos.
    
    
    Former-commit-id: 7bf38beb9d5213d4b42e13e72b9dc34fbd80d7e8

--- invest_natcap/malaria/malaria.py
+++ invest_natcap/malaria/malaria.py
@@ -7,11 +7,13 @@ def execute_30(**args):
 	Keyword arguments:
 	lulc_uri -- a uri to a gdal dataset specifying land cover types
 	dem_uri -- a uri to a gdal dataset specifying the DEM
-	wyield_uri - a uri to a gdal dataset specifying per pixel water yield
 	max_vector_flight -- a floating point number specifying the flight distance of
 		a vector from a breeding site
 	population_uri -- a uri to a gdal dataset specifying population density
 
 	"""
+	
+
+
 	pass
 	
