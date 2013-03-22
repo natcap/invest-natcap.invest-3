d94ca3ee33377df977453ab8614f0bb3360e04a2
richpsharp <devnull@localhost>
2013-03-22 14:42:18 -0700

    Update issue 1795
    Relcassifying the lulc raster to include suitibility indexes.
    
    
    Former-commit-id: 4942ae2dcb1309e90ab08a65518015f774fa6e18

--- invest_natcap/malaria/malaria.py
+++ invest_natcap/malaria/malaria.py
@@ -1,9 +1,10 @@
 import os
 import logging
 
+from osgeo import gdal
+
 from invest_natcap import raster_utils
 from invest_natcap.routing import routing_utils
-
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -34,12 +35,23 @@ def execute_30(**args):
         LOGGER.info('creating directory %s', output_dir)
         os.makedirs(output_dir)
 
-    #Parse out the table
-    lucode_to_suitability = raster_utils.get_lookup_from_csv(
-        args['breeding_suitability_table_uri'], 'lucode')
 
     LOGGER.info('calculating flow accumulation')
     flux_output_uri = os.path.join(output_dir, 'flow_accumulation.tif')
     routing_utils.flow_accumulation(args['dem_uri'], flux_output_uri)
 
-    
+    LOGGER.info('mapping breeding suitability to lulc')
+
+    #Parse out the table
+    lucode_table = raster_utils.get_lookup_from_csv(
+        args['breeding_suitability_table_uri'], 'lucode')
+    #Get a dictionary that maps lucode to suitability indexes
+    lucode_to_suitability = dict(
+        [(lucode, val['suitability_index']) for lucode, val in 
+         lucode_table.iteritems()])
+    suitability_uri = os.path.join(output_dir, 'suitability.tif')
+    #and reclassify the lulc dataset
+    raster_utils.reclassify_dataset_uri(
+        args['lulc_uri'], lucode_to_suitability, suitability_uri, 
+        gdal.GDT_Float32, -1.0, exception_flag='values_required')
+
