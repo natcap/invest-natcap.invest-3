157ee36a6a1e98b3d51158ab5458edefc5b543b2
richpsharp <devnull@localhost>
2013-03-22 11:26:58 -0700

    Update issue 1795
    Adding an empty test to kickstart the malaria model.
    
    
    Former-commit-id: 24b47d6083acccd00a6d2cd8ba77b66bfe140a34

---
+++ test/malaria_test.py
@@ -0,0 +1,33 @@
+"""URI level tests for the sediment module"""
+
+import unittest
+import logging
+import os
+import subprocess
+
+from osgeo import gdal
+from nose.plugins.skip import SkipTest
+import numpy as np
+
+from invest_natcap.malaria import malaria
+import invest_test_core
+from invest_natcap import raster_utils
+
+
+LOGGER = logging.getLogger('malaria_test')
+logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
+    %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+class TestMalaria(unittest.TestCase):
+    """Main testing class for the sediment tests"""
+    def test_malaria_re(self):
+        """Test for sediment function running with default InVEST 
+           sample input."""
+        args = {}
+        args['workspace_dir'] = './data/test_out/malaria_output'
+        base_dir = './data/sediment_test_data'
+        args['dem_uri'] = '%s/dem' % base_dir
+        args['lulc_uri'] = '%s/landuse_90.tif' % base_dir
+        args['max_vector_flight'] = 150.0
+        args['population_uri'] = 
+        malaria.execute_30(**args)
