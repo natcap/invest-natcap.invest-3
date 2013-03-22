1a290cb59b66103bb381ad70fda9beb92d7c76c2
richpsharp <devnull@localhost>
2013-03-22 11:08:12 -0700

    Update issue 1795
    Updating docstring for the sutability table.
    
    
    Former-commit-id: 7f8b4c14c0e574134568f971a36c5c8e571774a5

--- invest_natcap/malaria/malaria.py
+++ invest_natcap/malaria/malaria.py
@@ -6,14 +6,14 @@ def execute_30(**args):
     
     Keyword arguments:
     lulc_uri -- a uri to a gdal dataset specifying land cover types
+    breeding_suitability_table_uri -- a uri to a csv table mapping lulc
+        types to suitability values.
     dem_uri -- a uri to a gdal dataset specifying the DEM
-    max_vector_flight -- a floating point number specifying the flight distance of
-        a vector from a breeding site
+    max_vector_flight -- a floating point number specifying the flight 
+        distance of a vector from a breeding site
     population_uri -- a uri to a gdal dataset specifying population density
 
+    returns nothing
     """
-    
-
-
     pass
     
