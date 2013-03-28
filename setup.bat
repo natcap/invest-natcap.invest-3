1524860596ba8910faa83d0d6e3dbcf877e638bc
James Douglass <jdouglass@stanford.edu>
2013-03-28 10:13:43 -0700

    Upodate issue 1823
    Trying to activate the 64-bit env without the 'call' keyword.
    
    
    Former-commit-id: 094a62c7287107e46f6bf179486fa9d616c7c1ae

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 setlocal EnableDelayedExpansion
-CALL "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64 /release
+"C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64 /release
 set DISTUTILS_USE_SDK=1
 
