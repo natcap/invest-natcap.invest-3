f8eb6faab19e1ea70e8aabca8eecfd6c4d8d6c64
James Douglass <jdouglass@stanford.edu>
2013-03-28 10:08:08 -0700

    Update issue 1823
    Removing the python line in setup.bat, since it was specifically
    referring to Rich's jenkins slave python install.
    
    
    Former-commit-id: 1241072963465a251b588c1129d41ea980ff4846

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 setlocal EnableDelayedExpansion
 CALL "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64 /release
 set DISTUTILS_USE_SDK=1
-C:\Users\rpsharp\AppData\Jenkins\tools\Python\Windows7-Python-2.7\Python27_x64\python.exe setup.py build_ext --compiler=msvc py2exe
+
