64b243ba96de995dc37387ba813ca6e74590e38f
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-04-02 15:37:28 -0700

    Update issue 1835
    Completed fixing test for biodiversity biophysical.
    
    Removed a unneeded test from biodiversity core tests
    
    
    Former-commit-id: 7fdfb0be297b2e68a2d4676379d8edb0a04bb765

--- test/biodiversity_biophysical_test.py
+++ test/biodiversity_biophysical_test.py
@@ -29,7 +29,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -84,7 +85,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -139,7 +141,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         #args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         #args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         #args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -168,10 +171,14 @@ class TestBiodiversityBiophysical(unittest.TestCase):
 
         #invest_test_core.assertTwoDatasetEqualURI(self, reg_rarity_c, rarity_c)
         #invest_test_core.assertTwoDatasetEqualURI(self, reg_rarity_f, rarity_f)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_quality_c, quality_c)
-        #invest_test_core.assertTwoDatasetEqualURI(self, reg_quality_f, quality_f)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_deg_sum_c, deg_sum_c)
-        #invest_test_core.assertTwoDatasetEqualURI(self, reg_deg_sum_f, deg_sum_f)
+        invest_test_core.assertTwoDatasetEqualURI(
+                self, reg_quality_c, quality_c)
+        #invest_test_core.assertTwoDatasetEqualURI(
+        #   self, reg_quality_f, quality_f)
+        #invest_test_core.assertTwoDatasetEqualURI(
+        #        self, reg_deg_sum_c, deg_sum_c)
+        #invest_test_core.assertTwoDatasetEqualURI(
+            #self, reg_deg_sum_f, deg_sum_f)
     
     def test_biodiversity_biophysical_sample_regression_with_access(self):
         """A regression test for the biodiversity model with only the 
@@ -190,7 +197,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         #args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         #args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -219,10 +227,14 @@ class TestBiodiversityBiophysical(unittest.TestCase):
 
         #invest_test_core.assertTwoDatasetEqualURI(self, reg_rarity_c, rarity_c)
         #invest_test_core.assertTwoDatasetEqualURI(self, reg_rarity_f, rarity_f)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_quality_c, quality_c)
-        #invest_test_core.assertTwoDatasetEqualURI(self, reg_quality_f, quality_f)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_deg_sum_c, deg_sum_c)
-        #invest_test_core.assertTwoDatasetEqualURI(self, reg_deg_sum_f, deg_sum_f)
+        invest_test_core.assertTwoDatasetEqualURI(
+                self, reg_quality_c, quality_c)
+        #invest_test_core.assertTwoDatasetEqualURI(
+        #   self, reg_quality_f, quality_f)
+        invest_test_core.assertTwoDatasetEqualURI(
+                self, reg_deg_sum_c, deg_sum_c)
+        #invest_test_core.assertTwoDatasetEqualURI(
+        #   self, reg_deg_sum_f, deg_sum_f)
     
     def test_biodiversity_biophysical_sample_regression_base(self):
         """A regression test for the biodiversity model with both
@@ -241,7 +253,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         #args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         #args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -296,7 +309,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         #args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         #args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -351,7 +365,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         #args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -406,7 +421,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         #args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -461,7 +477,8 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         #args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
         # using 1 because we get more interesting results. Using a higher number
         # makes the quality_out raster have numbers that just bump to 1
@@ -499,76 +516,38 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         #invest_test_core.assertTwoDatasetEqualURI(
         #        self, reg_deg_sum_f, deg_sum_f)
     
-    def test_biodiversity_biophysical_sample_regression2(self):
-        """A regression test for the biodiversity model with Current and Future
-        landcover but no access or baseline"""
-        raise SkipTest
-        input_dir = './data/biodiversity_regression_data/samp_input'
-        out_dir = './data/biodiversity_regression_data/samp_input/output/'
+    def test_biodiversity_biophysical_default_smoke(self):
+        """Smoke test for biodiversity_biophysical function.  
+            Shouldn't crash with zero length inputs"""
         
-        if not os.path.isdir(out_dir):
-            os.makedirs(out_dir)
+        #raise SkipTest
+        input_dir = './data/biodiversity_regression_data/samp_input'
+        out_dir = './data/test_out/biodiversity/samp_workspace/smoke/'
+      
+        # copy the workspace to test_out so that it can be properly managed on
+        # everyones machine without polluting their test/data/ directory
+        shutil.copytree(input_dir, out_dir)
         
         args = {}
-        args['workspace_dir'] = input_dir
-        args['landuse_cur_uri'] = \
-                os.path.join(input_dir, 'lc_samp_cur_b.tif')
-        #args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
+        args['workspace_dir'] = out_dir
+        args['landuse_cur_uri'] = os.path.join(input_dir, 'lc_samp_cur_b.tif')
+        args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b.tif')
         args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b.tif')
         args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
-        #args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
-        args['half_saturation_constant'] = 1
-        ##args['suffix'] = ''
-
-        biodiversity_biophysical.execute(args)
-    
-        regression_dir = \
-            './data/biodiversity_regression_data/regression_outputs/'
-        
-        reg_quality_c = os.path.join(regression_dir, 'quality_out_ccf.tif')
-        reg_quality_f = os.path.join(regression_dir, 'quality_out_fcf.tif')
-        reg_deg_sum_c = os.path.join(regression_dir, 'deg_sum_out_ccf.tif')
-        reg_deg_sum_f = os.path.join(regression_dir, 'deg_sum_out_fcf.tif')
-
-        quality_c = os.path.join(out_dir, 'quality_out_c.tif')
-        quality_f = os.path.join(out_dir, 'quality_out_f.tif')
-        deg_sum_c = os.path.join(out_dir, 'deg_sum_out_c.tif')
-        deg_sum_f = os.path.join(out_dir, 'deg_sum_out_f.tif')
-
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_quality_c, quality_c)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_quality_f, quality_f)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_deg_sum_c, deg_sum_c)
-        invest_test_core.assertTwoDatasetEqualURI(self, reg_deg_sum_f, deg_sum_f)
-
-    def test_biodiversity_biophysical_default_smoke(self):
-        """Smoke test for biodiversity_biophysical function.  Shouldn't crash with \
-           zero length inputs"""
-        
-        raise SkipTest
-        
-        input_dir = './data/biodiversity_data/samp_input'
-        out_dir = './data/test_out/biodiversity/'
-        if not os.path.isdir(out_dir):
-            os.makedirs(out_dir)
-        args = {}
-        args['workspace_dir'] = input_dir
-        args['landuse_cur_uri'] = \
-                os.path.join(input_dir, 'lc_samp_cur_b/')
-        args['landuse_bas_uri'] = os.path.join(input_dir, 'lc_samp_bse_b/')
-        args['landuse_fut_uri'] = os.path.join(input_dir, 'lc_samp_fut_b/')
-        args['threats_uri'] = os.path.join(input_dir, 'threats_samp.csv')
-        args['sensitivity_uri'] = os.path.join(input_dir , 'sensitivity_samp.csv')
+        args['sensitivity_uri'] = os.path.join(
+                input_dir , 'sensitivity_samp.csv')
         args['access_uri'] = os.path.join(input_dir , 'access_samp.shp')
-        args['half_saturation_constant'] = 30
+        # using 1 because we get more interesting results. Using a higher number
+        # makes the quality_out raster have numbers that just bump to 1
+        args['half_saturation_constant'] = 1 
         #args['suffix'] = ''
 
         biodiversity_biophysical.execute(args)
 
     def test_biodiversity_biophysical_open_amb(self):
         """Test multiple uri types for open_ambiguous_raster and assert that
-            the proper behavior is seen """
-        raise SkipTest
+            the proper behavior is seen"""
+        #raise SkipTest
         reg_dir = './data/biodiversity_regression_data/'
         uri_1 = os.path.join(reg_dir, 'empty_dir')
         uri_2 = os.path.join(reg_dir, 'test_dir')
@@ -588,7 +567,7 @@ class TestBiodiversityBiophysical(unittest.TestCase):
     def test_biodiversity_biophysical_make_dict_from_csv(self):
         """Test a few hand made CSV files to make sure make_dict_from_csv
             returns properly """
-        raise SkipTest
+        #raise SkipTest
         reg_dir = './data/biodiversity_regression_data/'
         csv_uri = os.path.join(reg_dir, 'test_csv.csv')
         field = 'LULC'
@@ -596,7 +575,7 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         result_dict = {'0':{'LULC':'0','DESC':'farm','RARITY':'0.4'},
                        '3':{'LULC':'3','DESC':'water','RARITY':'1.0'},
                        '23':{'LULC':'23','DESC':'swamp','RARITY':'0.2'}}
-        created_dict =\
+        created_dict = \
             biodiversity_biophysical.make_dictionary_from_csv(csv_uri, field)
 
         self.assertEqual(result_dict, created_dict)
@@ -604,13 +583,13 @@ class TestBiodiversityBiophysical(unittest.TestCase):
     def test_biodiversity_biophysical_check_projections(self):
         """Test a list of gdal datasets and assert that we see success and
             failures where we would expect """
-        raise SkipTest
+        #raise SkipTest
         reg_dir = './data/biodiversity_regression_data/samp_input/'
         ds_1 = gdal.Open(os.path.join(reg_dir, 'lc_samp_bse_b.tif'))
         ds_2 = gdal.Open(os.path.join(reg_dir, 'lc_samp_cur_b.tif'))
         ds_3 = gdal.Open(os.path.join(reg_dir, 'lc_samp_fut_b.tif'))
 
-        ds_dict = {'_c':ds_2, '_b':ds_1,'_f':ds_3}
+        ds_dict = {'_c':ds_2, '_b':ds_1, '_f':ds_3}
 
         result = biodiversity_biophysical.check_projections(ds_dict, 1.0)
         
@@ -619,7 +598,7 @@ class TestBiodiversityBiophysical(unittest.TestCase):
     def test_biodiversity_biophysical_check_projections_fail(self):
         """Test a list of gdal datasets and assert that we see success and
             failures where we would expect """
-        raise SkipTest
+        #raise SkipTest
         reg_dir = './data/biodiversity_regression_data/'
         ds_1 = gdal.Open(os.path.join(reg_dir, 'samp_input/lc_samp_bse_b.tif'))
         ds_2 = gdal.Open(os.path.join(reg_dir, 'samp_input/lc_samp_cur_b.tif'))
@@ -635,21 +614,24 @@ class TestBiodiversityBiophysical(unittest.TestCase):
     def test_biodiversity_biophsyical_threat_names_match(self):
         """Test hand created dictionaries representing the formats of the
             threats and sensitivity CSV files """
-        raise SkipTest
-        threat_dict =\
+        #raise SkipTest
+        threat_dict = \
             {'crp':{'THREAT':'crp','MAX_DIST':'8','WEIGHT':0.3},
             'road':{'THREAT':'road','MAX_DIST':'5','WEIGHT':0.3},
              'bld':{'THREAT':'bld','MAX_DIST':'7','WEIGHT':0.3}}
         sens_dict = \
-            {'0':{'LULC':'0','HABITAT':'1','L_crp':'0.8','L_road':'0.5','L_bld':'0.9'},
-             '1': {'LULC':'0','HABITAT':'1','L_crp':'0.8','L_road':'0.5','L_bld':'0.9'},
-             '2': {'LULC':'0','HABITAT':'1','L_crp':'0.8','L_road':'0.5','L_bld':'0.9'},
-             '3': {'LULC':'0','HABITAT':'1','L_crp':'0.8','L_road':'0.5','L_bld':'0.9'}}
+            {'0':{'LULC':'0','HABITAT':'1','L_crp':'0.8',
+                  'L_road':'0.5','L_bld':'0.9'},
+             '1': {'LULC':'0','HABITAT':'1','L_crp':'0.8',
+                  'L_road':'0.5','L_bld':'0.9'},
+             '2': {'LULC':'0','HABITAT':'1','L_crp':'0.8',
+                   'L_road':'0.5','L_bld':'0.9'},
+             '3': {'LULC':'0','HABITAT':'1','L_crp':'0.8',
+                   'L_road':'0.5','L_bld':'0.9'}}
 
 
-        result = \
-            biodiversity_biophysical.threat_names_match(threat_dict, \
-                sens_dict, 'L_')
+        result = biodiversity_biophysical.threat_names_match(
+                threat_dict, sens_dict, 'L_')
 
         self.assertTrue(result)
     
@@ -657,16 +639,20 @@ class TestBiodiversityBiophysical(unittest.TestCase):
         """Test hand created dictionaries representing the formats of the
             threats and sensitivity CSV files. We purposely put an error in
             here so that the function will return False """
-        raise SkipTest
+        #raise SkipTest
         threat_dict =\
             {'crp':{'THREAT':'crp','MAX_DIST':'8','DECAY':'0','WEIGHT':0.3},
             'road':{'THREAT':'road','MAX_DIST':'5','DECAY':'1','WEIGHT':0.3},
              'bld':{'THREAT':'bld','MAX_DIST':'7','DECAY':'0','WEIGHT':0.3}}
         sens_dict = \
-            {'0':{'LULC':'0','HABITAT':'1','L_crp':'0.8','Lroad':'0.5','L_bld':'0.9'},
-             '1': {'LULC':'0','HABITAT':'1','L_crp':'0.8','Lroad':'0.5','L_bld':'0.9'},
-             '2': {'LULC':'0','HABITAT':'1','L_crp':'0.8','Lroad':'0.5','L_bld':'0.9'},
-             '3': {'LULC':'0','HABITAT':'1','L_crp':'0.8','Lroad':'0.5','L_bld':'0.9'}}
+            {'0':{'LULC':'0', 'HABITAT':'1', 'L_crp':'0.8',
+                'Lroad':'0.5', 'L_bld':'0.9'},
+             '1': {'LULC':'0', 'HABITAT':'1', 'L_crp':'0.8',
+                 'Lroad':'0.5', 'L_bld':'0.9'},
+             '2': {'LULC':'0', 'HABITAT':'1', 'L_crp':'0.8',
+                 'Lroad':'0.5', 'L_bld':'0.9'},
+             '3': {'LULC':'0', 'HABITAT':'1', 'L_crp':'0.8',
+                 'Lroad':'0.5', 'L_bld':'0.9'}}
 
 
         result = biodiversity_biophysical.\
