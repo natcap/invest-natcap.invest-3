efb4a048e4bc7b47f480d809d832b2c0a7004abf
richpsharp <devnull@localhost>
2013-04-06 22:10:00 -0700

    Update issue 1826
    Gathering the base data and the export from stream.
    
    
    Former-commit-id: 92b882d8025f6d103a98be4806c4b3b468ed71af

--- sediment_permitting.py
+++ sediment_permitting.py
@@ -5,7 +5,7 @@ from invest_natcap.routing import routing_utils
 
 def base_run(workspace_dir):
     args = {}
-    args['workspace_dir'] = workspace_dir
+    args['workspace_dir'] = os.path.join(workspace_dir, 'base_run')
     args['dem_uri'] = '../Pucallpa_subset/dem_fill'
     args['erosivity_uri'] = '../Pucallpa_subset/imf_erosivity'
     args['erodibility_uri'] = '../Pucallpa_subset/erod_k'
@@ -18,6 +18,11 @@ def base_run(workspace_dir):
 
     #First calculate the base sediment run
     sediment.execute(args)
+    pixel_export_uri = os.path.join(workspace_dir, 'base_run', 'Output', 'sed_export.tif')
+
+
+    
+
 
 if __name__ == '__main__':
     base_run('./base_sediment_run')
