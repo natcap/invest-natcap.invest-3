627c931c36bbddf6a31af3fdfd262f768cd10da4
richpsharp <devnull@localhost>
2013-04-06 21:59:45 -0700

    Update issue 1826
    Created a cython'd version of the LS function for faster calculation.  It was doing a lot of python powers and loops per pixel.
    
    
    Former-commit-id: 300679998ff3b0deae3ade39260082c3d439de66

--- sediment_permitting.py
+++ sediment_permitting.py
@@ -15,12 +15,9 @@ def base_run(workspace_dir):
     args['threshold_flow_accumulation'] = 1000
     args['slope_threshold'] = 70.0
     args['sediment_threshold_table_uri'] = '../Pucallpa_subset/sed_thresh.csv'
-    #routing_utils.flow_accumulation(args['dem_uri'], os.path.join(args['workspace_dir'], 'flux_output_uri.tif'))
-    #sediment.execute(args)
 
-    dem_uri = './test/data/sediment_test_data/dem'
-
-    routing_utils.flow_accumulation(dem_uri, os.path.join(args['workspace_dir'], 'willimate_flux_output.tif'))
+    #First calculate the base sediment run
+    sediment.execute(args)
 
 if __name__ == '__main__':
     base_run('./base_sediment_run')
