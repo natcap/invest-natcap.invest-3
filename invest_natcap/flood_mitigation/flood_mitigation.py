970da362ee80685512524a226221634d5eb299ad
James Douglass <jdouglass@stanford.edu>
2013-04-15 15:10:48 -0700

    Update issue 1881
    Adding part of the docstring to the new execute function.
    
    
    Former-commit-id: 21a995fa8b35d56c835156bf965184e738ea275a

--- invest_natcap/flood_mitigation/flood_mitigation.py
+++ invest_natcap/flood_mitigation/flood_mitigation.py
@@ -1,1 +1,17 @@
 """Functions for the InVEST Flood Mitigation model."""
+
+def execute(args):
+    """Perform time-domain calculations to estimate the flow of water across a
+    landscape in a flood situation.  The following files are saved to the user's
+    disk, relative to the defined workspace:
+
+        Rasters produced while calculating the Soil and Water Conservation
+        Service's stormflow estimation model:
+        <workspace>/intermediate/cn_season_adjusted.tif
+        <workspace>/intermediate/cn_slope_adjusted.tif
+        <workspace>/intermediate/soil_water_retention.tif
+        <workspace>/output/runoff_depth.tif
+
+
+
+        """
