356ae94fff20c9bdf42fb04a973bb64a71d17cbf
richpsharp <devnull@localhost>
2013-04-17 14:44:26 -0700

    Adding colored circles that scale based on max values.
    Former-commit-id: ad4a6b7a503937ed7f5d4d5ce602582e1164ffb5

--- invest_natcap/habitat_risk_assessment/plots.py
+++ invest_natcap/habitat_risk_assessment/plots.py
@@ -20,16 +20,29 @@ the_dicto = {
 }
 
 
+def plot_background_circle(max_value):
+    
+    circle_stuff = [(5, '#C44539'), (4.75, '#CF5B46'), (4.5, '#D66E54'), (4.25, '#E08865'), (4, '#E89D74'), (3.75, '#F0B686'), (3.5, '#F5CC98'), (3.25, '#FAE5AC'), (3, '#FFFFBF'), (2.75, '#EAEBC3'), (2.5, '#CFD1C5'), (2.25, '#B9BEC9'), (2, '#9FA7C9'), (1.75, '#8793CC'), (1.5, '#6D83CF'), (1.25, '#5372CF'), (1, '#305FCF')]
+ 
+    index = 0
+    for radius, color in circle_stuff:
+        index += 1
+        linestyle = 'solid' if index % 2 == 0 else 'dashed'
+        cir = matplotlib.pyplot.Circle((0,0), edgecolor='.25', linestyle=linestyle, radius=radius*max_value/5.0, fc=color)
+        matplotlib.pyplot.gca().add_patch(cir)
+
 plot_index = 0
+max_risk_value = 5
 for hab_name, stressor_dict in the_dicto.iteritems():
     for stressor_name, aoi_list in stressor_dict.iteritems():
         plot_index += 1
         matplotlib.pyplot.subplot(2, 2, plot_index)
+        plot_background_circle(max_risk_value)
         for aoi_dict in aoi_list:
             matplotlib.pyplot.plot(aoi_dict['E'], aoi_dict['C'], 'k^', markerfacecolor='black', markersize=8)
             matplotlib.pyplot.annotate(aoi_dict['Name'], xy=(aoi_dict['E'], aoi_dict['C']), xytext=(aoi_dict['E'], aoi_dict['C']+0.07))
         matplotlib.pyplot.title(hab_name + stressor_name)
-        matplotlib.pyplot.xlim([1, 5])
-        matplotlib.pyplot.ylim([1, 5])
+        matplotlib.pyplot.xlim([0.5, max_risk_value])
+        matplotlib.pyplot.ylim([0.5, max_risk_value])
 
 matplotlib.pyplot.show()
