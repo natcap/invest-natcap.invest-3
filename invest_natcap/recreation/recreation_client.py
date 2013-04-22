5469dc694ad7e72afba9588b87f78682abf66034
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2013-04-22 14:42:41 -0700

    Pylinted.
    Former-commit-id: a7edf58c7afff157e62ceb0e12a107b58548f47f

--- invest_natcap/recreation/recreation_client.py
+++ invest_natcap/recreation/recreation_client.py
@@ -1,3 +1,5 @@
+"""The front end to the recreation server side model.
+"""
 import os
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
@@ -16,11 +18,13 @@ from invest_natcap.raster_utils import temporary_filename
 import invest_natcap
 
 logging.basicConfig(format = '%(asctime)s %(name)-20s %(levelname)-8s \
-%(message)s', level = logging.DEBUG, datefmt = '%m/%d/%Y %H:%M:%S ')
+%(message)s', level = logging.INFO, datefmt = '%m/%d/%Y %H:%M:%S ')
 
 LOGGER = logging.getLogger('recreation_client_init')
 
 def urlopen(url, request, tries = 3, delay = 15, log = LOGGER):
+    """An error tolerant URL opener with retries and delays.
+    """
     for attempt in range(tries):
         log.info("Trying URL: %s.", url)
         try:
@@ -37,6 +41,8 @@ def urlopen(url, request, tries = 3, delay = 15, log = LOGGER):
     return msg
 
 def relogger(log, msg_type, msg):
+    """Logs a message to a logger using the method named by a string.
+    """
     msg_type = msg_type.strip()
     if msg_type == "INFO":
         log.info(msg)
@@ -50,7 +56,10 @@ def relogger(log, msg_type, msg):
     else:
         log.warn("Unknown logging message type %s: %s" % (msg_type, msg))    
 
-def log_check(url, flag = "Dropped intermediate tables.", delay = 15, log = LOGGER, line = 0):
+def log_check(url, flag = "Dropped intermediate tables.", delay = 15,
+              log = LOGGER, line = 0):
+    """Echoes a remote log until the flag message is found.
+    """
     log.debug("Begin log check at line %i.", line)
     complete = False
     count = 0
@@ -59,10 +68,13 @@ def log_check(url, flag = "Dropped intermediate tables.", delay = 15, log = LOGG
         server_log = urllib2.urlopen(url).read()
         server_log = server_log.strip().split("\n")
 
-        if (len(server_log) > 0) and (server_log[-1] != "") and (server_log[-1][-1] != "."):
+        if (len(server_log) > 0) and \
+           (server_log[-1] != "") and \
+           (server_log[-1][-1] != "."):
             server_log.pop(-1)
         
-        if (len(server_log[line + count:])) > 0 and (server_log[line + count:] != [""]):
+        if (len(server_log[line + count:]) > 0) and \
+           (server_log[line + count:] != [""]):
             for entry in server_log[line + count:]:
                 log.debug("Parsing log entry: %s.", repr(entry))
                 msg_type, msg = entry.split(",")[1:]
@@ -72,7 +84,7 @@ def log_check(url, flag = "Dropped intermediate tables.", delay = 15, log = LOGG
                     log.debug("Detected log check exit message.")
                     complete = True
                     if len(server_log[line + count:]) > 0:
-                        log.warn("There are unwritten messages on the remote log.")
+                        log.warn("Unwritten messages on the remote log.")
                     break
             
         else:
@@ -85,21 +97,26 @@ def log_check(url, flag = "Dropped intermediate tables.", delay = 15, log = LOGG
     return line
 
 def complete_shapefile(shapefile_name):
-    file_base = os.path.splitext(shapefile_name)[0]
+    """Checks that there is a .shp, .shx, .dbf, and .prj file by the same name.
+    """
+    file_name_base = os.path.splitext(shapefile_name)[0]
 
     complete = True
     for file_extension in ["shp", "shx", "dbf", "prj"]:
-        LOGGER.debug(os.extsep.join([file_base,file_extension]))
-        complete &= os.path.exists(os.extsep.join([file_base,file_extension]))
+        LOGGER.debug(os.extsep.join([file_name_base, file_extension]))
+        complete &= os.path.exists(os.extsep.join([file_name_base,
+                                                   file_extension]))
 
     return complete
             
-def execute(args):    
+def execute(args):
+    """The main function called by IUI.
+    """
     # Register the streaming http handlers with urllib2
     register_openers()
 
     #load configuration
-    config_file_name = os.path.dirname(os.path.abspath(__file__)) + os.sep + "recreation_client_config.json"
+    config_file_name = os.path.splitext(__file__)[0] + "_config.json"
     LOGGER.debug("Loading server configuration from %s.", config_file_name)
     config_file = open(config_file_name, 'r')
     config = json.loads(config_file.read())
@@ -127,11 +144,13 @@ def execute(args):
     args["sessid"] = sessid
 
     #check log and echo messages while not done
-    log_url = config["server"] + "/" + config["paths"]["relative"]["data"] + "/" + args["sessid"] + "/" + config["files"]["log"]
+    session_path = config["server"] + "/" + config["paths"]["relative"]["data"] + "/" + args["sessid"] + "/"
+    log_url = session_path + config["files"]["log"]
     log_line = 0
     
     LOGGER.info("Checking version.")
-    log_line = log_check(log_url, "End version PHP script.", 15, LOGGER, log_line)
+    log_line = log_check(log_url, "End version PHP script.", 15, LOGGER,
+                         log_line)
     
     LOGGER.info("Finished checking version.")
 
@@ -139,7 +158,8 @@ def execute(args):
     #VALIDATING MODEL PARAMETERS
     LOGGER.debug("Processing parameters.")
     if not args["mode"] in ["initial", "scenario"]:
-        raise ValueError, "The mode must be specified as \'initial\' or \'scenario\'."
+        msg = "The mode must be specified as \'initial\' or \'scenario\'."
+        raise ValueError, msg
 
     #adding os separator to paths if needed
     if args["workspace_dir"][-1] != os.sep:
@@ -151,7 +171,7 @@ def execute(args):
     #validating shapefile
     if args["mode"] == "initial":
         LOGGER.info("Validating AOI.")
-	if not complete_shapefile(args["aoi_file_name"]):
+        if not complete_shapefile(args["aoi_file_name"]):
             raise ValueError, "The AOI shapefile is missing a component."
 
     #scanning data directory for shapefiles
@@ -163,22 +183,28 @@ def execute(args):
             file_name_stem, file_extension = os.path.splitext(file_name)
             #checking for complete shapefile
             if file_extension == ".shp":
-                if os.path.exists(args["data_dir"] + file_name_stem + ".shx") and \
-                os.path.exists(args["data_dir"] + file_name_stem + ".shp") and \
-                os.path.exists(args["data_dir"] + file_name_stem + ".prj"):
+                if complete_shapefile(os.path.join(args["data_dir"],
+                                                   file_name)):
                     LOGGER.info("Found %s predictor.", file_name_stem)
                     predictors.append(file_name_stem)
                 else:
-                    LOGGER.error("Predictor %s is missing file(s).", file_name_stem)
+                    LOGGER.error("Predictor %s is missing file(s).",
+                                 file_name_stem)
             #checking if categorization table
             elif file_extension == ".tsv":
                 LOGGER.info("Found %s categorization.", file_name_stem)
                 user_categorization.append(file_name_stem)
 
+##    for cat_table in set(user_categorization).difference(predictors):
+##        msg = "Categorization table %s has no predictor and will be ignored."
+##        LOGGER.warn(msg, cat_table)
+##        user_categorization.pop(user_categorization.index(cat_table))
+
     #making sure there is not landscan categorization
-    if "landscan.tsv" in user_categorization:
-        LOGGER.error("The categorization of the Landscan data is not allowed.")
-        raise ValueError, "The categorization of the Landscan data is not allowed."
+    if "landscan" in user_categorization:
+        msg = "The categorization of the Landscan data is not allowed."
+        LOGGER.error(msg)
+        raise ValueError, msg
 
     #UPLOADING PREDICTORS
     LOGGER.info("Opening predictors for uploading.")
@@ -199,14 +225,20 @@ def execute(args):
         LOGGER.debug("Predictors will not be compressed.")
     
     for predictor in predictors:
-        zip_file.write(args["data_dir"] + predictor + ".shp", predictor + ".shp", compression)
-        zip_file.write(args["data_dir"] + predictor + ".shx", predictor + ".shx", compression)
-        zip_file.write(args["data_dir"] + predictor + ".dbf", predictor + ".dbf", compression)
-        zip_file.write(args["data_dir"] + predictor + ".prj", predictor + ".prj", compression)
+        predictor_base = args["data_dir"] + predictor
+        zip_file.write(predictor_base + ".shp", predictor + ".shp",
+                       compression)
+        zip_file.write(predictor_base + ".shx", predictor + ".shx",
+                       compression)
+        zip_file.write(predictor_base + ".dbf", predictor + ".dbf",
+                       compression)
+        zip_file.write(predictor_base + ".prj", predictor + ".prj",
+                       compression)
         
     #opening categorization table
     for tsv in user_categorization:
-        zip_file.write(args["data_dir"] + tsv + ".tsv", tsv + ".tsv", compression)
+        zip_file.write(args["data_dir"] + tsv + ".tsv", tsv + ".tsv",
+                       compression)
 
     zip_file.close()
     attachments["zip_file"] = open(zip_file_uri, 'rb')
@@ -229,7 +261,8 @@ def execute(args):
     LOGGER.debug("Server session %s.", sessid)
 
     #wait for server to finish saving predictors.
-    log_line = log_check(log_url, "End predictors PHP script.", 15, LOGGER, log_line)
+    log_line = log_check(log_url, "End predictors PHP script.", 15, LOGGER,
+                         log_line)
 
     #EXECUTING SERVER SIDE PYTHON SCRIPT
     LOGGER.info("Running server side processing.")
@@ -239,10 +272,14 @@ def execute(args):
 
     if args["mode"] == "initial":
         base_file_name = str(os.path.splitext(args["aoi_file_name"])[0])
-        attachments["aoiSHP"] = open(os.extsep.join([base_file_name, "shp"]), "rb")
-        attachments["aoiSHX"] = open(os.extsep.join([base_file_name, "shx"]), "rb")
-        attachments["aoiDBF"] = open(os.extsep.join([base_file_name, "dbf"]), "rb")
-        attachments["aoiPRJ"] = open(os.extsep.join([base_file_name, "prj"]), "rb")
+        attachments["aoiSHP"] = open(os.extsep.join([base_file_name, "shp"]),
+                                     "rb")
+        attachments["aoiSHX"] = open(os.extsep.join([base_file_name, "shx"]),
+                                     "rb")
+        attachments["aoiDBF"] = open(os.extsep.join([base_file_name, "dbf"]),
+                                     "rb")
+        attachments["aoiPRJ"] = open(os.extsep.join([base_file_name, "prj"]),
+                                     "rb")
     elif args["mode"] == "scenario":
         attachments["init"] = open(args["json"],'rb')
     else:
@@ -268,7 +305,8 @@ def execute(args):
 
     #check log and echo messages while not done
     LOGGER.info("Model running.")
-    log_line = log_check(log_url, "Dropped intermediate tables.", 15, LOGGER, log_line)
+    log_line = log_check(log_url, "Dropped intermediate tables.", 15, LOGGER,
+                         log_line)
     
     LOGGER.info("Finished processing data.")
 
@@ -288,7 +326,8 @@ def execute(args):
     sessid = urlopen(url, request, config["tries"], config["delay"], LOGGER)
 
     #check log and echo messages while not done
-    log_line = log_check(log_url, "End regression PHP script.", 15, LOGGER, log_line)
+    log_line = log_check(log_url, "End regression PHP script.", 15, LOGGER,
+                         log_line)
 
     #ZIP SERVER SIDE RESULTS
     url = config["server"] + config["files"]["PHP"]["results"]
@@ -303,13 +342,13 @@ def execute(args):
     sessid = urlopen(url, request, config["tries"], config["delay"], LOGGER)
     
     #download results
-    url = config["server"] + config["paths"]["relative"]["data"] + sessid + "/" + config["files"]["results"]
+    url = session_path + config["files"]["results"]
     LOGGER.info("URL: %s.", url)
 
     req = urllib2.urlopen(url)
     chunk_size = 16 * 1024
     zip_file_name_format = args["workspace_dir"] + "results%s.zip"
-    zip_file_name = zip_file_name_format %\
+    zip_file_name = zip_file_name_format % \
                     datetime.datetime.now().strftime("-%Y-%m-%d--%H_%M_%S")
     with open(zip_file_name, 'wb') as zip_file:
         while True:
