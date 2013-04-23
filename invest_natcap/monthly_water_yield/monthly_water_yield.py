039ba1cec1751b8a2346c4a1c1204ea8ddcbea4e
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-04-23 15:09:32 -0700

    Update issue 1904
    Adding comments and what I think needs to be done for each time step.
    
    This should be good enough for this issue. Further issues will become their own
    
    
    Former-commit-id: 62f1ad014a5930195df0bed771be85ce21983819

--- invest_natcap/monthly_water_yield/monthly_water_yield.py
+++ invest_natcap/monthly_water_yield/monthly_water_yield.py
@@ -19,3 +19,32 @@ def execute(args):
     
     """
     LOGGER.debug('Start Executing Model')
+
+    # Get input URIS
+
+
+
+    # Process data from time_step_data
+        # Read precip data into a dictionary
+        # 
+
+    # Make point shapefiles based on the current time step
+
+    # Use vectorize points to construct rasters based on points and fields
+
+    # Calculate Evapotranspiration
+
+    # Calculate Direct Flow (Runoff)
+
+    # Calculate Interflow
+
+    # Calculate Baseflow
+
+    # Calculate Streamflow
+
+    # Calculate Soil Moisture for current time step, to be used as previous time
+    # step in the next iteration
+
+    # Add values to output table
+
+    # Move on to next month
