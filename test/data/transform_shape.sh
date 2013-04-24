79d64fea6989b46d15ccfea67319716864848182
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-04-24 13:09:19 -0700

    Modifying transform_shape script
    Adding better comments and an example.
    
    Making it so it only takes in two arguments, the source shape and the out ESPG
    code. Makes the output uri on the fly
    
    
    Former-commit-id: 73cb0cadc76293a35c305b8f149e63a2ee229280

--- test/data/transform_shape.sh
+++ test/data/transform_shape.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-#SHPFILECLIP - THE SHAPE TO CLIP FROM
-SHPFILECLIP=$1
-#SHPFILEORIG - INPUT SHAPE TO CLIP
-SHPFILEORIG=$2
-CODE=$3
+#SHPFILEORIG - The source datasource name
+SHPFILEORIG=$1
+#$CODE - ESPG code to project to
+CODE=$2
 
-ogr2ogr -t_srs EPSG:$CODE ./transformed/`basename $SHPFILECLIP .shp`_transformed.shp $SHPFILEORIG
+# You must have a directory in the location of the script call 'transformed'.
+# The code below will output the projected datasource into that folder using the
+# same name as the input file, but appended with _transformed. This should
+# better allow for batching over multiple files
+
+# example : ./transform_shape.sh ../data/cool_shape.shp 4326
+# output location : ./transform/cool_shape_transform.shp
+
+ogr2ogr -t_srs EPSG:$CODE ./transformed/`basename $SHPFILEORIG .shp`_transformed.shp $SHPFILEORIG
