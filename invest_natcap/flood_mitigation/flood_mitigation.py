b8f0855999474a14aefdbcd9ada6ffc48c4eaf90
James Douglass <jdouglass@stanford.edu>
2013-04-26 10:28:59 -0700

    Update issue 1905
    Adding suffix information to the docstring.
    
    
    Former-commit-id: 72234dbb1d860a2a5f62ed86a3d6da5ecd63faa6

--- invest_natcap/flood_mitigation/flood_mitigation.py
+++ invest_natcap/flood_mitigation/flood_mitigation.py
@@ -61,43 +61,45 @@ def execute(args):
         'cn_amc_class' - A string indicating the Antecedent Soil Moisture class
             that should be used for CN adjustment.  One of ['Wet', 'Dry',
             'Average'].  Required only if args['cn_adjust'] == True.
+        'suffix' - (optional) a string to add to the end of all outputs from
+            this model
 
     The following files are saved to the user's disk, relative to the defined
     workspace:
         Rasters produced during preprocessing:
-        <workspace>/intermediate/mannings_coeff.tif
+        <workspace>/intermediate/mannings_coeff_<suffix>.tif
             A raster of the user's input Land Use/Land Cover, reclassified
             according to the user's defined table of Manning's numbers.
-        <workspace>/intermediate/slope.tif
+        <workspace>/intermediate/slope_<suffix>.tif
             A raster of the slope on the landscape.  Calculated from the DEM
             provided by the user as input to this model.
-        <workspace>/intermediate/flow_length.tif
+        <workspace>/intermediate/flow_length_<suffix>.tif
             TODO: Figure out if this is even an output raster.
-        <workspace>/intermediate/flow_direction.tif
+        <workspace>/intermediate/flow_direction_<suffix>.tif
             TODO: Figure out if this is even an output raster.
-        <workspace>/intermediate/fractional_flow.tif
+        <workspace>/intermediate/fractional_flow_<suffix>.tif
             TODO: Figure out if this is even an output raster.
 
         Rasters produced while calculating the Soil and Water Conservation
         Service's stormflow estimation model:
-        <workspace>/intermediate/cn_season_adjusted.tif
+        <workspace>/intermediate/cn_season_adjusted_<suffix>.tif
             A raster of the user's Curve Numbers, adjusted for the user's
             specified seasonality.
-        <workspace>/intermediate/cn_slope_adjusted.tif
+        <workspace>/intermediate/cn_slope_adjusted_<suffix>.tif
             A raster of the user's Curve Numbers that have been adjusted for
             seasonality and then adjusted for slope.
-        <workspace>/intermediate/soil_water_retention.tif
+        <workspace>/intermediate/soil_water_retention_<suffix>.tif
             A raster of the capcity of a given pixel to retain water in the
             soils on that pixel.
-        <workspace>/output/<time_step>/runoff_depth.tif
+        <workspace>/output/<time_step>/runoff_depth_<suffix>.tif
             A raster of the storm runoff depth per pixel in this timestep.
 
         Rasters produced while calculating the flow of water on the landscape
         over time:
-        <workspace>/output/<time_step>/floodwater_discharge.tif
+        <workspace>/output/<time_step>/floodwater_discharge_<suffix>.tif
             A raster of the floodwater discharge on the landscape in this time
             interval.
-        <workspace>/output/<time_step>/hydrograph.tif
+        <workspace>/output/<time_step>/hydrograph_<suffix>.tif
             A raster of the height of flood waters on the landscape at this time
             interval.
 
