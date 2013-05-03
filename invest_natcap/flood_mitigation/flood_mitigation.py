ec1558224c69ca26eaa7c99a5cee895acff05994
James Douglass <jdouglass@stanford.edu>
2013-05-03 11:49:56 -0700

    Update issue 1967
    Adding the beginning of the arrival time function.
    
    
    Former-commit-id: af46c16075927a2a805bde38ba85d3f56b1798e5

--- invest_natcap/flood_mitigation/flood_mitigation.py
+++ invest_natcap/flood_mitigation/flood_mitigation.py
@@ -863,4 +863,10 @@ def arrival_time(dem_uri, travel_time_uri, timestep, time_interval, output_uri):
 
         This function returns nothing.
         """
-    pass
+
+    flow_accumulation_uri = raster_utils.temporary_filename()
+    travel_time_pixel_size = raster_utils.get_cell_size_from_uri(travel_time_uri)
+
+    def _vectorized_arrival_time(travel_time):
+        """The per-pixel component of the arrival time calculations."""
+        return travel_time + ((timestep - 1) * time_interval)
