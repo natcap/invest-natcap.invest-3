70fcd318444862f98db7fa5755d45424bb3fdcf1
James Douglass <jdouglass@stanford.edu>
2013-05-03 11:54:21 -0700

    Update issue 1967
    Adding a call to vectorize_datasets to calculate the per-pixel stuff
    before calling down to route the flux across the landscape.
    
    
    Former-commit-id: 1a79a1235d6eeb446799c93d2d1d0bedac3e71de

--- invest_natcap/flood_mitigation/flood_mitigation.py
+++ invest_natcap/flood_mitigation/flood_mitigation.py
@@ -864,6 +864,7 @@ def arrival_time(dem_uri, travel_time_uri, timestep, time_interval, output_uri):
         This function returns nothing.
         """
 
+    modified_travel_time_uri = raster_utils.temporary_filename()
     flow_accumulation_uri = raster_utils.temporary_filename()
     travel_time_nodata = raster_utils.get_nodata_from_uri(travel_time_uri)
     travel_time_pixel_size = raster_utils.get_cell_size_from_uri(travel_time_uri)
@@ -873,3 +874,7 @@ def arrival_time(dem_uri, travel_time_uri, timestep, time_interval, output_uri):
         if travel_time == travel_time_nodata:
             return travel_time_nodata
         return travel_time + ((timestep - 1) * time_interval)
+
+    raster_utils.vectorize_datasets([travel_time_nodata],
+        _vectorized_arrival_time, modified_travel_time_uri, gdal.GDT_Float32,
+        travel_time_nodata, travel_time_pixel_size, 'intersection')
