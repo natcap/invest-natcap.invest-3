3b68a548d198ec6156797c34ae4bbcf69eb8d0be
James Douglass <jdouglass@stanford.edu>
2013-05-14 14:33:38 -0700

    Update issue 1890
    Adding a check that all matrixes input to _calculate_fid are the same
    shape.
    
    
    Former-commit-id: f54e7a50c0eca3ac88a012592f8571f7d6e0a8ed

--- invest_natcap/flood_mitigation/flood_mitigation.py
+++ invest_natcap/flood_mitigation/flood_mitigation.py
@@ -875,6 +875,7 @@ def _calculate_fid(flood_height, dem, channels, curve_nums, outflow_direction,
     visited = numpy.zeros(flood_height_matrix.shape, dtype=numpy.int)
     travel_distance = numpy.zeros(flood_height_matrix.shape, dtype=numpy.float)
 
+    matrix_shape = flood_height_matrix.shape
     for name, matrix, nodata in [
         ('flood height', flood_height_matrix, flood_height_nodata),
         ('dem', dem_matrix, dem_nodata),
@@ -886,6 +887,9 @@ def _calculate_fid(flood_height, dem, channels, curve_nums, outflow_direction,
         ('travel distance', travel_distance, None)]:
         LOGGER.debug('Matrix %-20s size=%-16s nodata=%-10s', name, matrix.shape,
             nodata)
+        assert matrix.shape == matrix_shape, ('Input rasters must all be the '
+            'same size.  %s, %s found.' % matrix.shape, matrix_shape)
+
 
     # to track our nearest channel cell, create a matrix that has two values for
     # each of the elements in the 2-d matrix.  These two extra values represent
