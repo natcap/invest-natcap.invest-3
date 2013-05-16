78f399186bd3450eef35bdec4d53f0511c5434c7
James Douglass <jdouglass@stanford.edu>
2013-05-16 14:58:56 -0700

    Update issue 2001
    Removing the non-cython version of the FID function.
    
    
    Former-commit-id: a710f668ae765ec01f9ef5bd2df706d4d5415be9

--- invest_natcap/flood_mitigation/flood_mitigation.py
+++ invest_natcap/flood_mitigation/flood_mitigation.py
@@ -828,7 +828,7 @@ def flood_height(discharge_uri, mannings_uri, slope_uri, output_uri):
         'intersection')
 
 def flood_inundation_depth(flood_height_uri, dem_uri, cn_uri,
-    channels_uri, outflow_direction_uri, output_uri, cython=False):
+    channels_uri, outflow_direction_uri, output_uri):
     """This function estimates flood inundation depth from flood height,
         elevation, and curve numbers.  This is equation 20 from the flood
         mitigation user's guide.
@@ -855,12 +855,8 @@ def flood_inundation_depth(flood_height_uri, dem_uri, cn_uri,
     pixel_size = raster_utils.get_cell_size_from_uri(outflow_direction_uri)
 
     LOGGER.info('Distributing flood waters')
-    if cython:
-        fid_matrix = flood_mitigation_cython_core.calculate_fid(flood_height_tuple, dem_tuple,
-            channel_tuple, cn_tuple, outflow_direction_tuple, pixel_size)
-    else:
-        fid_matrix = _calculate_fid(flood_height_tuple, dem_tuple,
-            channel_tuple, cn_tuple, outflow_direction_tuple, pixel_size)
+    fid_matrix = flood_mitigation_cython_core.calculate_fid(flood_height_tuple, dem_tuple,
+        channel_tuple, cn_tuple, outflow_direction_tuple, pixel_size)
     LOGGER.info('Finished distributing flood waters')
 
     raster_utils.new_raster_from_base_uri(dem_uri, output_uri, 'GTiff', -1,
@@ -868,178 +864,3 @@ def flood_inundation_depth(flood_height_uri, dem_uri, cn_uri,
     _write_matrix(output_uri, fid_matrix)
     LOGGER.debug('Finished calculating flood inundation depth')
 
-def _calculate_fid(flood_height, dem, channels, curve_nums, outflow_direction,
-    pixel_size):
-    """Actually perform the matrix calculations for the flood inundation depth
-        function.  This is equation 20 from the flood mitigation user's guide.
-
-        flood_height - a numpy matrix of flood water heights.
-        dem - a numpy matrix of elevations.
-        channels - a numpy matrix of channels.
-        curve_nums - a numpy matrix of curve numbers.
-        outflow_direction - a numpy matrix indicating which pixels flow into one
-            another.  See routing_utils for details on this matrix.
-        pixel_size -a numpy indicating the mean of the height and width of a
-            pixel.
-
-        All matrices MUST have the same sizes.
-
-        Returns a numpy matrix of the calculated flood inundation height."""
-
-    flood_height_matrix, flood_height_nodata = flood_height
-    dem_matrix, dem_nodata = dem
-    channels_matrix, channels_nodata = channels
-    cn_matrix, cn_nodata = curve_nums
-    outflow_direction_matrix, outflow_direction_nodata = outflow_direction
-
-    output = numpy.copy(flood_height_matrix)
-    visited = numpy.zeros(flood_height_matrix.shape, dtype=numpy.int)
-    travel_distance = numpy.zeros(flood_height_matrix.shape, dtype=numpy.float)
-
-    matrix_shape = flood_height_matrix.shape
-    for name, matrix, nodata in [
-        ('flood height', flood_height_matrix, flood_height_nodata),
-        ('dem', dem_matrix, dem_nodata),
-        ('channels', channels_matrix, channels_nodata),
-        ('curve numbers', cn_matrix, cn_nodata),
-        ('outflow direction',outflow_direction_matrix, outflow_direction_nodata),
-        ('output', output, -1),
-        ('visited', visited, None),
-        ('travel distance', travel_distance, None)]:
-        LOGGER.debug('Matrix %-20s size=%-16s nodata=%-10s', name, matrix.shape,
-            nodata)
-        assert matrix.shape == matrix_shape, ('Input rasters must all be the '
-            'same size.  %s, %s found.' % matrix.shape, matrix_shape)
-
-
-    # to track our nearest channel cell, create a matrix that has two values for
-    # each of the elements in the 2-d matrix.  These two extra values represent
-    # the index of the closes channel cell.
-#    nearest_channel = numpy.zeros(flood_height_matrix.shape + (2,),
-#        dtype=numpy.int)
-
-    # We know the diagonal distance thanks to trigonometry.  We're assuming that
-    # we measure from the center of this pixel to the center of the neighboring
-    # pixel.
-    diagonal_distance = pixel_size * math.sqrt(2)
-    indices = [
-        (0, (0, 1), pixel_size),
-        (1, (-1, 1), diagonal_distance),
-        (2, (-1, 0), pixel_size),
-        (3, (-1, -1), diagonal_distance),
-        (4, (0, -1), pixel_size),
-        (5, (1, -1), diagonal_distance),
-        (6, (1, 0), pixel_size),
-        (7, (1, 1), diagonal_distance)
-    ]
-
-    def _flows_from(source_index, neighbor_id):
-        """Indicate whether the source pixel flows into the neighbor identified
-        by neighbor_id.  This function returns a boolean."""
-
-        neighbor_value = outflow_direction_matrix[source_index]
-
-        # If there is no outflow direction, then there is no flow to the
-        # neighbor and we return False.
-        if neighbor_value == outflow_direction_nodata:
-            return False
-
-        if neighbor_id in INFLOW_NEIGHBORS[neighbor_value]:
-            return False
-        return True
-
-    def _fid(index, channel_floodwater, channel_elevation):
-        """Calculate the on-pixel flood inundation depth, as represented by
-            equation 20 in the flood mitigation user's guide.
-
-            index - the tuple index of the pixel on which to calculate FID
-            channel_floodwater - the numeric depth of the closest channel cell's
-                floodwaters
-            channel_elevation - the numeric depth of the closest channel cell's
-                elevation
-
-            Note that for this equation to be accurate, the dem and the
-            floodwaters must be in the same units.
-
-            Returns a float."""
-        pixel_elevation = dem_matrix[index]
-        curve_num = cn_matrix[index]
-
-        # If there is a channel cell that has no flood inundation on it, we
-        # should reasonably assume that there will not be any flood waters
-        # distributed from that cell.
-        # NOTE: This behavior is not explicitly stated in the user's guide.
-        # TODO: Verify with Rich and/or Yonas that this behavior is correct
-        if channel_floodwater == 0:
-            return 0.0
-
-        if (channel_floodwater == flood_height_nodata or
-            pixel_elevation == dem_nodata or
-            curve_num == cn_nodata or
-            channel_elevation == dem_nodata):
-            return 0.0
-
-        elevation_diff = pixel_elevation - channel_elevation
-        flooding = channel_floodwater - elevation_diff - curve_num
-
-        if flooding <= 0:
-            return 0.0
-#        if flooding > channel_floodwater:
-#            LOGGER.debug(str('p_elevation=%s, cn=%s, c_floodwater=%s, fid=%s, '
-#                'c_elevation=%s'), pixel_elevation, curve_num,
-#                channel_floodwater, flooding, channel_elevation)
-        return flooding
-
-    iterator = numpy.nditer([channels_matrix, flood_height_matrix, dem_matrix],
-        flags=['multi_index'])
-
-    LOGGER.debug('Visiting channel pixels')
-    for channel, channel_floodwater, channel_elevation in iterator:
-        if channel == 1:
-            channel_index = iterator.multi_index
-            pixels_to_visit = collections.deque([channel_index])
-
-            visited[channel_index] = 1
-#            nearest_channel[channel_index][0] = channel_index[0]
-#            nearest_channel[channel_index][1] = channel_index[1]
-
-            while True:
-                try:
-                    pixel_index = pixels_to_visit.pop()
-                except IndexError:
-                    # No more indexes to process.
-                    break
-
-                for n_id, neighbor_offset, n_distance in indices:
-                    n_index = tuple(map(sum, zip(pixel_index, neighbor_offset)))
-
-                    try:
-                        if n_index[0] < 0 or n_index[1] < 0:
-                            raise IndexError
-
-                        if channels_matrix[n_index] in [1, channels_nodata]:
-                            raise SkipNeighbor
-
-                        if _flows_from(n_index, n_id):
-                            fid = _fid(n_index, channel_floodwater,
-                                channel_elevation)
-
-                            if fid > 0:
-#                                if fid > channel_floodwater:
-#                                    raise Exception('fid=%s, floodwater=%s' %
-#                                        (fid, channel_floodwater))
-                                dist_to_n = travel_distance[pixel_index] + n_distance
-                                if visited[n_index] == 0 or (visited[n_index] == 1 and
-                                    dist_to_n < travel_distance[n_index]):
-                                    visited[n_index] = 1
-                                    travel_distance[n_index] = dist_to_n
-#                                    nearest_channel[n_index][0] = channel_index[0]
-#                                    nearest_channel[n_index][1] = channel_index[1]
-                                    output[n_index] = fid
-                                    pixels_to_visit.append(n_index)
-
-                    except (SkipNeighbor, IndexError, AlreadyVisited):
-                        pass
-
-    LOGGER.debug('Finished visiting channel pixels')
-    return output
