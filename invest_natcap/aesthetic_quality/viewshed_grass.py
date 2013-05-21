4c5cb88ba41b04f9f1c40a6236248395a00aa516
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2013-05-21 10:19:14 -0700

    Why is IDLE double indenting a class?!? Shame!
    Former-commit-id: 3bcc9a6a4363e1ace034e88fb43f9148ea751657

--- invest_natcap/aesthetic_quality/viewshed_grass.py
+++ invest_natcap/aesthetic_quality/viewshed_grass.py
@@ -13,65 +13,31 @@ import grass.script
 import grass.script.setup
 
 class grasswrapper():
-        def __init__(self, dbBase="", location="/home/mlacayo/workspace/newLocation", mapset="PERMANENT"):
-                '''
-                Wrapper of "python.setup.init" defined in GRASS python.
-                Initialize system variables to run scripts without starting GRASS explicitly.
-                
-                @param dbBase: path to GRASS database (default: '').
-                @param location: location name (default: '').
-                @param mapset: mapset within given location (default: 'PERMANENT')
+    def __init__(self, dbBase="", location="/home/mlacayo/workspace/newLocation", mapset="PERMANENT"):
+        '''
+        Wrapper of "python.setup.init" defined in GRASS python.
+        Initialize system variables to run scripts without starting GRASS explicitly.
 
-                @return: Path to gisrc file.
-                '''
-                self.gisbase = os.environ['GISBASE']
-                self.gisdb = dbBase
-                self.loc = location
-                self.mapset = mapset
-                grass.script.setup.init(self.gisbase, self.gisdb, self.loc, self.mapset)
+        @param dbBase: path to GRASS database (default: '').
+        @param location: location name (default: '').
+        @param mapset: mapset within given location (default: 'PERMANENT')
+
+        @return: Path to gisrc file.
+        '''
+        self.gisbase = os.environ['GISBASE']
+        self.gisdb = dbBase
+        self.loc = location
+        self.mapset = mapset
+        grass.script.setup.init(self.gisbase, self.gisdb, self.loc, self.mapset)
 
 def execute(args):
+    pass
 
 def viewshed(input, output, coordinate, obs_elev=1.75, tgt_elev=0.0, memory=4098, overwrite=True, quiet=True):
     g.run_command('r.in.gdal',
                   'o', #overide projection
                   input=args["in_raster"],
                   output='dem')
-
-    g.run_command('v.in.ogr',
-                  'o', #overide projection
-                  input=args["in_observer_features"],
-                  output='features')
-                  
-                  
-
-    print "\n\n\tList Rasters"
-    rast_list = grass.script.parse_command('g.list',
-                                            type='rast')
-
-    print rast_list
-
-    print "\n\n\tRegion Settings"
-    grass.script.run_command('g.region',
-                  'pec')
-
-    #change the rsolution
-    print "\n\n\tSet Region Resolution"
-    grass.script.run_command('g.region',
-                  res='15')
-    print "\n\n\tRegion Settings"
-    grass.script.run_command('g.region',
-                  'pec')
-
-
-    ##g.run_command('g.copy',
-    ##              rast="pop_vs,pop_vs2")
-                    
-    ##g.run_command('r.resample',
-    ##              input="/home/mlacayo/Desktop/test_out/pop_vs.tif",
-    ####              "211084.959353,372584.959353,5356029.35524,5495529.35524",
-    ####              1000,
-    ##              output="/home/mlacayo/Desktop/resample.tif")
 
     ##g.run_command('r.viewshed',
     ##              input='/home/mlacayo/Desktop/aq_sample/Input/claybark_dem.tif',
