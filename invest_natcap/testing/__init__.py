5bc2802f124142317a89ffaecdb915f390c4c2e2
James Douglass <jdouglass@stanford.edu>
2013-05-20 17:47:01 -0700

    Update issue 2004
    Pylinting.
    
    
    Former-commit-id: db6d1c2ea8d29fd7c4d20e02dc23ba682e6837a8

--- invest_natcap/testing/__init__.py
+++ invest_natcap/testing/__init__.py
@@ -2,6 +2,13 @@
 functionality."""
 
 import unittest
+import os
+import logging
+
+import numpy
+from osgeo import gdal
+
+LOGGER = logging.getLogger('invest_natcap.testing')
 
 class GISTest(unittest.TestCase):
     """A test class for our GIS testing functions."""
@@ -15,14 +22,14 @@ class GISTest(unittest.TestCase):
 
             returns True if a and b are equal to each other"""
 
-        logger.debug('Asserting datasets A: %s, B: %s', aUri, bUri)
+        LOGGER.debug('Asserting datasets A: %s, B: %s', a_uri, b_uri)
 
-        for uri in [aUri, bUri]:
+        for uri in [a_uri, b_uri]:
             if not os.path.exists(uri):
                 raise IOError('File "%s" not found on disk' % uri)
 
-        a_dataset = gdal.Open(aUri)
-        b_dataset = gdal.Open(bUri)
+        a_dataset = gdal.Open(a_uri)
+        b_dataset = gdal.Open(b_uri)
 
         self.assertEqual(a_dataset.RasterXSize, b_dataset.RasterXSize,
             "x dimensions are different a=%s, second=%s" %
@@ -32,19 +39,20 @@ class GISTest(unittest.TestCase):
             (a_dataset.RasterYSize, b_dataset.RasterYSize))
         self.assertEqual(a_dataset.RasterCount, b_dataset.RasterCount,
             "different number of rasters a=%s, b=%s" % (
-            (a_dataset.RasterCount, b_dataset.RasterCount))
+            (a_dataset.RasterCount, b_dataset.RasterCount)))
 
-        for bandNumber in range(1, a_dataset.RasterCount + 1):
-            band_a = a_dataset.GetRasterBand(bandNumber)
-            band_a = b_dataset.GetRasterBand(bandNumber)
+        for band_number in range(1, a_dataset.RasterCount + 1):
+            band_a = a_dataset.GetRasterBand(band_number)
+            band_b = b_dataset.GetRasterBand(band_number)
 
             a_array = band_a.ReadAsArray(0, 0, band_a.XSize, band_a.YSize)
             b_array = band_a.ReadAsArray(0, 0, band_b.XSize, band_b.YSize)
 
             try:
-                np.testing.assert_array_almost_equal(aArray, bArray)
+                numpy.testing.assert_array_almost_equal(a_array, b_array)
             except AssertionError:
-                for row_index in xrange(bandA.YSize):
-                    for a, b in zip(aArray[row_index], bArray[row_index]):
-                        self.assertAlmostEqual(
-                            a, b, msg='%s != %s ... Failed at row %s' % (a, b, row_index))
+                for row_index in xrange(band_a.YSize):
+                    for pixel_a, pixel_b in zip(a_array[row_index], b_array[row_index]):
+                        self.assertAlmostEqual(pixel_a, pixel_b,
+                            msg='%s != %s ... Failed at row %s' %
+                            (pixel_a, pixel_b, row_index))
