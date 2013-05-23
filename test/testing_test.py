1bbffe64b57949acaf4f4e85f4def26ffbe2ed67
James Douglass <jdouglass@stanford.edu>
2013-05-23 09:31:45 -0700

    Update issue 2004
    Adding an assertion to test the MD5 hash of an input file against a
    user-defined MD5 hash.
    
    
    Former-commit-id: 9d77bc7d689100a8b30c6b82c20f9bfb81fd95d2

--- test/testing_test.py
+++ test/testing_test.py
@@ -1,5 +1,19 @@
 import unittest
+import os
+
+import invest_natcap.testing as testing
+from invest_natcap.testing import data_storage
+
+POLLINATION_DATA = os.path.join('data', 'pollination', 'samp_input')
 
 class DataStorageTest(unittest.TestCase):
     def test_collect_parameters_simple(self):
-        pass
+        params = {
+            'a': 1,
+            'b': 2,
+            'c': os.path.join(POLLINATION_DATA, 'LU.csv'),
+        }
+
+        archive_uri = os.path.join('data', 'test_out', 'archive')
+
+        data_storage.collect_parameters(params, archive_uri)
