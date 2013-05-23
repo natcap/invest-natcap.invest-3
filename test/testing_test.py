eff6d5aa995cbb88d47860c22c2068f54d12110b
James Douglass <jdouglass@stanford.edu>
2013-05-23 11:45:22 -0700

    Update issue 2004
    Adding regression test for a simple case of the parameter collection
    archive.
    
    
    Former-commit-id: 8c0e3f9a28f00cdf6d6b69fd18985b9a04b79a9f

--- test/testing_test.py
+++ test/testing_test.py
@@ -6,7 +6,7 @@ from invest_natcap.testing import data_storage
 
 POLLINATION_DATA = os.path.join('data', 'pollination', 'samp_input')
 
-class DataStorageTest(unittest.TestCase):
+class DataStorageTest(testing.GISTest):
     def test_collect_parameters_simple(self):
         params = {
             'a': 1,
@@ -17,3 +17,9 @@ class DataStorageTest(unittest.TestCase):
         archive_uri = os.path.join('data', 'test_out', 'archive')
 
         data_storage.collect_parameters(params, archive_uri)
+        archive_uri = archive_uri + '.tar.gz'
+
+        regression_archive_uri = os.path.join('data', 'data_storage',
+            'regression', 'collect_parameters_simple.tar.gz')
+
+        self.assertArchive(archive_uri, regression_archive_uri)
