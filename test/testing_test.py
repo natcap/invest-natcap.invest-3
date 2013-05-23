86673cb2530b279f4ee1c4278f49419f559d21d7
James Douglass <jdouglass@stanford.edu>
2013-05-23 12:54:44 -0700

    Update issue 2004
    Making a few top-level variables.
    
    
    Former-commit-id: f7f8c9d70597be7e3fc9c0825c4e80cd95d9a51e

--- test/testing_test.py
+++ test/testing_test.py
@@ -5,6 +5,9 @@ import invest_natcap.testing as testing
 from invest_natcap.testing import data_storage
 
 POLLINATION_DATA = os.path.join('data', 'pollination', 'samp_input')
+REGRESSION_ARCHIVES = os.path.join('data', 'data_storage', 'regression')
+TEST_OUT = os.path.join('data', 'test_out')
+
 
 class DataStorageTest(testing.GISTest):
     def test_collect_parameters_simple(self):
@@ -14,12 +17,12 @@ class DataStorageTest(testing.GISTest):
             'c': os.path.join(POLLINATION_DATA, 'LU.csv'),
         }
 
-        archive_uri = os.path.join('data', 'test_out', 'archive')
+        archive_uri = os.path.join(TEST_OUT, 'archive')
 
         data_storage.collect_parameters(params, archive_uri)
         archive_uri = archive_uri + '.tar.gz'
 
-        regression_archive_uri = os.path.join('data', 'data_storage',
-            'regression', 'collect_parameters_simple.tar.gz')
+        regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
+            'collect_parameters_simple.tar.gz')
 
         self.assertArchive(archive_uri, regression_archive_uri)
