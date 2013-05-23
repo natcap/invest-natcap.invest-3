ea78ac8f0dd042996870a51ee1118b54e6983e4a
James Douglass <jdouglass@stanford.edu>
2013-05-23 13:53:00 -0700

    Update issue 2004
    Refactoring the parameter archiving function to recurse through a
    multi-level dictionary/list data structure.
    
    
    Former-commit-id: 9a368fe9e0766511a845d40e2df4403d752c2282

--- invest_natcap/testing/data_storage.py
+++ invest_natcap/testing/data_storage.py
@@ -48,22 +48,44 @@ def collect_parameters(parameters, archive_uri):
 
     temp_workspace = raster_utils.temporary_folder()
 
-    new_args = {}
+    def get_if_file(parameter):
+        try:
+            if os.path.exists(parameter):
+                new_filename = os.path.basename(parameter)
+                shutil.copyfile(parameter, os.path.join(temp_workspace,
+                    new_filename))
+                return new_filename
+        except TypeError:
+            # When the value is not a string.
+            pass
+        return parameter
+
+    def collect_list(parameter_list):
+        new_list = []
+        for parameter in parameter_list:
+            new_list.append(types[parameter.__class__](parameter))
+        return new_list
+
+    def collect_dict(parameter_dict):
+        new_dict = {}
+        for key, value in parameter_dict.iteritems():
+            new_dict[key] = types[value.__class__](value)
+        return new_dict
+
+    types = {
+        list: collect_list,
+        dict: collect_dict,
+        str: get_if_file,
+        unicode: get_if_file,
+        int: lambda x: x,
+        float: lambda x: x,
+    }
 
     # Recurse through the parameters to locate any URIs
     #   If a URI is found, copy that file to a new location in the temp
     #   workspace and update the URI reference.
     #   Duplicate URIs should also have the same replacement URI.
-    for key, value in parameters.iteritems():
-        try:
-            if os.path.exists(value):
-                new_filename = os.path.basename(value)
-                new_args[key] = new_filename
-                shutil.copyfile(value, os.path.join(temp_workspace,
-                    new_filename))
-        except TypeError:
-            # When the value is not a string.
-            new_args[key] = value
+    new_args = collect_dict(parameters)
 
     LOGGER.debug('new arguments: %s', new_args)
     # write parameters to a new json file in the temp workspace
