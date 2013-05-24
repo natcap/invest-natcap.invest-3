56ba2a489a313bf2c80622841e85531c8d2bff24
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2013-05-24 15:47:06 -0700

    Update 1925
    Testing out GRASS viewshed call for a single point.
    
    
    Former-commit-id: 4470a2a13fd4451fd491355adf4abff630ad2aba

--- invest_natcap/aesthetic_quality/grass_examples.py
+++ invest_natcap/aesthetic_quality/grass_examples.py
@@ -30,36 +30,51 @@ class grasswrapper():
                                 self.loc,
                                 self.mapset)
 
+if __name__ == '__main__':
+    #get raster path from first parameter
+    dataset_uri = sys.argv[1]
 
-def raster_test(dataset_uri):
+    #get coordinate from second and third parameter
+    coordinate = [sys.argv[2], sys.argv[3]]
+
     #get user's home directory
     home = os.path.expanduser("~")
 
-    random_name = random_string(6)
+    random_input_name = random_string(6)
+    random_output_name = random_string(6)
 
     #determine GRASS database path
     temp_uri = grass.script.parse_command('g.tempfile',
                                           pid = 1).keys()[0]
    
     database_uri = temp_uri.split('.tmp')[0]
-    location_uri = os.path.join(database_uri, random_name)
+    location_uri = os.path.join(database_uri, random_input_name)
 
     #create location with raster's reference system
     print 'Creating location %s with raster\'s reference system' % location_uri
     grass.script.run_command('g.proj',
                              'c',
                              georef = dataset_uri,
-                             location = random_name)
+                             location = random_input_name)
 
     #import raster into GRASS
     print 'Importing raster from ', dataset_uri
     grass.script.run_command('r.in.gdal',
                              input = dataset_uri,
-                             output = random_name)
+                             output = random_input_name)
+
+    #calculate viewshed
+    print 'Calculating viewshed'
+    grass.script.run_command('r.viewshed',
+                             'c',
+                             'b',
+                             input = random_input_name,
+                             output = random_output_name,
+                             coordinate = coordinate)
 
     #export raster from GRASS
     dataset_out_uri = os.path.join(home,
-                                   "%s.tif" % random_name)
+                                   "%s.tif" % random_output_name)
     print 'Saving raster to ', dataset_out_uri
     grass.script.run_command('r.out.tiff',
                              input = 'dem',
@@ -68,9 +83,13 @@ def raster_test(dataset_uri):
     #remove imported raster from GRASS
     print 'Removing imported raster.'
     grass.script.run_command('g.remove',
-                             rast = random_name)
+                             rast = random_input_name)
+
+    #remove viewshed
+    print 'Removing viewshed.'
+    grass.script.run_command('g.remove',
+                             rast = random_output_name)
 
     #remove the location from disk
     print 'Removing location %s' % location_uri
     os.system("rm -rf %s" % location_uri)
-
