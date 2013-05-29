5942682e80039c414bbaac2381b7419e12e764c3
James Douglass <jdouglass@stanford.edu>
2013-05-29 08:45:19 -0700

    Update issue 2004
    Refactoring the regression build function to make it more functional.
    
    
    Former-commit-id: 9a863cde6bed6469a7734123379b0837f29e21bb

--- regression-build.py
+++ regression-build.py
@@ -5,8 +5,10 @@ import shutil
 
 json_file = sys.argv[1]
 
-INPUT_ARCHIVE = ''
-OUTPUT_ARCHIVE = ''
+#CONFIG_DATA = {
+#    'Input archive': '',
+#    'Output archive': '',
+#}
 
 def _set_archive_name(keyword):
     input_archive_name = raw_input('Path to the %s archive: ' % keyword)
@@ -34,32 +36,68 @@ def _set_archive_name(keyword):
 
 
 def set_input_archive_name():
-    INPUT_ARCHIVE = _set_archive_name('input')
+    CONFIG_DATA['Input archive']['path'] = _set_archive_name('input')
 
 def set_output_archive_name():
-    OUTPUT_ARCHIVE = _set_archive_name('output')
+    CONFIG_DATA['Output archive']['path'] = _set_archive_name('output')
+
+CONFIG_DATA = {
+    'Input archive': {
+        'path': '',
+        'function': set_input_archive_name,
+    },
+    'Output archive': {
+        'path': '',
+        'function': set_output_archive_name
+    },
+}
+
+def configure_settings():
+    settings = list(sorted(CONFIG_DATA.iteritems(), key=lambda x: x[0]))
+
+    for item_no, (label, data) in enumerate(settings):
+        print '[%s] %s: %s' % (item_no, label, data['path'])
+
+    input_selection = raw_input('Select an item to configure: ')
+
+    while int(input_selection) not in range(len(settings)):
+        input_selection = raw_input('Input must be an option above: ')
+
+    input_selection = int(input_selection)
+    settings[input_selection][1]['function']()
 
 
-TEST_DIR = 'test'
-MANAGED_DATA = os.path.join(TEST_DIR, 'data', 'managed_data')
-MANAGED_INPUT = os.path.join(MANAGED_DATA, 'input')
-MANAGED_OUTPUT = os.path.join(MANAGED_DATA, 'output')
 
-test_file = raw_input('Test file to use: ')
-test_file = os.path.basename(test_file)
 
-test_class_name = raw_input('Name of the new test class: ')
-test_func_name = raw_input('Name of the new test_function: ')
+def main():
+    finished = False
+    while not finished:
+        configure_settings()
 
-test_file = open(os.path.join(TEST_DIR, test_file), "a")
+if __name__ == '__main__':
+    main()
 
-input_archive = os.path.join(MANAGED_INPUT, '-'.join([test_class_name,
-    test_func_name]))
-output_archive = os.path.join(MANAGED_OUTPUT, '-'.join([test_class_name,
-    test_func_name]))
 
-invest_natcap.testing.build_regression_archives(json_file, input_archive,
-    output_archive)
+#TEST_DIR = 'test'
+#MANAGED_DATA = os.path.join(TEST_DIR, 'data', 'managed_data')
+#MANAGED_INPUT = os.path.join(MANAGED_DATA, 'input')
+#MANAGED_OUTPUT = os.path.join(MANAGED_DATA, 'output')
+#
+#test_file = raw_input('Test file to use: ')
+#test_file = os.path.basename(test_file)
+#
+#test_class_name = raw_input('Name of the new test class: ')
+#test_func_name = raw_input('Name of the new test_function: ')
+#
+#test_file = open(os.path.join(TEST_DIR, test_file), "a")
+#
+#input_archive = os.path.join(MANAGED_INPUT, '-'.join([test_class_name,
+#    test_func_name]))
+#output_archive = os.path.join(MANAGED_OUTPUT, '-'.join([test_class_name,
+#    test_func_name]))
+#
+#invest_natcap.testing.build_regression_archives(json_file, input_archive,
+#    output_archive)
 
 #TAB = '    '
 #lines_to_write = [
