c3bec313e792dfad952ff4132330592c48728037
James Douglass <jdouglass@stanford.edu>
2013-05-30 09:30:51 -0700

    Update issue 2004
    Updating the tests for testing the data_storage module.
    
    
    Former-commit-id: e1ec9f96c053ae250b65fff20ce1b3adc6febc6d

--- test/testing_test.py
+++ test/testing_test.py
@@ -28,7 +28,7 @@ class DataStorageTest(testing.GISTest):
         regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'collect_parameters_simple.tar.gz')
 
-        self.assertArchive(archive_uri, regression_archive_uri)
+        self.assertArchives(archive_uri, regression_archive_uri)
 
     def test_collect_parameters_nested_dict(self):
         params = {
@@ -50,7 +50,7 @@ class DataStorageTest(testing.GISTest):
         regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'simple_nested_dict.tar.gz')
 
-        self.assertArchive(archive_uri, regression_archive_uri)
+        self.assertArchives(archive_uri, regression_archive_uri)
 
     def test_archive_arc_raster_nice(self):
         params = {
@@ -63,7 +63,7 @@ class DataStorageTest(testing.GISTest):
         archive_uri += '.tar.gz'
         regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'arc_raster_nice.tar.gz')
-        self.assertArchive(archive_uri, regression_archive_uri)
+        self.assertArchives(archive_uri, regression_archive_uri)
 
     def test_archive_arc_raster_messy(self):
         params = {
@@ -77,7 +77,7 @@ class DataStorageTest(testing.GISTest):
         archive_uri += '.tar.gz'
         regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'arc_raster_messy.tar.gz')
-        self.assertArchive(archive_uri, regression_archive_uri)
+        self.assertArchives(archive_uri, regression_archive_uri)
 
     def test_archive_esri_shapefile(self):
         params = {
@@ -90,7 +90,7 @@ class DataStorageTest(testing.GISTest):
         archive_uri += '.tar.gz'
         regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'vector_collected.tar.gz')
-        self.assertArchive(archive_uri, regression_archive_uri)
+        self.assertArchives(archive_uri, regression_archive_uri)
 
     def test_archive_pollination_input(self):
         params = {
@@ -115,27 +115,29 @@ class DataStorageTest(testing.GISTest):
         archive_uri += '.tar.gz'
         regression_archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'pollination_input.tar.gz')
-        self.assertArchive(archive_uri, regression_archive_uri)
+        self.assertArchives(archive_uri, regression_archive_uri)
 
     def test_extract_archive(self):
         workspace = raster_utils.temporary_folder()
         archive_uri = os.path.join(REGRESSION_ARCHIVES,
             'pollination_input.tar.gz')
+        input_folder = raster_utils.temporary_folder()
         parameters = data_storage.extract_parameters_archive(workspace,
-            archive_uri)
+            archive_uri, input_folder)
 
         self.maxDiff = None
         regression_params = {
             u'ag_classes': u'67 68 71 72 73 74 75 76 78 79 80 81 82 83 84 85 88 90 91 92',
             u'do_valuation': True,
-            u'farms_shapefile': os.path.join(workspace, u'vector_LCN2UV'),
-            u'guilds_uri': os.path.join(workspace, u'Guild.csv'),
+            u'farms_shapefile': os.path.join(input_folder, u'vector_LCN2UV'),
+            u'guilds_uri': os.path.join(input_folder, u'Guild.csv'),
             u'half_saturation': 0.125,
-            u'landuse_attributes_uri': os.path.join(workspace, u'LU.csv'),
-            u'landuse_cur_uri': os.path.join(workspace, u'raster_23KPVQ'),
-            u'landuse_fut_uri': os.path.join(workspace, u'raster_N243QT'),
+            u'landuse_attributes_uri': os.path.join(input_folder, u'LU.csv'),
+            u'landuse_cur_uri': os.path.join(input_folder, u'raster_23KPVQ'),
+            u'landuse_fut_uri': os.path.join(input_folder, u'raster_N243QT'),
             u'results_suffix': u'suff',
             u'wild_pollination_proportion': 1.0,
+            u'workspace_dir': workspace,
         }
 
         self.assertEqual(parameters, regression_params)
@@ -170,25 +172,28 @@ class DataStorageTest(testing.GISTest):
         self.maxDiff=None
 
         workspace = raster_utils.temporary_folder()
+        input_folder = raster_utils.temporary_folder()
         regression_parameters = {
             u'a': 1,
             u'b': 2,
             u'd': {
                 u'one': 1,
                 u'two': 2,
-                u'three': os.path.join(workspace, u'Guild.csv')
+                u'three': os.path.join(input_folder, u'Guild.csv')
             },
-            u'c': os.path.join(workspace, u'vector_1WEYE3'),
+            u'c': os.path.join(input_folder, u'vector_1WEYE3'),
             u'raster_list': [
-                os.path.join(workspace, u'raster_HD4O5B'),
+                os.path.join(input_folder, u'raster_HD4O5B'),
                 {
-                    u'lulc_samp_cur': os.path.join(workspace, u'raster_7ZA2EY'),
+                    u'lulc_samp_cur': os.path.join(input_folder, u'raster_7ZA2EY'),
                     u'do_biophysical': True,
                 }
             ],
-            u'c_again': os.path.join(workspace, u'vector_1WEYE3'),
+            u'c_again': os.path.join(input_folder, u'vector_1WEYE3'),
+            u'workspace_dir': workspace,
         }
-        parameters = data_storage.extract_parameters_archive(workspace, archive_uri)
+        parameters = data_storage.extract_parameters_archive(workspace,
+            archive_uri, input_folder)
         self.assertEqual(parameters, regression_parameters)
 
         files_to_check = [
@@ -201,6 +206,7 @@ class DataStorageTest(testing.GISTest):
 
         for file_uri in files_to_check:
             self.assertEqual(True, os.path.exists(file_uri))
+
 
 
 
