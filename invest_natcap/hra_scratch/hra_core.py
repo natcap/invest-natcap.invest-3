4debc2ab81b3dafb264d4122ba89d3de3ca107a4
kathryn.glowinski <devnull@localhost>
2013-07-05 11:32:59 -0700

    Update issue 2063
    Adding pre_calc_denoms_and_crits skeleton to new hra_core.
    
    
    Former-commit-id: 93cc3aae398530a2887d2ac05f9196299b1533ce

--- invest_natcap/hra_scratch/hra_core.py
+++ invest_natcap/hra_scratch/hra_core.py
@@ -82,3 +82,80 @@ def execute(args):
 
     Returns nothing.
     '''
+
+
+    inter_dir = os.path.join(args['workspace_dir'], 'Intermediate')
+    output_dir = os.path.join(args['workspace_dir'], 'Output')
+    
+    crit_lists, denoms = pre_calc_denoms_and_criteria(inter_dir, args['h_s_c'],
+                                    args['habitats'], args['h_s_e'])
+
+def pre_calc_denoms_and_criteria(dir, h_s_c, hab, h_s_e):
+    '''Want to return two dictionaries in the format of the following:
+    (Note: the individual num raster comes from the crit_ratings
+    subdictionary and should be pre-summed together to get the numerator
+    for that particular raster. )
+
+    Input:
+        dir- Directory into which the rasterized criteria can be placed. This
+            will need to have a subfolder added to it specifically to hold the
+            rasterized criteria for now.
+        h_s_c- A multi-level structure which holds all criteria ratings, 
+            both numerical and raster that apply to habitat and stressor 
+            overlaps. The structure, whose keys are tuples of 
+            (Habitat, Stressor) names and map to an inner dictionary will have
+            3 outer keys containing numeric-only criteria, raster-based
+            criteria, and a dataset that shows the potentially buffered overlap
+            between the habitat and stressor. The overall structure will be as
+            pictured:
+
+            {(Habitat A, Stressor 1): 
+                    {'Crit_Ratings': 
+                        {'CritName': 
+                            {'Rating': 2.0, 'DQ': 1.0, 'Weight': 1.0}
+                        },
+                    'Crit_Rasters': 
+                        {'CritName':
+                            {'DS': "CritName Raster URI", 'Weight': 1.0, 'DQ': 1.0}
+                        },
+                    'DS':  "A-1 Raster URI"
+                    }
+            }
+        hab- Similar to the h-s dictionary, a multi-level
+            dictionary containing all habitat-specific criteria ratings and
+            rasters. In this case, however, the outermost key is by habitat
+            name, and habitats['habitatName']['DS'] points to the rasterized
+            habitat shapefile URI provided by the user.
+        h_s_e- Similar to the h_s_c dictionary, a multi-level
+            dictionary containing habitat-stressor-specific criteria ratings and
+            rasters. The outermost key is by (habitat, stressor) pair, but the
+            criteria will be applied to the exposure portion of the risk calcs.
+    
+    Output:
+        Creates a version of every criteria for every h-s paring that is
+        burned with both a r/dq*w value for risk calculation, as well as a
+        r/dq burned raster for recovery potential calculations.
+    
+    Returns:     
+        crit_lists- A dictionary containing pre-burned criteria URI which can be
+            combined to get the E/C for that H-S pairing.
+
+            {'Risk': {  'h_s_c': { (hab1, stressA): ["indiv num raster", "raster 1", ...],
+                                   (hab1, stressB): ...
+                                 },
+                        'h':   { hab1: ["indiv num raster URI", "raster 1 URI", ...],
+                                ...
+                               },
+                        'h_s_e': { (hab1, stressA): ["indiv num raster URI", ...]
+                                 }
+                     }
+             'Recovery': { hab1: ["indiv num raster URI", ...],
+                           hab2: ...
+                         }
+            }
+        denoms- Dictionary containing the combined denominator for a given
+            H-S overlap. Once all of the rasters are combined, each H-S raster
+            can be divided by this. This dictionary will be the same structure
+            as crit_lists, but the innermost values will be floats instead of
+            lists.
+    '''
