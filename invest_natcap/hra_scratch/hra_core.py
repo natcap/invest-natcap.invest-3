29d4a5fb0c42ea17d36f33be5f8b258fb7d8ef32
kathryn.glowinski <devnull@localhost>
2013-07-09 15:22:04 -0700

    Adding things without taking into account the new structure of the crit_dict dictionary. Removed a function. will add back again now that the previous was fixed.
    Former-commit-id: 1a901d1f1783c060aff57cab7932df588d5875f2

--- invest_natcap/hra_scratch/hra_core.py
+++ invest_natcap/hra_scratch/hra_core.py
@@ -116,53 +116,6 @@ def execute(args):
     #different models.
     num_stress = make_risk_shapes(maps_dir, crit_lists, h_risk_dict, args['max_risk'])
 
-    #Now, combine all of the habitat rasters unto one overall ecosystem
-    #rasterusing the DS's from the previous function.
-    make_ecosys_risk_raster(maps_dir, h_risk_dict)
-
-def make_ecosys_risk_raster(dir, h_dict):
-    '''This will make the compiled raster for all habitats within the ecosystem.
-    The ecosystem raster will be a direct sum of each of the included habitat
-    rasters.
-
-    Input:
-        dir- The directory in which all completed should be placed.
-        h_dict- A dictionary of raster dataset URIs which can be combined to 
-            create an overall ecosystem raster. The key is the habitat name, 
-            and the value is the dataset URI.
-            
-            {'Habitat A': "Overall Habitat A Risk Map URI",
-            'Habitat B': "Overall Habitat B Risk URI"
-             ...
-            }
-    Output:
-        ecosys_risk.tif- An overall risk raster for the ecosystem. It will
-            be placed in the dir folder.
-
-    Returns nothing.
-    '''
-    #Need a straight list of the values from h_dict
-    h_list = h_dict.values()
-    pixel_size = raster_utils.get_cell_size_from_uri(h_list[0])
-
-    out_uri = os.path.join(dir, 'ecosys_risk.tif')
-
-    def add_e_pixels(*pixels):
-        '''Sum all habitat pixels for ecosystem raster.'''
- 
-        pixel_sum = 0.0
-        
-        for p in pixels:
- 
-            pixel_sum += p
- 
-        return pixel_sum
-     
-    raster_utils.vectorize_datasets(h_list, add_e_pixels, out_uri, 
-                gdal.GDT_Float32, 0., pixel_size, "union", 
-                resample_method_list=None, dataset_to_align_index=None,
-                aoi_uri=None)
-
 def make_risk_shapes(dir, crit_lists, h_dict, max_risk):
     '''This function will take in the current rasterized risk files for each
     habitat, and output a shapefile where the areas that are "HIGH RISK" (high
@@ -182,14 +135,14 @@ def make_risk_shapes(dir, crit_lists, h_dict, max_risk):
         crit_lists- A dictionary containing pre-burned criteria which can be
             combined to get the E/C for that H-S pairing.
 
-            {'Risk': {  'h-s': { (hab1, stressA): ["indiv num raster URI", 
+            {'Risk': {  'h_s_c': { (hab1, stressA): ["indiv num raster URI", 
                                     "raster 1 URI", ...],
                                  (hab1, stressB): ...
                                },
                         'h':   { hab1: ["indiv num raster URI", "raster 1 URI", ...],
                                 ...
                                },
-                        's':   { stressA: ["indiv num raster URI", ...]
+                        'h_s_e': {(hab1,  stressA): ["indiv num raster URI", ...]
                                }
                      }
              'Recovery': { hab1: ["indiv num raster URI", ...],
@@ -217,7 +170,7 @@ def make_risk_shapes(dir, crit_lists, h_dict, max_risk):
     #allows us to not have to think about whether or not a h-s pair was zero'd
     #out by weighting or DQ.
     num_stress = collections.Counter()
-    for pair in crit_lists['Risk']['h-s']:
+    for pair in crit_lists['Risk']['h_s_c']:
         h, _ = pair
         
         if h in num_stress:
