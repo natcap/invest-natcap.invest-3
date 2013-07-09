011f1fb00a32bea1a984121d1a5831e2f888fe92
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-07-09 09:48:09 -0700

    Update issue 1956
    Changing how the alpha values are being input. I did just have them hard coded,
    but now they come in as a CSV file. Created a function to unpack the CSV and
    load it into a dictionary like I had set it before.
    
    The Beta value is now also input through this table
    
    
    Former-commit-id: 7a452d7b01eeb3cf3f2610c1b48b338a4a6f9d6b

--- invest_natcap/monthly_water_yield/monthly_water_yield.py
+++ invest_natcap/monthly_water_yield/monthly_water_yield.py
@@ -79,6 +79,7 @@ def execute(args):
     lulc_uri = args['lulc_uri']
     lulc_data_uri = args['lulc_data_uri']
     watershed_uri = args['watersheds_uri']
+    model_params_uri = args['model_params_uri']
     
     try:
         sub_shed_uri = args['sub_watersheds_uri']
@@ -101,8 +102,6 @@ def execute(args):
         raster_utils.reclassify_dataset_uri(
                 lulc_uri, lulc_code_dict, code_uri, gdal.GDT_Float32,
                 float_nodata)
-
-    beta = 2.0
 
     # Get DEM WKT
     dem_wkt = raster_utils.get_dataset_projection_wkt_uri(dem_uri)
@@ -129,12 +128,12 @@ def execute(args):
     alpha_three_uri = os.path.join(intermediate_dir, 'alpha_three.tif')
     alpha_uri_list = [alpha_one_uri, alpha_two_uri, alpha_three_uri]
     
-    alpha_table = {'alpha_one':{'a_one':0.07, 'b_one':0.01, 'c_one':0.002},
-                   'alpha_two':{'a_two':0.2, 'b_two':2.2},
-                   'alpha_three':{'a_three':1.44, 'b_three':0.68}}
+    model_param_dict = model_parameters_to_dict(model_params_uri)
+    LOGGER.debug('MODEL PARAMETERS: %s', model_param_dict)
+    beta = model_param_dict['beta']['beta']
 
     calculate_alphas(
-        slope_uri, soil_text_uri, smax_uri, alpha_table, float_nodata,
+        slope_uri, soil_text_uri, smax_uri, model_param_dict, float_nodata,
         alpha_uri_list)
 
     absorption_uri = os.path.join(intermediate_dir, 'absorption.tif')
@@ -966,6 +965,32 @@ def calculate_alphas(
     raster_utils.vectorize_datasets(
             [smax_uri], alpha_three_op, output_uri_list[2], gdal.GDT_Float32,
             out_nodata, smax_cell_size, 'intersection')
+
+def model_parameters_to_dict(csv_uri):
+    """Build a dictionary from the model parameters CSV table
+
+        csv_uri - a URI to a CSV file for the model parameters 
+
+        returns - a dictionary with the following structure:
+            
+    """
+    data_file = open(csv_uri)
+    data_handler = csv.DictReader(data_file)
+    
+    # Make the fieldnames lowercase
+    data_handler.fieldnames = [f.lower() for f in data_handler.fieldnames]
+    LOGGER.debug('Lowercase Fieldnames : %s', data_handler.fieldnames)
+
+    param_dict = {}
+
+    for row in data_handler:
+        try:
+            param_dict[row['use']][row['param']] = float(row['value'])
+        except KeyError:
+            param_dict[row['use']] = {}
+            param_dict[row['use']][row['param']] = float(row['value'])
+
+    return param_dict
 
 def construct_lulc_lookup_dict(lulc_data_uri, field):
     """Parse a LULC lookup CSV table and construct a dictionary mapping the LULC
