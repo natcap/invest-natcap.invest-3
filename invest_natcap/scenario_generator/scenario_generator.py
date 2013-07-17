9bf3e7de90b1b59261ddd8bc01d771b7dc8d4297
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2013-07-17 14:18:50 -0700

    Update 2166
    Added program outline to script.
    
    
    Former-commit-id: 8c10597029f8bf3cfc88e16243c084784c0edc4f

--- invest_natcap/scenario_generator/scenario_generator.py
+++ invest_natcap/scenario_generator/scenario_generator.py
@@ -1,7 +1,43 @@
 def execute(args):
     pass
+
+    ###
+    #get parameters, set outputs
+    ###
+
+    ###
     #validate data
+    ###
+
+    #raise warning if nothing going to happen, ie no criteria provided
+
+    ###
+    #resample, align and rasterize data
+    ###
     
-    #calculate raster statistics
-    ###compute priority
+    ###
+    #compute intermediate data if needed
+    ###
+
+    #contraints raster (reclass using permability values, filters on clump size)
+
+    #normalize probabilities to be on a 10 point scale
+    #probability raster (reclass using probability matrix)
+
+    #proximity raster (gaussian for each landcover type, using max distance)
+    #InVEST 2 uses 4-connectedness?
+
+    #combine rasters for weighting into sutibility raster, multiply proximity by 0.3
+    #[suitability * (1-factor weight)] + (factors * factor weight) or only single raster
     
+    ###
+    #reallocate pixels (disk heap sort, randomly reassign equal value pixels, applied in order)
+    ###
+
+    #reallocate
+
+    #apply override
+
+    ###
+    #tabulate coverages
+    ###
