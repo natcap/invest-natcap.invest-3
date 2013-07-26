78c29e94199f2cd178aeab69a42ef02925049ab0
Adam Perelman <adamperelman@gmail.com>
2013-07-26 12:14:37 -0700

    start fleshing out html api
    Former-commit-id: 81b045a4cd01ec83d26f220b36143354db9b479e

--- invest_natcap/report_generation/html.py
+++ invest_natcap/report_generation/html.py
@@ -1,3 +1,35 @@
+DEFAULT_PALETTE = 0
+BEACH_PALETTE = 1
+
 class HTMLWriter(object):
-    def __init__(self, uri):
+    def __init__(self, uri, title, header):
         self.uri = uri
+        self.title = title
+        self.header = header
+        self.body = ''
+        self.style = ''
+
+    def flush(self):
+        '''Creates and writes to an HTML file.'''
+        f = open(self.uri, 'w')
+
+        def write_elem(tag, text, attr=''):
+            if attr:
+                attr = ' ' + attr
+            f.write('<%s%s>%s</%s>' % (tag, attr, text, tag))
+
+        f.write('<html>')
+
+        f.write('<head>')
+        write_elem('title', self.title)
+        if self.style:
+            write_elem('style', self.style, 'type="text/css"')
+        f.write('<head>')
+
+        f.write('<body>')
+        f.write('<center><h1>%s</h1></center>' % self.header)
+        f.write(self.body)
+        f.write('</body>')
+        f.write('</html>')
+
+        f.close()
