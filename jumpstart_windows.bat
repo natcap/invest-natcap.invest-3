2c3528e915ea225593d2dcf500a1a6a55e945f72
jandrewjohnson <jandrewjohnson@gmail.com>
2013-08-08 13:25:14 -0500

    Jumpstart file for installing windows based scripting environment.
    Former-commit-id: 8c7d9f4bef0247aa187fd45bf49620337ce3bd26

---
+++ jumpstart_windows.bat
@@ -0,0 +1,9 @@
+SET ENVDIR=invest_python_environment
+DEL /S /Q build
+DEL /S /Q %ENVDIR%
+python bootstrap_invest_environment.py > setup_environment.py
+python setup_environment.py --clear --system-site-packages %ENVDIR%
+copy C:\Python27\Lib\distutils\distutils.cfg .\%ENVDIR%\Lib\distutils\distutils.cfg
+%ENVDIR%\Scripts\python setup.py install
+
+%ENVDIR%\Scripts\python invest_carbon_biophysical.py
