fd328b15f3e771aa7831e69459b497ca8371dc9a
richpsharp <richpsharp@gmail.com>
2013-08-13 19:00:59 -0700

    Update issue 2234
    Identifying flat downhill areas.
    
    
    Former-commit-id: fa2d3395c7937d52adbb8c7fd10f72621b98cbb7

--- invest_natcap/routing/flat_direction_resolution.py
+++ invest_natcap/routing/flat_direction_resolution.py
@@ -7,10 +7,17 @@ dem_array = numpy.array(
 	 [8,6,6,6,6,6,9],
 	 [7,6,6,6,6,6,8],
 	 [7,7,5,7,7,8,8]])
-	 
+
+
+flat_cells = numpy.zeros(dem_array.shape, dtype=numpy.bool)	 
 #Identify flat downhill cells (neighboring uphill doesn't count)
+for row_index in range(1, flat_cells.shape[0] - 1):
+	for col_index in range(1, flat_cells.shape[1] - 1):
+		flat_cells[row_index, col_index] = (dem_array[row_index-1:row_index+2, col_index-1:col_index+2] >= dem_array[row_index, col_index]).all()
+print flat_cells
 #Identify sink cells
 #Iterate out from sink increasing along the way
 
 #Identify edge increasing cells
 #Iterate out from increasing cells
+
