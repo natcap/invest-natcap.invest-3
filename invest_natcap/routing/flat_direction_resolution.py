d32690608e5a9085e5bfb6d2eddb0c0a6a2b889c
richpsharp <richpsharp@gmail.com>
2013-08-13 18:33:59 -0700

    Update issue 2234
    Commenting algorithm.
    
    
    Former-commit-id: 37ee96526e1bfdb5d9a70c0fead85148c49b8eea

--- invest_natcap/routing/flat_direction_resolution.py
+++ invest_natcap/routing/flat_direction_resolution.py
@@ -7,3 +7,10 @@ dem_array = numpy.array(
 	 [8,6,6,6,6,6,9],
 	 [7,6,6,6,6,6,8],
 	 [7,7,5,7,7,8,8]])
+	 
+#Identify flat downhill cells (neighboring uphill doesn't count)
+#Identify sink cells
+#Iterate out from sink increasing along the way
+
+#Identify edge increasing cells
+#Iterate out from increasing cells
