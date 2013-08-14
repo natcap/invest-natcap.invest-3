50eb13166fd5b3b7547748d8896bd7c36077ef81
richpsharp <richpsharp@gmail.com>
2013-08-13 22:26:02 -0700

    Update issue 2234
    Attempting to set the flat sink cells in regions where it touches a flat area and a drain.
    
    
    Former-commit-id: 08ef1624b4e4c2f4bae43d344ddd94b2194f2886

--- invest_natcap/routing/flat_direction_resolution.py
+++ invest_natcap/routing/flat_direction_resolution.py
@@ -15,7 +15,16 @@ for row_index in range(1, flat_cells.shape[0] - 1):
 	for col_index in range(1, flat_cells.shape[1] - 1):
 		flat_cells[row_index, col_index] = (dem_array[row_index-1:row_index+2, col_index-1:col_index+2] >= dem_array[row_index, col_index]).all()
 print flat_cells
+print '\n'
 #Identify sink cells
+sink_cells = numpy.zeros(dem_array.shape, dtype=numpy.bool)	 
+for row_index in range(1, flat_cells.shape[0] - 1):
+	for col_index in range(1, flat_cells.shape[1] - 1):
+		if (flat_cells[row_index-1:row_index+2, col_index-1:col_index+2].any() and not flat_cells[row_index, col_index]):
+			
+			sink_cells[numpy.where(dem_array[row_index-1:row_index+2, col_index-1:col_index+2] < dem_array[row_index, col_index])] = True
+print sink_cells
+
 #Iterate out from sink increasing along the way
 
 #Identify edge increasing cells
