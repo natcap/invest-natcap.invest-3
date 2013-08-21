b90dd138d63406eca995d3013982c5b8de16ec5f
nicolas.chaumont <devnull@localhost>
2013-08-21 15:20:39 -0700

    Update issue 2235: Started to implement the function that will compute each cell's (horizontal) angular footprint from the viewpoint + plug it to test functions.
    Former-commit-id: 1e61ec80a62b62312cc5a9c020f8fe751b8e33a0

--- test/aesthetic_quality_core_test.py
+++ test/aesthetic_quality_core_test.py
@@ -1,9 +1,16 @@
 import unittest
 import logging
 
+from invest_natcap.aesthetic_quality import aesthetic_quality_core
+
 class TestAestheticQualityCore(unittest.TestCase):
     def SetUp(self):
         pass
+
+    def test_extreme_cell_angles(self):
+        array_shape = (3, 3)
+        viewpoint = (1, 1)
+        aesthetic_quality_core.list_extreme_cell_angles(array_shape, viewpoint)
 
     def test_viewshed(self):
         pass
