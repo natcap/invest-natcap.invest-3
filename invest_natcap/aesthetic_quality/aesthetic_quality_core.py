ff3d979bff8f2e36569621e7c014576518920e7d
nicolas.chaumont <devnull@localhost>
2013-08-22 17:08:05 -0700

    Update issue 2265: Implementing the inner loop that computes extreme angles.
    Former-commit-id: 7a6cfbf2ebc86258208a8ef999f32f5d03f5ec38

--- invest_natcap/aesthetic_quality/aesthetic_quality_core.py
+++ invest_natcap/aesthetic_quality/aesthetic_quality_core.py
@@ -19,15 +19,22 @@ def list_extreme_cell_angles(array_shape, viewpoint_coords):
         returns a tuple of 2 arrays (min, max) as does numpy.where() with min
             and max the minimum and maximum angles spanned by each raster pixel
     """
+    viewpoint = np.array(viewpoint_coords)
+
+    pi = math.pi
+    two_pi = 2. * pi
+    rad_to_deg = 180.0 / pi
+    deg_to_rad = 1.0 / rad_to_deg
+
     extreme_cell_points = [ \
-    {'min_angle':[0.5, -0.5]}, {'max_angle':[-0.5,-0.5]}, \
-    {'min_angle':[0.5, 0.5]}, {'max_angle':[-0.5, -0.5]}, \
-    {'min_angle':[0.5, 0.5]}, {'max_angle':[0.5, -0.5]}, \
-    {'min_angle':[-0.5, 0.5]}, {'max_angle':[0.5, -0.5]}, \
-    {'min_angle':[-0.5, 0.5]}, {'max_angle':[0.5, 0.5]}, \
-    {'min_angle':[-0.5, -0.5]}, {'max_angle':[0.5, 0.5]}, \
-    {'min_angle':[-0.5, -0.5]}, {'max_angle':[-0.5, 0.5]}, \
-    {'min_angle':[0.5, -0.5]}, {'max_angle':[-0.5, 0.5]}]
+    [{'min_angle':[0.5, -0.5]}, {'max_angle':[-0.5,-0.5]}], \
+    [{'min_angle':[0.5, 0.5]}, {'max_angle':[-0.5, -0.5]}], \
+    [{'min_angle':[0.5, 0.5]}, {'max_angle':[0.5, -0.5]}], \
+    [{'min_angle':[-0.5, 0.5]}, {'max_angle':[0.5, -0.5]}], \
+    [{'min_angle':[-0.5, 0.5]}, {'max_angle':[0.5, 0.5]}], \
+    [{'min_angle':[-0.5, -0.5]}, {'max_angle':[0.5, 0.5]}], \
+    [{'min_angle':[-0.5, -0.5]}, {'max_angle':[-0.5, 0.5]}], \
+    [{'min_angle':[0.5, -0.5]}, {'max_angle':[-0.5, 0.5]}]]
 
     extreme_cell_angles = []
     x_range = np.array(range(array_shape[0]))
@@ -36,9 +43,17 @@ def list_extreme_cell_angles(array_shape, viewpoint_coords):
     print(x_range)
     print(y_range)
 
-    print(extreme_cell_points)
+    for entry in extreme_cell_points:
+        print(entry)
 
-    #xx = np.meshgrid()
+    for row in range(array_shape[0]):
+        for col in range(array_shape[1]):
+            cell = np.array([row, col])
+            viewpoint_to_cell = cell - viewpoint
+            angle = math.arctan2(-viewpoint_to_cell[0], viewpoint_to_cell[1])
+            angle = (angle + two_pi) % two_pi 
+            print(cell, viewpoint_to_cell, angle)
+    
 
     return extreme_cell_angles
 
