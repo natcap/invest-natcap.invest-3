954f3a98fe881b812e09b6a7d928e286df8b9fb6
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-08-29 16:18:46 -0700

    Update issue 2286
    Adding a new hydropower test that now holds regression tests via James's
    awesome framework.
    
    Also syncing up sub repo invest-data
    
    
    Former-commit-id: d84b3f1b61039616c44510c09c54d885f3396a7a

---
+++ test/hydropower_test.py
@@ -0,0 +1,30 @@
+import invest_natcap.testing
+import invest_natcap.testing
+import invest_natcap.testing
+
+import invest_natcap.testing
+class HydropowerRegressionTest(invest_natcap.testing.GISTest):
+    @invest_natcap.testing.regression(
+        input_archive="invest-data/test/data/hydropower_regression_data/hydro_input_archive_valuation.tar.gz",
+        workspace_archive="invest-data/test/data/hydropower_regression_data/hydro_output_archive_valuation.tar.gz")
+    def test_hydropower_regression_test_valuation(self):
+        invest_natcap.hydropower.hydropower_water_yield.execute(self.args)
+
+    @invest_natcap.testing.regression(
+        input_archive="invest-data/test/data/hydropower_regression_data/hydro_input_archive_scarcity.tar.gz",
+        workspace_archive="invest-data/test/data/hydropower_regression_data/hydro_output_archive_scarcity.tar.gz")
+    def test_hydropower_regression_test_scarcity(self):
+        invest_natcap.hydropower.hydropower_water_yield.execute(self.args)
+
+    @invest_natcap.testing.regression(
+        input_archive="invest-data/test/data/hydropower_regression_data/hydro_input_archive_wy_sub_sheds.tar.gz",
+        workspace_archive="invest-data/test/data/hydropower_regression_data/hydro_output_archive_wy_sub_sheds.tar.gz")
+    def test_hydropower_regression_test_wy_sub_sheds(self):
+        invest_natcap.hydropower.hydropower_water_yield.execute(self.args)
+
+    @invest_natcap.testing.regression(
+        input_archive="invest-data/test/data/hydropower_regression_data/hydro_input_archive_water_yield.tar.gz",
+        workspace_archive="invest-data/test/data/hydropower_regression_data/hydro_output_archive_water_yield.tar.gz")
+    def test_hydropower_regression_test_water_yield(self):
+        invest_natcap.hydropower.hydropower_water_yield.execute(self.args)
+
