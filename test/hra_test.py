cecf0e493978640ccd9c5c4c38a2be7429b8e990
kathryn.glowinski <devnull@localhost>
2013-09-05 14:48:41 -0700

    Update issue 1288
    Updating the regression test to change the file path to the one timestamped file that I have to check the contents of. Tests now all pass.
    
    
    Former-commit-id: b3db7c5b322647230b6e93537f9992f42a77224c

--- test/hra_test.py
+++ test/hra_test.py
@@ -113,7 +113,7 @@ class TestHRA(invest_natcap.testing.GISTest):
                 elif tail == '.html':
                     #There is only one file here that we want to compare against.
                     #Just explicitly call it so I can be done.
-                    exp_html = './invest-data/test/data/hra_regression_data/Output/HTML_Plots/Sub_Region_Averaged_Results_[2013-08-09_14_49].html'
+                    exp_html = './invest-data/test/data/hra_regression_data/Output/HTML_Plots/Sub_Region_Averaged_Results_[2013-09-05_14_34].html'
 
                     self.assertTrue(filecmp.cmp(item, exp_html))
         
