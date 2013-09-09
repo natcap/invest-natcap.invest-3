97377a092182b0449adc44db6393ad2fa7bc33b0
richpsharp <richpsharp@gmail.com>
2013-09-09 14:48:38 -0700

    Update isue 2301
    Added the forest fragmentation scenario.
    
    
    Former-commit-id: 9cd4fecc6d3cf6963d11c14ab611a0e13fcfa272

--- utils/Mato_Grosso_Soybean/hydropower_water_yield.py
+++ utils/Mato_Grosso_Soybean/hydropower_water_yield.py
@@ -5,6 +5,7 @@ import numpy
 
 import invest_natcap.hydropower.hydropower_water_yield
 from invest_natcap import raster_utils
+import carbon_expansion_scenarios
 
 #Premade scenario
 def premade_water_yield_scenario(args):
@@ -115,7 +116,91 @@ def analyze_lu_expansion(args):
         converted_lulc_dataset = None
         
         
+def analyze_forest_core_fragmentation(args):
+    """This function does a simulation of cropland expansion by
+        expanding into the most core regions of the forest on every simulation
+        step.
+
+        args['base_biomass_filename'] - a raster that contains carbon densities
+            per Ha.
+        args['base_landcover_filename'] - a raster of same dimensions as
+            'base_biomass_filename' that contains lucodes for the landscape
+            regression analysis
+        args['carbon_pool_table_filename'] - a CSV file containing carbon
+            biomass values for given lucodes.  Must contain at least the
+            columns 'LULC' and 'C_ABOVE_MEAN'
+        args['forest_lucodes'] - a list of lucodes that are used to determine
+            forest landcover types
+        args['regression_lucodes'] - a list of lucodes that will use the
+            linear regression to determine forest biomass given edge
+            distance
+        args['biomass_from_table_lucodes'] - a list of lucodes that will use
+            the carbon pool csv table to determine biomass
+        args['converting_crop'] - when a pixel is converted to crop, it uses
+            this lucode.
+        args['scenario_conversion_steps'] - the number of steps to run in
+            the simulation
+        args['pixels_to_convert_per_step'] - each step of the simulation
+            converts this many pixels
+        args['output_table_filename'] - this is the filename of the CSV
+            output table.
+        args['scenario_lulc_base_map_filename'] - the base LULC map used for
+            the scenario runs
+        """
+
+    print 'starting forest core fragmentation scenario'
     
+    #Load the base landcover map that we use in the scenarios
+    scenario_lulc_dataset = gdal.Open(args['scenario_lulc_base_map_filename'])
+    cell_size = scenario_lulc_dataset.GetGeoTransform()[1]
+    scenario_lulc_array = scenario_lulc_dataset.GetRasterBand(1).ReadAsArray()
+
+    #Write converted scenario array to dataset
+    converted_lulc_uri = raster_utils.temporary_filename()
+    raster_utils.new_raster_from_base(
+        scenario_lulc_dataset, converted_lulc_uri, 'GTiff', -1, gdal.GDT_Int16)
+    converted_lulc_dataset = gdal.Open(converted_lulc_uri, gdal.GA_Update)
+    converted_lulc_band = converted_lulc_dataset.GetRasterBand(1)
+    converted_lulc_band.WriteArray(scenario_lulc_array)
+    converted_lulc_band = None
+    converted_lulc_dataset = None
+    args['lulc_uri'] = converted_lulc_uri
+    
+    #Open a .csv file to dump the grassland expansion scenario
+    output_table = open(args['output_table_filename'], 'wb')
+    output_table.write(
+        'Percent Soy Expansion,Total Water Yield Volume\n')
+
+    #This index will keep track of the number of forest pixels converted.
+    deepest_edge_index = 0
+    for percent in range(args['scenario_conversion_steps'] + 1):
+        print 'calculating water yield for forest fragmentation step %s' % percent
+
+        #invest_natcap.hydropower.hydropower_water_yield.execute(args)
+        water_yield_shapefile_uri = os.path.join(
+            args['workspace_dir'], 'output', 'wyield_sheds.shp')
+        ws_table = raster_utils.extract_datasource_table_by_key(
+            water_yield_shapefile_uri, 'ws_id')
+        output_table.write('%s,%.2f\n' % (percent, ws_table[1]['wyield_vol']))
+
+        deepest_edge_index += args['pixels_to_convert_per_step']
+        scenario_edge_distance = carbon_expansion_scenarios.calculate_forest_edge_distance(
+            scenario_lulc_array, args['forest_lucodes'], cell_size)
+
+        #We want to visit the edge pixels in decreasing distance order starting
+        #from the core pixel in.
+        decreasing_distances = numpy.argsort(scenario_edge_distance.flat)[::-1]
+        scenario_lulc_array.flat[
+            decreasing_distances[0:deepest_edge_index]] = (
+                args['converting_crop'])
+        #Write converted scenario array to dataset
+        converted_lulc_dataset = gdal.Open(converted_lulc_uri, gdal.GA_Update)
+        converted_lulc_band = converted_lulc_dataset.GetRasterBand(1)
+        converted_lulc_band.WriteArray(scenario_lulc_array)
+        converted_lulc_band = None
+        converted_lulc_dataset = None
+        
+                
 if __name__ == '__main__':
     ARGS = {
         u'biophysical_table_uri': u'Water_Yield/Parameters.csv',
@@ -140,9 +225,14 @@ if __name__ == '__main__':
     ARGS['output_table_filename'] = os.path.join(
         ARGS['workspace_dir'], 'savanna_expansion_water_yield_change.csv')
     ARGS['scenario_conversion_steps'] = 400
-    analyze_lu_expansion(ARGS)
+    #analyze_lu_expansion(ARGS)
 
     ARGS['output_table_filename'] = os.path.join(
         ARGS['workspace_dir'], 'premade_landcover_water_yield_change.csv')
 #    premade_water_yield_scenario(ARGS)
     
+    ARGS['forest_lucodes'] = [1, 2, 3, 4, 5]
+
+    ARGS['output_table_filename'] = os.path.join(
+        ARGS['workspace_dir'], 'forest_core_fragmentation_water_yield_change.csv')
+    analyze_forest_core_fragmentation(ARGS)
