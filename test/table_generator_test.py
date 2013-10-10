96aa82fa965fb9274605421cd6de391d81cbca52
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-10-10 09:40:49 -0700

    Update issue 2383
    Wrote two test for the function get_row_data in the table_generator module.
    
    
    Former-commit-id: 9a1e7bdc56620719aa433d9d14fcbb9f505c2efd

--- test/table_generator_test.py
+++ test/table_generator_test.py
@@ -26,7 +26,8 @@ class TestTableGenerator(unittest.TestCase):
         self.assertEqual(expected_result, col_headers)
     
     def test_get_column_headers_robust(self):
-        """Unit test for getting the column headers from a dictionary"""
+        """Unit test for getting the column headers from a more complicated
+            dictionary setup"""
         #raise SkipTest
         sample_dict = {
                 'date' : {'id': 1, 'time':'day'},
@@ -39,11 +40,52 @@ class TestTableGenerator(unittest.TestCase):
         col_headers = table_generator.get_column_headers(sample_dict)
 
         self.assertEqual(expected_result, col_headers)
+    
+    def test_get_row_data(self):
+        """Unit test for getting the row data from a dictionary"""
+        #raise SkipTest
+        sample_dict = {
+                    0: {'col_1':'value_1', 'col_2':'value_4'},
+                    1: {'col_1':'value_2', 'col_2':'value_5'},
+                    2: {'col_1':'value_3', 'col_2':'value_6'}}
+
+        col_headers = ['col_1', 'col_2']
+
+        expected_result = [
+                ['value_1', 'value_4'],
+                ['value_2', 'value_5'],
+                ['value_3', 'value_6']]
+
+        row_data = table_generator.get_row_data(sample_dict, col_headers)
+
+        self.assertEqual(expected_result, row_data)
+    
+    def test_get_row_data_robust(self):
+        """Unit test for getting the row data from a more complicated
+            dictionary"""
+        #raise SkipTest
+        sample_dict = {
+                3: {'date':'09-13', 'price':.54, 'product':'chips'},
+                0: {'date':'08-14', 'price':23.4, 'product':'mustard'},
+                1: {'date':'04-13', 'price':100, 'product':'hats'},
+                2: {'date':'06-12', 'price':56.50, 'product':'gloves'}}
+
+        col_headers = ['product', 'price', 'date']
+
+        expected_result = [
+                ['mustard', 23.4, '08-14'],
+                ['hats', 100, '04-13'],
+                ['gloves', 56.50, '06-12'],
+                ['chips', .54, '09-13']]
+
+        row_data = table_generator.get_row_data(sample_dict, col_headers)
+
+        self.assertEqual(expected_result, row_data)
 
     def test_generate_table(self):
         """Unit test for creating a table from a dictionary as a string
             representing html"""
-        #raise SkipTest
+        raise SkipTest
         sample_dict = {
                 'cols':{
                     'date' : {'id': 1, 'time':'day'},
