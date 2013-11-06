02fee1475d3717e1ec56dd83eb77d974b9d9e739
nicolas.chaumont <devnull@localhost>
2013-11-06 14:53:00 -0800

    Update issue 2437: Added a lot of comments.
    Former-commit-id: 7ea1a76f4bd2c97e30bcce446f26e47a394385ed

--- invest_natcap/aesthetic_quality/aesthetic_quality_cython_core.pyx
+++ invest_natcap/aesthetic_quality/aesthetic_quality_cython_core.pyx
@@ -30,25 +30,38 @@ def list_extreme_cell_angles_cython(array_shape, viewpoint_coords):
         coordinate of each point.
     """
     cdef:
+        # constants
         double pi = 3.141592653589793238462643
         double two_pi = 2. * pi
-
+        # viewpoint coordinates
         int viewpoint_row = viewpoint_coords[0]
         int viewpoint_col = viewpoint_coords[1]
-        int viewpoint_to_cell_row = 0
-        int viewpoint_to_cell_col = 0
+        # vector from viewpoint to currently processed cell
+        int viewpoint_to_cell_row
+        int viewpoint_to_cell_col
+        # array sizes
         int array_rows = array_shape[0]
         int array_cols = array_shape[1]
+        # Number of cells processed
         int cell_count = array_rows * array_cols
-        double min_corner_row = 0
-        double min_corner_col = 0
-        double max_corner_row = 0
-        double max_corner_col = 0
-        double min_corner_offset_row = 0
-        double min_corner_offset_col = 0
-        double max_corner_offset_row = 0
-        double max_corner_offset_col = 0
-
+        # This array stores the offset coordinates to recover the first 
+        # and last corners of a cell reached by the sweep line. Since the 
+        # line sweeps from angle 0 to angle 360, the first corner 
+        # is associated with the lowest sweep line angle (min angle), and the
+        # last corner with the highest angle (max angle). 
+        # Each group of 4 values correspond to a sweep line-related angular 
+        # sector:
+        # -row 0: cell centers at angle a = 0 (east of viewpoint)
+        # -row 1: cell centers at angle 0 < a < 90
+        # -row 2: cell centers at angle a = 90 (north of viewpoint)
+        # -row 3: cell centers at angle 90 < a < 180
+        # -row 4: cell centers at angle a = 180 (west of viewpoint)
+        # -row 5: cell centers at angle 180 < a < 270
+        # -row 6: cell centers at angle a = 270 (south of viewpoint)
+        # -row 7: cell centers at angle 270 < a < 360
+        # The 4 values encode 2 pairs of offsets:
+        #   -first 2 values: [row, col] first corner offset coordinates 
+        #   -last 2 values: [row, col] last corner offset coordinates 
         double *extreme_cell_points = [ \
         0.5, -0.5, -0.5, -0.5, \
         0.5, 0.5, -0.5, -0.5, \
@@ -58,21 +71,39 @@ def list_extreme_cell_angles_cython(array_shape, viewpoint_coords):
         -0.5, -0.5, 0.5, 0.5, \
         -0.5, -0.5, -0.5, 0.5, \
         0.5, -0.5, -0.5, 0.5]
-        size_t SECTOR_SIZE = 4
-        size_t POINT_SIZE = 2
-        size_t min_point_id
-        size_t max_point_id
-
+        # constants for fast axess of extreme_cell_points
+        size_t SECTOR_SIZE = 4 # values per sector
+        size_t POINT_SIZE = 2 # values per point
+        size_t min_point_id # first corner base index
+        size_t max_point_id # last corner base index
+        # row of the first corner met by the sweep line
+        double min_corner_row
+        double min_corner_col
+        # row of the last corner met by the sweep line
+        double max_corner_row
+        double max_corner_col
+        # offset from the cell center to the first corner
+        double min_corner_offset_row
+        double min_corner_offset_col
+        # offset from the cell center to the last corner
+        double max_corner_offset_row
+        double max_corner_offset_col
+        # C array that will be used in the loop
+        # pointer to min angle values
         double *min_a_ptr = <double *>malloc((cell_count-1) * sizeof(double))
+        # pointer to cell center angle values
         double *a_ptr = <double *>malloc((cell_count-1) * sizeof(double))
+        # pointer to max angle values
         double *max_a_ptr = <double *>malloc((cell_count-1) * sizeof(double))
+        # pointer to the cells row number
         long *I_ptr = <long *>malloc((cell_count-1) * sizeof(long))
+        # pointer to the cells column number
         long *J_ptr = <long *>malloc((cell_count-1) * sizeof(long))
-
-        int cell_id = 0
-        int row = 0
-        int col = 0
-        int sector = 0
+        # variables used in the loop
+        int cell_id = 0 # processed cell counter
+        int row # row counter
+        int col # column counter
+        int sector # current sector
 
     # Loop through the rows
     for row in range(array_rows):
@@ -98,27 +129,29 @@ def list_extreme_cell_angles_cython(array_shape, viewpoint_coords):
             # The if statement adjusts for all the 8 angles
             if abs(viewpoint_to_cell_row * viewpoint_to_cell_col) > 0:
                 sector += 1
-            # adjust wrt 8 angles
+            # compute extreme corners
             min_point_id = sector * SECTOR_SIZE
+            max_point_id = min_point_id + POINT_SIZE
+            # offset from current cell center to first corner
             min_corner_offset_row = extreme_cell_points[min_point_id]
             min_corner_offset_col = extreme_cell_points[min_point_id + 1]
-            max_point_id = min_point_id + POINT_SIZE
+            # offset from current cell center to last corner
             max_corner_offset_row = extreme_cell_points[max_point_id]
             max_corner_offset_col = extreme_cell_points[max_point_id + 1]
-            # Use the offset to compute extreme angles
+            # Compute the extreme corners from the offsets
             min_corner_row = viewpoint_to_cell_row + min_corner_offset_row
             min_corner_col = viewpoint_to_cell_col + min_corner_offset_col
-            min_angle = atan2(-min_corner_row, min_corner_col)
-            min_a_ptr[cell_id] = (min_angle + two_pi) % two_pi 
-            
             max_corner_row = viewpoint_to_cell_row + max_corner_offset_row
             max_corner_col = viewpoint_to_cell_col + max_corner_offset_col
+            # Compute the angles associated with the extreme corners
+            min_angle = atan2(-min_corner_row, min_corner_col)
             max_angle = atan2(-max_corner_row, max_corner_col)
+            # Save the angles in the fast C arrays
+            min_a_ptr[cell_id] = (min_angle + two_pi) % two_pi 
             max_a_ptr[cell_id] = (max_angle + two_pi) % two_pi
             cell_id += 1
     # Copy C-array contents to numpy arrays:
     # TODO: use memcpy if possible (or memoryviews?)
-    # Copy C arrays to numpy arrays
     min_angles = np.ndarray(cell_count -1, dtype = np.float)
     angles = np.ndarray(cell_count -1, dtype = np.float)
     max_angles = np.ndarray(cell_count -1, dtype = np.float)
@@ -131,6 +164,7 @@ def list_extreme_cell_angles_cython(array_shape, viewpoint_coords):
         max_angles[i] = max_a_ptr[i]
         I[i] = I_ptr[i]
         J[i] = J_ptr[i]
+    # clean-up
     free(I_ptr)
     free(J_ptr)
     free(min_a_ptr)
