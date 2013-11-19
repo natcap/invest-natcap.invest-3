3aa9c71ad64ab7daf59c796c5fa441ac169004cb
kathryn.glowinski <devnull@localhost>
2013-11-18 17:27:07 -0800

    Update issue 1564
    Fixed a bug wherein not all points were getting plotted on the matplotlib plots. Now all stressors shoudl be represented, and not skip the first in each new habitat cluster.
    
    
    Former-commit-id: 8efb9bd15eb9c8578a27ed73711fcde3e7585f68

--- invest_natcap/habitat_risk_assessment/hra_core.py
+++ invest_natcap/habitat_risk_assessment/hra_core.py
@@ -227,6 +227,8 @@ def make_risk_plots(out_dir, aoi_pairs, max_risk, max_stress, num_stress, num_ha
 
     for aoi_name, aoi_list in aoi_pairs.iteritems():
 
+        LOGGER.debug("AOI list for %s: %s" % (aoi_name, aoi_list))
+
         fig = matplotlib.pyplot.figure(plot_index)
         plot_index += 1
         matplotlib.pyplot.suptitle(aoi_name)
@@ -269,6 +271,12 @@ def make_risk_plots(out_dir, aoi_pairs, max_risk, max_stress, num_stress, num_ha
             matplotlib.pyplot.title(curr_hab_name)
             matplotlib.pyplot.xlim([-.5, max_risk])
             matplotlib.pyplot.ylim([-.5, max_risk])
+            
+            #We still need to plot the element that gets us here.
+            matplotlib.pyplot.plot(element[2], element[3], 'k^', 
+                    markerfacecolor='black', markersize=8)
+            matplotlib.pyplot.annotate(element[1], xy=(element[2], 
+                    element[3]), xytext=(element[2], element[3]+0.07))
 
         out_uri = os.path.join(out_dir, 'risk_plot_' + 'AOI[' + aoi_name+ '].png')
 
