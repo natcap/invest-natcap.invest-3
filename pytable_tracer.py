140c29f166903fde28af934d889a4d0d17350965
richpsharp <richpsharp@gmail.com>
2013-11-30 00:26:02 -0800

    Update issue 2513
    Example of how to load datasets and have arbitrary expressions.
    
    
    Former-commit-id: 0f60a0db395ad56a3047fd28642badfa01bbf075

--- pytable_tracer.py
+++ pytable_tracer.py
@@ -2,20 +2,65 @@ import numpy as np
 import tables as tb
 import gdal
 
-ds1 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_cur')
-ds2 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_fut')
+from invest_natcap import raster_utils
 
-ndim = 20000
-h5file = tb.openFile('test.h5', mode='w', title="Test Array")
-root = h5file.root
-a = h5file.createCArray(root,'a',tb.Float64Atom(),shape=(ndim,ndim))
-a[:100,:100] = np.random.random(size=(100,100)) # Now put in some data
+ds_args = {
+    'ds1': './test/invest-data/test/data/base_data/terrestrial/lulc_samp_cur',
+    'ds2': './test/invest-data/test/data/base_data/terrestrial/lulc_samp_fut',
+    }
+out_uri = './out.tif'
 
-expr = tb.Expr("a * a")
+def calc_it(
+    ds_args, ds_out, operation):
+    
+    h5file = tb.openFile('test.h5', mode='w', title="calc_it data")
+    root = h5file.root
+    
+    ds = gdal.Open(ds_args.values()[0])
+    
+    
+    user_args = {}
+    for id, uri in ds_args.iteritems():
+        user_args[id] = h5file.createCArray(
+            root, id, tb.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
+            
+        ds = gdal.Open(uri)
+        band = ds.GetRasterBand(1)
+        for row_index in xrange(ds.RasterYSize):
+            user_args[id][row_index,:] = band.ReadAsArray(0, row_index, ds.RasterXSize, 1)[0]
+        
+    out = h5file.createCArray(
+        root, 'out', tb.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
+        
+    expr = tb.Expr(operation, uservars=user_args)
+    expr.set_output(out)
+    expr.eval()
 
-out = h5file.createCArray(root,'out',tb.Float64Atom(),shape=(ndim,ndim))
-expr.set_output(out, append_mode=False)
-expr.eval()
-print out[:100,:100]
+    raster_utils.new_raster_from_base_uri(
+        ds_args.values()[0], ds_out, 'GTiff', -1, gdal.GDT_Int32)
+    ds = gdal.Open(ds_out, gdal.GA_Update)
+    band = ds.GetRasterBand(1)
+    
+    for row_index in xrange(ds.RasterYSize):
+        band.WriteArray(out[row_index,:].reshape((1,ds.RasterXSize)), 0, row_index)
+        
+calc_it(ds_args, out_uri, 'ds1 + ds2')
+ 
+    
+# ds1 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_cur')
+# ds2 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_fut')
 
-h5file.close()
+# ndim = 20000
+# h5file = tb.openFile('test.h5', mode='w', title="Test Array")
+# root = h5file.root
+# a = h5file.createCArray(root,'a',tb.Float64Atom(),shape=(ndim,ndim))
+# a[:100,:100] = np.random.random(size=(100,100)) # Now put in some data
+
+# expr = tb.Expr("a * a")
+
+# out = h5file.createCArray(root,'out',tb.Float64Atom(),shape=(ndim,ndim))
+# expr.set_output(out, append_mode=False)
+# expr.eval()
+# print out[:100,:100]
+
+# h5file.close()
