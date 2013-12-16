0591ffe101ed96eff54eae75af0f3bb4b0d76745
Doug Denu ddenu <Doug Denu ddenu@stanford.edu>
2013-12-16 13:58:09 -0800

    Update issue 2407
    Fixing some pylint indent issues.
    
    Commenting out function trying to convert shapfile to image
    
    
    Former-commit-id: 56707d883a8274a477d0e051b05ea1f7691a00f3

--- invest_natcap/style.py
+++ invest_natcap/style.py
@@ -5,7 +5,6 @@ import Image
 import logging
 from osgeo import gdal
 from osgeo import ogr
-from shapely.geometry import Polygon
 from shapely.wkb import loads
 from matplotlib import pyplot
 
@@ -42,13 +41,13 @@ def grayscale_raster(raster_in_uri, raster_out_uri):
     # Get the raster statistics, looking for Min and Max specifcally
     stats = raster_utils.get_statistics_from_uri(raster_in_uri)
     # Get Min, Max values from raster
-    min = stats[0]
-    max = stats[1]
+    raster_min = stats[0]
+    raster_max = stats[1]
 
-    LOGGER.debug('Min:Max : %s:%s', min, max)
+    LOGGER.debug('Min:Max : %s:%s', raster_min, raster_max)
 
     # Set the x ranges to interpolate from
-    x_range = [min, max]
+    x_range = [raster_min, raster_max]
     # Set the y ranges to interpolate to
     y_range = [gray_min, gray_max]
      
@@ -71,7 +70,7 @@ def grayscale_raster(raster_in_uri, raster_out_uri):
         pixel_size, 'intersection')
            
 def tif_to_png(tiff_uri, png_uri):
-   """Save a tif of type BYTE as a png file 
+    """Save a tif of type BYTE as a png file 
    
        raster_in_uri - a URI to a gdal raster of type BYTE
        
@@ -79,14 +78,14 @@ def tif_to_png(tiff_uri, png_uri):
            png image
        
        returns - nothing
-   """
+    """
     
-   # If the output URI already exists, remove it
-   if os.path.isfile(png_uri):
-       os.remove(png_uri)
+    # If the output URI already exists, remove it
+    if os.path.isfile(png_uri):
+        os.remove(png_uri)
 
-   img = Image.open(tiff_uri)
-   img.save(png_uri, 'PNG')
+    img = Image.open(tiff_uri)
+    img.save(png_uri, 'PNG')
  
 def create_thumbnail(image_in_uri, thumbnail_out_uri, size):
     """Generates a thumbnail image as a PNG file given of size 'size'
@@ -108,34 +107,33 @@ def create_thumbnail(image_in_uri, thumbnail_out_uri, size):
     img.thumbnail(size)
     img.save(thumbnail_out_uri, 'PNG')
 
-def shape_to_image(shape_in_uri, image_out_uri):
-    """
+#def shape_to_image(shape_in_uri, image_out_uri):
+#   """
 
-    """
-    
-    def drawPolygon(polygon, graph):
-        xLista, yLista = polygon.exterior.xy
-        graph.fill(xLista, yLista, "y")
-        graph.plot(xLista, yLista, "k-")
-    
-    
-    fig = pyplot.figure(figsize=(4, 4), dpi=180)
-    ax = fig.add_subplot(111)
-    
-    ds = ogr.Open(shape_in_uri)
-    layer = ds.GetLayer()
-    for feat in layer:
-        geom = feat.GetGeometryRef()
-        geom_wkt = geom.ExportToWkb()
-        geom_shply = loads(geom_wkt)
-        drawPolygon(geom_shply, ax)
-    
-    pyplot.savefig(image_out_uri)
-    #pixel_size = 30
-    
-    #raster_utils.create_raster_from_vector_extents_uri(
-    #    shape_in_uri, pixel_size, gdal_format, nodata_out_value, output_uri)
-    
-    #raster_utils.rasterize_layer_uri(
-    #    raster_uri, shapefile_uri, burn_values=[], option_list=[])
- 
+#   """
+#   
+#   def drawPolygon(polygon, graph):
+
+#       graph.plot(xLista, yLista, "k-")
+#   
+#   
+#   fig = pyplot.figure(figsize=(4, 4), dpi=180)
+#   ax = fig.add_subplot(111)
+#   
+#   ds = ogr.Open(shape_in_uri)
+#   layer = ds.GetLayer()
+#   for feat in layer:
+#       geom = feat.GetGeometryRef()
+#       geom_wkt = geom.ExportToWkb()
+#       geom_shply = loads(geom_wkt)
+#       drawPolygon(geom_shply, ax)
+#   
+#   pyplot.savefig(image_out_uri)
+#   #pixel_size = 30
+#   
+#   #raster_utils.create_raster_from_vector_extents_uri(
+#   #    shape_in_uri, pixel_size, gdal_format, nodata_out_value, output_uri)
+#   
+#   #raster_utils.rasterize_layer_uri(
+#   #    raster_uri, shapefile_uri, burn_values=[], option_list=[])
+#
