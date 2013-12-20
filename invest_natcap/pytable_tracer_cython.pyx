3d51faff89e35e0fd35e997382e8f156badb5249
richpsharp <richpsharp@gmail.com>
2013-12-20 12:58:04 -0800

    Update issue 2341
    Migrating pytables sample to a cython file.
    
    
    Former-commit-id: e1fc8a04f1605e1528344da6b179066f5645729d

--- invest_natcap/pytable_tracer_cython.pyx
+++ invest_natcap/pytable_tracer_cython.pyx
@@ -1,2 +1,37 @@
-def foo():
-    pass
+import numpy 
+import tables
+import gdal
+
+from invest_natcap import raster_utils
+
+def calc_it(ds_args, ds_out, operation):
+    h5_filename = raster_utils.temporary_filename()
+    h5file = tables.openFile(h5_filename, mode='w', title="calc_it data")
+    root = h5file.root
+    
+    ds = gdal.Open(ds_args.values()[0])
+    
+    user_args = {}
+    for id, uri in ds_args.iteritems():
+        user_args[id] = h5file.createCArray(
+            root, id, tables.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
+            
+        ds = gdal.Open(uri)
+        band = ds.GetRasterBand(1)
+        for row_index in xrange(ds.RasterYSize):
+            user_args[id][row_index,:] = band.ReadAsArray(0, row_index, ds.RasterXSize, 1)[0]
+        
+    out = h5file.createCArray(
+        root, 'out', tables.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
+        
+    expr = tables.Expr(operation, uservars=user_args)
+    expr.set_output(out)
+    expr.eval()
+
+    raster_utils.new_raster_from_base_uri(
+        ds_args.values()[0], ds_out, 'GTiff', -1, gdal.GDT_Int32)
+    ds = gdal.Open(ds_out, gdal.GA_Update)
+    band = ds.GetRasterBand(1)
+    
+    for row_index in xrange(ds.RasterYSize):
+        band.WriteArray(out[row_index,:].reshape((1,ds.RasterXSize)), 0, row_index)
--- pytable_tracer.py
+++ pytable_tracer.py
@@ -2,7 +2,6 @@ import numpy as np
 import tables as tb
 import gdal
 
-from invest_natcap import raster_utils
 import pytable_tracer_cython
 
 
@@ -12,40 +11,8 @@ ds_args = {
     }
 out_uri = './out.tif'
 
-def calc_it(
-    ds_args, ds_out, operation):
-    h5_filename = raster_utils.temporary_filename()
-    h5file = tb.openFile(h5_filename, mode='w', title="calc_it data")
-    root = h5file.root
-    
-    ds = gdal.Open(ds_args.values()[0])
-    
-    user_args = {}
-    for id, uri in ds_args.iteritems():
-        user_args[id] = h5file.createCArray(
-            root, id, tb.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
-            
-        ds = gdal.Open(uri)
-        band = ds.GetRasterBand(1)
-        for row_index in xrange(ds.RasterYSize):
-            user_args[id][row_index,:] = band.ReadAsArray(0, row_index, ds.RasterXSize, 1)[0]
         
-    out = h5file.createCArray(
-        root, 'out', tb.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
-        
-    expr = tb.Expr(operation, uservars=user_args)
-    expr.set_output(out)
-    expr.eval()
-
-    raster_utils.new_raster_from_base_uri(
-        ds_args.values()[0], ds_out, 'GTiff', -1, gdal.GDT_Int32)
-    ds = gdal.Open(ds_out, gdal.GA_Update)
-    band = ds.GetRasterBand(1)
-    
-    for row_index in xrange(ds.RasterYSize):
-        band.WriteArray(out[row_index,:].reshape((1,ds.RasterXSize)), 0, row_index)
-        
-calc_it(ds_args, out_uri, 'where((ds1==255) | (ds2==255), -1, ds1 + ds2)')
+pytable_tracer_cython.calc_it(ds_args, out_uri, 'where((ds1==255) | (ds2==255), -1, ds1 + ds2)')
  
     
 # ds1 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_cur')
