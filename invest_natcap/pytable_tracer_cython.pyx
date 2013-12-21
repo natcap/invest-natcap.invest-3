fe178677148c966793afe4b7d9c8d72870423cb1
richpsharp <richpsharp@gmail.com>
2013-12-20 18:20:42 -0800

    Update issue 2341
    Testing timings for going row by row and direct numexpr.  About the same order of mag.
    
    
    Former-commit-id: e7213d35912f84f8ab194fc97e33f54e522aec54

--- invest_natcap/pytable_tracer_cython.pyx
+++ invest_natcap/pytable_tracer_cython.pyx
@@ -1,6 +1,6 @@
-import numpy 
 import tables
 import gdal
+cimport numpy
 
 from invest_natcap import raster_utils
 
@@ -12,21 +12,32 @@ def calc_it(ds_args, ds_out, operation):
     ds = gdal.Open(ds_args.values()[0])
     
     user_args = {}
+    cdef int row_index, col_index, n_rows, n_cols
+
     for id, uri in ds_args.iteritems():
         user_args[id] = h5file.createCArray(
             root, id, tables.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
             
         ds = gdal.Open(uri)
         band = ds.GetRasterBand(1)
-        for row_index in xrange(ds.RasterYSize):
+        for row_index in range(ds.RasterYSize):
             user_args[id][row_index,:] = band.ReadAsArray(0, row_index, ds.RasterXSize, 1)[0]
         
     out = h5file.createCArray(
         root, 'out', tables.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
-        
-    expr = tables.Expr(operation, uservars=user_args)
-    expr.set_output(out)
-    expr.eval()
+
+    n_rows = ds.RasterYSize
+    n_cols = ds.RasterXSize
+    cdef numpy.ndarray[numpy.npy_int32, ndim=1] row
+    
+    for row_index in range(n_rows):
+        row = user_args['ds1'][row_index,:]
+        for col_index in range(n_cols):
+            row[col_index] = row[col_index]**2
+        out[row_index,:] = row
+    #expr = tables.Expr(operation, uservars=user_args)
+    #expr.set_output(out)
+    #expr.eval()
 
     raster_utils.new_raster_from_base_uri(
         ds_args.values()[0], ds_out, 'GTiff', -1, gdal.GDT_Int32)
--- pytable_tracer.py
+++ pytable_tracer.py
@@ -1,9 +1,10 @@
-import numpy as np
-import tables as tb
+import numpy
+import tables
 import gdal
+import time
 
 import pytable_tracer_cython
-
+from invest_natcap import raster_utils
 
 ds_args = {
     'ds1': './test/invest-data/test/data/base_data/terrestrial/lulc_samp_cur',
@@ -11,10 +12,59 @@ ds_args = {
     }
 out_uri = './out.tif'
 
-        
-pytable_tracer_cython.calc_it(ds_args, out_uri, 'where((ds1==255) | (ds2==255), -1, ds1 + ds2)')
- 
+
+def calc_it(ds_args, ds_out, operation):
+    h5_filename = raster_utils.temporary_filename()
+    h5file = tables.openFile(h5_filename, mode='w', title="calc_it data")
+    root = h5file.root
     
+    ds = gdal.Open(ds_args.values()[0])
+    
+    user_args = {}
+    
+    for id, uri in ds_args.iteritems():
+        user_args[id] = h5file.createCArray(
+            root, id, tables.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
+            
+        ds = gdal.Open(uri)
+        band = ds.GetRasterBand(1)
+        for row_index in range(ds.RasterYSize):
+            user_args[id][row_index,:] = band.ReadAsArray(0, row_index, ds.RasterXSize, 1)[0]
+        
+    out = h5file.createCArray(
+        root, 'out', tables.Int32Atom(), shape=(ds.RasterYSize,ds.RasterXSize))
+
+    n_rows = ds.RasterYSize
+    n_cols = ds.RasterXSize
+    
+    #for row_index in range(n_rows):
+    #    row = user_args['ds1'][row_index,:]
+    #    for col_index in range(n_cols):
+    #        row[col_index] = row[col_index]**2
+    #    out[row_index,:] = row
+    expr = tables.Expr(operation, uservars=user_args)
+    expr.set_output(out)
+    expr.eval()
+
+    raster_utils.new_raster_from_base_uri(
+        ds_args.values()[0], ds_out, 'GTiff', -1, gdal.GDT_Int32)
+    ds = gdal.Open(ds_out, gdal.GA_Update)
+    band = ds.GetRasterBand(1)
+    
+    for row_index in xrange(ds.RasterYSize):
+        band.WriteArray(out[row_index,:].reshape((1,ds.RasterXSize)), 0, row_index)
+
+start = time.clock()
+pytable_tracer_cython.calc_it(ds_args, out_uri, 'where((ds1==255) | (ds2==255), -1, ds1 + ds2)')
+end = time.clock() - start
+print "%f seconds" % end
+
+start = time.clock()
+calc_it(ds_args, out_uri, 'where((ds1==255) | (ds2==255), -1, ds1 + ds2)')
+end = time.clock() - start
+print "%f seconds" % end
+
+
 # ds1 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_cur')
 # ds2 = gdal.Open('./test/invest-data/test/data/base_data/terrestrial/lulc_samp_fut')
 
