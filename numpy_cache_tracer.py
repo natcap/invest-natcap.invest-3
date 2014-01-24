e0dc1a87c862f8b46043d3352690dd1f971e7b21
richpsharp <richpsharp@gmail.com>
2014-01-24 11:41:30 -0800

    Update issue 2341
    IMplementing an ordered dictionary to help with a fully associative cache.
    
    
    Former-commit-id: d4f53f86a2fa6f4bab86fe10ca3757ad759c0d61

--- numpy_cache_tracer.py
+++ numpy_cache_tracer.py
@@ -6,12 +6,46 @@ from invest_natcap import raster_utils
 dem_uri = './test/invest-data/Base_Data/Freshwater/dem'
 out_uri = 'out.tif'
 
+from UserDict import DictMixin
+
+class odict(DictMixin):
+    
+    def __init__(self):
+        self._keys = []
+        self._data = {}
+        
+        
+    def __setitem__(self, key, value):
+        if key not in self._data:
+            self._keys.append(key)
+        self._data[key] = value
+        
+        
+    def __getitem__(self, key):
+        return self._data[key]
+    
+    
+    def __delitem__(self, key):
+        del self._data[key]
+        self._keys.remove(key)
+        
+        
+    def keys(self):
+        return list(self._keys)
+    
+    
+    def copy(self):
+        copyDict = odict()
+        copyDict._data = self._data.copy()
+        copyDict._keys = self._keys[:]
+        return copyDict
+
+
+    
 nodata_value = raster_utils.get_nodata_from_uri(dem_uri)
-
-raster_utils.new_raster_from_base_uri(
-    dem_uri, out_uri, 'GTiff', nodata_value, gdal.GDT_Float32,
-    INF)
-
+#raster_utils.new_raster_from_base_uri(
+#    dem_uri, out_uri, 'GTiff', nodata_value, gdal.GDT_Float32,
+#    INF)
 
 dem_ds = gdal.Open(dem_uri)
 dem_band = dem_ds.GetRasterBand(1)
@@ -21,35 +55,26 @@ n_rows = dem_ds.RasterYSize
 
 cache_rows = 10
 
-cache_data_array = numpy.empty((cache_rows, 1, n_cols), dtype=numpy.float)
-cache_tag_array = numpy.empty((cache_rows,), dtype=numpy.int)
-cache_tag_array[:] = -1
-cache_dirty_array = numpy.empty((cache_rows,), dtype=numpy.byte)
-cache_dirty_array[:] = 0
+row_cache = odict()
 
 for row_index in xrange(30):
-    print cache_tag_array
     for row_offset in xrange(3):
         offset_row_index = row_index + row_offset
 
         if offset_row_index < 0 or offset_row_index >= n_rows:
             continue
 
-        cache_tag = offset_row_index / cache_rows
-        cache_index = offset_row_index % cache_rows
-
-        if (cache_tag_array[cache_index] != cache_tag):
-            if cache_dirty_array[cache_index] and cache_tag_array[cache_index] != -1:
-                print 'write back cache to row %d' % (cache_rows)
-                dem_band.WriteArray(
-                    cache_data_array[cache_index], xoff=0, yoff=cache_rows)
-                cache_dirty_array[cache_index] = 0
-
-            if cache_tag_array[cache_index] != -1:
-                print 'unloading row %d' % (cache_index + cache_tag_array[cache_index] * cache_rows)
-            cache_tag_array[cache_index] = cache_tag
-            dem_band.ReadAsArray(
-                xoff=0, yoff=offset_row_index,
-                win_xsize=n_cols, win_ysize=1, buf_obj=cache_data_array[cache_index])
+        if offset_row_index not in row_cache:
+            if len(row_cache) > cache_rows:
+                #we need to bump something out of the cache
+                cached_row_index, (row_data, cache_dirty) = row_cache.popitem()
+                if cache_dirty:
+                    print 'write back cache to row %d' % (cached_row_index)
+                    dem_band.WriteArray(
+                        row_data, xoff=0, yoff=cached_row_index)
+            
+            row_data = dem_band.ReadAsArray(
+                xoff=0, yoff=offset_row_index, win_xsize=n_cols, win_ysize=1)
+            row_cache[offset_row_index] = (row_data, False)
         else:
             print "we've got row %d loaded" % (offset_row_index)
