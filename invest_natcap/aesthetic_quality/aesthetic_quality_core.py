5c6b151cd0e74ad7cdac93accb77e0c1ba2559fd
nicolas.chaumont <devnull@localhost>
2014-01-27 17:05:43 -0800

    Update issue 2425: Changed variable name "difference" to "correction" because it's really the correction from the refraction coefficient. Changed default value viewpoint from 1.0 to 1.75 (closer to ESRI's), and commented out ref_coeff assignment so we can have values different from 0.13.
    Former-commit-id: c61b6ce17455cc9e916382ae77773074a9f49b0a

--- invest_natcap/aesthetic_quality/aesthetic_quality_core.py
+++ invest_natcap/aesthetic_quality/aesthetic_quality_core.py
@@ -1190,11 +1190,11 @@ def compute_viewshed(input_array, nodata, coordinates, obs_elev, \
     # http://resources.arcgis.com/en/help/main/10.1/index.html#//00q90000008v000000
     D_earth = 12740000 # Diameter of the earth in meters
     # 
-    difference = (distances*cell_size)**2 * (1 - refraction_coeff) / D_earth 
+    correction = (distances*cell_size)**2 * (1 - refraction_coeff) / D_earth 
     print("refraction coeff", refraction_coeff)
-    print("abs diff", np.sum(np.absolute(difference)), "rel diff" \
-    np.sum(np.absolute(difference))/ np.sum(np.absolute(visibility)))
-    visibility += difference 
+    print("abs correction", np.sum(np.absolute(correction)), "rel correction", \
+    np.sum(np.absolute(correction))/ np.sum(np.absolute(visibility)))
+    visibility += correction
     # 3- Divide the height by the distance to get a visibility score
     visibility /= distances
 
