8fe7d1dfb85711eadfa8af2974bede59ac3c4850
James Douglass <jdouglass@stanford.edu>
2014-03-05 16:16:09 -0800

    Update issue 2653
    Raising an exception when we need a field that is not in the
    user-provided vector.
    
    
    Former-commit-id: d18aa8eab729355eb5ec5c00df51cc212c68a75f

--- invest_natcap/raster_utils.py
+++ invest_natcap/raster_utils.py
@@ -654,15 +654,20 @@ def aggregate_raster_values_uri(
     shapefile_layer = shapefile.GetLayer()
     if shapefile_field is not None:
         LOGGER.debug('Looking up field %s', shapefile_field)
-        
+
         #Make sure that the layer name refers to an integer 
         layer_d = shapefile_layer.GetLayerDefn()
         fd = layer_d.GetFieldDefn(layer_d.GetFieldIndex(shapefile_field))
+        if fd == -1:  # -1 returned when field does not exist.
+            # Raise exception if user provided a field that's not in vector
+            raise AttributeError(('Vector %s must have a field named %s' %
+                (shapefile_uri, shapefile_field)))
+
         if fd.GetTypeName() != 'Integer':
             raise TypeError(
                 'Can only aggreggate by integer based fields, requested '
                 'field is of type  %s' % fd.GetTypeName())
-    
+
         gdal.RasterizeLayer(
             mask_dataset, [1], shapefile_layer,
             options=['ATTRIBUTE=%s' % shapefile_field, 'ALL_TOUCHED=TRUE'])
