3ccdc2e485b421d1dad15adbfb6e7db9621ae3a0
nicolas.chaumont <devnull@localhost>
2014-03-12 13:48:38 -0700

    Fixes issue 2700: Items moved to Advanced tabs.
    Former-commit-id: fda57b8c9b29d3f3cfac3411a26791a4fc359d8f

--- invest_natcap/aesthetic_quality/aesthetic_quality.py
+++ invest_natcap/aesthetic_quality/aesthetic_quality.py
@@ -182,7 +182,7 @@ def compute_viewshed_uri(in_dem_uri, out_viewshed_uri, in_structure_uri,
     refr_coeff, args)
 
 
-def compute_viewshed(input_array, visibility_uri, in_structure_uri, \
+def compute_viewshed(input_array, output_uri, in_structure_uri, \
     cell_size, rows, cols, nodata, GT, I_uri, J_uri, curvature_correction, \
     refr_coeff, args):
     """ array-based function that computes the viewshed as is defined in ArcGIS
@@ -263,7 +263,7 @@ def compute_viewshed(input_array, visibility_uri, in_structure_uri, \
     assert edge_value >= 0., message
         
     # Base path uri
-    base_uri = os.path.split(visibility_uri)[0]
+    base_uri = os.path.split(output_uri)[0]
 
     # Temporary files that will be used 
     distance_uri = raster_utils.temporary_filename()
@@ -325,6 +325,9 @@ def compute_viewshed(input_array, visibility_uri, in_structure_uri, \
 
         array_shape = (rows, cols)
     
+        visibility_uri = raster_utils.temporary_filename()
+        raster_utils.new_raster_from_base_uri(in_dem_uri, visibility_uri, 'GTiff', \
+            255, gdal.GDT_Byte, fill_value = 255)
         aesthetic_quality_core.viewshed(input_array, cell_size, \
         array_shape, nodata, visibility_uri, (i,j), obs_elev, tgt_elev, \
         max_dist, refr_coeff)
@@ -366,7 +369,7 @@ def compute_viewshed(input_array, visibility_uri, in_structure_uri, \
         array = raster.GetRasterBand(1).ReadAsArray()
         accum_array += array
     # Store the accumulated value in the output uri
-    raster = gdal.Open(visibility_uri, gdal.GA_Update)
+    raster = gdal.Open(output_uri, gdal.GA_Update)
     band = raster.GetRasterBand(1)
     band.WriteArray(accum_array)
 
