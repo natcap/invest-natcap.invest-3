14c074224599ed29b86e0228853b088f8d541dc1
richpsharp <richpsharp@gmail.com>
2014-03-16 22:07:58 -0700

    Update issue 2712
    Refactored the cython based vectorize datasets so it's all in a vector.  Okay to remove the cython based core and sediment_core.py
    
    
    Former-commit-id: 5108f3c957dae6edb73314ccc0f16da1187fa028

--- invest_natcap/sediment/sediment_core.py
+++ invest_natcap/sediment/sediment_core.py
@@ -53,14 +53,63 @@ def calculate_ls_factor(flow_accumulation_uri, slope_uri,
 
             returns the ls_factor calculation for this point"""
 
-        return sediment_cython_core.ls_factor_function(
-            float(aspect_angle), float(slope), float(flow_accumulation), float(aspect_nodata), float(slope_nodata), float(flow_accumulation_nodata), float(ls_nodata), float(cell_area), float(cell_size))
+        #Skip the calculation if any of the inputs are nodata
+        nodata_mask = (
+            (aspect_angle == aspect_nodata) | (slope == slope_nodata) |
+            (flow_accumulation == flow_accumulation_nodata))
+        
+        #Here the aspect direction can range from 0 to 2PI, but the purpose
+        #of the term is to determine the length of the flow path on the
+        #pixel, thus we take the absolute value of each trigonometric
+        #function to keep the computation in the first quadrant
+        xij = (numpy.abs(numpy.sin(aspect_angle)) +
+            numpy.abs(numpy.cos(aspect_angle)))
 
+        contributing_area = (flow_accumulation-1) * cell_area
+
+        #To convert to radians, we need to divide the slope by 100 since
+        #it's a percent. :(
+        slope_in_radians = numpy.arctan(slope / 100.0)
+
+        #From Equation 4 in "Extension and validation of a geographic
+        #information system ..."
+        slope_factor = numpy.where(slope < 9.0,
+            10.8 * numpy.sin(slope_in_radians) + 0.03, 
+            16.8 * numpy.sin(slope_in_radians) - 0.5)
+        
+        #Set the m value to the lookup table that's Table 1 in 
+        #InVEST Sediment Model_modifications_10-01-2012_RS.docx in the
+        #FT Team dropbox
+        beta = ((numpy.sin(slope_in_radians) / 0.0896) /
+            (3 * numpy.sin(slope_in_radians)**0.8 + 0.56))
+
+        #slope table in percent
+        slope_table = [1., 3.5, 5., 9.]
+        exponent_table = [0.2, 0.3, 0.4, 0.5]
+        #Look up the correct m value from the table
+        m_exp = beta/(1+beta)
+        for i in range(4):
+            m_exp[slope <= slope_table[i]] = exponent_table[i]
+                
+        #The length part of the ls_factor:
+        l_factor = (
+            ((contributing_area + cell_area)**(m_exp+1) - 
+             contributing_area ** (m_exp+1)) / 
+            ((cell_size ** (m_exp + 2)) * (xij**m_exp) * (22.13**m_exp)))
+
+        #From the McCool paper "as a final check against excessively long slope
+        #length calculations ... cap of 333m"
+        l_factor[l_factor > 333] = 333
+            
+        #This is the ls_factor
+        return numpy.where(nodata_mask, ls_nodata, l_factor * slope_factor)
+        
     #Call vectorize datasets to calculate the ls_factor
     dataset_uri_list = [aspect_uri, slope_uri, flow_accumulation_uri]
     raster_utils.vectorize_datasets(
         dataset_uri_list, ls_factor_function, ls_factor_uri, gdal.GDT_Float32,
-            ls_nodata, cell_size, "intersection", dataset_to_align_index=0)
+        ls_nodata, cell_size, "intersection", dataset_to_align_index=0,
+        vectorize_op=False)
 
 
 def calculate_rkls(
@@ -87,7 +136,7 @@ def calculate_rkls(
     usle_nodata = -1.0
 
     cell_size = raster_utils.get_cell_size_from_uri(ls_factor_uri)
-    cell_area = cell_size ** 2
+    cell_area_ha = cell_size ** 2 / 10000.0
 
     def rkls_function(ls_factor, erosivity, erodibility, v_stream):
         """Calculates the USLE equation
@@ -102,14 +151,14 @@ def calculate_rkls(
             defined, nodata if some are not defined, 0 if in a stream
             (v_stream)"""
 
-        if (ls_factor == ls_factor_nodata or erosivity == erosivity_nodata or 
-            erodibility == erodibility_nodata or v_stream == stream_nodata):
-            return usle_nodata
-        if v_stream == 1:
-            return 0.0
-        #current unit is tons/ha, multiply by ha/cell (cell area in m^2/100**2)
-        return ls_factor * erosivity * erodibility * cell_area / 10000.0
-
+        rkls = numpy.where(
+            v_stream == 1, 0.0,
+            ls_factor * erosivity * erodibility * cell_area_ha)
+        return numpy.where(
+            (ls_factor == ls_factor_nodata) | (erosivity == erosivity_nodata) |
+            (erodibility == erodibility_nodata) | (v_stream == stream_nodata),
+            usle_nodata, rkls)
+        
     dataset_uri_list = [
         ls_factor_uri, erosivity_uri, erodibility_uri, stream_uri]
 
@@ -117,4 +166,5 @@ def calculate_rkls(
     #aligned with LULCs
     raster_utils.vectorize_datasets(
         dataset_uri_list, rkls_function, rkls_uri, gdal.GDT_Float32,
-        usle_nodata, cell_size, "intersection", dataset_to_align_index=3)
+        usle_nodata, cell_size, "intersection", dataset_to_align_index=3,
+        vectorize_op=False)
