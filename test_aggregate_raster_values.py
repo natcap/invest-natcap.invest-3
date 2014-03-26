0dcb91e9adc4d6f3c158c2b3e1b70ec38dcf0e6c
richpsharp <richpsharp@gmail.com>
2014-03-26 16:31:57 -0700

    Update issue 2378
    Got polygons loaded in shapely and testing.
    
    
    Former-commit-id: da1267e5ad2c620f9b797c7494ee2596e0930d2c

--- test_aggregate_raster_values.py
+++ test_aggregate_raster_values.py
@@ -16,3 +16,29 @@ values = raster_utils.aggregate_raster_values_uri(
     ignore_value_list=[], process_pool=None)
 
 print values
+
+from osgeo import ogr
+import shapely.wkt
+import shapely.ops
+from shapely import speedups
+
+shapefile = ogr.Open(shapefile_uri)
+shapefile_layer = shapefile.GetLayer()
+poly_list = []
+
+for poly_feat in shapefile_layer:
+    # Get the geometry of the polygon in WKT format
+    poly_wkt = poly_feat.GetGeometryRef().ExportToWkt()
+    # Load the geometry into shapely making it a shapely object
+    shapely_polygon = shapely.wkt.loads(poly_wkt)
+    # Add the shapely polygon geometry to a list, but first simplify the
+    # geometry which smooths the edges making operations a lot faster
+    poly_list.append(
+        shapely_polygon.simplify(0.01, preserve_topology=False))
+        
+for p1 in poly_list:
+    for p2 in poly_list:
+        print p1.disjoint(p2)
+        
+
+
