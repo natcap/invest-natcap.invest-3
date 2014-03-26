7eb21dd4f5046284437dc8626660947ce0c84e61
richpsharp <richpsharp@gmail.com>
2014-03-26 16:20:08 -0700

    Update issue 2378
    Creating a test case for the 11 columbia polygons.
    
    
    Former-commit-id: 23aaef75d5311ee2e0729e7d549676fd4a2b6451

--- test_aggregate_raster_values.py
+++ test_aggregate_raster_values.py
@@ -8,11 +8,11 @@ from invest_natcap import raster_utils
 
 import pyximport
 pyximport.install()
-raster_uri = 'overlapping_polygons/sample_stat_impact_map.tif'
-shapefile_uri = 'servicesheds_col.shp'
-raster_utils.aggregate_raster_values_uri(
-    raster_uri, shapefile_uri, shapefile_field=None,
+raster_uri = 'overlapping_polygons/sample_static_impact_map.tif'
+shapefile_uri = 'overlapping_polygons/servicesheds_col.shp'
+values = raster_utils.aggregate_raster_values_uri(
+    raster_uri, shapefile_uri, shapefile_field='OBJECTID',
     ignore_nodata=True, threshold_amount_lookup=None,
     ignore_value_list=[], process_pool=None)
 
-print 'if we got here everything passed!'
+print values
