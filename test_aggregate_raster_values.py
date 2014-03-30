ff482ff7caa13b46e483dda005bd005fd728fd45
richpsharp <richpsharp@gmail.com>
2014-03-30 09:42:35 -0700

    Update issue 2378
    Stable version of looping over every polygon.
    
    
    Former-commit-id: 539e2fb51e2b141315255aea5c8c1a6cec5c7487

--- test_aggregate_raster_values.py
+++ test_aggregate_raster_values.py
@@ -1,23 +1,33 @@
 import cProfile
 import pstats
 
+import os
+import glob
 import time
 import gdal
 import numpy
 from osgeo import ogr
 
-
 from invest_natcap import raster_utils
 
 import pyximport
 pyximport.install()
-raster_uri = 'overlapping_polygons/sample_static_impact_map.tif'
-shapefile_uri = 'overlapping_polygons/servicesheds_col.shp'
+raster_uri = 'test/invest-data/Base_Data/Freshwater/dem'
+shapefile_uri = 'test/invest-data/Base_Data/Freshwater/subwatersheds.shp'
 values = raster_utils.aggregate_raster_values_uri(
-    raster_uri, shapefile_uri, shapefile_field='OBJECTID',
+    raster_uri, shapefile_uri, shapefile_field='subws_id',
     ignore_nodata=True, threshold_amount_lookup=None,
     ignore_value_list=[], process_pool=None)
 
+output_uri = 'mask.tif'    
+mask_nodata = -9999.0
+raster_utils.new_raster_from_base_uri(
+    raster_uri, output_uri, 'GTiff', mask_nodata, gdal.GDT_Float32, 
+    fill_value=mask_nodata)
+    
+mask_ds = gdal.Open(output_uri, gdal.GA_Update)
+mask_band = mask_ds.GetRasterBand(1)
+    
 print values
 
 from osgeo import ogr
@@ -32,8 +42,14 @@ poly_list = []
 # Make a geometry, from Shapely object
 # Now convert it to a shapefile with OGR    
 driver = ogr.GetDriverByName('ESRI Shapefile')
+for fl in glob.glob('layer_out.*'):
+    #Do what you want with the file
+    os.remove(fl)
 ds = driver.CreateDataSource('layer_out.shp')
-layer = ds.CreateLayer('layer_out', None, ogr.wkbPolygon)
+
+spat_ref = raster_utils.get_spatial_ref_uri(shapefile_uri)
+
+layer = ds.CreateLayer('layer_out', spat_ref, ogr.wkbPolygon)
 layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
 count = 0
 for poly_feat in shapefile_layer:
@@ -56,9 +72,9 @@ for poly_feat in shapefile_layer:
     feat.SetGeometry(geom)
     layer.CreateFeature(feat)
     
-    #gdal.RasterizeLayer(
-    #    mask_dataset, [1], shapefile_layer,
-    #    options=['ATTRIBUTE=%s' % shapefile_field, 'ALL_TOUCHED=TRUE'])
+    gdal.RasterizeLayer(
+        mask_ds, [1], layer, burn_values=[1],
+        options=['ALL_TOUCHED=TRUE'])
     
     layer.DeleteFeature(count)
     count += 1
