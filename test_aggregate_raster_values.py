b81bf9a336de441395301069c5adf19d48157024
richpsharp <richpsharp@gmail.com>
2014-03-30 10:19:39 -0700

    Update issue 2378
    Altering loop so it uses temporary files and directories for the shapefile and mask layer.
    
    
    Former-commit-id: 0fa0e393cdd1532cf9944d3893a72f9c85c5e2c2

--- test_aggregate_raster_values.py
+++ test_aggregate_raster_values.py
@@ -8,52 +8,52 @@ import gdal
 import numpy
 from osgeo import ogr
 
-from invest_natcap import raster_utils
-
-import pyximport
-pyximport.install()
-raster_uri = 'test/invest-data/Base_Data/Freshwater/dem'
-shapefile_uri = 'test/invest-data/Base_Data/Freshwater/subwatersheds.shp'
-values = raster_utils.aggregate_raster_values_uri(
-    raster_uri, shapefile_uri, shapefile_field='subws_id',
-    ignore_nodata=True, threshold_amount_lookup=None,
-    ignore_value_list=[], process_pool=None)
-
-output_uri = 'mask.tif'    
-mask_nodata = -9999.0
-raster_utils.new_raster_from_base_uri(
-    raster_uri, output_uri, 'GTiff', mask_nodata, gdal.GDT_Float32, 
-    fill_value=mask_nodata)
-    
-mask_ds = gdal.Open(output_uri, gdal.GA_Update)
-mask_band = mask_ds.GetRasterBand(1)
-    
-print values
-
-from osgeo import ogr
 import shapely.wkt
 import shapely.ops
 from shapely import speedups
 
+from invest_natcap import raster_utils
+
+import pyximport
+pyximport.install()
+
+
+raster_uri = 'test/invest-data/Base_Data/Freshwater/dem'
+shapefile_uri = 'test/invest-data/Base_Data/Freshwater/subwatersheds.shp'
+
+mask_uri = raster_utils.temporary_filename()
+mask_nodata = 255
+raster_utils.new_raster_from_base_uri(
+    raster_uri, mask_uri, 'GTiff', mask_nodata, gdal.GDT_Byte, 
+    fill_value=mask_nodata)
+
+#Need this for a call to gdal.RasterizeLayer
+mask_ds = gdal.Open(mask_uri, gdal.GA_Update)
+mask_band = mask_ds.GetRasterBand(1)
+
 shapefile = ogr.Open(shapefile_uri)
 shapefile_layer = shapefile.GetLayer()
-poly_list = []
 
-# Make a geometry, from Shapely object
-# Now convert it to a shapefile with OGR    
+#make a shapefile that non-overlapping layers can be added to
 driver = ogr.GetDriverByName('ESRI Shapefile')
-for fl in glob.glob('layer_out.*'):
-    #Do what you want with the file
-    os.remove(fl)
-ds = driver.CreateDataSource('layer_out.shp')
 
+layer_dir = raster_utils.temporary_folder()
+layer_datasouce = driver.CreateDataSource(os.path.join(layer_dir, 'layer_out.shp'))
 spat_ref = raster_utils.get_spatial_ref_uri(shapefile_uri)
-
-layer = ds.CreateLayer('layer_out', spat_ref, ogr.wkbPolygon)
+layer = layer_datasouce.CreateLayer('layer_out', spat_ref, ogr.wkbPolygon)
 layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
+# Add one feature
+layer_definition = layer.GetLayerDefn()
+#values = raster_utils.aggregate_raster_values_uri(
+#    raster_uri, shapefile_uri, shapefile_field='subws_id',
+#    ignore_nodata=True, threshold_amount_lookup=None,
+#    ignore_value_list=[], process_pool=None)
+
+poly_list = []
 count = 0
-for poly_feat in shapefile_layer:
+for poly_index in range(shapefile_layer.GetFeatureCount()):
     print 'looping'
+    poly_feat = shapefile_layer.GetFeature(poly_index)
     # Get the geometry of the polygon in WKT format
     poly_wkt = poly_feat.GetGeometryRef().ExportToWkt()
     # Load the geometry into shapely making it a shapely object
@@ -62,10 +62,8 @@ for poly_feat in shapefile_layer:
     # geometry which smooths the edges making operations a lot faster
     poly_list.append(
         shapely_polygon.simplify(0.01, preserve_topology=False))
-        
-    # Add one feature
-    defn = layer.GetLayerDefn()
-    feat = ogr.Feature(defn)
+
+    feat = ogr.Feature(layer_definition)
     feat.SetField('id', 123)
     
     geom = ogr.CreateGeometryFromWkb(shapely_polygon.wkb)
@@ -76,20 +74,12 @@ for poly_feat in shapefile_layer:
         mask_ds, [1], layer, burn_values=[1],
         options=['ALL_TOUCHED=TRUE'])
     
-    layer.DeleteFeature(count)
+    layer.DeleteFeature(feat.GetFID())
     count += 1
     
 feat = geom = None  # destroy these
 # Save and close everything
 ds = layer = feat = geom = None
     
-    
-
-        
-        
-#for p1 in poly_list:
-#    for p2 in poly_list:
-#        print p1.disjoint(p2)
-        
-        
-    
+for x in [mask_uri, layer_dir]:
+    shutil.rmtree(x)
