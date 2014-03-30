1d3a6d4f63e7b68798d3353df418a44601a19828
richpsharp <richpsharp@gmail.com>
2014-03-29 23:48:53 -0700

    Update issue 2378
    Example of looping through polygon features, and adding them to an OGR that could be rasterized.
    
    
    Former-commit-id: a36337d84a019c03066055fc6293b2d6c3fc22fe

--- test_aggregate_raster_values.py
+++ test_aggregate_raster_values.py
@@ -29,7 +29,15 @@ shapefile = ogr.Open(shapefile_uri)
 shapefile_layer = shapefile.GetLayer()
 poly_list = []
 
+# Make a geometry, from Shapely object
+# Now convert it to a shapefile with OGR    
+driver = ogr.GetDriverByName('ESRI Shapefile')
+ds = driver.CreateDataSource('layer_out.shp')
+layer = ds.CreateLayer('layer_out', None, ogr.wkbPolygon)
+layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
+count = 0
 for poly_feat in shapefile_layer:
+    print 'looping'
     # Get the geometry of the polygon in WKT format
     poly_wkt = poly_feat.GetGeometryRef().ExportToWkt()
     # Load the geometry into shapely making it a shapely object
@@ -39,52 +47,33 @@ for poly_feat in shapefile_layer:
     poly_list.append(
         shapely_polygon.simplify(0.01, preserve_topology=False))
         
+    # Add one feature
+    defn = layer.GetLayerDefn()
+    feat = ogr.Feature(defn)
+    feat.SetField('id', 123)
+    
+    geom = ogr.CreateGeometryFromWkb(shapely_polygon.wkb)
+    feat.SetGeometry(geom)
+    layer.CreateFeature(feat)
+    
+    #gdal.RasterizeLayer(
+    #    mask_dataset, [1], shapefile_layer,
+    #    options=['ATTRIBUTE=%s' % shapefile_field, 'ALL_TOUCHED=TRUE'])
+    
+    layer.DeleteFeature(count)
+    count += 1
+    
+feat = geom = None  # destroy these
+# Save and close everything
+ds = layer = feat = geom = None
+    
+    
 
-out_shape = raster_utils.temporary_folder()
-driver = ogr.GetDriverByName('ESRI Shapefile')
-datasource = driver.CreateDataSource(out_shape)
-
-# Create the layer name from the uri paths basename without the extension
-layer_name = 'temporary_shapefile'
-layer = datasource.CreateLayer(layer_name, spat_ref, ogr.wkbLineString)
-
-# Add a single ID field
-field = ogr.FieldDefn('id', ogr.OFTReal)
-layer.CreateField(field)
-
-# Create the 3 other points that will make up the vertices for the lines 
-top_left = (start_point[0], start_point[1] + y_len)
-top_right = (start_point[0] + x_len, start_point[1] + y_len)
-bottom_right = (start_point[0] + x_len, start_point[1])
-
-# Create a new feature, setting the field and geometry
-line = ogr.Geometry(ogr.wkbLineString)
-line.AddPoint(start_point[0], start_point[1])
-line.AddPoint(top_left[0], top_left[1])
-line.AddPoint(top_right[0], top_right[1])
-line.AddPoint(bottom_right[0], bottom_right[1])
-line.AddPoint(start_point[0], start_point[1])
-
-feature = ogr.Feature(layer.GetLayerDefn())
-feature.SetGeometry(line)
-feature.SetField(0, 1)
-layer.CreateFeature(feature)
-
-feature = None
-layer = None
-
-datasource.SyncToDisk()
-datasource = None        
         
         
+#for p1 in poly_list:
+#    for p2 in poly_list:
+#        print p1.disjoint(p2)
         
-for p1 in poly_list:
-    for p2 in poly_list:
-        print p1.disjoint(p2)
         
-        
-gdal.RasterizeLayer(
-    mask_dataset, [1], shapefile_layer,
-    options=['ATTRIBUTE=%s' % shapefile_field, 'ALL_TOUCHED=TRUE'])
-
     
