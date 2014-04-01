1c9d98d1a786abb69abdcffd91f3a4947d286303
richpsharp <richpsharp@gmail.com>
2014-04-01 10:39:40 -0700

    Update issue 2378
    Building a lookup table of polygons to other polygons.
    
    
    Former-commit-id: 2212004ba37963da565af95a8ea619544e00cacb

--- test_aggregate_raster_values.py
+++ test_aggregate_raster_values.py
@@ -11,14 +11,14 @@ from osgeo import ogr
 
 import shapely.wkt
 import shapely.ops
+import shapely.prepared
 from shapely import speedups
-
 from invest_natcap import raster_utils
 
 import pyximport
 pyximport.install()
 
-raster_uri = 'Peru_for_Rich/dem_50km'
+#raster_uri = 'Peru_for_Rich/dem_50km'
 shapefile_uri = 'Peru_for_Rich/serv_clip50in.shp'
 
 #raster_uri =    'overlapping_polygons/sample_static_impact_map.tif'
@@ -27,67 +27,73 @@ shapefile_uri = 'Peru_for_Rich/serv_clip50in.shp'
 #raster_uri = 'test/invest-data/Base_Data/Freshwater/dem'
 #shapefile_uri = 'test/invest-data/Base_Data/Freshwater/subwatersheds.shp'
 
-mask_uri = raster_utils.temporary_filename()
-mask_nodata = 255
-raster_utils.new_raster_from_base_uri(
-    raster_uri, mask_uri, 'GTiff', mask_nodata, gdal.GDT_Byte, 
-    fill_value=mask_nodata)
+#mask_uri = raster_utils.temporary_filename()
+#mask_nodata = 255
+#raster_utils.new_raster_from_base_uri(
+#    raster_uri, mask_uri, 'GTiff', mask_nodata, gdal.GDT_Byte, 
+#    fill_value=mask_nodata)
 
 #Need this for a call to gdal.RasterizeLayer
-mask_ds = gdal.Open(mask_uri, gdal.GA_Update)
-mask_band = mask_ds.GetRasterBand(1)
+#mask_ds = gdal.Open(mask_uri, gdal.GA_Update)
+#mask_band = mask_ds.GetRasterBand(1)
 
 shapefile = ogr.Open(shapefile_uri)
 shapefile_layer = shapefile.GetLayer()
 
 #make a shapefile that non-overlapping layers can be added to
-driver = ogr.GetDriverByName('ESRI Shapefile')
-layer_dir = raster_utils.temporary_folder()
-layer_datasouce = driver.CreateDataSource(os.path.join(layer_dir, 'layer_out.shp'))
-spat_ref = raster_utils.get_spatial_ref_uri(shapefile_uri)
-layer = layer_datasouce.CreateLayer('layer_out', spat_ref, ogr.wkbPolygon)
-layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
+#driver = ogr.GetDriverByName('ESRI Shapefile')
+#layer_dir = raster_utils.temporary_folder()
+#layer_datasouce = driver.CreateDataSource(os.path.join(layer_dir, 'layer_out.shp'))
+#spat_ref = raster_utils.get_spatial_ref_uri(shapefile_uri)
+#layer = layer_datasouce.CreateLayer('layer_out', spat_ref, ogr.wkbPolygon)
+#layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
 # Add one feature
-layer_definition = layer.GetLayerDefn()
+#layer_definition = layer.GetLayerDefn()
 
-values = raster_utils.aggregate_raster_values_uri(
-    raster_uri, shapefile_uri, shapefile_field='ws_id',
-    ignore_nodata=True, threshold_amount_lookup=None,
-    ignore_value_list=[], process_pool=None)
-print values
-    
-poly_list = []
-for poly_index in range(shapefile_layer.GetFeatureCount()):
-    print 'looping'
-    poly_feat = shapefile_layer.GetFeature(poly_index)
-    # Get the geometry of the polygon in WKT format
+#values = raster_utils.aggregate_raster_values_uri(
+#    raster_uri, shapefile_uri, shapefile_field='ws_id',
+#    ignore_nodata=True, threshold_amount_lookup=None,
+#    ignore_value_list=[], process_pool=None)
+#print values
+
+poly_intersection_lookup = {}
+for poly_feat in shapefile_layer:
+    #print 'looping'
+    #poly_feat = shapefile_layer.GetFeature(poly_index)
     poly_wkt = poly_feat.GetGeometryRef().ExportToWkt()
-    # Load the geometry into shapely making it a shapely object
     shapely_polygon = shapely.wkt.loads(poly_wkt)
+    poly_fid = poly_feat.GetFID()
+    print poly_fid
+    poly_intersection_lookup[poly_fid] = {
+        'poly': shapely_polygon,
+        'intersects': set(),
+    }
+    # Get the geometry of the polygon in WKT format
+    # Load the geometry into shapely making it a shapely object
     # Add the shapely polygon geometry to a list, but first simplify the
     # geometry which smooths the edges making operations a lot faster
-    poly_list.append(
-        shapely_polygon.simplify(0.01, preserve_topology=False))
+    #poly_list.append(shapely_polygon)
 
-    feat = ogr.Feature(layer_definition)
-    feat.SetField('id', 123)
-    
-    geom = ogr.CreateGeometryFromWkb(shapely_polygon.wkb)
-    feat.SetGeometry(geom)
-    layer.CreateFeature(feat)
-    
-    gdal.RasterizeLayer(
-        mask_ds, [1], layer, burn_values=[1],
-        options=['ALL_TOUCHED=TRUE'])
-    #done with that rasterize, remove it
-    layer.DeleteFeature(feat.GetFID())
-    
+shapefile_layer.ResetReading()
+
+for poly_fid in poly_intersection_lookup:
+    print poly_fid
+    for intersect_poly_fid in poly_intersection_lookup:
+        polygon = shapely.prepared.prep(
+            poly_intersection_lookup[poly_fid]['poly'])
+        if polygon.intersects(
+            poly_intersection_lookup[intersect_poly_fid]['poly']):
+            poly_intersection_lookup[poly_fid]['intersects'].add(
+                intersect_poly_fid)
+
+                
+print poly_intersection_lookup
 #Clean up temporary files
-gdal.Dataset.__swig_destroy__(mask_ds)
-ogr.DataSource.__swig_destroy__(layer_datasouce)
+#gdal.Dataset.__swig_destroy__(mask_ds)
+#ogr.DataSource.__swig_destroy__(layer_datasouce)
     
-for x in [mask_uri, layer_dir]:
-    try:
-        shutil.rmtree(x)
-    except:
-        os.remove(x)
+#for x in [mask_uri, layer_dir]:
+#    try:
+#        shutil.rmtree(x)
+#    except:
+#        os.remove(x)
