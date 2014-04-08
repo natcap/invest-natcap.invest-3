1fcd6c61e9b76f5e50e11d12ac3f0b79d8eed152
richpsharp <richpsharp@gmail.com>
2014-04-07 21:11:47 -0700

    Update issue 2781
    Making call to distance to stream.
    
    
    Former-commit-id: 3dfc65633ccfbc425b14cced0b3501dcdcabfc41

--- test_distance_from_stream.py
+++ test_distance_from_stream.py
@@ -14,6 +14,7 @@ flow_direction_uri = 'test_flow.tif'
 flow_accumulation_uri = 'test_flux.tif'
 flow_threshold = 1000
 stream_uri = 'test_stream.tif'
+distance_uri = 'test_distance_to_stream.tif'
 routing_utils.resolve_flat_regions_for_drainage(
     dem_uri, resolved_dem_uri)
 
@@ -22,4 +23,6 @@ routing_utils.flow_accumulation(
     flow_direction_uri, resolved_dem_uri, flow_accumulation_uri)
 routing_utils.stream_threshold(
     flow_accumulation_uri, flow_threshold, stream_uri)
+routing_utils.distance_to_stream(
+    flow_direction_uri, stream_uri, distance_uri)
     
