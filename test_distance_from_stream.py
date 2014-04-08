a435b013608117ea074e705604af168e1714f8b0
richpsharp <richpsharp@gmail.com>
2014-04-07 21:09:20 -0700

    Update issue 2781
    Need to resolve flat regions first...
    
    
    Former-commit-id: 51b960715696239c4036477e06bc570f8c96ebc9

--- test_distance_from_stream.py
+++ test_distance_from_stream.py
@@ -9,13 +9,17 @@ import routing_cython_core
 
 
 dem_uri = 'test/invest-data/Base_Data/Freshwater/dem'
+resolved_dem_uri = 'test_resolved_dem.tif'
 flow_direction_uri = 'test_flow.tif'
 flow_accumulation_uri = 'test_flux.tif'
 flow_threshold = 1000
 stream_uri = 'test_stream.tif'
-routing_utils.flow_direction_inf(dem_uri, flow_direction_uri)
+routing_utils.resolve_flat_regions_for_drainage(
+    dem_uri, resolved_dem_uri)
+
+routing_utils.flow_direction_inf(resolved_dem_uri, flow_direction_uri)
 routing_utils.flow_accumulation(
-    flow_direction_uri, dem_uri, flow_accumulation_uri)
+    flow_direction_uri, resolved_dem_uri, flow_accumulation_uri)
 routing_utils.stream_threshold(
     flow_accumulation_uri, flow_threshold, stream_uri)
     
