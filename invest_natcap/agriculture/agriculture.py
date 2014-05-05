8e8b0350ec9191ae303730d022ca317440540b11
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2014-05-05 15:27:24 -0700

    Update issue 2902
    Added crop summary table to report.
    
    
    Former-commit-id: 6d9b2b3a189c11f7693426baecd9ffce2074e8da

--- invest_natcap/agriculture/agriculture.py
+++ invest_natcap/agriculture/agriculture.py
@@ -1,6 +1,7 @@
-import scipy.io.netcdf
-
 import logging
+
+import os
+from invest_natcap import raster_utils
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -8,4 +9,28 @@ logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 LOGGER = logging.getLogger('agriculture')
 
 def execute(args):
-    pass
+    workspace_dir = args["workspace_dir"]
+
+    report_name = "report.htm"
+
+    crop_cover_uri = args["crop_file_name"]
+    
+    report_uri = os.path.join(workspace_dir, report_name)
+
+    report = open(report_uri, 'w')
+    report.write("<HTML>")
+    
+    report.write("\n<TABLE>")
+    report.write("\n<TR><TD>Crop</TD><TD>Count</TD></TR>")
+
+    crop_counts = raster_utils.unique_raster_values_count(crop_cover_uri)
+    crop_counts_keys = crop_counts.keys()
+    crop_counts_keys.sort()
+
+    for crop in crop_counts_keys:
+        report.write("\n<TR><TD>%i</TD><TD>%i</TD></TR>" % (crop,
+                                                            crop_counts_keys[crop]))
+
+    report.write("\n<\TABLE>")
+    report.write("\n</HTML>")
+    report.close()
