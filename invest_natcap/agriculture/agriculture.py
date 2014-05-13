6e1d2bcebd1ccd216c6f4cbc9e521c503fb4bf30
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2014-05-12 17:03:35 -0700

    Update issue 2935
    Added look up of existing yields.
    
    
    Former-commit-id: d282e2d000a17bae6a9b05cf0eaabfc0e1dba956

--- invest_natcap/agriculture/agriculture.py
+++ invest_natcap/agriculture/agriculture.py
@@ -14,7 +14,9 @@ LOGGER = logging.getLogger('agriculture')
 
 def execute(args):
     gdal_type_cover = gdal.GDT_Int32
+    gdal_type_float = gdal.GDT_Float32
     nodata_int = -1
+    nodata_float = -1
     
     intermediate_dir = "intermediate"
 
@@ -30,20 +32,28 @@ def execute(args):
     reclass_table_field_invest = "InVEST Value"
 
     raster_table_uri = args["raster_table"]
+    raster_path = os.path.dirname(raster_table_uri)
     raster_table_field_key = "Id"
     raster_table_field_short_name = "Short Name"
     raster_table_other_short_name = "Other"
 
+    raster_table_field_yield = "Monfreda_yield"
 
-    reclass_crop_cover_uri = os.path.join(os.path.join(workspace_dir,
-                                                       intermediate_dir),
+    crop_yield_name = "%i_yield.tif"
+    
+
+    intermediate_uri = os.path.join(workspace_dir, intermediate_dir)
+    
+    reclass_crop_cover_uri = os.path.join(intermediate_uri,
                                           reclass_name)
     
     report_uri = os.path.join(workspace_dir, report_name)
     
     #data validation and setup
-    if not os.path.exists(os.path.join(workspace_dir, intermediate_dir)):
-        os.makedirs(os.path.join(workspace_dir, intermediate_dir))
+    if not os.path.exists(intermediate_uri):
+        os.makedirs(intermediate_uri)
+
+    LOGGER.debug("Raster path: %s.", raster_path)
 
     cell_size = raster_utils.get_cell_size_from_uri(crop_cover_uri)
     LOGGER.debug("Crop cover cell size %s square meters.", cell_size)
@@ -75,13 +85,44 @@ def execute(args):
                                         exception_flag = "values_required",
                                         assert_dataset_projected = False)
 
+    invest_crops = raster_utils.unique_raster_values_count(reclass_crop_cover_uri).keys()
+    invest_crops.sort()
+    if invest_crops[0] == 0:
+        invest_crops.pop(0)
+
+    def yield_op_closure(crop):
+        def yield_op(cover, crop_yield):
+            if crop_yield == nodata_float:
+                return nodata_float
+            elif cover != crop:
+                return 0
+            else:
+                return crop_yield
+
+        return yield_op
+
+    for crop in invest_crops:
+        LOGGER.debug("Separating out crop %i.", crop)
+        crop_yield_uri = os.path.join(intermediate_uri, crop_yield_name % crop)
+        raster_utils.vectorize_datasets([reclass_crop_cover_uri,
+                                         os.path.join(raster_path,
+                                                      raster_table_csv_dict[crop][raster_table_field_yield])],
+                                        yield_op_closure(crop),
+                                        crop_yield_uri,
+                                        gdal_type_float,
+                                        nodata_float,
+                                        cell_size,
+                                        "dataset",
+                                        dataset_to_bound_index=0,
+                                        assert_datasets_projected=False)
+
     #create report
     report = open(report_uri, 'w')
     report.write("<HTML>")
 
     report.write("<B>Crop Cover</B>")
     report.write("\n<TABLE BORDER=1>")
-    row_html = "\n<TR><TD ALIGN=CENTER>%s</TD><TD ALIGN=CENTER>%s</TD><TD ALIGN=CENTER>%s</TD><TD ALIGN=RIGHT>%s</TD></TR>"
+    row_html = "\n<TR>" + ("<TD ALIGN=CENTER>%s</TD>" * 3)+ "<TD ALIGN=RIGHT>%s</TD></TR>"
     report.write(row_html % (reclass_table_field_key,
                              reclass_table_field_invest,
                              raster_table_field_short_name,
