1e96a11d320a9f355b4f10dc2ea4d42a2cecc03d
richpsharp <richpsharp@gmail.com>
2014-05-14 09:42:21 -0700

    Update issue 2932
    Adding docstring to execute that matches with the IUI MVP.
    
    
    Former-commit-id: 6d36af94737530a87daddcb0659a91c9173284d2

--- invest_natcap/habitat_suitability/habitat_suitability.py
+++ invest_natcap/habitat_suitability/habitat_suitability.py
@@ -1,5 +1,22 @@
 """Habitat suitability model"""
 
 def execute(args):
-    """ENTRY POINT"""
+    """Calculates habitat suitability scores and patches given biophysical 
+        rasters and classification curves. 
+        
+        workspace_dir - uri to workspace directory for output files
+        output_cell_size - (optional) size of output cells
+        depth_biophysical_uri - uri to a depth raster 
+        salinity_biophysical_uri - uri to salinity raster
+        temperature_biophysical_uri - uri to temperature raster
+        oyster_habitat_suitability_depth_table_uri - uri to a csv table that
+            has that has columns "Suitability" in (0,1) and "Depth" in
+            range(depth_biophysical_uri)
+        oyster_habitat_suitability_salinity_table_uri -  uri to a csv table that
+            has that has columns "Suitability" in (0,1) and "Salinity" in 
+            range(salinity_biophysical_uri)
+        oyster_habitat_suitability_temperature_table_uri - uri to a csv table
+            that has that has columns "Suitability" in (0,1) and  "Temperature"
+            in range(temperature_biophysical_uri)
+           """
     pass
