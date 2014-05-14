fe5845d0d8853e1e313f78589cc9aa00f5519a38
richpsharp <richpsharp@gmail.com>
2014-05-14 09:53:50 -0700

    Update issue 2932
    Aligning all the input biophysical layers.
    
    
    Former-commit-id: 381aadd003901fce4e47823d13d79d48903f3ccb

--- invest_natcap/habitat_suitability/habitat_suitability.py
+++ invest_natcap/habitat_suitability/habitat_suitability.py
@@ -1,5 +1,16 @@
 """Habitat suitability model"""
 
+import os
+import logging
+
+from invest_natcap import raster_utils
+
+logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
+%(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('sediment')
+
+
 def execute(args):
     """Calculates habitat suitability scores and patches given biophysical 
         rasters and classification curves. 
@@ -19,4 +30,41 @@ def execute(args):
             that has that has columns "Suitability" in (0,1) and  "Temperature"
             in range(temperature_biophysical_uri)
            """
-    pass
+    try:
+        file_suffix = args['suffix']
+        if file_suffix != "" and not file_suffix.startswith('_'):
+            file_suffix = '_' + file_suffix
+    except KeyError:
+        file_suffix = ''
+        
+    intermediate_dir = os.path.join(args['workspace_dir'], 'intermediate')
+    output_dir = os.path.join(args['workspace_dir'], 'output')
+
+    #Sets up the intermediate and output directory structure for the workspace
+    for directory in [output_dir, intermediate_dir]:
+        if not os.path.exists(directory):
+            LOGGER.info('creating directory %s', directory)
+            os.makedirs(directory)
+            
+    aligned_raster_stack = {
+        'salinity_biophysical_uri': os.path.join(
+            intermediate_dir, 'aligned_salinity.tif'),
+        'temperature_biophysical_uri': os.path.join(
+            intermediate_dir, 'aligned_temperature.tif'),
+        'depth_biophysical_uri':  os.path.join(
+            intermediate_dir, 'algined_depth.tif')
+    }
+    biophysical_keys = [
+        'salinity_biophysical_uri', 'temperature_biophysical_uri',
+        'depth_biophysical_uri']
+    dataset_uri_list = [args[x] for x in biophysical_keys]
+    dataset_out_uri_list = [aligned_raster_stack[x] for x in biophysical_keys]
+    
+    out_pixel_size = min(
+        [raster_utils.get_cell_size_from_uri(x) for x in dataset_uri_list])
+    
+    raster_utils.align_dataset_list(
+        dataset_uri_list, dataset_out_uri_list,
+        ['nearest'] * len(dataset_out_uri_list),
+        out_pixel_size, 'intersection', 0)
+    
