83f24699a493e5ed67acb6e8bc4b2a899a199610
richpsharp <richpsharp@gmail.com>
2014-05-14 10:20:26 -0700

    Update issue 2932
    Building up an interpolation table.
    
    
    Former-commit-id: db4aba84a567331bf9c415a5b802b7887cd52db9

--- invest_natcap/habitat_suitability/habitat_suitability.py
+++ invest_natcap/habitat_suitability/habitat_suitability.py
@@ -2,6 +2,9 @@
 
 import os
 import logging
+import csv
+
+import scipy
 
 from invest_natcap import raster_utils
 
@@ -45,7 +48,8 @@ def execute(args):
         if not os.path.exists(directory):
             LOGGER.info('creating directory %s', directory)
             os.makedirs(directory)
-            
+           
+    #align the raster lists
     aligned_raster_stack = {
         'salinity_biophysical_uri': os.path.join(
             intermediate_dir, 'aligned_salinity.tif'),
@@ -68,3 +72,27 @@ def execute(args):
         ['nearest'] * len(dataset_out_uri_list),
         out_pixel_size, 'intersection', 0)
     
+    
+    #build up the interpolation functions for the habitat
+    biophysical_to_table = {
+        'salinity_biophysical_uri': 
+            ('oyster_habitat_suitability_salinity_table_uri', 'Salinity'),
+        'temperature_biophysical_uri': 
+            ('oyster_habitat_suitability_temperature_table_uri', 'Temperature'),
+        'depth_biophysical_uri': 
+            ('oyster_habitat_suitability_depth_table_uri', 'Depth'),
+        }
+    biophysical_to_interp = {}
+    for biophysical_uri, (habitat_suitability_table_uri, key) in \
+            biophysical_to_table.iteritems():
+        csv_dict_reader = csv.DictReader(
+            open(args[habitat_suitability_table_uri], 'rU'))
+        suitability_list = []
+        value_list = []
+        for row in csv_dict_reader:
+            suitability_list.append(row['Suitability'])
+            value_list.append(row[key])
+        biophysical_to_interp[biophysical_uri] = scipy.interpolate.interp1d(
+            value_list, suitability_list, kind='linear', copy=True,
+            bounds_error=False, fill_value=0.0)
+            
