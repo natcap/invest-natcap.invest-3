f0a8019e92f6299136c49428c0c7588bbd1fcaeb
richpsharp <richpsharp@gmail.com>
2014-05-16 11:51:49 -0700

    Update issue 2901
    Mixed up rows and column entries.
    
    
    Former-commit-id: 87404aec38987183c736138fccc495ac8cae5b1a

--- distance_transform_prototype.py
+++ distance_transform_prototype.py
@@ -27,26 +27,27 @@ def distance_transform_edt(input_mask_uri, output_distance_uri):
     numerical_inf = n_cols + n_rows
     
     g_array = numpy.empty(input_mask_array.shape, dtype=numpy.float)
-    
+    print g_array.shape
+    print n_rows
     for col_index in xrange(n_cols):
-        b_array = input_mask_array[col_index, :] == 0
+        b_array = input_mask_array[:, col_index] == 0
         if b_array[0]:
-            g_array[col_index, 0] = 0.0
+            g_array[0, col_index] = 0.0
         else:
-            g_array[col_index, 0] = numerical_inf
+            g_array[0, col_index] = numerical_inf
         
         for row_index in xrange(1, n_rows):
             if b_array[0]:
-                g_array[col_index, row_index] = 0.0
+                g_array[row_index, col_index] = 0.0
             else:
-                g_array[col_index, row_index] = (
-                    1.0 + g_array[col_index, row_index - 1])
+                g_array[row_index, col_index] = (
+                    1.0 + g_array[row_index - 1, col_index])
     
         for row_index in xrange(n_rows-2, -1, -1):
-            if (g_array[col_index, row_index + 1] < 
-                g_array[col_index, row_index]):
-                g_array[col_index, row_index] = (
-                    1.0 + g_array[col_index, row_index + 1])
+            if (g_array[row_index + 1, col_index] < 
+                g_array[row_index, col_index]):
+                g_array[row_index, col_index] = (
+                    1.0 + g_array[row_index + 1, col_index])
     
     #phase one, calculate column G(x,y)
     
@@ -55,3 +56,8 @@ def distance_transform_edt(input_mask_uri, output_distance_uri):
     
     pass
     
+    
+if __name__ == '__main__':    
+    input_mask_uri = 'C:/Users/rich/Documents/HabitatSuitability/output/oyster_habitat_suitability_mask.tif'
+    output_mask_uri = 'dt.tif'
+    distance_transform_edt(input_mask_uri, output_mask_uri)
