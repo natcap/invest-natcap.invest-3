ca6dd4e38ebd2d1478ce02c9966490f73b7f0bf9
richpsharp <richpsharp@gmail.com>
2014-05-16 11:43:02 -0700

    Update issue 2901
    Implemented phase 1 of distance transform: column pass.
    
    
    Former-commit-id: 9160590813fe431dd290d3cd58a41fa56e274f70

--- distance_transform_prototype.py
+++ distance_transform_prototype.py
@@ -1,6 +1,7 @@
 """Distance transform prototype"""
 
 import gdal
+import numpy
 
 def distance_transform_edt(input_mask_uri, output_distance_uri):
     """Calculate the Euclidean distance transform on input_mask_uri and output
@@ -15,6 +16,42 @@ def distance_transform_edt(input_mask_uri, output_distance_uri):
             0 pixel.
             
         returns nothing"""
+    
+    input_mask_ds = gdal.Open(input_mask_uri)
+    input_mask_band = input_mask_ds.GetRasterBand(1)
+    input_mask_array = input_mask_band.ReadAsArray()
+    
+    n_cols = input_mask_ds.RasterXSize
+    n_rows = input_mask_ds.RasterYSize
+    
+    numerical_inf = n_cols + n_rows
+    
+    g_array = numpy.empty(input_mask_array.shape, dtype=numpy.float)
+    
+    for col_index in xrange(n_cols):
+        b_array = input_mask_array[col_index, :] == 0
+        if b_array[0]:
+            g_array[col_index, 0] = 0.0
+        else:
+            g_array[col_index, 0] = numerical_inf
         
+        for row_index in xrange(1, n_rows):
+            if b_array[0]:
+                g_array[col_index, row_index] = 0.0
+            else:
+                g_array[col_index, row_index] = (
+                    1.0 + g_array[col_index, row_index - 1])
+    
+        for row_index in xrange(n_rows-2, -1, -1):
+            if (g_array[col_index, row_index + 1] < 
+                g_array[col_index, row_index]):
+                g_array[col_index, row_index] = (
+                    1.0 + g_array[col_index, row_index + 1])
+    
+    #phase one, calculate column G(x,y)
+    
+    
+    
+    
     pass
- 
+    
