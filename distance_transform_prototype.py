2595fbe75c064a3ea98656ac84344e5e8a096f2e
richpsharp <richpsharp@gmail.com>
2014-05-21 15:31:32 -0700

    Update issue 2901
    Got the complete transform in there.
    
    
    Former-commit-id: 382eb8d71428d159a57ee019a8a61378e7492053

--- distance_transform_prototype.py
+++ distance_transform_prototype.py
@@ -26,14 +26,15 @@ def distance_transform_edt(input_mask_uri, output_distance_uri):
     
     numerical_inf = n_cols + n_rows
     
-    g_array = numpy.empty(input_mask_array.shape, dtype=numpy.float)
+    g_array = numpy.empty(input_mask_array.shape, dtype=numpy.int)
     print g_array.shape
     print n_rows
+    print 'phase 1'
     #phase one, calculate column G(x,y)
     for col_index in xrange(n_cols):
         b_array = input_mask_array[:, col_index] == 0
         if b_array[0]:
-            g_array[0, col_index] = 0.0
+            g_array[0, col_index] = 0
         else:
             g_array[0, col_index] = numerical_inf
         
@@ -42,25 +43,46 @@ def distance_transform_edt(input_mask_uri, output_distance_uri):
                 g_array[row_index, col_index] = 0.0
             else:
                 g_array[row_index, col_index] = (
-                    1.0 + g_array[row_index - 1, col_index])
+                    1 + g_array[row_index - 1, col_index])
     
         for row_index in xrange(n_rows-2, -1, -1):
             if (g_array[row_index + 1, col_index] < 
                 g_array[row_index, col_index]):
                 g_array[row_index, col_index] = (
-                    1.0 + g_array[row_index + 1, col_index])
+                    1 + g_array[row_index + 1, col_index])
     
     #phase 2
+    print 'phase 2'
+    
+    dt = numpy.zeros(input_mask_array.shape)
     for row_index in xrange(n_rows):
+    
+        def f(x, i):
+            return (x - i)**2 + g_array[row_index, i]**2
+        
+        def sep(i, u):
+            return (u**2 - i**2 + g_array[row_index, u]**2 - g_array[row_index, i]**2) / (2 * (u - i))
+            
         q_index = 0
         s_array = numpy.zeros(n_cols)
         t_array = numpy.zeros(n_cols)
-        for u_index in xrange(n_rows)
-    
-    
-    
-    
-    pass
+        for u_index in xrange(1, n_cols):
+            while q_index >= 0 and f(t_array[q_index], s_array[q_index]) > f(t_array[q_index], u_index):
+                q_index -= 1
+            if q_index < 0:
+               q_index = 0
+               s_array[0] = u_index
+            else:
+                w = 1 + sep(s_array[q_index], u_index)
+                if w < n_cols:
+                    q_index += 1
+                    s_array[q_index] = u_index
+                    t_array[q_index] = w
+        
+        for u_index in xrange(n_cols - 1, 0, -1):
+            dt[row_index, u_index] = f(u_index, s_array[q_index])
+            if u_index == t_array[q_index]:
+                q_index -= 1
     
     
 if __name__ == '__main__':    
