b81c2863222da2c3f3b955bcf5edea45f73726fb
richpsharp <richpsharp@gmail.com>
2014-05-27 19:55:53 -0700

    Update issue 2901
    Profiling code for the distance transform.
    
    
    Former-commit-id: 243c62222e3e9e5b547211d5c9830df3833cbc08

--- distance_transform_prototype.py
+++ distance_transform_prototype.py
@@ -1,8 +1,13 @@
-"""Distance transform prototype"""
+import cProfile
+import pstats
 
 from invest_natcap import raster_utils
 
 if __name__ == '__main__':    
     input_mask_uri = 'v_stream.tif'
     output_mask_uri = 'dt.tif'
-    raster_utils.distance_transform_edt(input_mask_uri, output_mask_uri)
+    
+    cProfile.run('raster_utils.distance_transform_edt(input_mask_uri, output_mask_uri)', 'stats')
+    p = pstats.Stats('stats')
+    p.sort_stats('time').print_stats(20)
+    p.sort_stats('cumulative').print_stats(20)
