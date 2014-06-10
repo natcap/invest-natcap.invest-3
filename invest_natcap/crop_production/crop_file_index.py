36c2e5257e2175e8d82bf509f3f46a2c1227c652
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2014-06-10 11:50:15 -0700

    Update issue 2966
    Removed mod yield files from index.
    
    
    Former-commit-id: 60d516a44b1943d3071184dd2d071c95a7aad4be

--- invest_natcap/crop_production/crop_file_index.py
+++ invest_natcap/crop_production/crop_file_index.py
@@ -10,7 +10,6 @@ fertilizer_dir_uri = os.path.join(data_dir_uri, "Fertilizer")
 climate_dir_uri = os.path.join(data_dir_uri, "ClimateBinAnalysis")
 production_dir_uri = os.path.join(data_dir_uri, "Monfreda")
 cbi_dir_uri = os.path.join(data_dir_uri, "CBI")
-cbi_mod_yield_dir_uri = os.path.join(data_dir_uri, "CBI_mod_yield")
 cbi_yield_dir_uri = os.path.join(data_dir_uri, "CBI_yield")
 income_climate_dir_uri = os.path.join(data_dir_uri, "income_climate")
 yield_mod_dir_uri = os.path.join(data_dir_uri, "yield_mod")
@@ -19,7 +18,6 @@ fertilizer_pattern = "([a-z]+)([0-9A-Z]+)([a-z]+)"
 climate_pattern = "([A-Za-z]+[_])([a-z]+)([_])([A-Za-z]+[_][0-9])([_][A-Za-z]+[_][0-9]+[_][A-Za-z]+[_][0-9]+[x][0-9]+[_][a-z]+[_])(BinMatrix)"
 production_pattern = "([a-z]+)([_])([a-z]+)"
 cbi_pattern = "([A-Z]+)([_][a-z]+[_])([a-z]+)"
-cbi_mod_yield_pattern = "([A-Z]+[_][a-z]+[_])([a-z]+)"
 cbi_yield_pattern = "([A-Z]+[_][a-z]+)([_])([a-z]+)"
 income_climate_pattern = "([A-Z]+[_][a-z]+[_])([a-z]+)"
 yield_mod_pattern = "([a-z]+)"
@@ -86,22 +84,6 @@ for base_name in os.listdir(cbi_dir_uri):
         if m != None:
             crop = m.group(3)
             column = m.group(1)
-            column_header.add(column)
-
-            if not (crop in rasters):
-                rasters[crop] = {}
-                
-            rasters[crop][column] = raster_uri[len(data_dir_uri)+1:]
-        else:
-            unknown_files.append(raster_uri)
-
-for base_name in os.listdir(cbi_mod_yield_dir_uri):
-    raster_uri = os.path.join(cbi_mod_yield_dir_uri, base_name)
-    if os.path.isfile(raster_uri):
-        m = re.search(cbi_mod_yield_pattern, base_name)
-        if m != None:
-            crop = m.group(2)
-            column = "CBI_mod_yield"
             column_header.add(column)
 
             if not (crop in rasters):
