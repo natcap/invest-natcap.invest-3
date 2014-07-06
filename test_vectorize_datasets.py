1487bfdec71eac5177ce2131af80c70ee586e432
richpsharp <richpsharp@gmail.com>
2014-07-05 22:48:22 -0700

    Update issue 3023
    Calculates distance from forest vs. biomass table.
    
    
    Former-commit-id: deb265dfaed99d92193007bf4d19da688bbf6cd2

--- test_vectorize_datasets.py
+++ test_vectorize_datasets.py
@@ -10,21 +10,26 @@ from invest_natcap import raster_utils
 
 if __name__ == '__main__':
     
-    lulc_uri = "C:\\Users\\rich\\Desktop\\am.tif"
-    #lulc_uri = "C:\\Users\\rich\\Documents\\Base_Data\\Freshwater\\landuse_90"
-    '''biomass_uri = "C:\\Users\\rich\\Desktop\\am_biov2ct1.tif"'''
-    '''biomass_uri = "C:\\Users\\rich\\Desktop\\am_biov2ct1.tif"'''
+    lulc_raw_uri = "C:\\Users\\rich\\Desktop\\am.tif"
+    biomass_raw_uri = "C:\\Users\\rich\\Desktop\\am_biov2ct1.tif"
     
-    lulc_nodata = raster_utils.get_nodata_from_uri(
-        lulc_uri)
-    '''biomass_nodata = raster_utils.get_nodata_from_uri(
-        biomass_uri)'''
+    cell_size = raster_utils.get_cell_size_from_uri(lulc_raw_uri)
+    lulc_uri = "C:\\Users\\rich\\Desktop\\lulc_aligned.tif"
+    biomass_uri = "C:\\Users\\rich\\Desktop\\biomass_aligned.tif"
+    
+    raster_utils.align_dataset_list(
+        [lulc_raw_uri, biomass_raw_uri], [lulc_uri, biomass_uri], ['nearest']*2,
+        cell_size, 'intersection', 0, dataset_to_bound_index=None,
+        aoi_uri=None, assert_datasets_projected=True, process_pool=None)
+        
+    lulc_nodata = raster_utils.get_nodata_from_uri(lulc_uri)
+    biomass_nodata = raster_utils.get_nodata_from_uri(biomass_uri)
+    
     forest_lulc_codes = [1, 2, 3, 4, 5]
-    #forest_lulc_codes = [22, 23, 24, 25, 26]
     
     mask_uri = "C:\\Users\\rich\\Desktop\\mask.tif"
     mask_nodata = 2
-    cell_size = raster_utils.get_cell_size_from_uri(lulc_uri)
+    
     
     def mask_forest(lulc):
         mask = numpy.empty(lulc.shape, dtype=numpy.int8)
@@ -47,3 +52,26 @@ if __name__ == '__main__':
     p = pstats.Stats('stats')
     p.sort_stats('time').print_stats(20)
     p.sort_stats('cumulative').print_stats(20)
+    
+    forest_edge_nodata = raster_utils.get_nodata_from_uri(forest_edge_distance_uri)
+    biomass_stats_uri = "C:\\Users\\rich\\Desktop\\am.csv"
+    outfile = open(biomass_stats_uri, 'w')
+    
+    biomass_ds = gdal.Open(biomass_uri)
+    biomass_band = biomass_ds.GetRasterBand(1)
+    
+    forest_edge_distance_ds = gdal.Open(forest_edge_distance_uri)
+    forest_edge_distance_band = forest_edge_distance_ds.GetRasterBand(1)
+    
+    n_rows, n_cols = raster_utils.get_row_col_from_uri(
+        biomass_uri)
+    for row_index in xrange(n_rows):
+        print row_index, n_rows
+        biomass_row = biomass_band.ReadAsArray(0, row_index, n_cols, 1)
+        forest_edge_distance_row = forest_edge_distance_band.ReadAsArray(
+            0, row_index, n_cols, 1)
+        for col_index in xrange(n_cols):
+            if forest_edge_distance_row[0, col_index] != forest_edge_nodata:
+                outfile.write("%f,%f\n" % (forest_edge_distance_row[0, col_index], biomass_row[0, col_index]))
+            
+            
