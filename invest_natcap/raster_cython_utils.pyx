45be771a4a3b43e865ac5efc028aad78a8794072
richpsharp <richpsharp@gmail.com>
2014-08-13 00:22:46 -0700

    Update issue 3051
    By adding an insert for tilage I dinked up the test for [] and modified an input parameter.  Now putting everything under a test and making a copy first.
    
    
    Former-commit-id: 7f268135db52100fb4ff7aa8211b571ea5fb0f94

--- invest_natcap/raster_cython_utils.pyx
+++ invest_natcap/raster_cython_utils.pyx
@@ -418,18 +418,20 @@ def new_raster_from_base(
     base_band = base.GetRasterBand(1)
     block_size = base_band.GetBlockSize()
     
-    #first, should it be tiled?  yes if it's not striped
-    if block_size[0] != n_cols and block_size[1] != n_rows:
-        dataset_options.insert(0, 'TILED=YES')
-        #if it's tiled can't have tiled block sizes less than 16
-        for block_index in [0, 1]:
-            if block_size[block_index] < 16:
-                block_size[block_index] = 16
+    dataset_options = dataset_options.copy()
+
     if dataset_options == []:
-        dataset_options = [
-            'BLOCKXSIZE=%d' % block_size[0],
-            'BLOCKYSIZE=%d' % block_size[1],
-            'BIGTIFF=IF_SAFER']
+        #first, should it be tiled?  yes if it's not striped
+        if block_size[0] != n_cols and block_size[1] != n_rows:
+            dataset_options.insert(0, 'TILED=YES')
+            #if it's tiled can't have tiled block sizes less than 16
+            for block_index in [0, 1]:
+                if block_size[block_index] < 16:
+                    block_size[block_index] = 16
+            dataset_options = [
+                'BLOCKXSIZE=%d' % block_size[0],
+                'BLOCKYSIZE=%d' % block_size[1],
+                'BIGTIFF=IF_SAFER']
     LOGGER.info('dataset_options=%s' % str(dataset_options))
     driver.Create(
         output_uri.encode('utf-8'), n_cols, n_rows, 1, datatype,
