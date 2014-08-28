23e52c2b96e9626e003be1a6e867d7322564e5e4
richpsharp <richpsharp@gmail.com>
2014-08-28 15:12:08 -0700

    Update issue 3086
    Adding percent to sink back to the cython core since it's used in the routing_utils module.  It was a mistake to delete it.
    
    
    Former-commit-id: 06d88759ee51cf3e18c47a61369c20149be5794a

--- invest_natcap/routing/routing_cython_core.pyx
+++ invest_natcap/routing/routing_cython_core.pyx
@@ -2355,3 +2355,235 @@ def cache_block_experiment(ds_uri, out_uri):
 
     #save off the dirty cache
     block_cache.flush_cache()
+
+
+def percent_to_sink(
+    sink_pixels_uri, export_rate_uri, outflow_direction_uri,
+    outflow_weights_uri, effect_uri):
+    """This function calculates the amount of load from a single pixel
+        to the source pixels given the percent export rate per pixel.
+        
+        sink_pixels_uri - the pixels of interest that will receive flux.
+            This may be a set of stream pixels, or a single pixel at a
+            watershed outlet.
+
+        export_rate_uri - a GDAL floating point dataset that has a percent
+            of flux exported per pixel
+
+        outflow_direction_uri - a uri to a byte dataset that indicates the
+            first counter clockwise outflow neighbor as an index from the
+            following diagram
+
+            3 2 1
+            4 x 0
+            5 6 7
+
+        outflow_weights_uri - a uri to a float32 dataset whose elements
+            correspond to the percent outflow from the current cell to its
+            first counter-clockwise neighbor
+
+        effect_uri - the output GDAL dataset that shows the percent of flux
+            emanating per pixel that will reach any sink pixel
+
+        returns nothing"""
+
+    LOGGER.info("calculating percent to sink")
+    start_time = time.clock()
+
+    cdef double effect_nodata = -1.0
+    raster_utils.new_raster_from_base_uri(
+        sink_pixels_uri, effect_uri, 'GTiff', effect_nodata,
+        gdal.GDT_Float32, fill_value=effect_nodata)
+    effect_dataset = gdal.Open(effect_uri, gdal.GA_Update)
+    effect_band = effect_dataset.GetRasterBand(1)
+
+    cdef int n_cols = effect_dataset.RasterXSize
+    cdef int n_rows = effect_dataset.RasterYSize
+    
+    cdef int CACHE_ROWS = 2**12
+    if CACHE_ROWS > n_rows:
+        CACHE_ROWS = n_rows
+
+    cdef numpy.ndarray[numpy.npy_float32, ndim=2] effect_cache = (
+        numpy.empty((CACHE_ROWS, n_cols), dtype=numpy.float32))
+
+    cdef numpy.ndarray[numpy.npy_byte, ndim=2] sink_pixels_cache = (
+        numpy.empty((CACHE_ROWS, n_cols), dtype=numpy.int8))
+    sink_pixels_dataset = gdal.Open(sink_pixels_uri)
+    sink_pixels_band = sink_pixels_dataset.GetRasterBand(1)
+    cdef int sink_pixels_nodata = raster_utils.get_nodata_from_uri(
+        sink_pixels_uri)
+    
+    cdef numpy.ndarray[numpy.npy_float32, ndim=2] export_rate_cache = (
+        numpy.empty((CACHE_ROWS, n_cols), dtype=numpy.float32))
+    export_rate_dataset = gdal.Open(export_rate_uri)
+    export_rate_band = export_rate_dataset.GetRasterBand(1)
+    cdef double export_rate_nodata = raster_utils.get_nodata_from_uri(
+        export_rate_uri)
+    
+    cdef numpy.ndarray[numpy.npy_byte, ndim=2] outflow_direction_cache = (
+        numpy.empty((CACHE_ROWS, n_cols), dtype=numpy.int8))
+    outflow_direction_dataset = gdal.Open(outflow_direction_uri)
+    outflow_direction_band = outflow_direction_dataset.GetRasterBand(1)
+    cdef int outflow_direction_nodata = raster_utils.get_nodata_from_uri(
+        outflow_direction_uri)
+    
+    cdef numpy.ndarray[numpy.npy_float32, ndim=2] outflow_weights_cache = (
+        numpy.empty((CACHE_ROWS, n_cols), dtype=numpy.float32))
+    outflow_weights_dataset = gdal.Open(outflow_weights_uri)
+    outflow_weights_band = outflow_weights_dataset.GetRasterBand(1)
+    cdef double outflow_weights_nodata = raster_utils.get_nodata_from_uri(
+        outflow_weights_uri)
+    
+    #Diagonal offsets are based off the following index notation for neighbors
+    #    3 2 1
+    #    4 p 0
+    #    5 6 7
+
+    cdef int *row_offsets = [0, -1, -1, -1,  0,  1, 1, 1]
+    cdef int *col_offsets = [1,  1,  0, -1, -1, -1, 0, 1]
+    cdef int *inflow_offsets = [4, 5, 6, 7, 0, 1, 2, 3]
+
+    cdef int index, row_index, col_index, cache_row_index, neighbor_row_index, cache_neighbor_row_index, neighbor_col_index, neighbor_index, neighbor_outflow_direction, cache_row_offset, old_row_index
+    cdef double outflow_weight, neighbor_outflow_weight
+    
+    cdef numpy.ndarray[numpy.npy_int32, ndim=1] cache_tag = (
+        numpy.empty((CACHE_ROWS,), dtype=numpy.int32))
+    #initially nothing is loaded in the cache, use -1 to indicate that as a tag
+    cache_tag[:] = -1
+    cdef numpy.ndarray[numpy.npy_byte, ndim=1] cache_dirty = (
+        numpy.zeros((CACHE_ROWS,), dtype=numpy.int8))
+    cache_dirty[:] = 0
+    
+    
+    cdef queue[int] process_queue
+    #Queue the sinks
+    for row_index in xrange(n_rows):
+        effect_band.ReadAsArray(
+            xoff=0, yoff=row_index, win_xsize=n_cols,
+            win_ysize=1, buf_obj=effect_cache[0].reshape((1,n_cols)))
+        sink_pixels_band.ReadAsArray(
+            xoff=0, yoff=row_index, win_xsize=n_cols,
+            win_ysize=1, buf_obj=sink_pixels_cache[0].reshape((1,n_cols)))
+        for col_index in xrange(n_cols):
+            if sink_pixels_cache[0, col_index] == 1:
+                effect_cache[0, col_index] = 1.0
+                process_queue.push(row_index * n_cols + col_index)
+        effect_band.WriteArray(
+            effect_cache[0].reshape((1,n_cols)), xoff=0, yoff=row_index)
+
+    while process_queue.size() > 0:
+        index = process_queue.front()
+        process_queue.pop()
+        row_index = index / n_cols
+        col_index = index % n_cols
+
+        for cache_row_offset in range(-1, 2):
+            neighbor_row_index = row_index + cache_row_offset
+            #see if that row is out of bounds
+            if neighbor_row_index < 0 or neighbor_row_index >= n_rows:
+                continue
+            #otherwise check if the cache needs an update
+            cache_row_index = neighbor_row_index % CACHE_ROWS
+            cache_row_tag = neighbor_row_index / CACHE_ROWS
+            
+            if cache_tag[cache_row_index] == cache_row_tag:
+                #cache is up to date, so skip
+                continue
+                
+            #see if we need to save the cache
+            if cache_dirty[cache_row_index]:
+                old_row_index = cache_tag[cache_row_index] * CACHE_ROWS + cache_row_index
+                effect_band.WriteArray(
+                    effect_cache[cache_row_index].reshape((1,n_cols)), xoff=0, yoff=old_row_index)
+                cache_dirty[cache_row_index] = 0
+                
+            #load a new row
+            sink_pixels_band.ReadAsArray(
+                xoff=0, yoff=neighbor_row_index, win_xsize=n_cols,
+                win_ysize=1, buf_obj=sink_pixels_cache[cache_row_index].reshape((1,n_cols)))
+            export_rate_band.ReadAsArray(
+                xoff=0, yoff=neighbor_row_index, win_xsize=n_cols,
+                win_ysize=1, buf_obj=export_rate_cache[cache_row_index].reshape((1,n_cols)))
+            outflow_direction_band.ReadAsArray(
+                xoff=0, yoff=neighbor_row_index, win_xsize=n_cols,
+                win_ysize=1, buf_obj=outflow_direction_cache[cache_row_index].reshape((1,n_cols)))
+            outflow_weights_band.ReadAsArray(
+                xoff=0, yoff=neighbor_row_index, win_xsize=n_cols,
+                win_ysize=1, buf_obj=outflow_weights_cache[cache_row_index].reshape((1,n_cols)))
+            effect_band.ReadAsArray(
+                xoff=0, yoff=neighbor_row_index, win_xsize=n_cols,
+                win_ysize=1, buf_obj=effect_cache[cache_row_index].reshape((1,n_cols)))
+            cache_tag[cache_row_index] = cache_row_tag
+                
+        cache_row_index = row_index % CACHE_ROWS
+        
+        if export_rate_cache[cache_row_index, col_index] == export_rate_nodata:
+            continue
+
+        #if the outflow weight is nodata, then not a valid pixel
+        outflow_weight = outflow_weights_cache[cache_row_index, col_index]
+        if outflow_weight == outflow_weights_nodata:
+            continue
+
+        for neighbor_index in range(8):
+            cache_neighbor_row_index = (cache_row_index + row_offsets[neighbor_index]) % CACHE_ROWS
+            neighbor_row_index = row_index + row_offsets[neighbor_index]
+            if neighbor_row_index < 0 or neighbor_row_index >= n_rows:
+                #out of bounds
+                continue
+
+            neighbor_col_index = col_index + col_offsets[neighbor_index]
+            if neighbor_col_index < 0 or neighbor_col_index >= n_cols:
+                #out of bounds
+                continue
+
+            if sink_pixels_cache[cache_neighbor_row_index, neighbor_col_index] == 1:
+                #it's already a sink
+                continue
+
+            neighbor_outflow_direction = (
+                outflow_direction_cache[cache_neighbor_row_index, neighbor_col_index])
+            #if the neighbor is no data, don't try to set that
+            if neighbor_outflow_direction == outflow_direction_nodata:
+                continue
+
+            neighbor_outflow_weight = (
+                outflow_weights_cache[cache_neighbor_row_index, neighbor_col_index])
+            #if the neighbor is no data, don't try to set that
+            if neighbor_outflow_weight == outflow_direction_nodata:
+                continue
+
+            it_flows_here = False
+            if neighbor_outflow_direction == inflow_offsets[neighbor_index]:
+                #the neighbor flows into this cell
+                it_flows_here = True
+
+            if (neighbor_outflow_direction - 1) % 8 == inflow_offsets[neighbor_index]:
+                #the offset neighbor flows into this cell
+                it_flows_here = True
+                neighbor_outflow_weight = 1.0 - neighbor_outflow_weight
+
+            if it_flows_here:
+                #If we haven't processed that effect yet, set it to 0 and append to the queue
+                if effect_cache[cache_neighbor_row_index, neighbor_col_index] == effect_nodata:
+                    process_queue.push(neighbor_row_index * n_cols + neighbor_col_index)
+                    effect_cache[cache_neighbor_row_index, neighbor_col_index] = 0.0
+
+                #the percent of the pixel upstream equals the current percent 
+                #times the percent flow to that pixels times the 
+                effect_cache[cache_neighbor_row_index, neighbor_col_index] += (
+                    effect_cache[cache_row_index, col_index] *
+                    neighbor_outflow_weight *
+                    export_rate_cache[cache_row_index, col_index])
+                cache_dirty[cache_neighbor_row_index] = 1
+
+    for cache_row_index in range(CACHE_ROWS):
+        if cache_dirty[cache_row_index]:
+            old_row_index = cache_tag[cache_row_index] * CACHE_ROWS + cache_row_index
+            effect_band.WriteArray(
+                effect_cache[cache_row_index].reshape((1,n_cols)), xoff=0, yoff=old_row_index)
+            cache_dirty[cache_row_index] = 0
+                
+    LOGGER.info('Done calculating percent to sink elapsed time %ss' % \
+                    (time.clock() - start_time))
