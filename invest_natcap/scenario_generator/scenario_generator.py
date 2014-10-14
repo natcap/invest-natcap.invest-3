9e3d738ba7efd0d5d7b4ddc52b1277bb52144533
Nicolas Chaumont <nicolas.chaumont@gmail.com>
2014-10-14 14:39:11 -0700

    Update issue 2817 Added a check that prevents the user to specify the same CSV for both transition and suitability tables.
    Former-commit-id: bce4cbb33b792ce235a580e5b0eefe86ce199ab0

--- invest_natcap/scenario_generator/scenario_generator.py
+++ invest_natcap/scenario_generator/scenario_generator.py
@@ -470,6 +470,11 @@ def execute(args):
     #overiding, non-standard field names
     ###
 
+    # Preliminary tests
+    assert args['transition'] != args['suitability'], \
+        'Transition and suitability tables are the same: ' + \
+        args['transition'] + '. The model expects different tables.'
+
     #transition table fields
     args["transition_id"] = "Id"
     args["percent_field"] = "Percent Change"
