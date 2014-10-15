4bcfefa1bce403921b0857171f7d699fa25df5a0
Rich Sharp <richpsharp@gmail.com>
2014-10-15 16:33:47 -0700

    Update issue 2763
    making convolution memory efficient by reading by blocks.
    
    
    Former-commit-id: 4009ebe372a24535f90c9ab60809326aa0684aea

--- test/test_fft_convolution.py
+++ test/test_fft_convolution.py
@@ -8,62 +8,35 @@ import scipy.ndimage.morphology
 
 from invest_natcap import raster_utils
 
+def make_distance_kernel(max_distance):
+    kernel_size = max_distance * 2 + 1
+    distance_kernel = numpy.empty((kernel_size, kernel_size), dtype=numpy.float)
+    for row_index in xrange(kernel_size):
+        for col_index in xrange(kernel_size):
+            distance_kernel[row_index, col_index] = numpy.sqrt(
+                (row_index - max_distance) ** 2 + (col_index - max_distance) ** 2)
+    return distance_kernel
 
-def distance(row_index, col_index, kernel_size, kernel_type, max_distance):
-    """closure for an euclidan distance calc"""
-    dist = math.sqrt(
-        (row_index - kernel_size - 1) ** 2 +
-        (col_index - kernel_size - 1) ** 2)
-    if dist > max_distance:
-        return 0.0
-    if kernel_type == 0: #'linear'
-        return 1 - dist/max_distance
-    elif kernel_type == 1: #'exponential'
-        return  math.exp(-(2.99/max_distance) * dist)
+def make_linear_kernel(max_distance):
+    distance_kernel = make_distance_kernel(max_distance)
+    kernel = numpy.where(
+        distance_kernel > max_distance, 0.0, 1 - distance_kernel / max_distance)
+    return kernel / numpy.sum(kernel)
 
-def fft_convolution(weight_uri, output_uri, kernel_type, max_distance):
-	#weight_uri = "C:/InVEST_dev107_3_0_1 [61c19dc4b887]_x86/Sedimentation/3/intermediate/stream.tif"
-	for uri in [output_uri]:
-		raster_utils.new_raster_from_base_uri(
-		    weight_uri, uri, 'GTiff', -1, gdal.GDT_Float32)
 
-	weight_nodata = raster_utils.get_nodata_from_uri(weight_uri)
-	weight_ds = gdal.Open(weight_uri)
-	weight_band = weight_ds.GetRasterBand(1)
-	weight_array = weight_band.ReadAsArray()
-	weight_array[weight_array == weight_nodata] = 0.0
-	
-	kernel_size = max_distance * 2 + 1
-	kernel = numpy.empty((kernel_size, kernel_size), dtype=numpy.float)
-	if kernel_type == 'linear':
-	    kernel_type_id = 0
-	if kernel_type == 'exponential':
-	    kernel_type_id = 1
-	print 'preping kernel'
-	for row_index in xrange(kernel_size):
-	    for col_index in xrange(kernel_size):
-	        kernel[row_index, col_index] = distance(
-	            row_index, col_index, kernel_size, kernel_type_id, max_distance)
-
-	kernel /= numpy.sum(kernel)
-
-	print 'scipy fft convolve'
-	result_array = scipy.signal.fftconvolve(weight_array, kernel, 'valid')
-
-	result_ds = gdal.Open(output_uri, gdal.GA_Update)
-	result_band = result_ds.GetRasterBand(1)
-	result_band.WriteArray(result_array)
+def make_exponential_kernel(max_distance):
+    kernel = make_distance_kernel(max_distance)
+    kernel = numpy.where(
+        distance_kernel > max_distance, 0.0, numpy.exp(-2.99 / max_distance * dist))
+    return kernel / numpy.sum(kernel)
 
 
 if __name__ == '__main__':
-	#weight_uri = "C:/Users/rpsharp/Box Sync/Unilever/Input_MatoGrosso_global_Unilever_10_09_2014/Input_MatoGrosso_global_Unilever_10_09_2014/SRTM_90m_MatoGrosso_final_basins.tif"
-	weight_uri = "C:/InVEST_dev107_3_0_1 [61c19dc4b887]_x86/Base_Data/Freshwater/dem"
-	output_uri = "C:/Users/rpsharp/Documents/convolution/result.tif"
-	kernel_type = 'linear'
-	max_distance = 5
-	try:
-		print 'trying fft convolution'
-		fft_convolution(weight_uri, output_uri, kernel_type, max_distance)
-	except MemoryError:
-		print 'backup convolve 2d'
-		raster_utils.convolve_2d(weight_uri, kernel_type, max_distance, output_uri)
+    weight_uri = "C:/Users/rpsharp/Box Sync/Unilever/Input_MatoGrosso_global_Unilever_10_09_2014/Input_MatoGrosso_global_Unilever_10_09_2014/SRTM_90m_MatoGrosso_final_basins.tif"
+    #weight_uri = "C:/InVEST_dev107_3_0_1 [61c19dc4b887]_x86/Base_Data/Freshwater/dem"
+    output_uri = "C:/Users/rpsharp/Documents/convolution/result.tif"
+    max_distance = 100
+    print 'make kernel'
+    kernel = make_linear_kernel(max_distance)
+    print 'convolve 2d'
+    raster_utils.convolve_2d(weight_uri, kernel, output_uri)
