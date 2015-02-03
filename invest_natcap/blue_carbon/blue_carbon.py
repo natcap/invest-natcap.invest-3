fff58755f1d4c95064a9fb351433b461b0207e35
Will Bierbower <wbierbower@gmail.com>
2015-02-02 16:18:47 -0800

    updating execute doc-string in Blue Carbon model
    Former-commit-id: 371deea54fcf24d176ea18d303a1bd08fa5b69ed

--- invest_natcap/blue_carbon/blue_carbon.py
+++ invest_natcap/blue_carbon/blue_carbon.py
@@ -238,34 +238,74 @@ def execute(args):
     """
     Entry point for the blue carbon model.
 
-    :param args["workspace_dir"]: The directory to hold output from a
-        particular model run
-    :type args["workspace_dir"]: str
-    :param args["lulc_uri_1"]: The land use land cover raster for time 1.
-    :type args["lulc_uri_1"]: str
-    :param args["year_1"]: The year for the land use land cover raster
-        for time 1.
-    :type args["year_1"]: int
-    :param args["lulc_uri_2"]: The land use land cover raster for time 2.
-    :type args["lulc_uri_2"]: str
-    :param args["year_2"]: The year for the land use land cover raster for time
-        2.
-    :type args["year_2"]: int
-    :param args["lulc_uri_3"]: The land use land cover raster for time 3.
-    :type args["lulc_uri_3"]: str
-    :param args["year_3"]: The year for the land use land cover raster for
-        time 3.
-    :type args["year_3"]: int
-    :param args["lulc_uri_4"]: The land use land cover raster for time 4.
-    :type args["lulc_uri_4"]: str
-    :param args["year_4"]: The year for the land use land cover raster for
-        time 4.
-    :type args["year_4"]: int
-    :param args["lulc_uri_5"]: The land use land cover raster for time 5.
-    :type args["lulc_uri_5"]: str
-    :param args["year_5"]: The year for the land use land cover raster for
-        time 5.
-    :type args["year_5"]: int
+    Args:
+        workspace_dir (string): the directory to hold output from a particular
+            model run
+        lulc_uri_1 (string): the land use land cover raster for time 1.
+        year_1 (int): the year for the land use land cover raster for time 1.
+        lulc_uri_2 (string): the land use land cover raster for time 2.
+        year_2 (int): the year for the land use land cover raster for time 2.
+        lulc_uri_3 (string): the year for the land use land cover raster for
+            time 3.
+        year_3 (int): the year for the land use land cover raster for time 3.
+        lulc_uri_4 (string): the year for the land use land cover raster for
+            time 4.
+        year_4 (int): the year for the land use land cover raster for time 4.
+        lulc_uri_5 (string): the year for the land use land cover raster for
+            time 5.
+        year_5 (int): the year for the land use land cover raster for time 5.
+        analysis_year (int): analysis end year
+        soil_disturbance_csv_uri (string): soil disturbance csv file
+        biomass_disturbance_csv_uri (string): biomass disturbance csv file
+        carbon_pools_uri (string): Carbon in Metric Tons per Hectacre
+            (t ha-1) stored in each of the four fundamental pools for
+            each land-use/land-cover class.
+        half_life_csv_uri (string): carbon half-lives csv file
+        transition_matrix_uri (string): Coefficients for the carbon storage
+            rates for the transtion between each of the land-use/land-cover
+            classes. Values above 1 indicate an increase, values below 1
+            indicate a decrease. Absent transitions are assigned a value
+            of 1, representing no change.
+        snapshots (boolean): enable snapshots
+        start (int): start year
+        step (int): years between snapshots
+        stop (int): stop year
+        private_valuation (boolean): enable private valuation
+        carbon_units (string): the units for the price per unit of carbon
+            type.
+        discount_rate (int): the discount rate as an integer percent.
+        price_table (boolean): enable price table
+        carbon_schedule (string): the social cost of carbon table.
+        carbon_value (float): the price per unit ton of carbon or C02 as
+            defined in the carbon price units.
+        rate_change (float): the integer percent increase of the price of
+            carbon per year.
+
+    Example Args::
+
+        args = {
+            'workspace_dir': '/path/to/workspace_dir/',
+            'lulc_uri_1': '/path/to/lulc_uri_1',
+            'year_1': 2004,
+            'lulc_uri_2': '/path/to/lulc_uri_2',
+            'year_2': 2050,
+            'lulc_uri_3': '/path/to/lulc_uri_3',
+            'year_3': 2100,
+            'lulc_uri_4': '/path/to/lulc_uri_4',
+            'analysis_year': 2150,
+            'soil_disturbance_csv_uri': '/path/to/csv',
+            'biomass_disturbance_csv_uri': '/path/to/csv',
+            'carbon_pools_uri': '/path/to/csv',
+            'half_life_csv_uri': '/path/to/csv',
+            'transition_matrix_uri': '/path/to/csv',
+            'private_valuation': True,
+            'carbon_units': 'Carbon Dioxide (CO2)',
+            'discount_rate': 5,
+            'price_table': True,
+            'carbon_schedule': '/path/to/csv',
+            'carbon_value': 43.00,
+            'rate_change': 0,
+        }
 
     """
     ##preprocess args for possible ease of adoption of future IUI features
