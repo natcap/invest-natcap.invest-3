abc348e67655054a63d615d2e5e747a074e72712
Richard Sharp <richpsharp@gmail.com>
2015-02-03 14:44:06 -0800

    Update issue 3248
    Tracking a test function for running routing quickly.
    
    
    Former-commit-id: 111802817f942287e58f2d7f08647f8d050a2493

---
+++ test_routing.py
@@ -0,0 +1,59 @@
+import cProfile
+import pstats
+import sys
+import os
+import logging
+import subprocess
+
+import gdal
+
+from invest_natcap.routing import routing_utils
+import routing_cython_core
+from invest_natcap import raster_utils
+
+logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
+%(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('routedem')
+
+def main():
+    #dem_uri = r"C:\Users\rpsharp\Dropbox_stanford\Dropbox\brian_packard_dem_nutrient_bug\dem"
+    dem_uri = r"C:\Users\rpsharp\Documents\invest-natcap.invest-3\test\invest-data\Base_Data\Freshwater\dem"
+
+    args = {
+        'flow_direction_filename': "flow_direction.tif",
+        'flow_accumulation_filename': "flow_accumulation.tif",
+        'resolve_plateaus_filename': 'resolved_plateaus_dem.tif',
+        'sink_filename': 'sinks.tif',
+    }
+
+    output_directory = "C:/Users/rpsharp/Documents/routing_test/"
+    raster_utils.create_directories([output_directory])
+    file_suffix = ''
+
+    LOGGER.info("resolve_plateaus")
+    prefix, suffix = os.path.splitext(args['resolve_plateaus_filename'])
+    dem_offset_uri = os.path.join(output_directory, prefix + file_suffix + suffix)
+    routing_cython_core.resolve_flat_regions_for_drainage(dem_uri, dem_offset_uri)
+    dem_uri = dem_offset_uri
+
+    LOGGER.info("calculating flow direction")
+    prefix, suffix = os.path.splitext(args['flow_direction_filename'])
+    flow_direction_uri = os.path.join(
+        output_directory, prefix + file_suffix + suffix)
+    routing_cython_core.flow_direction_inf(dem_uri, flow_direction_uri)
+
+    LOGGER.info("calculating flow accumulation")
+    prefix, suffix = os.path.splitext(args['flow_accumulation_filename'])
+    flow_accumulation_uri = os.path.join(
+        output_directory, prefix + file_suffix + suffix)
+    routing_utils.flow_accumulation(flow_direction_uri, dem_uri, flow_accumulation_uri)
+
+    qgis_bin = r"C:\Program Files\QGIS Brighton\bin\qgis.bat"
+    subprocess.Popen([qgis_bin, flow_accumulation_uri], cwd=os.path.dirname(qgis_bin))
+
+if __name__ == '__main__':
+    cProfile.run('main()', 'stats')
+    p = pstats.Stats('stats')
+    p.sort_stats('time').print_stats(20)
+    p.sort_stats('cumulative').print_stats(20)
