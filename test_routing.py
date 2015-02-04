35e795974bf9a21231a84280df16f278fe57c17e
Richard Sharp <richpsharp@gmail.com>
2015-02-03 16:50:31 -0800

    Update issue 3248
    pylinting test_routing.py.
    
    
    Former-commit-id: 0590043d37d3bf147fc8b24643914ebcae89279a

--- test_routing.py
+++ test_routing.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""This is a script I use to develop the efficient drainage functionality in
+    invest_natcap.routing.  It should be deleted if not in its own feature
+    branch"""
+
 import cProfile
 import pstats
 import os
@@ -5,7 +12,7 @@ import logging
 import subprocess
 
 from invest_natcap.routing import routing_utils
-import routing_cython_core
+import routing_cython_core #pylint: disable=F0401
 from invest_natcap import raster_utils
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
@@ -13,45 +20,57 @@ logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 
 LOGGER = logging.getLogger('routedem')
 
+
 def main():
-    #dem_uri = r"C:\Users\rpsharp\Dropbox_stanford\Dropbox\brian_packard_dem_nutrient_bug\dem"
-    dem_uri = r"C:\Users\rpsharp\Documents\invest-natcap.invest-3\test\invest-data\Base_Data\Freshwater\dem"
+    """main entry"""
+    #dem_uri = r"C:\Users\rpsharp\Dropbox_stanford\Dropbox" \
+    #    r"\brian_packard_dem_nutrient_bug\dem"
+
+    dem_uri = \
+        'C:\\Users\\rpsharp\\Documents\\invest-natcap.invest-3\\test\\' \
+        + 'invest-data\\Base_Data\\Freshwater\\dem'
 
     args = {
-        'flow_direction_filename': "flow_direction.tif",
-        'flow_accumulation_filename': "flow_accumulation.tif",
+        'flow_direction_filename': 'flow_direction.tif',
+        'flow_accumulation_filename': 'flow_accumulation.tif',
         'resolve_plateaus_filename': 'resolved_plateaus_dem.tif',
         'sink_filename': 'sinks.tif',
     }
 
-    output_directory = "C:/Users/rpsharp/Documents/routing_test/"
+    output_directory = 'C:/Users/rpsharp/Documents/routing_test/'
     raster_utils.create_directories([output_directory])
     file_suffix = ''
 
-    LOGGER.info("resolve_plateaus")
-    prefix, suffix = os.path.splitext(args['resolve_plateaus_filename'])
-    dem_offset_uri = os.path.join(output_directory, prefix + file_suffix + suffix)
-    routing_cython_core.resolve_flat_regions_for_drainage(dem_uri, dem_offset_uri)
+    LOGGER.info('resolve_plateaus')
+    (prefix, suffix) = os.path.splitext(
+        args['resolve_plateaus_filename'])
+    dem_offset_uri = os.path.join(output_directory, prefix
+                                  + file_suffix + suffix)
+    routing_cython_core.resolve_flat_regions_for_drainage(dem_uri,
+                                                          dem_offset_uri)
     dem_uri = dem_offset_uri
 
-    LOGGER.info("calculating flow direction")
-    prefix, suffix = os.path.splitext(args['flow_direction_filename'])
+    LOGGER.info('calculating flow direction')
+    (prefix, suffix) = os.path.splitext(args['flow_direction_filename'])
     flow_direction_uri = os.path.join(
         output_directory, prefix + file_suffix + suffix)
     routing_cython_core.flow_direction_inf(dem_uri, flow_direction_uri)
 
-    LOGGER.info("calculating flow accumulation")
-    prefix, suffix = os.path.splitext(args['flow_accumulation_filename'])
+    LOGGER.info('calculating flow accumulation')
+    (prefix, suffix) = (
+        os.path.splitext(args['flow_accumulation_filename']))
     flow_accumulation_uri = os.path.join(
         output_directory, prefix + file_suffix + suffix)
-    routing_utils.flow_accumulation(flow_direction_uri, dem_uri, flow_accumulation_uri)
+    routing_utils.flow_accumulation(
+        flow_direction_uri, dem_uri, flow_accumulation_uri)
 
     qgis_bin = r"C:\Program Files\QGIS Brighton\bin\qgis.bat"
-    subprocess.Popen([qgis_bin, flow_accumulation_uri], cwd=os.path.dirname(qgis_bin))
+    subprocess.Popen([qgis_bin, flow_accumulation_uri],
+                     cwd=os.path.dirname(qgis_bin))
 
 if __name__ == '__main__':
     cProfile.run('main()', 'stats')
-    p = pstats.Stats('stats')
-    p.sort_stats('time').print_stats(20)
-    p.sort_stats('cumulative').print_stats(20)
+    P_STATS = pstats.Stats('stats')
+    P_STATS.sort_stats('time').print_stats(20)
+    P_STATS.sort_stats('cumulative').print_stats(20)
     os.remove('stats')
