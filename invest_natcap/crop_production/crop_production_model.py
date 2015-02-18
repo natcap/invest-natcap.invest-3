796c1d5dce4435d074c1d156fcff1886770e5e10
Will Bierbower <wbierbower@gmail.com>
2015-02-17 17:35:52 -0800

    Update Issue 3233
    Setting up testing functions for Crop Production model
    
    
    Former-commit-id: 81fd7da81070f7c76f2f7d6d3c63087740cbc843

--- invest_natcap/crop_production/crop_production_model.py
+++ invest_natcap/crop_production/crop_production_model.py
@@ -4,6 +4,8 @@ The Crop Production Model module contains functions for running the model
 
 import logging
 
+from invest_natcap import raster_utils as ru
+
 LOGGER = logging.getLogger('CROP_PRODUCTION')
 logging.basicConfig(format='%(asctime)s %(name)-15s %(levelname)-8s \
     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -101,4 +103,50 @@ def calc_economic_returns(vars_dict):
             ...
         }
     '''
+    pass
+
+
+# Raster Utils Wrapper for High-level Functions
+def clip_raster_over_aoi():
+    '''
+    '''
+    # Check that datasets are in same projection
+    ru.assert_datasets_in_same_projection()
+
+    # Reproject AOI onto Raster, find bounding box
+    ru.reproject_dataset_uri()
+
+    # Clip raster around bounding box
+    ru.clip_dataset_uri()
+
+    # Reproject Clipped Raster onto AOI
+    ru.reproject_dataset_uri()
+
+    ru.align_dataset_list()
+
+    # Save/return clipped raster
+    pass
+
+
+def sum_cells_in_raster():
+    '''
+    '''
+    # Option 1: use sum or mean in GDAL's raster stats
+    # Option 2: extract numpy array using GDAL
+    # Option 3: use Raster_Utils functionality memmap for large rasters
+
+    pass
+
+
+def element_wise_operation():
+    '''
+    '''
+    ru.vectorize_datasets(
+        dataset_uri_list,
+        dataset_pixel_op,
+        dataset_out_uri,
+        datatype_out,
+        nodata_out,
+        pixel_size_out,
+        bounding_box_mode)
     pass
