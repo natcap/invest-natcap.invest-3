77631486debde6874ac257f0c56d4f489f835367
Nicolas Chaumont <nicolas.chaumont@gmail.com>
2015-03-19 12:22:09 -0700

    Update issue 3087 Dumping the next remove event to see if there is a numerical problem with Windows.
    Former-commit-id: 52f6ea93fdfdf6ddfd29bcc3132768c09f705b76

--- invest_natcap/scenic_quality/scenic_quality_cython_core.pyx
+++ invest_natcap/scenic_quality/scenic_quality_cython_core.pyx
@@ -620,7 +620,9 @@ def sweep_through_angles( \
 #            else:
 #                row.append(ID)
 
-            row.append((remove_event_id, remove_events[arg_max[remove_event_id]]))
+            row.append((remove_event_id, \
+                remove_events[arg_max[remove_event_id]], \
+                remove_events[arg_max[remove_event_id + 1]]))
 
             # Sanity check
             assert ID>=0 and ID<max_line_length
