0fb94d8bcf654284583225e6fe4dbd2036c5e7e5
Rich Sharp <richpsharp@gmail.com>
2015-03-18 22:45:31 -0700

    Update issue #2983
    Patching the issue that was a parallel in SDR  where the slope was not in m/m.
    
    
    Former-commit-id: 421f06ea6c934bc1ef7b928638d59e9363d8c7fc

--- invest_natcap/ndr/ndr.py
+++ invest_natcap/ndr/ndr.py
@@ -581,7 +581,7 @@ def _prepare(**args):
         original_slope_uri)
     def threshold_slope(slope):
         '''Threshold slope between 0.001 and 1.0'''
-        slope_copy = slope.copy()
+        slope_copy = slope / 100
         nodata_mask = slope == slope_nodata
         slope_copy[slope < 0.001] = 0.001
         slope_copy[slope > 1.0] = 1.0
