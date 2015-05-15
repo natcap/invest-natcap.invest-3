4d87bbce03d945cce761c993f3f5f95c967ce547
Nicolas Chaumont <nicolas.chaumont@gmail.com>
2015-05-15 15:41:52 -0700

    Update issue 3380 Generating graphs with habitats. Need to add user-defined increments, transects to be rendered, and legend.
    Former-commit-id: 6a80dce374984521373592b7d734be65fb13b46b

--- invest_natcap/nearshore_wave_and_erosion/nearshore_wave_and_erosion_core.py
+++ invest_natcap/nearshore_wave_and_erosion/nearshore_wave_and_erosion_core.py
@@ -396,12 +396,14 @@ def transfer_tidal_forcing(transect_data_file_tiff, transect_data_file_hdf5):
 def plot_transects(args):
     # Set of 26 maximally different colors, used for habitats. Taken from
     # http://graphicdesign.stackexchange.com/questions/3682/where-can-i-find-a-large-palette-set-of-contrasting-colors-for-coloring-many-d
-    RGB_color_map = [[240,163,255],[0,117,220],[153,63,0],[76,0,92], \
-        [25,25,25],[0,92,49],[43,206,72],[255,204,153],[128,128,128], \
-        [148,255,181],[143,124,0],[157,204,0],[194,0,136],[0,51,128], \
+    RGB_color_map = numpy.array([[240,163,255],[0,117,220],[153,63,0], \
+        [76,0,92], [25,25,25],[0,92,49],[43,206,72],[255,204,153], \
+        [128,128,128],[148,255,181],[143,124,0],[157,204,0],[194,0,136],\
+        [0,51,128], \
         [255,164,5],[255,168,187],[66,102,0],[255,0,16],[94,241,242], \
         [0,153,143],[224,255,102],[116,10,255],[153,0,0],[255,255,128], \
-        [255,255,0],[255,80,5]]
+        [255,255,0],[255,80,5]]) / 255.
+    color_count = RGB_color_map.shape[0]
 
     matplotlib.colors.ListedColormap(RGB_color_map, name = 'custom_colormap')
 
@@ -434,7 +436,7 @@ def plot_transects(args):
     # Loop through the transects
     LOGGER.debug('Generating transect graphs...')
     progress_step = max(bathymetry_dataset_hdf5.shape[0] / 39, 1)
-    for transect in range(0,bathymetry_dataset_hdf5.shape[0],50):
+    for transect in range(0,bathymetry_dataset_hdf5.shape[0],10):
         if transect  % progress_step == 0:
             print '.',
 
@@ -485,9 +487,21 @@ def plot_transects(args):
         plt.subplot(212)
         plt.plot(x_hdf5, \
             bathymetry_dataset_hdf5[transect, start_hdf5:end_hdf5], 'k')
-        # Load the habitats
+
+        # Load and plot the habitats
         habitat_types = habitats_dataset_hdf5[transect, start_hdf5:end_hdf5]
-        habitat_codes = numpy.unique(habitat_types)
+        habitat_codes = numpy.unique(habitat_types)[1:]
+
+        for habitat in habitat_codes:
+            habitat_ids = np.where(habitat_types == habitat)[0]
+            habitats_dx = habitat_ids / dx_hdf5
+            habitats_bathy = \
+                bathymetry_dataset_hdf5[transect, start_hdf5:end_hdf5]
+            habitats_bathy = habitats_bathy[habitat_ids]
+
+            # Plot habitats and cycle through colors if too many habitats
+            plt.plot(habitats_dx, habitats_bathy, '.', \
+                c=RGB_color_map[(habitat + 2) % color_count])
 
         plt.savefig(os.path.join( \
             args['intermediate_dir'], 'profile_' + str(transect) + '.png'))
