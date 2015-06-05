8f554c98361adf8cdbfb8e3ccec78778a4538771
Richard Sharp <richpsharp@gmail.com>
2015-06-05 15:59:20 -0700

    Update issue 3385
    Checking to make sure there is at least one point inside the raster
    
    
    Former-commit-id: 46793b430e94395f1d557600c37d56818702e045

--- invest_natcap/carbon_edge_effect/interpolate_grid.py
+++ invest_natcap/carbon_edge_effect/interpolate_grid.py
@@ -7,6 +7,7 @@ import gdal
 import ogr
 import osr
 import pygeoprocessing
+import numpy
 
 def main():
     """main entry point"""
@@ -15,7 +16,7 @@ def main():
     table_uri = r"C:\Users\rich\Documents\invest-natcap.invest-3\test\invest-data\carbon_edge_effect\carbon_edge_regression_coefficients.csv"
     regression_table = pygeoprocessing.get_lookup_from_table(table_uri, 'id')
 
-    raster_uri = r"C:\Users\rich\Documents\invest-natcap.invest-3\test\invest-data\carbon_edge_effect\edge_carbon_lu_sample_map.tif"
+    raster_uri = r"C:\Users\rich\Documents\Dropbox\globio_sample_data_mgds\lulc_2008.tif"
     raster_ds = gdal.Open(raster_uri)
 
     ds_projection_wkt = raster_ds.GetProjection()
@@ -44,7 +45,23 @@ def main():
     grid_points_layer = grid_points_datasource.CreateLayer(
         'grid_points', output_sr, ogr.wkbPoint)
 
+    field_indexes = {
+        0: 'id',
+        1: 'method',
+        2: 'theta1',
+        3: 'theta2',
+        4: 'theta3'
+    }
+
     grid_points_layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
+    grid_points_layer.CreateField(ogr.FieldDefn('method', ogr.OFTInteger))
+    grid_points_layer.CreateField(ogr.FieldDefn('theta1', ogr.OFTReal))
+    grid_points_layer.CreateField(ogr.FieldDefn('theta2', ogr.OFTReal))
+    grid_points_layer.CreateField(ogr.FieldDefn('theta3', ogr.OFTReal))
+
+    ds_geotransform = raster_ds.GetGeoTransform()
+    print ds_geotransform
+    point_list = []
     for grid_id in regression_table:
         grid_coords = coord_trans.TransformPoint(
             regression_table[grid_id]['meanlon'],
@@ -52,12 +69,28 @@ def main():
         point_geometry = ogr.Geometry(ogr.wkbPoint)
         point_geometry.AddPoint(grid_coords[0], grid_coords[1])
 
+        grid_col = (grid_coords[0] - ds_geotransform[0]) / ds_geotransform[1]
+        grid_row = (grid_coords[1] - ds_geotransform[3]) / ds_geotransform[5]
+
+        point_list.append([grid_col, grid_row])
+        if grid_col >= 0 and grid_col < raster_ds.RasterXSize and grid_row >= 0 and grid_row < raster_ds.RasterYSize:
+            print grid_col, grid_row
         # Get the output Layer's Feature Definition
         feature_def = grid_points_layer.GetLayerDefn()
         grid_point_feature = ogr.Feature(feature_def)
         grid_point_feature.SetGeometry(point_geometry)
-        grid_point_feature.SetField(0, grid_id)
+        for field_index in field_indexes:
+            try:
+                grid_point_feature.SetField(
+                    field_index,
+                    float(regression_table[grid_id][field_indexes[field_index]]))
+            except:
+                grid_point_feature.SetField(field_index, -1.0)
         grid_points_layer.CreateFeature(grid_point_feature)
+
+    print numpy.array(point_list)
+
+    #scipy.interpolate.griddata(points, values, xi, method='linear', fill_value=nan)[source]
 
     #import scipy
     #scipy.interpolate.griddata(points, values, xi, method='linear', fill_value=nan)[source]
